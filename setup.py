b1feb8b83a3e6f4f89db2f77125f6ca363f9cac6
Aku Rouhe <aku.rouhe@aalto.fi>
2020-03-04 16:27:00 -0500

    First version of setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(name="SpeechBrain",
+        version="0.0.1",
+        description="All-in-one speech toolkit in pure Python and Pytorch",
+        author="Mirco Ravanelli",
+        author_email = "speechbrain@gmail.com",
+        packages = ["speechbrain"],
+        python_requires = ">=3.6",
+)
