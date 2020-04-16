b7e3f2f43b491cb5eb5eb995c52dd2b321fd55ea
Aku Rouhe <rouheaku@mila.quebec>
2020-04-16 14:31:49 -0400

    Fix WER integration and Experiment checkpoint behaviour

--- recipes/ASR_CTC/TIMIT/VGG2_BLSTM_MLP/experiment.py
+++ recipes/ASR_CTC/TIMIT/VGG2_BLSTM_MLP/experiment.py
@@ -4,7 +4,9 @@ import collections
 import speechbrain.utils.edit_distance as edit_distance 
 import speechbrain.data_io.wer as wer_io
 from speechbrain.data_io.data_io import IterativeCSVWriter 
+from speechbrain.data_io.data_io import convert_index_to_lab
 from speechbrain.decoders.ctc import ctc_greedy_decode
+from speechbrain.decoders.decoders import undo_padding 
 from tqdm.contrib import tzip
 from speechbrain.core import Experiment
 with open('recipes/ASR_CTC/TIMIT/VGG2_BLSTM_MLP/params.yaml') as fi:
@@ -45,42 +47,51 @@ def main():
         # Iterate validataion to check progress
         sb.model.eval()
         valid_losses = []
-        valid_wer_stats = collections.Counter()
+        valid_wer_details = []
         for wav, phn in tzip(*valid_set):
             ids, wav, wav_len = prepare_for_computations(wav)
             ids, phn, phn_len = prepare_for_computations(phn)
             pout = neural_computations(sb.model, wav, wav_len)
-            detached_loss, valid_wer_stats = validation(
-                ids, pout, phn, wav_len, phn_len, valid_wer_stats)
+            detached_loss, wer_details = validation(
+                ids, pout, phn, wav_len, phn_len)
             valid_losses.append(detached_loss)
-
+            valid_wer_details.extend(wer_details)
+        valid_wer_summary = edit_distance.wer_summary(valid_wer_details)
+        valid_wers = [utt['WER'] for utt in valid_wer_details]
+        
         train_stats = {"loss": mean(train_losses)}
         valid_stats = {'loss': mean(valid_losses), 
-                       'wer': valid_wer_stats['WER']}
+                       'wer': valid_wer_summary['WER']}
 
-        sb.lr_annealing([sb.optimizer], epoch, valid_stats['wer'])
-        sb.save_and_keep_only({'wer': valid_stats['wer']}, min_keys=['wer'])
+        sb.lr_annealing([sb.optimizer], epoch, valid_wer_summary['WER'])
         sb.log_epoch_stats(epoch, train_stats, valid_stats)
+        sb.save_and_keep_only({'wer': valid_wer_summary['WER']}, 
+                min_keys=['wer'],
+                end_of_epoch=True)
 
     # Load best model, evaluate that: 
     sb.recover_if_possible(min_key='wer')
     sb.model.eval()
     details_by_utt = []
+    ind2lab = sb.train_loader.label_dict["phn"]["index2lab"]
     with open(sb.predictions_file, "w") as fo:
         hyp_writer = IterativeCSVWriter(fo, ["predictions"])
         for wav, phn in tzip(*test_set):
             ids, wav, wav_len = prepare_for_computations(wav)
             ids, phn, phn_len = prepare_for_computations(phn)
             pout = neural_computations(sb.model, wav, wav_len)
-            hyps, batch_details = evaluation(ids, pout, phn, wav_len, phn_len)
+            hyps, batch_details = evaluation(
+                    ids, pout, phn, wav_len, phn_len, ind2lab)
             details_by_utt.extend(batch_details)
-            write_hyps(hyp_writer, hyps, wav_len, wav.shape[-1])  # Time last 
+            write_hyps(hyp_writer, ids, 
+                    hyps, wav_len, wav.shape[-1])  # Time last 
 
     summary_details = edit_distance.wer_summary(details_by_utt)
     with open(sb.wer_file, "w") as fo:
         wer_io.print_wer_summary(summary_details, file=fo)
         wer_io.print_alignments(details_by_utt, file=fo)
     # Print to stdout too:
+    print("Final test set WER:")
     wer_io.print_wer_summary(summary_details)
 
 
@@ -91,12 +102,6 @@ def prepare_for_computations(data):
 
 def mean(loss):
     return float(sum(loss) / len(loss))
-
-
-def to_output_format(ids, seqs):
-    out = [[sb.train_loader.label_dict["phn"]["index2lab"][int(ind)]
-            for ind in seq] for seq in seqs]
-    return dict(zip(ids, out))
 
 
 def neural_computations(model, wav, wav_len):
@@ -111,37 +116,42 @@ def learn(model, pout, phn, wav_len, phn_len):
     return loss.detach()
 
 
-def validation(ids, pout, phn, wav_len, phn_len, wer_stats):
+def validation(ids, pout, phn, wav_len, phn_len):
     with torch.no_grad():
         loss = sb.compute_cost(pout, phn, [wav_len, phn_len])
         batch_outputs = ctc_greedy_decode(pout, 
                 wav_len, 
                 blank_id = -1)
-        wer_stats = edit_distance.accumulatable_wer_stats(
-                phn.tolist(), batch_outputs, stats = wer_stats
+        phn_unpadded = undo_padding(phn, phn_len)
+        wer_details = edit_distance.wer_details_for_batch(
+                ids, phn_unpadded, batch_outputs
         )
-    return loss.detach(), wer_stats
+    return loss.detach(), wer_details
 
 
-def evaluation(ids, pout, phn, wav_len, phn_len):
+def evaluation(ids, pout, phn, wav_len, phn_len, ind2lab):
     with torch.no_grad():
         batch_outputs = ctc_greedy_decode(pout, 
                 wav_len,
                 blank_id = -1)
-        refs = to_output_format(ids, phn.tolist())
-        hyps = to_output_format(ids, batch_outputs)
-        details_by_utt = edit_distance.wer_details_by_utterance(refs, 
-                hyps, 
+        phn_unpadded = undo_padding(phn, phn_len)
+        batch_outputs = convert_index_to_lab(
+                batch_outputs, ind2lab)
+        phn_unpadded = convert_index_to_lab(
+                phn_unpadded, ind2lab)
+        details_by_utt = edit_distance.wer_details_for_batch(
+                ids,
+                phn_unpadded, 
+                batch_outputs, 
                 compute_alignments=True)
-        return hyps, details_by_utt
+        return batch_outputs, details_by_utt
 
 
-def write_hyps(hyp_writer, hyps, wav_lens, max_len):
-    for hyp_item, wav_len in zip(hyps.items(), wav_lens):
+def write_hyps(hyp_writer, ids, hyps, wav_lens, max_len):
+    for ID, hyp_item, wav_len in zip(ids, hyps, wav_lens):
         duration = int(wav_len * max_len) / sb.sample_rate 
-        ID, decoded = hyp_item
         hyp_writer.write(ID=ID, duration=duration, 
-                predictions = f'"{" ".join(decoded)}"',
+                predictions = f'"{" ".join(hyp_item)}"',
                 predictions_format = "string")
 
 
