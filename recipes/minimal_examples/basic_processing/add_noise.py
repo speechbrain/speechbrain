db021d86a34bc44e11ef20600c39c5c43bc88a3f
Peter Plantinga <plantinga.peter@gmail.com>
2020-05-01 12:32:25 -0400

    Update minimal recipes to newest format

--- recipes/minimal_examples/basic_processing/add_noise.py
+++ recipes/minimal_examples/basic_processing/add_noise.py
@@ -1,14 +1,20 @@
-import sys
-from speechbrain.core import Experiment
+import speechbrain as sb
 
+output_folder = "exp/minimal/add_noise"
 overrides = {
-    "constants": {"task": "add_noise"},
-    "functions": {"add_noise": {"snr_low": 0, "snr_high": 0}},
+    "output_folder": output_folder,
+    "add_noise": {"snr_low": 0, "snr_high": 0},
 }
-with open("recipes/minimal_examples/basic_processing/params.yaml") as fin:
-    sb = Experiment(fin, overrides, commandline_args=sys.argv[1:])
+params_file = "recipes/minimal_examples/basic_processing/params.yaml"
+with open(params_file) as fin:
+    params = sb.yaml.load_extended_yaml(fin, overrides)
 
-for (batch,) in zip(*sb.sample_data()):
-    id, wav, wav_len = batch
-    wav_noise = sb.add_noise(wav, wav_len)
-    sb.save(wav_noise, id, wav_len)
+sb.core.create_experiment_directory(
+    experiment_directory=output_folder,
+    params_to_save=params_file,
+    overrides=overrides,
+)
+
+for ((id, wav, wav_len),) in zip(*params.sample_data()):
+    wav_noise = params.add_noise(wav, wav_len)
+    params.save(wav_noise, id, wav_len)
--- recipes/minimal_examples/basic_processing/add_reverb.py
+++ recipes/minimal_examples/basic_processing/add_reverb.py
@@ -1,11 +1,17 @@
-import sys
-from speechbrain.core import Experiment
+import speechbrain as sb
 
-overrides = {"constants": {"task": "add_reverb"}}
-with open("recipes/minimal_examples/basic_processing/params.yaml") as fin:
-    sb = Experiment(fin, overrides, commandline_args=sys.argv[1:])
+output_folder = "exp/minimal/add_reverb"
+overrides = {"batch_size": 1, "output_folder": output_folder}
+params_file = "recipes/minimal_examples/basic_processing/params.yaml"
+with open(params_file) as fin:
+    params = sb.yaml.load_extended_yaml(fin, overrides)
 
-for (batch,) in zip(*sb.sample_data()):
-    id, wav, wav_len = batch
-    wav_reverb = sb.add_reverb(wav, wav_len)
-    sb.save(wav_reverb, id, wav_len)
+sb.core.create_experiment_directory(
+    experiment_directory=output_folder,
+    params_to_save=params_file,
+    overrides=overrides,
+)
+
+for ((id, wav, wav_len),) in zip(*params.sample_data()):
+    wav_reverb = params.add_reverb(wav, wav_len)
+    params.save(wav_reverb, id, wav_len)
