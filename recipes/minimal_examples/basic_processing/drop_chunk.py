c7b29cba5e22215ff85fcad2baa9bdffe9644ee7
Peter Plantinga <plantinga.peter@gmail.com>
2020-05-01 16:55:49 -0400

    Add all minimal examples for augmentation

---
+++ recipes/minimal_examples/basic_processing/drop_chunk.py
@@ -0,0 +1,17 @@
+import speechbrain as sb
+
+output_folder = "exp/minimal/drop_chunk"
+overrides = {"output_folder": output_folder}
+params_file = "recipes/minimal_examples/basic_processing/params.yaml"
+with open(params_file) as fin:
+    params = sb.yaml.load_extended_yaml(fin, overrides)
+
+sb.core.create_experiment_directory(
+    experiment_directory=output_folder,
+    params_to_save=params_file,
+    overrides=overrides,
+)
+
+for ((id, wav, wav_len),) in zip(*params.sample_data()):
+    wav_drop = params.drop_chunk(wav, wav_len)
+    params.save(wav_drop, id, wav_len)
