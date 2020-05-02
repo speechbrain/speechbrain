eeb1855d7250871b08c6ba2f772af9dcb96d2a82
Peter Plantinga <plantinga.peter@gmail.com>
2020-05-02 15:26:52 -0400

    Add minimal CTC recipe

---
+++ recipes/minimal_examples/neural_networks/ASR_CTC/experiment.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import speechbrain as sb
+from speechbrain.decoders.ctc import ctc_greedy_decode
+from speechbrain.decoders.decoders import undo_padding
+from speechbrain.utils.edit_distance import wer_summary
+from speechbrain.utils.edit_distance import wer_details_for_batch
+
+params_file = "recipes/minimal_examples/neural_networks/ASR_CTC/params.yaml"
+with open(params_file) as fin:
+    params = sb.yaml.load_extended_yaml(fin)
+
+sb.core.create_experiment_directory(
+    experiment_directory=params.output_folder, params_to_save=params_file,
+)
+
+
+class CTCBrain(sb.core.Brain):
+    def forward(self, x, init_params=False):
+        id, wavs, lens = x
+        feats = params.compute_features(wavs, init_params)
+        feats = params.mean_var_norm(feats, lens)
+
+        x = params.rnn(feats, init_params)
+        x = params.lin(x, init_params)
+        outputs = params.softmax(x)
+
+        return outputs, lens
+
+    def compute_objectives(self, predictions, targets, train=True):
+        predictions, lens = predictions
+        ids, phns, phn_lens = targets
+        loss = params.compute_cost(predictions, phns, [lens, phn_lens])
+
+        if not train:
+            seq = ctc_greedy_decode(predictions, lens, blank_id=-1)
+            phns = undo_padding(phns, phn_lens)
+            stats = {"PER": wer_details_for_batch(ids, phns, seq)}
+            return loss, stats
+
+        return loss
+
+    def summarize(self, stats, write=False):
+        summary = {"loss": float(sum(s["loss"] for s in stats) / len(stats))}
+
+        if "PER" in stats[0]:
+            per_stats = [item for stat in stats for item in stat["PER"]]
+            per_summary = wer_summary(per_stats)
+            summary["PER"] = per_summary["WER"]
+
+        return summary
+
+
+ctc_brain = CTCBrain([params.rnn, params.lin], params.optimizer)
+ctc_brain.fit(params.train_loader(), params.valid_loader(), params.N_epochs)
+ctc_brain.evaluate(params.train_loader())
