8d32f53d016b57e9338f3ce67b251e71ac9387ad
mirco <mirco.ravabelli@gmail.com>
2020-05-02 16:55:34 -0400

    added some unit-tests

---
+++ tests/test_linear.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn
+
+
+def test_linear():
+
+    from speechbrain.nnet.linear import linear
+
+    inputs = torch.rand(1, 2, 4)
+    lin_t = linear(inputs.shape[-1], False)
+    lin_t.init_params(inputs)
+    lin_t.w.weight = torch.nn.Parameter(torch.eye(inputs.shape[-1]))
+    outputs = lin_t(inputs)
+    assert torch.all(torch.eq(inputs, outputs))
