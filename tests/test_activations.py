c11824274574f19b13d85db71299cfa969804a87
Peter Plantinga <plantinga.peter@gmail.com>
2020-05-04 10:16:31 -0400

    Convert string parameter in Softmax to boolean

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -7,6 +7,6 @@ def test_softmax():
     from speechbrain.nnet.activations import softmax
 
     inputs = torch.tensor([1, 2, 3]).float()
-    act = softmax(softmax_type="softmax")
+    act = softmax(apply_log=False)
     outputs = act(inputs)
     assert torch.argmax(outputs) == 2
