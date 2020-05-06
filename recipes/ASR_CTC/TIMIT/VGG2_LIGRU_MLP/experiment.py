f888997e58507f27a0ef2cef99e292844563acf7
Peter Plantinga <plantinga.peter@gmail.com>
2020-05-06 13:31:01 -0400

    Make train logger optional, and other transparency improvements

--- recipes/ASR_CTC/TIMIT/VGG2_LIGRU_MLP/experiment.py
+++ recipes/ASR_CTC/TIMIT/VGG2_LIGRU_MLP/experiment.py
@@ -2,11 +2,13 @@
 import sys
 import torch
 import speechbrain as sb
-import speechbrain.utils.train_logger as tl
-from speechbrain.utils.edit_distance import wer_details_for_batch
+import speechbrain.data_io.wer as wer_io
+import speechbrain.utils.edit_distance as edit_distance
 from speechbrain.data_io.data_io import convert_index_to_lab
 from speechbrain.decoders.ctc import ctc_greedy_decode
 from speechbrain.decoders.decoders import undo_padding
+from speechbrain.utils.checkpoints import ckpt_recency
+from speechbrain.utils.train_logger import TrainLogger
 
 # Load hyperparameters file with command-line overrides
 params_file, overrides = sb.core.parse_arguments(sys.argv[1:])
@@ -20,6 +22,18 @@ sb.core.create_experiment_directory(
     experiment_directory=params.output_folder,
     params_to_save=params_file,
     overrides=overrides,
+)
+
+train_logger = TrainLogger(save_file=params.train_log)
+checkpointer = sb.utils.checkpoints.Checkpointer(
+    checkpoints_dir=params.save_folder,
+    recoverables={
+        "model": params.model,
+        "optimizer": params.optimizer,
+        "scheduler": params.lr_annealing,
+        "normalizer": params.normalize,
+        "counter": params.epoch_counter,
+    },
 )
 
 
@@ -44,7 +58,7 @@ class ASR(sb.core.Brain):
             sequence = convert_index_to_lab(sequence, ind2lab)
             phns = undo_padding(phns, phn_lens)
             phns = convert_index_to_lab(phns, ind2lab)
-            stats = wer_details_for_batch(
+            stats = edit_distance.wer_details_for_batch(
                 ids, phns, sequence, compute_alignments=True
             )
             stats = {"PER": stats}
@@ -52,36 +66,45 @@ class ASR(sb.core.Brain):
 
         return loss
 
+    def summarize(self, stats, test=False):
+        summary = {"loss": float(sum(stats["loss"]) / len(stats["loss"]))}
+        if "PER" in stats:
+            per_summary = edit_distance.wer_summary(stats["PER"])
+            summary["PER"] = per_summary["WER"]
+            if test:
+                with open(params.wer_file, "w") as fo:
+                    wer_io.print_wer_summary(per_summary, fo)
+                    wer_io.print_alignments(stats["PER"], fo)
+        return summary
 
-train_logger = tl.TextLogger(
-    save_file=params.train_log,
-    summary_fns={"loss": tl.float_summary, "PER": tl.error_rate_summary},
-)
-checkpointer = sb.utils.checkpoints.Checkpointer(
-    checkpoints_dir=params.save_folder,
-    recoverables={
-        "model": params.model,
-        "optimizer": params.optimizer,
-        "scheduler": params.lr_annealing,
-        "normalizer": params.normalize,
-    },
-)
+    def on_epoch_end(self, epoch, train_stats, valid_stats=None):
+        old_lr, new_lr = params.lr_annealing(
+            [params.optimizer], epoch, valid_stats["PER"],
+        )
+        epoch_stats = {"epoch": epoch, "lr": old_lr}
+        train_logger.log_epoch(epoch_stats, train_stats, valid_stats)
+
+        checkpointer.save_and_keep_only(
+            meta=valid_stats,
+            importance_keys=[ckpt_recency, lambda c: -c.meta["PER"]],
+        )
+
+
+# Experiment setup
+params.prepare_timit()
+train_set = params.train_loader()
+valid_set = params.valid_loader()
 asr_brain = ASR(
     modules=[params.model],
-    train_logger=train_logger,
     optimizer=params.optimizer,
-    scheduler=params.lr_annealing,
-    checkpointer=checkpointer,
+    first_input=next(iter(train_set[0])),
 )
 
-# Experiment
-params.prepare_timit()
+# Load latest checkpoint to resume training
+checkpointer.recover_if_possible()
+asr_brain.fit(params.epoch_counter, train_set, valid_set)
 
-asr_brain.fit(
-    train_set=params.train_loader(),
-    valid_set=params.valid_loader(),
-    number_of_epochs=params.number_of_epochs,
-    min_keys=["PER"],
-)
-
-asr_brain.evaluate(params.test_loader(), min_key="PER")
+# Load best checkpoint for evaluation
+test_set = params.test_loader()
+checkpointer.recover_if_possible(lambda c: -c.meta["PER"])
+asr_brain.evaluate(params.test_loader())
--- speechbrain/utils/train_logger.py
+++ speechbrain/utils/train_logger.py
@@ -1,61 +1,34 @@
 import logging
-from speechbrain.utils.edit_distance import wer_summary
 
 logger = logging.getLogger(__name__)
 
 
 class TrainLogger:
-    def __init__(self, summary_fns):
-        self.summary_fns = summary_fns
-        self.stats = {}
-
-    def add_batch(self, stats, phase):
-        if phase not in self.stats:
-            self.stats[phase] = {}
-
-        for key in stats:
-            if key not in self.stats[phase]:
-                self.stats[phase][key] = []
-            if isinstance(stats[key], list):
-                self.stats[phase][key].extend(stats[key])
-            else:
-                self.stats[phase][key].append(stats[key])
-
-    def summarize(self, epoch_stats):
-        log_string = " - ".join(f"{k} {v}" for k, v in epoch_stats.items())
-        self.summary = {}
-        for phase in self.stats:
-            self.summary[phase] = {}
-            for key in self.stats[phase]:
-                value = self.summary_fns[key](self.stats[phase][key])
-                self.summary[phase][key] = value
-                log_string += f" - {phase} {key}: {value:.02f}"
-        self.stats = {}
-
-        return log_string
-
-    def log_epoch(self, epoch_stats):
-        logger.info(self.summarize(epoch_stats))
-
-
-class TextLogger(TrainLogger):
-    def __init__(self, summary_fns, save_file):
-        self.summary_fns = summary_fns
+    def __init__(self, save_file):
         self.save_file = save_file
         self.stats = {}
 
-    def log_epoch(self, epoch_stats, verbose=True):
-        summary = self.summarize(epoch_stats)
+    def _item_to_string(self, key, value):
+        if isinstance(value, float):
+            value = f"{value:.2f}"
+        return f"{key}: {value}"
+
+    def _stats_to_string(self, epoch_stats, train_stats, valid_stats=None):
+        log_string = " - ".join(
+            [self._item_to_string(k, v) for k, v in epoch_stats.items()]
+        )
+        for key, value in train_stats.items():
+            log_string += " - train " + self._item_to_string(key, value)
+        if valid_stats is not None:
+            for key, value in valid_stats.items():
+                log_string += " - valid " + self._item_to_string(key, value)
+        return log_string
+
+    def log_epoch(
+        self, epoch_stats, train_stats, valid_stats=None, verbose=True,
+    ):
+        summary = self._stats_to_string(epoch_stats, train_stats, valid_stats)
         with open(self.save_file, "a") as fout:
             print(summary, file=fout)
         if verbose:
             logger.info(summary)
-
-
-def float_summary(float_list):
-    return float(sum(float_list) / len(float_list))
-
-
-def error_rate_summary(error_rate_list):
-    summary = wer_summary(error_rate_list)
-    return summary["WER"]
