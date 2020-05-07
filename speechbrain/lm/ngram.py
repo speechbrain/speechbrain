d381f5e707e8bfb4ba69291695916383bc149c37
Aku Rouhe <aku.rouhe@aalto.fi>
2020-05-06 21:31:22 -0400

    Blacken and flake8en

--- speechbrain/lm/ngram.py
+++ speechbrain/lm/ngram.py
@@ -1,18 +1,19 @@
 """
 Description:
-    This module implements N-gram counting, discounting, interpolation, 
+    This module implements N-gram counting, discounting, interpolation,
     and backoff
 Author:
     Aku Rouhe
 """
 
-# The following functions are essentially copying the NLTK ngram counting 
+
+# The following functions are essentially copying the NLTK ngram counting
 # pipeline with minor differences. Written from scratch, but with enough
 # inspiration that I feel I want to mention the inspiration source:
 # NLTK is licenced under the Apache 2.0 Licence, same as SpeechBrain
 # See https://github.com/nltk/nltk
 # The NLTK implementation is highly focused on getting lazy evaluation.
-def pad_ends(sequence, n, left_pad_symbol = '<s>', right_pad_symbol = '</s>'):
+def pad_ends(sequence, n, left_pad_symbol="<s>", right_pad_symbol="</s>"):
     """
     Description:
         In speech recognition it is important to predict the end of sentence
@@ -28,16 +29,18 @@ def pad_ends(sequence, n, left_pad_symbol = '<s>', right_pad_symbol = '</s>'):
     else:
         return (left_pad_symbol,) + tuple(sequence) + (right_pad_symbol,)
 
+
 def ngrams(sequence, n, pad=True):
     """
     Description:
         Produce all ngrams from the sequence. This will generally be used in an
         ngram counting pipeline.
     Returns:
-         
+
     Author:
         Aku Rouhe
     """
     if pad:
         sequence = pad_ends(sequence)
     ngrams = []
+    del ngrams  # TODO
