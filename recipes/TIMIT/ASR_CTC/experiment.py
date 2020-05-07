66e53b87adc25199dd948fd0322c826e724b0e54
Peter Plantinga <plantinga.peter@gmail.com>
2020-05-07 15:33:50 -0400

    Revert moving preparation to dataset directory, due to import error

--- recipes/TIMIT/ASR_CTC/experiment.py
+++ recipes/TIMIT/ASR_CTC/experiment.py
@@ -7,7 +7,6 @@ import speechbrain.utils.edit_distance as edit_distance
 from speechbrain.data_io.data_io import convert_index_to_lab
 from speechbrain.decoders.ctc import ctc_greedy_decode
 from speechbrain.decoders.decoders import undo_padding
-from ..timit_prepare import TIMITPreparer
 
 # Load hyperparameters file with command-line overrides
 params_file, overrides = sb.core.parse_arguments(sys.argv[1:])
@@ -90,12 +89,7 @@ asr_brain = ASR(
 )
 
 # Experiment
-prepare_timit = TIMITPreparer(
-    data_folder=params.data_folder,
-    splits=["train", "dev", "test"],
-    save_folder=params.data_folder,
-)
-prepare_timit()
+params.prepare_timit()
 
 asr_brain.fit(
     train_set=params.train_loader(),
