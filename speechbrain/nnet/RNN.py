ee49a2e568717019c04df349cea40d3f53845479
Jerry Chou <jjery2243542@gmail.com>
2020-05-10 01:18:18 +0800

    add attentional decoder, not yet test

--- speechbrain/nnet/RNN.py
+++ speechbrain/nnet/RNN.py
@@ -7,8 +7,26 @@ Author
 import torch
 import logging
 import torch.nn as nn
+import math
+
+from speechbrain.nnet.attention import (
+    LocationAwareAttention,
+    ContentBasedAttention,
+)
 
 logger = logging.getLogger(__name__)
+
+
+def init_rnn_module(module):
+    """
+    This function is used to initialize the RNN weight with orthogonality.
+    """
+    for param in module.parameters():
+        if len(param.shape) >= 2:
+            nn.init.orthogonal_(param)
+        else:
+            stdv = 1.0 / math.sqrt(param.shape[0])
+            nn.init.uniform_(param, -stdv, stdv)
 
 
 class RNN(torch.nn.Module):
@@ -73,7 +91,7 @@ class RNN(torch.nn.Module):
 
     def init_params(self, first_input):
         """
-        Initializes the parameters of the convolutional layer.
+        Initializes the parameters of the RNN layer.
 
         Arguments
         ---------
@@ -434,3 +452,226 @@ class LiGRU_Layer(torch.jit.ScriptModule):
             drop_mask = self.drop_mask_te
 
         return drop_mask
+
+
+class AttentionalRNNDecoder(nn.Module):
+    def __init__(
+        self,
+        rnn_type,
+        attn_type,
+        n_neurons,
+        attn_dim,
+        attn_out_dim,
+        vocab_dim,
+        emb_dim,
+        num_layers,
+        weight_tying=False,
+        scaling=1.0,
+        channels=10,
+        kernel_size=100,
+        bias=True,
+        dropout=0.0,
+        bos_index=-1,
+        eos_index=-1,
+    ):
+        super(AttentionalRNNDecoder, self).__init__()
+
+        self.rnn_type = rnn_type
+        self.attn_type = attn_type
+        self.n_neurons = n_neurons
+        self.attn_dim = attn_dim
+        self.attn_out_dim = attn_out_dim
+        self.vocab_dim = vocab_dim
+
+        if self.weight_tying and emb_dim != n_neurons:
+            raise ValueError(
+                "Weight tying must have the same emb_dim and n_neurons"
+            )
+
+        self.emb_dim = emb_dim
+        self.num_layers = num_layers
+        self.weight_tying = weight_tying
+        self.scaling = scaling
+        self.bias = bias
+        self.dropout = dropout
+
+        # only for location-aware attention
+        self.channels = channels
+        self.kernel_size = kernel_size
+
+        self.bos_index = (
+            bos_index if bos_index >= 0 else self.vocab_dim + bos_index
+        )
+        self.eos_index = (
+            eos_index if eos_index >= 0 else self.vocab_dim + eos_index
+        )
+
+        self.reshape = False
+
+    def init_params(self, first_input):
+        enc_states, wav_len, tokens = first_input
+
+        if len(enc_states.shape) > 3:
+            self.reshape = True
+            self.enc_dim = torch.prod(torch.tensor(enc_states.shape[2:]))
+
+        self.emb = nn.Embedding(self.vocab_dim, self.emb_dim).to(
+            enc_states.device
+        )
+
+        # dimension reduction
+        self.proj = nn.Linear(
+            self.n_neurons + self.attn_out_dim, self.n_neurons
+        ).to(enc_states.device)
+        self.out = nn.Linear(self.n_neurons, self.vocab_dim).to(
+            enc_states.device
+        )
+
+        if self.weight_tying:
+            self.out.weight = self.emb.weight
+
+        if self.attn_type == "content":
+            self.attn = ContentBasedAttention(
+                enc_dim=self.enc_dim,
+                dec_dim=self.n_neurons * self.num_layers,
+                attn_dim=self.attn_dim,
+                output_dim=self.attn_out_dim,
+                scaling=self.scaling,
+            ).to(enc_states.device)
+
+        elif self.attn_type == "location":
+            self.attn = LocationAwareAttention(
+                enc_dim=self.enc_dim,
+                dec_dim=self.n_neurons * self.num_layers,
+                attn_dim=self.attn_dim,
+                output_dim=self.attn_out_dim,
+                conv_channels=self.channels,
+                kernel_size=self.kernel_size,
+                scaling=self.scaling,
+            ).to(enc_states.device)
+
+        else:
+            raise ValueError(f"{self.attn_type} not implemented.")
+
+        self.drop = nn.Dropout(p=self.dropout).to(enc_states.device)
+
+        # input = [context, emb]
+        input_dim = self.emb_dim + self.attn_out_dim
+        # Recurrent connections
+        if self.rnn_type == "rnn":
+            self.rnn = torch.nn.RNN(
+                input_size=input_dim,
+                hidden_size=self.n_neurons,
+                num_layers=self.num_layers,
+                bias=self.bias,
+                dropout=self.dropout,
+                batch_first=True,
+            ).to(enc_states.device)
+            self.state_has_cell = False
+        if self.rnn_type == "lstm":
+            self.rnn = torch.nn.LSTM(
+                input_size=input_dim,
+                hidden_size=self.n_neurons,
+                num_layers=self.num_layers,
+                bias=self.bias,
+                dropout=self.dropout,
+                batch_first=True,
+            ).to(enc_states.device)
+
+            # LSTMs have two hidden variables
+            self.state_has_cell = True
+        if self.rnn_type == "gru":
+            self.rnn = torch.nn.GRU(
+                input_size=input_dim,
+                hidden_size=self.n_neurons,
+                num_layers=self.num_layers,
+                bias=self.bias,
+                dropout=self.dropout,
+                batch_first=True,
+            ).to(enc_states.device)
+
+            self.state_has_cell = False
+
+        # orthogonal initialization
+        init_rnn_module(self.rnn)
+
+    def init_state(self, batch_size, device):
+        """Initialize the state for RNN
+        """
+        if self.state_has_cell:
+            hidden_state = (
+                torch.zeros(
+                    batch_size, self.num_layers, self.n_neurons, device=device
+                ),
+                torch.zeros(
+                    batch_size, self.num_layers, self.n_neurons, device=device
+                ),
+            )
+        else:
+            hidden_state = torch.zeros(
+                batch_size, self.num_layers, self.n_neurons, device=device
+            )
+
+        return hidden_state
+
+    def forward_step(self, y_t, hs, c, enc_states, enc_len):
+        e = self.emb(y_t)
+        cell_inp = torch.cat([e, c], dim=-1).unsqueeze(0)
+        cell_inp = self.drop(cell_inp)
+        cell_out, hs = self.rnn(cell_inp, hs)
+        cell_out = cell_out.squeeze(1)
+
+        # flattening the decoder hidden states
+        if self.state_has_cell:
+            dec_flatten = hs[0].reshape(-1, self.n_neurons * self.num_layers)
+        else:
+            dec_flatten = hs.reshape(-1, self.n_neurons * self.num_layers)
+
+        c, w = self.attn(enc_states, enc_len, dec_flatten)
+        dec_out = torch.cat([c, cell_out], dim=1)
+        dec_out = self.proj(dec_out)
+        logits = self.out(dec_out)
+
+        return logits, hs, c, w
+
+    def forward(self, enc_states, wav_len, tokens, init_params=False):
+        if init_params:
+            self.init_params([enc_states, wav_len, tokens])
+
+        if self.reshape:
+            enc_states = enc_states.reshape(
+                -1, enc_states.size(1), enc_states.size(2) * enc_states.size(3)
+            )
+
+        # calculating the actual length of enc_states
+        enc_len = torch.round(enc_states.size(1) * wav_len).long()
+
+        tokens = tokens.long()
+        batch_size = enc_states.size(0)
+
+        # preparing input for the decoder
+        bos = tokens.new_ones(batch_size, 1).long() * self.bos_index
+        y_in = torch.cat([bos, tokens], dim=1)
+
+        # initialization
+        hs = self.init_state(batch_size, enc_states.device)
+        self.attn.reset()
+        c = torch.zeros(batch_size, self.attn_out_dim).to(enc_states.device)
+
+        # store predicted tokens
+        logits_lst, attn_lst = [], []
+
+        for t in range(y_in.size(1)):
+            logits, hs, c, w = self.forward_step(
+                y_in[:, t], hs, c, enc_states, enc_len
+            )
+            logits_lst.append(logits)
+            attn_lst.append(w)
+
+        # [B, L_d, vocab_size]
+        logits = torch.stack(logits_lst, dim=1)
+
+        # [B, L_d, L_e]
+        attn = torch.stack(attn_lst, dim=1)
+
+        return logits, attn
