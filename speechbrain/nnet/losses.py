96036c96d6e6c44327a9dd501a28d091e652e4f8
mirco <mirco.ravabelli@gmail.com>
2020-05-12 20:21:08 -0400

    refactored cost function + adding some unit tests

--- speechbrain/nnet/losses.py
+++ speechbrain/nnet/losses.py
@@ -56,25 +56,24 @@ class ComputeCost(nn.Module):
         super().__init__()
         self.allow_lab_diff = allow_lab_diff
         self.cost_type = cost_type
-        
+
         if cost_type == "nll":
-            self.cost = torch.nn.NLLLoss(reduction='none')
+            self.cost = torch.nn.NLLLoss(reduction="none")
 
         if cost_type == "error":
             self.cost = self._compute_error
 
         if cost_type == "mse":
-            self.cost = nn.MSELoss(reduction='none')
+            self.cost = nn.MSELoss(reduction="none")
 
         if cost_type == "l1":
-           self.cost = nn.L1Loss(reduction='none')
+            self.cost = nn.L1Loss(reduction="none")
 
         if cost_type == "ctc":
             if blank_index is None:
                 raise ValueError("Must pass blank index for CTC")
             self.blank_index = blank_index
             self.cost = nn.CTCLoss(blank=self.blank_index)
-
 
     def forward(self, prediction, target, lengths):
         """Returns the cost function given predictions and targets.
@@ -91,34 +90,39 @@ class ComputeCost(nn.Module):
 
         # Check on input and label shapes
         self._check_inp(prediction, target, lengths)
-        
+
         # Computing actual target and prediction lengths
-        pred_len, target_len= self._compute_len(prediction, target, lengths)
+        pred_len, target_len = self._compute_len(prediction, target, lengths)
         target = target.to(prediction.device)
 
-        if self.cost_type == 'ctc':
+        if self.cost_type == "ctc":
             target = target.int()
             prediction = prediction.transpose(0, 1)
             loss = self.cost(prediction, target, pred_len, target_len)
 
-        else:            
+        else:
             # Mask to avoid zero-padded time steps from  the total loss
             mask = length_to_mask(target_len)
 
-            if self.cost_type in ['nll', 'error']:
+            if self.cost_type in ["nll", "error"]:
                 prediction = prediction[:, 0 : target.shape[1], :]
-                prediction = prediction.reshape(prediction.shape[0] * prediction.shape[1],prediction.shape[2])
-                target = target.reshape(target.shape[0] * target.shape[1]).long()
-                mask = mask.reshape(mask.shape[0]* mask.shape[1])
-            
-            if self.cost_type in ['mse', 'l1']:
-                mask = mask.unsqueeze(2).repeat(1,1,target.shape[2])
-                
-            loss = self.cost(prediction, target)* mask
+                prediction = prediction.reshape(
+                    prediction.shape[0] * prediction.shape[1],
+                    prediction.shape[2],
+                )
+                target = target.reshape(
+                    target.shape[0] * target.shape[1]
+                ).long()
+                mask = mask.reshape(mask.shape[0] * mask.shape[1])
+
+            if self.cost_type in ["mse", "l1"]:
+                mask = mask.unsqueeze(2).repeat(1, 1, target.shape[2])
+
+            loss = self.cost(prediction, target) * mask
             loss = torch.sum(loss * mask) / torch.sum(mask)
-            
+
         return loss
-    
+
     def _check_inp(self, prediction, target, lengths):
         """Peforms some check on prediction, targets and lengths.
 
@@ -131,9 +135,9 @@ class ComputeCost(nn.Module):
         lengths : torch.Tensor
             tensor containing the relative lengths of each sentence
         """
-        
+
         if "ctc" not in self.cost_type:
-    
+
             # Shapes cannot be too different (max 3 time steps)
             diff = abs(prediction.shape[1] - target.shape[1])
             if diff > self.allow_lab_diff:
@@ -142,27 +146,26 @@ class ComputeCost(nn.Module):
                     "output probabilities. (Got %i vs %i)"
                     % (target.shape[1], prediction.shape[1])
                 )
-    
+
                 logger.error(err_msg, exc_info=True)
             prediction = prediction[:, 0 : target.shape[1], :]
-    
+
         else:
-    
+
             if not isinstance(lengths, list):
                 err_msg = (
                     "The third input to the compute_cost function must "
                     "be a list [wav_len, lab_len] when ctc is the cost. "
                 )
-    
-                logger.error(err_msg, exc_info=True)       
-        
+
+                logger.error(err_msg, exc_info=True)
+
         # Regression case (no reshape)
         self.reshape = True
         if len(prediction.shape) == len(target.shape):
             self.reshape = False
-            
 
-    def _compute_len(self,prediction, target, lengths):
+    def _compute_len(self, prediction, target, lengths):
         """Compute the actual length of prediction and targets given
         the relative length tensor.
 
@@ -181,19 +184,18 @@ class ComputeCost(nn.Module):
             tensor contaning the length of each sentence in the batch 
         target_len : torch.Tensor
             tensor contaning the length of each target in the batch 
-        """   
-        
-        if isinstance(lengths,list) and len(lengths) == 2:
+        """
+
+        if isinstance(lengths, list) and len(lengths) == 2:
             pred_len, target_len = lengths
         else:
-             pred_len = target_len = lengths
-             
-        pred_len = torch.round(pred_len *  prediction.shape[1]).int()
+            pred_len = target_len = lengths
+
+        pred_len = torch.round(pred_len * prediction.shape[1]).int()
         target_len = torch.round(target_len * target.shape[1]).int()
-        
+
         return pred_len, target_len
-        
-        
+
     def _compute_error(self, prob, lab):
         """Computes the classification error at frame level.
 
