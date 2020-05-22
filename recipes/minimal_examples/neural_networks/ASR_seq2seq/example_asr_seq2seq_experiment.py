741cb1a37505a10174888596345006799308b861
Jerry Chou <jjery2243542@gmail.com>
2020-05-23 00:50:53 +0800

    make max_decode_steps int and add PER for recipe

--- recipes/minimal_examples/neural_networks/ASR_seq2seq/example_asr_seq2seq_experiment.py
+++ recipes/minimal_examples/neural_networks/ASR_seq2seq/example_asr_seq2seq_experiment.py
@@ -2,7 +2,11 @@
 import os
 import speechbrain as sb
 from speechbrain.data_io.data_io import put_bos_token
+from speechbrain.decoders.seq2seq import RNNGreedySearcher
 from speechbrain.utils.train_logger import summarize_average
+from speechbrain.utils.train_logger import summarize_error_rate
+from speechbrain.decoders.decoders import undo_padding
+from speechbrain.utils.edit_distance import wer_details_for_batch
 
 experiment_dir = os.path.dirname(os.path.abspath(__file__))
 params_file = os.path.join(experiment_dir, "params.yaml")
@@ -10,6 +14,14 @@ data_folder = "../../../../../samples/audio_samples/nn_training_samples"
 data_folder = os.path.abspath(experiment_dir + data_folder)
 with open(params_file) as fin:
     params = sb.yaml.load_extended_yaml(fin, {"data_folder": data_folder})
+
+searcher = RNNGreedySearcher(
+    modules=[params.emb, params.decoder, params.lin, params.softmax],
+    bos_index=params.bos,
+    eos_index=params.eos,
+    min_decode_ratio=0,
+    max_decode_ratio=0.1,
+)
 
 
 class seq2seqBrain(sb.core.Brain):
@@ -26,11 +38,25 @@ class seq2seqBrain(sb.core.Brain):
         logits = params.lin(h, init_params=init_params)
         outputs = params.softmax(logits)
 
+        if not train_mode:
+            seq, _ = searcher(x, wav_lens)
+            return outputs, seq
+
         return outputs
 
     def compute_objectives(self, predictions, targets, train_mode=True):
+        if train_mode:
+            outputs = predictions
+        else:
+            outputs, seq = predictions
+
         ids, phns, phn_lens = targets
-        loss = params.compute_cost(predictions, phns, [phn_lens, phn_lens])
+        loss = params.compute_cost(outputs, phns, [phn_lens, phn_lens])
+
+        if not train_mode:
+            phns = undo_padding(phns, phn_lens)
+            stats = {"PER": wer_details_for_batch(ids, phns, seq)}
+            return loss, stats
 
         return loss
 
@@ -45,13 +71,15 @@ class seq2seqBrain(sb.core.Brain):
     def evaluate_batch(self, batch):
         inputs, targets = batch
         out = self.compute_forward(inputs, targets, train_mode=False)
-        loss = self.compute_objectives(out, targets, train_mode=False)
-        return {"loss": loss.detach()}
+        loss, stats = self.compute_objectives(out, targets, train_mode=False)
+        stats["loss"] = loss.detach()
+        return stats
 
     def on_epoch_end(self, epoch, train_stats, valid_stats):
         print("Epoch %d complete" % epoch)
         print("Train loss: %.2f" % summarize_average(train_stats["loss"]))
         print("Valid loss: %.2f" % summarize_average(valid_stats["loss"]))
+        print("Valid PER: %.2f" % summarize_error_rate(valid_stats["PER"]))
 
 
 train_set = params.train_loader()
@@ -64,3 +92,7 @@ seq2seq_brain = seq2seqBrain(
 seq2seq_brain.fit(range(params.N_epochs), train_set, params.valid_loader())
 test_stats = seq2seq_brain.evaluate(params.test_loader())
 print("Test loss: %.2f" % summarize_average(test_stats["loss"]))
+
+
+def test_error():
+    assert summarize_average(test_stats["loss"]) < 15.0
