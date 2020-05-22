7217112ac8aad1d5cc19f5fdb81a467b9566f219
Abdelwahab Heba <a.heba@irit.fr>
2020-05-22 09:46:15 -0400

    Add greedy search for transducer sequence

--- speechbrain/decoders/transducer.py
+++ speechbrain/decoders/transducer.py
@@ -38,30 +38,42 @@ def decode_batch(F, decode_network_lst, Tjoint, classif_network_lst, blank_id):
         >>> import torch
         >>> from speechbrain.decoders.transducer import decode_batch
         >>> from speechbrain.nnet.RNN import RNN
+        >>> from speechbrain.nnet.activations import Softmax
         >>> from speechbrain.nnet.embedding import Embedding
         >>> from speechbrain.nnet.transducer.transducer_joint import Transducer_joint
         >>> from speechbrain.nnet.linear import Linear
         >>> TN = RNN(rnn_type="gru", n_neurons=5, num_layers=1, bidirectional=True)
         >>> TN_lin = Linear(n_neurons=35, bias=True)
-        >>> blank_id = 0
-        >>> PN_emb = Embedding(embeddings_dim=34 , consider_as_one_hot=True, blank_id=blank_id)
-        >>> PN = RNN(rnn_type="gru", n_neurons=5, num_layers=1, bidirectional=False)
+        >>> blank_id = 35
+        >>> PN_emb = Embedding(embeddings_dim=35, consider_as_one_hot=True, blank_id=blank_id)
+        >>> PN = RNN(rnn_type="gru", n_neurons=5, num_layers=1, bidirectional=False, return_hidden=True)
         >>> PN_lin = Linear(n_neurons=35, bias=True)
         >>> Tjoint = Transducer_joint(joint="sum")
         >>> Out_lin = Linear(n_neurons=35)
-        >>> decode_batch(TN_out, [PN_emb,PN,PN_lin], Tjoint, [Out_lin], blank_id)
+        >>> log_softmax = Softmax(apply_log=False)
+        >>> inputs = torch.randn((3,40,35))
+        >>> TN_out = TN(inputs, init_params=True)
+        >>> TN_out = TN_lin(TN_out, init_params=True)
+        >>> # Initialize modules...
+        >>> test_emb = PN_emb(torch.Tensor([[1]]).long(), init_params=True)
+        >>> test_PN, _ = PN(test_emb, init_params=True)
+        >>> test_PN = PN_lin(test_PN, init_params=True)
+        >>> joint_tensor = Tjoint(TN_out, test_PN)
+        >>> out = Out_lin(joint_tensor, init_params=True)
+        >>> out_decode = decode_batch(TN_out, [PN_emb,PN,PN_lin], Tjoint, [Out_lin,log_softmax], blank_id)
 
     Author:
         Abdelwahab HEBA 2020
     """
     hidden = None
     list_outs = []
-    #### Prepare Blank prediction
+    # Prepare Blank prediction
     input_PN = (
         torch.ones((F.size(0), 1), device=F.device, dtype=torch.int64)
         * blank_id
     )
     out_PN = input_PN
+    # First forward on PN
     hidden = None
     for layer in decode_network_lst:
         if layer.__class__.__name__ == "RNN":
@@ -71,33 +83,42 @@ def decode_batch(F, decode_network_lst, Tjoint, classif_network_lst, blank_id):
                 out_PN = layer(out_PN, init_params=False)
             else:
                 out_PN = layer(out_PN, init_params=False)
-    #### For each time step
+    # For each time step
     for t_step in range(F.size(1)):
         # Join predictions (TN & PN)
-        # print(out_PN.shape)
-        # print(F[:,t_step,:].unsqueeze(1).unsqueeze(1).shape)
-        # input()
         out = Tjoint(
             F[:, t_step, :].unsqueeze(1).unsqueeze(1), out_PN.unsqueeze(1)
         )
-        # Classifiers layers
+        # forward the output layers + activation + save logits
+        activation = False
         for layer in classif_network_lst:
             out = layer(out)
-        list_outs.append(out)
-        prob_targets, positions = torch.max(
-            out.log_softmax(dim=-1).squeeze(1).squeeze(1), dim=1
-        )
-        # print(positions)
-        # print(positions.shape)
+            if layer.__class__.__name__ == "Softmax":
+                activation = True
+                list_outs.append(out)
+                out = layer(out)
+        # Sort outputs at time T
+        if activation:
+            prob_targets, positions = torch.max(
+                out.squeeze(1).squeeze(1), dim=1
+            )
+        else:
+            prob_targets, positions = torch.max(
+                out.log_softmax(dim=-1).squeeze(1).squeeze(1), dim=1
+            )
+
+        # Batch hidden update
         have_update_hyp = []
         for i in range(positions.size(0)):
+            # Update hiddens only if
+            # 1- current prediction is non blank
+            # 2- current prediction is different from the previous
             if positions[i] != blank_id and positions[i] != input_PN[i][0]:
                 input_PN[i][0] = positions[i]
-                # out_PN=input_PN
                 have_update_hyp.append(i)
         if len(have_update_hyp) > 0:
             out_update_hyp = input_PN[have_update_hyp, :]
-            # for LSTM Hidden
+            # for LSTM hiddens (hn, hc)
             if isinstance(hidden, tuple):
                 hidden0_update_hyp = hidden[0][:, have_update_hyp, :]
                 hidden1_update_hyp = hidden[1][:, have_update_hyp, :]
@@ -114,6 +135,7 @@ def decode_batch(F, decode_network_lst, Tjoint, classif_network_lst, blank_id):
                     out_update_hyp = layer(out_update_hyp)
 
             out_PN[have_update_hyp] = out_update_hyp
+            # for LSTM hiddens (hn, hc)
             if isinstance(hidden, tuple):
                 hidden[0][:, have_update_hyp, :] = hidden_update_hyp[0]
                 hidden[1][:, have_update_hyp, :] = hidden_update_hyp[1]
