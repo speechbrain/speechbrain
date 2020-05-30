30b4e66fe095353d3ab0169315be3ca9430a96c0
mirco <mirco.ravabelli@gmail.com>
2020-05-30 11:51:22 -0400

    updated SincNet model

--- speechbrain/lobes/models/SinCRDNN.py
+++ speechbrain/lobes/models/SinCRDNN.py
@@ -3,105 +3,138 @@
 Authors: Mirco Ravanelli 2020, Peter Plantinga 2020, Ju-Chieh Chou 2020,
     Titouan Parcollet 2020, Abdel 2020
 """
-import os
-import torch  # noqa: F401
-from speechbrain.nnet.pooling import Pooling
-from speechbrain.nnet.containers import Sequential, ReplicateBlock
+import torch
+from speechbrain.nnet.RNN import LiGRU
+from speechbrain.nnet.CNN import SincConv, Conv1d
+from speechbrain.nnet.linear import Linear
+from speechbrain.nnet.pooling import Pooling1d
+from speechbrain.nnet.dropout import Dropout2d
+from speechbrain.nnet.containers import Sequential
+from speechbrain.nnet.normalization import BatchNorm1d
 
 
 class SinCRDNN(Sequential):
-    """This model is a combination of CNNs, RNNs, and DNNs.
+    """This model is a combination of SincConv, CNNs, LiGRU, and DNNs.
 
-    The default CNN model is based on VGG.
 
     Arguments
     ---------
-    output_size : int
-        The length of the output (number of target classes).
+    activation : torch class
+        A class used for constructing the activation layers. For cnn and dnn.
+    dropout : float
+        Neuron dropout rate, applied to cnn, rnn, and dnn.
     cnn_blocks : int
         The number of convolutional neural blocks to include.
-    cnn_overrides : mapping
-        Additional parameters overriding the CNN parameters.
-    rnn_blocks : int
-        The number of recurrent neural blocks to include.
-    rnn_overrides : mapping
-        Additional parameters overriding the RNN parameters.
+    cnn_channels : list of ints
+        A list of the number of output channels for each cnn block.
+    cnn_kernelsize : tuple of ints
+        The size of the convolutional kernels.
+    time_pooling : bool
+        Whether to pool the utterance on the time axis before the LiGRU.
+    time_pooling_size : int
+        The number of elements to pool on the time axis.
+    time_pooling_stride : int
+        The number of elements to increment by when iterating the time axis.
+    rnn_layers : int
+        The number of recurrent LiGRU layers to include.
+    rnn_neurons : int
+        Number of neurons in each layer of the LiGRU.
+    rnn_bidirectional : bool
+        Whether this model will process just forward or both directions.
     dnn_blocks : int
         The number of linear neural blocks to include.
-    dnn_overrides : mapping
-        Additional parameters overriding the DNN parameters.
+    dnn_neurons : int
+        The number of neurons in the linear layers.
 
-    CNN Block Parameters
-    --------------------
-        .. include:: cnn_block.yaml
-
-    RNN Block Parameters
-    --------------------
-        .. include:: rnn_block.yaml
-
-    DNN Block Parameters
-    --------------------
-        .. include:: dnn_block.yaml
+    Example
+    -------
+    >>> model = SinCRDNN()
+    >>> inputs = torch.rand([10, 1600])
+    >>> outputs = model(inputs, init_params=True)
+    >>> outputs.shape
+    torch.Size([10, 10, 512])
     """
 
     def __init__(
         self,
-        sinc_blocks=1,
-        sinc_overrides={},
-        cnn_blocks=1,
-        cnn_overrides={},
-        rnn_blocks=1,
-        rnn_overrides={},
-        dnn_blocks=1,
-        dnn_overrides={},
+        activation=torch.nn.LeakyReLU,
+        dropout=0.15,
+        sinc_channels=512,
+        sinc_kernel_size=129,
+        sinc_stride=1,
+        cnn_blocks=3,
+        cnn_channels=[256, 128, 128],
+        cnn_kernel_sizes=[5, 5, 5],
+        cnn_strides=[4, 4, 10],
         time_pooling=False,
-        time_pooling_stride=2,
         time_pooling_size=2,
+        rnn_layers=4,
+        rnn_neurons=512,
+        rnn_bidirectional=True,
+        dnn_blocks=2,
+        dnn_neurons=512,
     ):
         blocks = []
 
-        model_dir = os.path.dirname(os.path.abspath(__file__))
-
-        blocks.append(
-            ReplicateBlock(
-                param_file=os.path.join(model_dir, "sincnet_block.yaml"),
-                yaml_overrides=sinc_overrides,
-                replication_count=sinc_blocks,
-            )
+        # SincConv Block
+        blocks.extend(
+            [
+                SincConv(
+                    out_channels=sinc_channels,
+                    kernel_size=sinc_kernel_size,
+                    stride=sinc_stride,
+                ),
+                BatchNorm1d(),
+                activation(),
+                Dropout2d(drop_rate=dropout),
+            ]
         )
 
-        # blocks.append(
-        #    ReplicateBlock(
-        #        param_file=os.path.join(model_dir, "cnn_block.yaml"),
-        #        yaml_overrides=cnn_overrides,
-        #        replication_count=cnn_blocks,
-        #    )
-        # )
+        # Conv 1d blocks
+        for block_index in range(cnn_blocks):
+            blocks.extend(
+                [
+                    Conv1d(
+                        out_channels=cnn_channels[block_index],
+                        kernel_size=cnn_kernel_sizes[block_index],
+                        stride=cnn_strides[block_index],
+                    ),
+                    BatchNorm1d(),
+                    activation(),
+                    Dropout2d(drop_rate=dropout),
+                ]
+            )
 
+        # Time pooling
         if time_pooling:
             blocks.append(
-                Pooling(
-                    pool_type="max",
-                    stride=time_pooling_stride,
-                    kernel_size=time_pooling_size,
-                    pool_axis=1,
+                Pooling1d(
+                    pool_type="max", kernel_size=time_pooling_size, pool_axis=1,
                 )
             )
 
-        blocks.append(
-            ReplicateBlock(
-                param_file=os.path.join(model_dir, "rnn_block.yaml"),
-                yaml_overrides=rnn_overrides,
-                replication_count=rnn_blocks,
+        # RNN layers
+        if rnn_layers > 0:
+            blocks.append(
+                LiGRU(
+                    hidden_size=rnn_neurons,
+                    num_layers=rnn_layers,
+                    dropout=dropout,
+                    bidirectional=rnn_bidirectional,
+                )
             )
-        )
 
-        blocks.append(
-            ReplicateBlock(
-                param_file=os.path.join(model_dir, "dnn_block.yaml"),
-                yaml_overrides=dnn_overrides,
-                replication_count=dnn_blocks,
+        # DNN blocks
+        for block_index in range(dnn_blocks):
+            blocks.extend(
+                [
+                    Linear(
+                        n_neurons=dnn_neurons, bias=True, combine_dims=False,
+                    ),
+                    BatchNorm1d(),
+                    activation(),
+                    torch.nn.Dropout(p=dropout),
+                ]
             )
-        )
 
         super().__init__(*blocks)
