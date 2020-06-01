4753e7c388ee6072295da3b63bf3f13f8d76cbbb
jerrygood0703 <jerrygood0703@gmail.com>
2020-06-01 16:35:26 +0800

    Adapt to the refactored brain class, dns_prepare is now a function

--- recipes/DNS/Baseline/experiment_baseline.py
+++ recipes/DNS/Baseline/experiment_baseline.py
@@ -3,18 +3,19 @@ import os
 import sys
 import torch
 import speechbrain as sb
+import torchaudio
+from tqdm.contrib import tqdm
 from speechbrain.utils.train_logger import summarize_average
 from speechbrain.processing.features import spectral_magnitude
+from speechbrain.nnet.containers import Sequential
 
 # This hack needed to import data preparation script from ..
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(current_dir))
-from dns_prepare import DNSPreparer  # noqa E402
+from dns_prepare import prepare_dns
 
 # Load hyperparameters file with command-line overrides
 params_file, overrides = sb.core.parse_arguments(sys.argv[1:])
-if "seed" in overrides:
-    torch.manual_seed(overrides["seed"])
 with open(params_file) as fin:
     params = sb.yaml.load_extended_yaml(fin, overrides)
 
@@ -25,52 +26,53 @@ sb.core.create_experiment_directory(
     overrides=overrides,
 )
 
-
 if params.use_tensorboard:
     from speechbrain.utils.train_logger import TensorboardLogger
 
     train_logger = TensorboardLogger(params.tensorboard_logs)
 
+# Create the folder to save enhanced files
+if not os.path.exists(params.enhanced_folder):
+    os.mkdir(params.enhanced_folder)
 
-class AutoBrain(sb.core.Brain):
+model = Sequential(
+    params.conv1,
+    params.activation,
+    params.conv2,
+    params.activation,
+    params.conv3,
+    params.activation,
+    params.conv4,
+    params.activation,
+    params.conv5,
+    params.activation,
+    params.conv6,
+    params.activation,
+    params.conv7,
+    params.activation,
+    params.linear,
+    params.output_activation,
+)
+
+class SEBrain(sb.core.Brain):
     def compute_forward(self, x, train_mode=True, init_params=False):
         ids, wavs, lens = x
         wavs, lens = wavs.to(params.device), lens.to(params.device)
-        feats = params.compute_features(wavs)
-        feats = spectral_magnitude(feats)
-        feats = params.mean_var_norm(feats, lens)
+        feats = params.compute_stft(wavs)
+        feats = spectral_magnitude(feats, power=0.5)
+        feats = torch.log1p(feats)
 
-        encoded = params.conv1(feats, init_params)
-        encoded = params.activation(encoded)
+        output = model(feats, init_params)
 
-        encoded = params.conv2(encoded, init_params)
-        encoded = params.activation(encoded)
-
-        encoded = params.conv3(encoded, init_params)
-        encoded = params.activation(encoded)
-
-        encoded = params.conv4(encoded, init_params)
-        encoded = params.activation(encoded)
-
-        encoded = params.conv5(encoded, init_params)
-        encoded = params.activation(encoded)
-
-        encoded = params.conv6(encoded, init_params)
-        encoded = params.activation(encoded)
-
-        encoded = params.conv7(encoded, init_params)
-        encoded = params.activation(encoded)
-
-        decoded = params.linear(encoded, init_params)
-
-        return decoded
+        return output
 
     def compute_objectives(self, predictions, targets, train_mode=True):
         ids, wavs, lens = targets
         wavs, lens = wavs.to(params.device), lens.to(params.device)
-        feats = params.compute_features(wavs)
-        feats = spectral_magnitude(feats)
-        # feats = params.mean_var_norm(feats, lens)
+        feats = params.compute_stft(wavs)
+        feats = spectral_magnitude(feats, power=0.5)
+        feats = torch.log1p(feats)
+
         return params.compute_cost(predictions, feats, lens)
 
     def fit_batch(self, batch):
@@ -81,31 +83,84 @@ class AutoBrain(sb.core.Brain):
         self.optimizer(self.modules)
         return {"loss": loss.detach()}
 
-    def evaluate_batch(self, batch):
+    def evaluate_batch(self, epoch, batch):
         inputs, targets = batch
-        predictions = self.compute_forward(inputs)
+        predictions = self.compute_forward(inputs, train_mode=False)
+
+        # Create the folder to save enhanced files
+        if not os.path.exists(os.path.join(params.enhanced_folder, str(epoch))):
+            os.mkdir(os.path.join(params.enhanced_folder, str(epoch)))
+
+        # Write batch enhanced files to directory
+        self.write_wavs(torch.expm1(predictions), targets, epoch)
+
         loss = self.compute_objectives(predictions, targets, train_mode=False)
         return {"loss": loss.detach()}
 
     def on_epoch_end(self, epoch, train_stats, valid_stats):
         if params.use_tensorboard:
             train_logger.log_stats({"Epoch": epoch}, train_stats, valid_stats)
+
+        params.checkpointer.save_checkpoint()
+
         print("Completed epoch %d" % epoch)
         print("Train loss: %.3f" % summarize_average(train_stats["loss"]))
         print("Valid loss: %.3f" % summarize_average(valid_stats["loss"]))
 
+    def fit(self, epoch_counter, train_set, valid_set=None):
+        train_stats, valid_stats = {}, {}
+        for epoch in epoch_counter:
+            self.modules.train()
+            train_stats = {}
+            for batch in tqdm(train_set):
+                stats = self.fit_batch(batch)
+                self.add_stats(train_stats, stats)
 
-prepare = DNSPreparer(
-    data_folder=params.data_folder, save_folder=params.csv_folder,
+            valid_stats = {}
+            if valid_set is not None:
+                self.modules.eval()
+                with torch.no_grad():
+                    for batch in tqdm(valid_set):
+                        stats = self.evaluate_batch(epoch, batch)
+                        self.add_stats(valid_stats, stats)
+
+            self.on_epoch_end(epoch, train_stats, valid_stats)
+
+        return train_stats, valid_stats
+
+    def write_wavs(self, predictions, inputs, epoch):
+        ids, wavs, lens = inputs
+        predictions = predictions.cpu()
+
+        feats = params.compute_stft(wavs)
+        phase = torch.atan2(feats[:,:,:,1], feats[:,:,:,0])
+        complex_predictions = torch.mul(
+            torch.unsqueeze(predictions, -1), 
+            torch.cat(
+                (
+                    torch.unsqueeze(torch.cos(phase), -1), 
+                    torch.unsqueeze(torch.sin(phase), -1),
+                ),
+                -1,
+            )
+        )
+        pred_wavs = params.compute_istft(complex_predictions)
+
+        for name, pred_wav in zip(ids, pred_wavs):
+            enhance_path = os.path.join(params.enhanced_folder, str(epoch), name)
+            torchaudio.save(enhance_path, pred_wav, 16000)
+
+prepare_dns(
+    data_folder=params.data_folder, 
+    save_folder=params.data_folder,
+    seg_size=10.0,
 )
-prepare()
+
 train_set = params.train_loader()
 valid_set = params.valid_loader()
-first_x, first_y = next(zip(*train_set))
+first_x, first_y = next(iter(train_set))
 
-train_set = params.train_loader()
-first_x = next(iter(train_set[0]))
-auto_brain = AutoBrain(
+se_brain = SEBrain(
     modules=[
         params.conv1,
         params.conv2,
@@ -119,11 +174,9 @@ auto_brain = AutoBrain(
     optimizer=params.optimizer,
     first_inputs=[first_x],
 )
-auto_brain.fit(range(params.N_epochs), train_set, valid_set)
-# test_stats = auto_brain.evaluate(params.test_loader())
-# print("Test loss: %.3f" % summarize_average(test_stats["loss"]))
 
+# Load latest checkpoint to resume training
+params.checkpointer.add_recoverable("model", model)
+params.checkpointer.recover_if_possible()
+se_brain.fit(params.epoch_counter, train_set, valid_set)
 
-# If training is successful, reconstruction loss is less than 0.2
-# def test_loss():
-#     assert summarize_average(test_stats["loss"]) < 0.2
