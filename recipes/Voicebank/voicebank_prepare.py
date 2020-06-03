5d584126a31324274124891bafc3b73cfdb19e63
JasonSWFu <weisberger2009@gmail.com>
2020-06-03 16:31:57 +0800

    update to new version

--- recipes/Voicebank/voicebank_prepare.py
+++ recipes/Voicebank/voicebank_prepare.py
@@ -2,7 +2,7 @@
 """
 Data preparation.
 
-Download: https://datashare.is.ed.ac.uk/handle/10283/1942
+Download: https://datashare.is.ed.ac.uk/handle/10283/2791
 
 Author
 ------
@@ -11,23 +11,23 @@ Szu-Wei Fu, 2020
 
 import os
 import csv
-import torch
 import logging
 from speechbrain.utils.data_utils import get_all_files
 from speechbrain.data_io.data_io import read_wav_soundfile
 
 logger = logging.getLogger(__name__)
+TRAIN_CSV = "train.csv"
+TEST_CSV = "test.csv"
+SAMPLERATE = 16000
 
 
-class VoicebankPreparer(torch.nn.Module):
+def prepare_voicebank(data_folder, save_folder):
     """
     Prepares the csv files for the Voicebank dataset.
     Arguments
     ---------
     data_folder : str
         Path to the folder where the original Voicebank dataset is stored.
-    splits : list
-        List of splits to prepare from ['train', 'dev', 'test']
     save_folder : str
         The directory where to store the csv files.
 
@@ -36,234 +36,222 @@ class VoicebankPreparer(torch.nn.Module):
     This example requires the actual Voicebank dataset.
     The noisy_vctk_prepare.py can be used to download the dataset.
     ```
-    local_folder='/path/to/datasets/Voicebank'
-    save_folder='exp/Voicebank_exp'
-    # Definition of the config dictionary
-    data_folder = local_folder
-    # Initialization of the class
+    data_folder = '/path/to/datasets/Voicebank'
+    save_folder = 'exp/Voicebank_exp'
     VoicebankPreparer(data_folder, save_folder)
     ```
-    Author
-    ------
-    Szu-Wei Fu
     """
 
-    def __init__(
-        self, data_folder, save_folder,
-    ):
-        # Expected inputs when calling the class (no inputs in this case)
-        super().__init__()
-        self.data_folder = data_folder
-        self.save_folder = save_folder
+    train_clean_folder = os.path.join(
+        data_folder, "clean_trainset_28spk_wav_16k/"
+    )
+    train_noisy_folder = os.path.join(
+        data_folder, "noisy_trainset_28spk_wav_16k/"
+    )
+    test_clean_folder = os.path.join(data_folder, "clean_testset_wav_16k/")
+    test_noisy_folder = os.path.join(data_folder, "noisy_testset_wav_16k/")
 
-        self.train_clean_folder = os.path.join(
-            self.data_folder, "clean_trainset_28spk_wav_16k/"
-        )
-        self.train_noisy_folder = os.path.join(
-            self.data_folder, "noisy_trainset_28spk_wav_16k/"
-        )
-        self.test_clean_folder = os.path.join(
-            self.data_folder, "clean_testset_wav_16k/"
-        )
-        self.test_noisy_folder = os.path.join(
-            self.data_folder, "noisy_testset_wav_16k/"
-        )
+    # Setting file extension.
+    extension = [".wav"]
 
-        # Other variables
-        self.samplerate = 16000
+    # Setting the save folder
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
 
-        # Setting file extension.
-        self.extension = [".wav"]
+    # Setting ouput files
+    save_csv_train = os.path.join(save_folder, TRAIN_CSV)
+    save_csv_test = os.path.join(save_folder, TEST_CSV)
 
-        # Setting the save folder
-        if not os.path.exists(self.save_folder):
-            os.makedirs(self.save_folder)
+    # Check if this phase is already done (if so, skip it)
+    if skip(save_csv_train, save_csv_test):
 
-        # Setting ouput files
-        self.save_csv_train = self.save_folder + "/train.csv"
-        self.save_csv_test = self.save_folder + "/test.csv"
-
-        # Check if this phase is already done (if so, skip it)
-        if self.skip():
-
-            msg = "\t%s sucessfully created!" % (self.save_csv_train)
-            logger.debug(msg)
-
-            msg = "\t%s sucessfully created!" % (self.save_csv_test)
-            logger.debug(msg)
-
-            return
-
-    def __call__(self):
-        # Additional checks to make sure the data folder contains Voicebank
-        self.check_Voicebank_folders()
-
-        msg = "\tCreating csv file for the Voicebank Dataset.."
+        msg = "\t%s sucessfully created!" % (save_csv_train)
         logger.debug(msg)
 
-        # Creating csv file for training data
-        wav_lst_train = get_all_files(
-            self.train_noisy_folder, match_and=self.extension,
-        )
-
-        self.create_csv(
-            wav_lst_train, self.save_csv_train, is_train_folder=True,
-        )
-
-        # Creating csv file for testing data
-        wav_lst_test = get_all_files(
-            self.test_noisy_folder, match_and=self.extension,
-        )
-
-        self.create_csv(
-            wav_lst_test, self.save_csv_test, is_train_folder=False,
-        )
+        msg = "\t%s sucessfully created!" % (save_csv_test)
+        logger.debug(msg)
 
         return
 
-    def skip(self):
-        """
-        Detects if the Voicebank data_preparation has been already done.
-        If the preparation has been done, we can skip it.
-        Returns
-        -------
-        bool
-            if True, the preparation phase can be skipped.
-            if False, it must be done.
-        """
+    # Additional checks to make sure the data folder contains Voicebank
+    check_Voicebank_folders(
+        train_clean_folder,
+        train_noisy_folder,
+        test_clean_folder,
+        test_noisy_folder,
+    )
 
-        # Checking folders and save options
-        skip = False
+    msg = "\tCreating csv file for the Voicebank Dataset.."
+    logger.debug(msg)
 
-        if os.path.isfile(self.save_csv_train) and os.path.isfile(
-            self.save_csv_test
-        ):
-            skip = True
+    # Creating csv file for training data
+    wav_lst_train = get_all_files(train_noisy_folder, match_and=extension,)
 
-        return skip
+    create_csv(
+        wav_lst_train,
+        save_csv_train,
+        train_clean_folder,
+        test_clean_folder,
+        is_train_folder=True,
+    )
 
-    # TODO: Consider making this less complex
-    def create_csv(self, wav_lst, csv_file, is_train_folder):  # noqa: C901
-        """
-        Creates the csv file given a list of wav files.
-        Arguments
-        ---------
-        wav_lst : list
-            The list of wav files of a given data split.
-        csv_file : str
-            The path of the output csv file
-        Returns
-        -------
-        None
-        """
+    # Creating csv file for testing data
+    wav_lst_test = get_all_files(test_noisy_folder, match_and=extension,)
 
-        # Adding some Prints
-        msg = '\t"Creating csv lists in  %s..."' % (csv_file)
-        logger.debug(msg)
+    create_csv(
+        wav_lst_test,
+        save_csv_test,
+        train_clean_folder,
+        test_clean_folder,
+        is_train_folder=False,
+    )
 
-        csv_lines = [
-            [
-                "ID",
-                "duration",
-                "noisy_wav",
-                "noisy_wav_format",
-                "noisy_wav_opts",
-                "clean_wav",
-                "clean_wav_format",
-                "clean_wav_opts",
-            ]
+
+def skip(save_csv_train, save_csv_test):
+    """
+    Detects if the Voicebank data_preparation has been already done.
+    If the preparation has been done, we can skip it.
+    Returns
+    -------
+    bool
+        if True, the preparation phase can be skipped.
+        if False, it must be done.
+    """
+
+    # Checking folders and save options
+    skip = False
+
+    if os.path.isfile(save_csv_train) and os.path.isfile(save_csv_test):
+        skip = True
+
+    return skip
+
+
+def create_csv(
+    wav_lst, csv_file, train_clean_folder, test_clean_folder, is_train_folder
+):
+    """
+    Creates the csv file given a list of wav files.
+    Arguments
+    ---------
+    wav_lst : list
+        The list of wav files.
+    csv_file : str
+        The path of the output csv file
+    Returns
+    -------
+    None
+    """
+
+    # Adding some Prints
+    msg = '\t"Creating csv lists in  %s..."' % (csv_file)
+    logger.debug(msg)
+
+    csv_lines = [
+        [
+            "ID",
+            "duration",
+            "noisy_wav",
+            "noisy_wav_format",
+            "noisy_wav_opts",
+            "clean_wav",
+            "clean_wav_format",
+            "clean_wav_opts",
+        ]
+    ]
+
+    # Processing all the wav files in the list
+    for wav_file in wav_lst:  # ex:p203_122.wav
+
+        # Example wav_file: p232_001.wav
+        snt_id = wav_file.split("/")[-1]
+
+        if is_train_folder:
+            clean_folder = train_clean_folder
+        else:
+            clean_folder = test_clean_folder
+        clean_wav = clean_folder + snt_id
+
+        # Reading the signal (to retrieve duration in seconds)
+        signal = read_wav_soundfile(wav_file)
+        duration = signal.shape[0] / SAMPLERATE
+
+        # Composition of the csv_line
+        csv_line = [
+            snt_id.replace(".wav", ""),
+            str(duration),
+            wav_file,
+            "wav",
+            "",
+            clean_wav,
+            "wav",
+            "",
         ]
 
-        # Processing all the wav files in the list
-        for wav_file in wav_lst:  # ex:p203_122.wav
+        # Adding this line to the csv_lines list
+        csv_lines.append(csv_line)
 
-            # Example wav_file: p232_001.wav
-            # Getting fileids
-            snt_id = wav_file.split("/")[-1]
+    # Writing the csv lines
+    with open(csv_file, mode="w") as csv_f:
+        csv_writer = csv.writer(
+            csv_f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
 
-            if is_train_folder:
-                clean_folder = self.train_clean_folder
-            else:
-                clean_folder = self.test_clean_folder
-            clean_wav = clean_folder + snt_id
+        for line in csv_lines:
+            csv_writer.writerow(line)
 
-            # Reading the signal (to retrieve duration in seconds)
-            signal = read_wav_soundfile(wav_file)
-            duration = signal.shape[0] / self.samplerate
+    # Final prints
+    msg = "\t%s sucessfully created!" % (csv_file)
+    logger.debug(msg)
 
-            # Composition of the csv_line
-            csv_line = [
-                snt_id.replace(".wav", ""),
-                str(duration),
-                wav_file,
-                "wav",
-                "",
-                clean_wav,
-                "wav",
-                "",
-            ]
 
-            # Adding this line to the csv_lines list
-            csv_lines.append(csv_line)
+def check_Voicebank_folders(
+    train_clean_folder, train_noisy_folder, test_clean_folder, test_noisy_folder
+):
+    """
+    Check if the data folder actually contains the Voicebank dataset.
+    If not, raises an error.
+    Returns
+    -------
+    None
+    Raises
+    ------
+    FileNotFoundError
+        If data folder doesn't contain Voicebank dataset.
+    """
 
-        # Writing the csv lines
-        with open(csv_file, mode="w") as csv_f:
-            csv_writer = csv.writer(
-                csv_f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
-            )
+    # Checking train_clean folder
+    if not os.path.exists(train_clean_folder):
 
-            for line in csv_lines:
-                csv_writer.writerow(line)
+        err_msg = (
+            "the folder %s does not exist (it is expected in "
+            "the Voicebank dataset)" % (train_clean_folder)
+        )
+        raise FileNotFoundError(err_msg)
 
-        # Final prints
-        msg = "\t%s sucessfully created!" % (csv_file)
-        logger.debug(msg)
+    # Checking train_noisy folder
+    if not os.path.exists(train_noisy_folder):
 
-    def check_Voicebank_folders(self):
-        """
-        Check if the data folder actually contains the Voicebank dataset.
-        If not, raises an error.
-        Returns
-        -------
-        None
-        Raises
-        ------
-        FileNotFoundError
-            If data folder doesn't contain Voicebank dataset.
-        """
+        err_msg = (
+            "the folder %s does not exist (it is expected in "
+            "the Voicebank dataset)" % (train_noisy_folder)
+        )
+        raise FileNotFoundError(err_msg)
 
-        # Checking train_clean folder
-        if not os.path.exists(self.train_clean_folder):
+    # Checking test_clean folder
+    if not os.path.exists(test_clean_folder):
 
-            err_msg = (
-                "the folder %s does not exist (it is expected in "
-                "the DNS dataset)" % (self.train_clean_folder)
-            )
-            raise FileNotFoundError(err_msg)
+        err_msg = (
+            "the folder %s does not exist (it is expected in "
+            "the Voicebank dataset)" % (test_clean_folder)
+        )
+        raise FileNotFoundError(err_msg)
 
-        # Checking train_noisy folder
-        if not os.path.exists(self.train_noisy_folder):
+    # Checking test_noisy folder
+    if not os.path.exists(test_noisy_folder):
 
-            err_msg = (
-                "the folder %s does not exist (it is expected in "
-                "the DNS dataset)" % (self.train_noisy_folder)
-            )
-            raise FileNotFoundError(err_msg)
-
-        # Checking test_clean folder
-        if not os.path.exists(self.test_clean_folder):
-
-            err_msg = (
-                "the folder %s does not exist (it is expected in "
-                "the DNS dataset)" % (self.test_clean_folder)
-            )
-            raise FileNotFoundError(err_msg)
-
-        # Checking test_noisy folder
-        if not os.path.exists(self.test_noisy_folder):
-
-            err_msg = (
-                "the folder %s does not exist (it is expected in "
-                "the DNS dataset)" % (self.test_noisy_folder)
-            )
-            raise FileNotFoundError(err_msg)
+        err_msg = (
+            "the folder %s does not exist (it is expected in "
+            "the Voicebank dataset)" % (test_noisy_folder)
+        )
+        raise FileNotFoundError(err_msg)
