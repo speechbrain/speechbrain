be63aa0a94ff33341acdf4e7976aab18ee9968fb
williamaris <william.aris@usherbrooke.ca>
2020-06-11 00:30:38 -0400

    Added an example and a unit test for GCC-PHAT

--- tests/unittests/test_signal_processing.py
+++ tests/unittests/test_signal_processing.py
@@ -1,21 +1,33 @@
 import torch
 
 
-# WORK IN PROGRESS
 def test_gccphat():
 
     from speechbrain.processing.features import STFT
-    from speechbrain.processing.signal_processing import GCCPHAT
+    import speechbrain.processing.signal_processing as sp
 
+    # Creating the test signal
     fs = 16000
-    inp = torch.randn([2, 16000])
-    inp = torch.stack(3 * [inp], -1)
+
+    padding = 120
+    n_fft = 400
+    delay = n_fft - padding
+
+    sig = torch.randn([10, fs])
+    sig_delayed = torch.cat((torch.zeros([10, padding]), sig[:, 0:-padding]), 1)
+
+    xs = torch.stack((sig, sig_delayed), -1)
 
     compute_stft = STFT(sample_rate=fs)
-    compute_gccphat = GCCPHAT()
+    xs = compute_stft(xs)
 
-    xs = compute_stft(inp)
-    compute_gccphat(xs)
+    # Computing the covariance matrix for GCC-PHAT
+    rxx = sp.cov(xs)
 
-    # TODO: Change condition and complete test
-    assert torch.sum(xs == xs) == xs.numel()
+    compute_gccphat = sp.GCCPHAT()
+    xxs = compute_gccphat(rxx)
+
+    # Extracting every delay found by GCC-PHAT
+    _, gccphat_delays = torch.max(xxs[3, :, :, 1], 1)
+
+    assert torch.sum(gccphat_delays == delay) == xs.shape[1]
