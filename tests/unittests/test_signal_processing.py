4b6404706c22d6824b07ecb09b74029536ddaace
popcornell <cornellsamuele@gmail.com>
2020-07-13 19:04:31 +0200

    added tests for signal_processing.py normalization utils.

--- tests/unittests/test_signal_processing.py
+++ tests/unittests/test_signal_processing.py
@@ -1,6 +1,31 @@
 import torch
 
 
+def test_normalize():
+
+    from speechbrain.processing.signal_processing import compute_amplitude
+    from speechbrain.processing.signal_processing import rescale
+    import random
+    import numpy as np
+
+    for scale in ["dB", "linear"]:
+        for amp_type in ["peak", "avg"]:
+            for test_vec in [torch.rand((10, 100)), torch.rand((10, 100, 5))]:
+                amp = compute_amplitude(
+                    test_vec, test_vec.size(1), amp_type, scale
+                )
+                scaled_back = rescale(
+                    random.random() * test_vec,
+                    test_vec.size(1),
+                    amp,
+                    amp_type,
+                    scale,
+                )
+                np.testing.assert_array_almost_equal(
+                    scaled_back.numpy(), test_vec.numpy()
+                )
+
+
 def test_gccphat():
 
     from speechbrain.processing.features import STFT
