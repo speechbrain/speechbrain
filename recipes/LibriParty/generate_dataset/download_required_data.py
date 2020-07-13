e17d146ccf464bb1dc55c112cd83ed46a1452fcb
popcornell <cornellsamuele@gmail.com>
2020-07-13 11:11:55 +0200

    data downloading and resampling script

--- recipes/LibriParty/generate_dataset/download_required_data.py
+++ recipes/LibriParty/generate_dataset/download_required_data.py
@@ -1,11 +1,18 @@
 import argparse
 import os
 from speechbrain.utils.data_utils import download_file
+from local.resample_folder import resample_folder
 
 LIBRISPEECH_URLS = ["http://www.openslr.org/resources/12/test-clean.tar.gz"]
 
 OPENRIR_URL = "http://www.openslr.org/resources/28/rirs_noises.zip"
-QUT_TIMIT_URL = ""
+QUT_TIMIT_URLS = [
+    "https://data.researchdatafinder.qut.edu.au/dataset/a0eed5af-abd8-441b-b14a-8e064bc3d732/resource/9b0f10ed-e3f5-40e7-b503-73c2943abfb1/download/qutnoisecafe.zip",
+    "https://data.researchdatafinder.qut.edu.au/dataset/a0eed5af-abd8-441b-b14a-8e064bc3d732/resource/7412452a-92e9-4612-9d9a-6b00f167dc15/download/qutnoisecar.zip",
+    "https://data.researchdatafinder.qut.edu.au/dataset/a0eed5af-abd8-441b-b14a-8e064bc3d732/resource/35cd737a-e6ad-4173-9aee-a1768e864532/download/qutnoisehome.zip",
+    "https://data.researchdatafinder.qut.edu.au/dataset/a0eed5af-abd8-441b-b14a-8e064bc3d732/resource/164d38a5-c08e-4e20-8272-793534eb10c7/download/qutnoisereverb.zip",
+    "https://data.researchdatafinder.qut.edu.au/dataset/a0eed5af-abd8-441b-b14a-8e064bc3d732/resource/10eeceae-9f0c-4556-b33a-dcf35c4f4db9/download/qutnoisestreet.zip",
+]
 
 parser = argparse.ArgumentParser(
     "Python script to download required recipe data"
@@ -19,33 +26,41 @@ args = parser.parse_args()
 # QUT-TIMIT/
 # rirs_noises/
 
-# stage 0 download librispeech
 if args.stage <= 0:
     print("Stage 0: Downloading LibriSpeech")
     for url in LIBRISPEECH_URLS:
         name = url.split("/")[-1]
         download_file(url, os.path.join(args.output_folder, name), unpack=True)
 
-# stage 1 download rirs and noises
 if args.stage <= 1:
     print("Stage 1: Downloading RIRs and Noises")
-    name = url.split("/")[-1]
+    name = OPENRIR_URL.split("/")[-1]
     download_file(
         OPENRIR_URL, os.path.join(args.output_folder, name), unpack=True
     )
 
-
 if args.stage <= 2:
     print("Stage 2: Downloading QUT-TIMIT background noises")
-    name = url.split("/")[-1]
-    download_file(
-        QUT_TIMIT_URL, os.path.join(args.output_folder, name), unpack=True
-    )
+    for url in QUT_TIMIT_URLS:
+        name = url.split("/")[-1]
+        os.makedirs(
+            os.path.join(args.output_folder, "QUT_NOISE"), exist_ok=True
+        )
+        download_file(
+            url,
+            os.path.join(args.output_folder, "QUT_NOISE", name),
+            unpack=True,
+        )
 
 if args.stage <= 3:
-    print("Stage 3: Resampling QUT-TIMIT background noises")
-
+    print("Stage 3: Resampling QUT noise background noises")
+    resample_folder(
+        os.path.join(args.output_folder, "QUT_NOISE"),
+        os.path.join(args.output_folder, "QUT_NOISE_16kHz"),
+        16000,
+        ".wav",
+    )
     print(
-        "Resampling done. Original QUT-TIMIT files can be deleted "
+        "Resampling done. Original QUT noise files can be deleted "
         "as they will not be used."
     )
