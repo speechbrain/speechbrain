1fb677eee29712ed2e5d3cd900dc66a7f44c909c
popcornell <cornellsamuele@gmail.com>
2020-07-13 17:19:14 +0200

    updated creation functions

--- recipes/LibriParty/generate_dataset/local/create_mixtures_from_metadata.py
+++ recipes/LibriParty/generate_dataset/local/create_mixtures_from_metadata.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import json
 import numpy as np
@@ -15,36 +16,36 @@ def create_mixture(session_n, output_dir, params, metadata):
     speakers = [x for x in metadata.keys() if x not in ["noises", "background"]]
 
     tot_length = int(
-        np.ceil(metadata["background"]["stop"] * params["samplerate"])
+        np.ceil(metadata["background"]["stop"] * params.samplerate)
     )
     mixture = torch.zeros(tot_length)  # total mixture file
     # step 1
     for spk in speakers:
         session_meta[spk] = []
         # we create mixture for each speaker and we optionally save it.
-        if params["save_dry_sources"]:
+        if params.save_dry_sources:
             dry = torch.zeros(tot_length)
         wet = torch.zeros(tot_length)
 
         for utt in metadata[spk]:
             c_audio, fs = torchaudio.load(
-                os.path.join(params["librispeech_root"], utt["file"])
+                os.path.join(params.librispeech_root, utt["file"])
             )
-            assert fs == params["samplerate"]
+            assert fs == params.samplerate
             if len(c_audio.shape) > 1:  # multichannel
                 c_audio = c_audio[utt["channel"], :]
                 c_audio = c_audio - torch.mean(c_audio)
             c_audio = rescale(c_audio, utt["lvl"], scale="dB")
             # we save it in dry
-            dry_start = int(utt["start"] * params["samplerate"])
+            dry_start = int(utt["start"] * params.samplerate)
             dry_stop = dry_start + c_audio.shape[-1]
-            if params["save_dry_sources"]:
+            if params.save_dry_sources:
                 dry[dry_start:dry_stop] += c_audio
             # we add now reverb and put it in wet
             c_rir, fs = torchaudio.load(
-                os.path.join(params["rirs_root"], utt["rir"])
+                os.path.join(params.rirs_root, utt["rir"])
             )
-            assert fs == params["samplerate"]
+            assert fs == params.samplerate
             c_rir = c_rir[utt["rir_channel"], :]
 
             c_audio = reverberate(c_audio, c_rir).squeeze(0)
@@ -55,8 +56,8 @@ def create_mixture(session_n, output_dir, params, metadata):
 
             session_meta[spk].append(
                 {
-                    "start": np.round(wet_start / params["samplerate"], 3),
-                    "stop": np.round(wet_stop / params["samplerate"], 3),
+                    "start": np.round(wet_start / params.samplerate, 3),
+                    "stop": np.round(wet_stop / params.samplerate, 3),
                     "lvl": utt["lvl"],
                     "words": utt["words"],
                     "file": utt["file"],
@@ -71,7 +72,7 @@ def create_mixture(session_n, output_dir, params, metadata):
         # we allow for clipping as it occurs also in real recordings.
 
         # save per speaker clean sources
-        if params["save_dry_sources"]:
+        if params.save_dry_sources:
             torchaudio.save(
                 os.path.join(
                     output_dir,
@@ -79,10 +80,10 @@ def create_mixture(session_n, output_dir, params, metadata):
                     "session_{}_spk_{}_dry.wav".format(session_n, spk),
                 ),
                 torch.clamp(dry, min=-1, max=1),
-                params["samplerate"],
+                params.samplerate,
             )
 
-        if params["save_wet_sources"]:
+        if params.save_wet_sources:
             torchaudio.save(
                 os.path.join(
                     output_dir,
@@ -90,7 +91,7 @@ def create_mixture(session_n, output_dir, params, metadata):
                     "session_{}_spk_{}_wet.wav".format(session_n, spk),
                 ),
                 torch.clamp(wet, min=-1, max=1),
-                params["samplerate"],
+                params.samplerate,
                 precision=32,
             )
 
@@ -103,21 +104,21 @@ def create_mixture(session_n, output_dir, params, metadata):
     for noise_event in metadata["noises"]:
 
         c_audio, fs = torchaudio.load(
-            os.path.join(params["impulsive_noises_root"], noise_event["file"])
+            os.path.join(params.noises_root, noise_event["file"])
         )
-        assert fs == params["samplerate"]
+        assert fs == params.samplerate
         if len(c_audio.shape) > 1:  # multichannel
             c_audio = c_audio[noise_event["channel"], :]
             c_audio = c_audio - torch.mean(c_audio)
         c_audio = rescale(c_audio, noise_event["lvl"], scale="dB")
         # we save it in dry
-        dry_start = int(noise_event["start"] * params["samplerate"])
+        dry_start = int(noise_event["start"] * params.samplerate)
         dry_stop = dry_start + c_audio.shape[-1]
         # we add now reverb and put it in wet
         c_rir, fs = torchaudio.load(
-            os.path.join(params["rirs_root"], noise_event["rir"])
+            os.path.join(params.rirs_root, noise_event["rir"])
         )
-        assert fs == params["samplerate"]
+        assert fs == params.samplerate
         c_rir = c_rir[noise_event["rir_channel"], :]
         # early_rev_samples = get_early_rev_samples(c_rir) NOT SURE ABOUT THIS
 
@@ -131,12 +132,12 @@ def create_mixture(session_n, output_dir, params, metadata):
     if metadata["background"]["file"]:
         c_audio, fs = torchaudio.load(
             os.path.join(
-                params["backgrounds_root"], metadata["background"]["file"]
+                params.backgrounds_root, metadata["background"]["file"]
             ),
             offset=metadata["background"]["orig_start"],
             num_frames=mixture.shape[-1],
         )
-        assert fs == params["samplerate"]
+        assert fs == params.samplerate
         if len(c_audio.shape) > 1:  # multichannel
             c_audio = c_audio[metadata["background"]["channel"], :]
             c_audio = c_audio - torch.mean(c_audio)
@@ -156,38 +157,5 @@ def create_mixture(session_n, output_dir, params, metadata):
     torchaudio.save(
         os.path.join(output_dir, session_n, "{}_mixture.wav".format(session_n)),
         mixture,
-        params["samplerate"],
+        params.samplerate,
     )
-
-
-if __name__ == "__main__":
-    import os
-    import sys
-    import yaml
-    import argparse
-    from tqdm import tqdm
-
-    parser = argparse.ArgumentParser(
-        "Creating synthetic mixtures from metadata"
-    )
-    parser.add_argument("params_file", type=str)
-    parser.add_argument("metadata_file", type=str)
-    parser.add_argument("output_dir", type=str)
-
-    # This hack needed to import data preparation script from ..
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.dirname(current_dir))
-    from create_mixtures_metadata import create_metadata  # noqa E402
-
-    args = parser.parse_args()
-
-    # we load parameters
-    with open(args.params_file, "r") as f:
-        params = yaml.load(f)
-
-    with open(args.metadata_file, "r") as f:
-        metadata = json.load(f)
-
-    for session in tqdm(metadata.keys()):
-        create_mixture(session, args.output_dir, params, metadata[session])
-        # open metadata and create session
