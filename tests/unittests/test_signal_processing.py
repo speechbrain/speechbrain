926746c4d73a6f574c7b051ce1a838ca80f9e154
popcornell <cornellsamuele@gmail.com>
2020-07-13 19:26:19 +0200

    updated tests

--- tests/unittests/test_signal_processing.py
+++ tests/unittests/test_signal_processing.py
@@ -10,16 +10,20 @@ def test_normalize():
 
     for scale in ["dB", "linear"]:
         for amp_type in ["peak", "avg"]:
-            for test_vec in [torch.rand((10, 100)), torch.rand((10, 100, 5))]:
-                amp = compute_amplitude(
-                    test_vec, test_vec.size(1), amp_type, scale
+            for test_vec in [
+                torch.rand((100)),
+                torch.rand((10, 100)),
+                torch.rand((10, 100, 5)),
+            ]:
+
+                lengths = (
+                    test_vec.size(1)
+                    if len(test_vec.shape) > 1
+                    else test_vec.size(0)
                 )
+                amp = compute_amplitude(test_vec, lengths, amp_type, scale)
                 scaled_back = rescale(
-                    random.random() * test_vec,
-                    test_vec.size(1),
-                    amp,
-                    amp_type,
-                    scale,
+                    random.random() * test_vec, lengths, amp, amp_type, scale,
                 )
                 np.testing.assert_array_almost_equal(
                     scaled_back.numpy(), test_vec.numpy()
