8fd35071dfc2c1e99819e40535206f41fb3e9df3
cem <csubakan@gmail.com>
2020-07-13 23:40:32 -0400

    pushing the latest updates

--- speechbrain/nnet/conv_tasnet.py
+++ speechbrain/nnet/conv_tasnet.py
@@ -450,118 +450,117 @@ class GlobalLayerNorm(nn.Module):
         return gLN_y
 
 
-class PermInvariantSISDR(nn.Module):
-    """!
-    Class for SISDR computation between reconstructed signals and
-    target wavs by also regulating it with learned target masks."""
+def cal_loss(source, estimate_source, source_lengths):
+    """
+    Args:
+        source: [B, C, T], B is batch size
+        estimate_source: [B, C, T]
+        source_lengths: [B]
+    """
+    max_snr, perms, max_snr_idx = cal_si_snr_with_pit(
+        source, estimate_source, source_lengths
+    )
+    loss = 0 - torch.mean(max_snr)
+    reorder_estimate_source = reorder_source(
+        estimate_source, perms, max_snr_idx
+    )
+    return loss, max_snr, estimate_source, reorder_estimate_source
 
-    def __init__(
-        self,
-        batch_size=None,
-        zero_mean=False,
-        n_sources=None,
-        backward_loss=True,
-        improvement=False,
-        return_individual_results=False,
-    ):
-        """
-        Initialization for the results and torch tensors that might
-        be used afterwards
-        :param batch_size: The number of the samples in each batch
-        :param zero_mean: If you want to perform zero-mean across
-        last dimension (time dim) of the signals before SDR computation
-        """
-        super().__init__()
-        self.bs = batch_size
-        self.perform_zero_mean = zero_mean
-        self.backward_loss = backward_loss
-        self.permutations = list(
-            itertools.permutations(torch.arange(n_sources))
-        )
-        self.improvement = improvement
-        self.n_sources = n_sources
-        self.return_individual_results = return_individual_results
 
-    def normalize_input(self, pr_batch, t_batch, initial_mixtures=None):
-        min_len = min(pr_batch.shape[-1], t_batch.shape[-1])
-        if initial_mixtures is not None:
-            min_len = min(min_len, initial_mixtures.shape[-1])
-            initial_mixtures = initial_mixtures[:, :, :min_len]
-        pr_batch = pr_batch[:, :, :min_len]
-        t_batch = t_batch[:, :, :min_len]
+def cal_si_snr_with_pit(source, estimate_source, source_lengths):
+    """Calculate SI-SNR with PIT training.
+    Args:
+        source: [B, C, T], B is batch size
+        estimate_source: [B, C, T]
+        source_lengths: [B], each item is between [0, T]
+    """
+    assert source.size() == estimate_source.size()
+    B, C, T = source.size()
+    # mask padding position along T
+    mask = get_mask(source, source_lengths)
+    estimate_source *= mask
 
-        if self.perform_zero_mean:
-            pr_batch = pr_batch - torch.mean(pr_batch, dim=-1, keepdim=True)
-            t_batch = t_batch - torch.mean(t_batch, dim=-1, keepdim=True)
-            if initial_mixtures is not None:
-                initial_mixtures = initial_mixtures - torch.mean(
-                    initial_mixtures, dim=-1, keepdim=True
-                )
-        return pr_batch, t_batch, initial_mixtures
+    # Step 1. Zero-mean norm
+    num_samples = source_lengths.view(-1, 1, 1).float()  # [B, 1, 1]
+    mean_target = torch.sum(source, dim=2, keepdim=True) / num_samples
+    mean_estimate = (
+        torch.sum(estimate_source, dim=2, keepdim=True) / num_samples
+    )
+    zero_mean_target = source - mean_target
+    zero_mean_estimate = estimate_source - mean_estimate
+    # mask padding position along T
+    zero_mean_target *= mask
+    zero_mean_estimate *= mask
 
-    @staticmethod
-    def dot(x, y):
-        return torch.sum(x * y, dim=-1, keepdim=True)
+    # Step 2. SI-SNR with PIT
+    # reshape to use broadcast
+    s_target = torch.unsqueeze(zero_mean_target, dim=1)  # [B, 1, C, T]
+    s_estimate = torch.unsqueeze(zero_mean_estimate, dim=2)  # [B, C, 1, T]
+    # s_target = <s', s>s / ||s||^2
+    pair_wise_dot = torch.sum(
+        s_estimate * s_target, dim=3, keepdim=True
+    )  # [B, C, C, 1]
+    s_target_energy = (
+        torch.sum(s_target ** 2, dim=3, keepdim=True) + EPS
+    )  # [B, 1, C, 1]
+    pair_wise_proj = pair_wise_dot * s_target / s_target_energy  # [B, C, C, T]
+    # e_noise = s' - s_target
+    e_noise = s_estimate - pair_wise_proj  # [B, C, C, T]
+    # SI-SNR = 10 * log_10(||s_target||^2 / ||e_noise||^2)
+    pair_wise_si_snr = torch.sum(pair_wise_proj ** 2, dim=3) / (
+        torch.sum(e_noise ** 2, dim=3) + EPS
+    )
+    pair_wise_si_snr = 10 * torch.log10(pair_wise_si_snr + EPS)  # [B, C, C]
 
-    def compute_permuted_sisnrs(
-        self, permuted_pr_batch, t_batch, t_t_diag, eps=10e-8
-    ):
-        s_t = self.dot(permuted_pr_batch, t_batch) / (t_t_diag + eps) * t_batch
-        e_t = permuted_pr_batch - s_t
-        sisnrs = 10 * torch.log10(
-            self.dot(s_t, s_t) / (self.dot(e_t, e_t) + eps)
-        )
-        return sisnrs
+    # Get max_snr of each utterance
+    # permutations, [C!, C]
+    perms = source.new_tensor(
+        list(itertools.permutations(range(C))), dtype=torch.long
+    )
+    # one-hot, [C!, C, C]
+    index = torch.unsqueeze(perms, 2)
+    perms_one_hot = source.new_zeros((*perms.size(), C)).scatter_(2, index, 1)
+    # [B, C!] <- [B, C, C] einsum [C!, C, C], SI-SNR sum of each permutation
+    snr_set = torch.einsum("bij,pij->bp", [pair_wise_si_snr, perms_one_hot])
+    max_snr_idx = torch.argmax(snr_set, dim=1)  # [B]
+    # max_snr = torch.gather(snr_set, 1, max_snr_idx.view(-1, 1))  # [B, 1]
+    max_snr, _ = torch.max(snr_set, dim=1, keepdim=True)
+    max_snr /= C
+    return max_snr, perms, max_snr_idx
 
-    def compute_sisnr(
-        self, pr_batch, t_batch, initial_mixtures=None, eps=10e-8
-    ):
 
-        t_t_diag = self.dot(t_batch, t_batch)
+def reorder_source(source, perms, max_snr_idx):
+    """
+    Args:
+        source: [B, C, T]
+        perms: [C!, C], permutations
+        max_snr_idx: [B], each item is between [0, C!)
+    Returns:
+        reorder_source: [B, C, T]
+    """
+    B, C, *_ = source.size()
+    # [B, C], permutation whose SI-SNR is max of each utterance
+    # for each utterance, reorder estimate source according this permutation
+    max_snr_perm = torch.index_select(perms, dim=0, index=max_snr_idx)
+    # print('max_snr_perm', max_snr_perm)
+    # maybe use torch.gather()/index_select()/scatter() to impl this?
+    reorder_source = torch.zeros_like(source)
+    for b in range(B):
+        for c in range(C):
+            reorder_source[b, c] = source[b, max_snr_perm[b][c]]
+    return reorder_source
 
-        sisnr_l = []
-        for perm in self.permutations:
-            permuted_pr_batch = pr_batch[:, perm, :]
-            sisnr = self.compute_permuted_sisnrs(
-                permuted_pr_batch, t_batch, t_t_diag, eps=eps
-            )
-            sisnr_l.append(sisnr)
-        all_sisnrs = torch.cat(sisnr_l, -1)
-        best_sisdr = torch.max(all_sisnrs.mean(-2), -1)[0]
 
-        if self.improvement:
-            initial_mix = initial_mixtures.repeat(1, self.n_sources, 1)
-            base_sisdr = self.compute_permuted_sisnrs(
-                initial_mix, t_batch, t_t_diag, eps=eps
-            )
-            best_sisdr -= base_sisdr.mean()
-
-        if not self.return_individual_results:
-            best_sisdr = best_sisdr.mean()
-
-        if self.backward_loss:
-            return -best_sisdr
-        return best_sisdr
-
-    def forward(self, pr_batch, t_batch, eps=1e-9, initial_mixtures=None):
-        """!
-        :param pr_batch: Reconstructed wavs: Torch Tensors of size:
-                         batch_size x self.n_sources x length_of_wavs
-        :param t_batch: Target wavs: Torch Tensors of size:
-                        batch_size x self.n_sources x length_of_wavs
-        :param eps: Numerical stability constant.
-        :param initial_mixtures: Initial Mixtures for SISDRi: Torch Tensor
-                                 of size: batch_size x 1 x length_of_wavs
-        :returns results_buffer Buffer for loading the results directly
-                 to gpu and not having to reconstruct the results matrix: Torch
-                 Tensor of size: batch_size x 1
-        """
-        pr_batch, t_batch, initial_mixtures = self.normalize_input(
-            pr_batch, t_batch, initial_mixtures=initial_mixtures
-        )
-
-        sisnr_l = self.compute_sisnr(
-            pr_batch, t_batch, eps=eps, initial_mixtures=initial_mixtures
-        )
-
-        return sisnr_l
+def get_mask(source, source_lengths):
+    """
+    Args:
+        source: [B, C, T]
+        source_lengths: [B]
+    Returns:
+        mask: [B, 1, T]
+    """
+    B, _, T = source.size()
+    mask = source.new_ones((B, 1, T))
+    for i in range(B):
+        mask[i, :, source_lengths[i] :] = 0
+    return mask
