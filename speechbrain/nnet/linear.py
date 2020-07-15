238502c90b94329059aef6febba421a8cf16d987
Titouan Parcollet <parcollet.titouan@gmail.com>
2020-07-15 12:39:39 +0200

    Linear init

--- speechbrain/nnet/linear.py
+++ speechbrain/nnet/linear.py
@@ -23,6 +23,10 @@ class Linear(torch.nn.Module):
         if True, the additive bias b is adopted.
     combine_dims : bool
         if True and the input is 4D, combine 3rd and 4th dimensions of input.
+    act_fct_for_init : str, optional
+        Default: leaky_relu
+        This parameter is used to compute gain for the weight Initialization.
+        See the official pytorch documentation on initialization.
 
     Example
     -------
@@ -33,11 +37,18 @@ class Linear(torch.nn.Module):
     torch.Size([10, 50, 100])
     """
 
-    def __init__(self, n_neurons, bias=True, combine_dims=False):
+    def __init__(
+        self,
+        n_neurons,
+        bias=True,
+        combine_dims=False,
+        act_fct_for_init="leaky_relu",
+    ):
         super().__init__()
         self.n_neurons = n_neurons
         self.bias = bias
         self.combine_dims = combine_dims
+        self.act_fct_for_init = act_fct_for_init
 
     def init_params(self, first_input):
         """
@@ -51,6 +62,12 @@ class Linear(torch.nn.Module):
             fea_dim = first_input.shape[2] * first_input.shape[3]
 
         self.w = nn.Linear(fea_dim, self.n_neurons, bias=self.bias)
+        # Initialize weights and biases
+        nn.init.xavier_uniform_(
+            self.w.weight.data,
+            gain=nn.init.calculate_gain(self.act_fct_for_init),
+        )
+        self.w.bias.data.fill_(0)
         self.w.to(first_input.device)
 
     def forward(self, x, init_params=False):
