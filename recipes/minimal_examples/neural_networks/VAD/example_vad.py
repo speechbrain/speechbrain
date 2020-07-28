566ea85da8f249012e909c979fd3764f42aa7338
popcornell <cornellsamuele@gmail.com>
2020-07-28 15:55:32 +0200

    vad minimal example now trains

--- recipes/minimal_examples/neural_networks/VAD/example_vad.py
+++ recipes/minimal_examples/neural_networks/VAD/example_vad.py
@@ -4,49 +4,63 @@ import speechbrain as sb
 from speechbrain.decoders.decoders import undo_padding
 from speechbrain.utils.train_logger import summarize_average
 from speechbrain.utils.train_logger import summarize_error_rate
+import torch
 
 experiment_dir = os.path.dirname(os.path.abspath(__file__))
 params_file = os.path.join(experiment_dir, "params.yaml")
-data_folder = "../../../../../samples/audio_samples/nn_training_samples"
+data_folder = "../../../../../samples/speech_labelling"
 data_folder = os.path.abspath(experiment_dir + data_folder)
 with open(params_file) as fin:
     params = sb.yaml.load_extended_yaml(fin, {"data_folder": data_folder})
 
 
 class VADBrain(sb.core.Brain):
-    def compute_forward(self, x, train_mode=True, init_params=False):
+    def compute_forward(self, x, train_mode=True, init_params=False, stage=None):
         id, wavs, lens = x
         feats = params.compute_features(wavs, init_params)
         feats = params.mean_var_norm(feats, lens)
         x = params.rnn(feats, init_params=init_params)
         x = params.lin(x, init_params)
-        outputs = params.sigmoid(x)
+        outputs = x
 
         return outputs, lens
 
-    def compute_objectives(self, predictions, targets, train_mode=True):
+    def create_targets_mat(self, predictions, targets):
+        # create targets here
+        gt = torch.zeros_like(predictions).to(predictions.device)
+
+        for b in range(len(targets[1])): #kinda hacky
+            tmp = [int(float(x)/0.01) for x in targets[1][b]]
+            tmp = list(zip(tmp[::2], tmp[1::2]))
+            for indxs in tmp:
+                start, stop = indxs
+                gt[b][start:stop] = 1
+
+        return gt
+
+    def compute_objectives(self, predictions, targets, stage=True):
         predictions, lens = predictions
-        ids, targets, wav_lens = targets
-        loss = params.compute_cost(predictions, targets, [lens, wav_lens])
 
-        if not train_mode:
-            # compute DER
-            #seq = ctc_greedy_decode(predictions, lens, blank_id=-1)
-            #phns = undo_padding(phns, phn_lens)
-            stats = {"DER": wer_details_for_batch(ids, phns, seq)}
-            return loss, stats
+        targets = self.create_targets_mat(predictions, targets)
 
-        return loss
+        loss = params.compute_cost(torch.nn.BCEWithLogitsLoss(reduction="none"), predictions, targets, lens)
+
+
+        # compute DER
+        stats = {"DER": loss} # dummy for now
+        return loss, stats
+
+
 
     def on_epoch_end(self, epoch, train_stats, valid_stats):
         print("Epoch %d complete" % epoch)
-        print("Train loss: %.2f" % summarize_average(train_stats["loss"]))
-        print("Valid loss: %.2f" % summarize_average(valid_stats["loss"]))
-        print("Valid DER: %.2f" % summarize_error_rate(valid_stats["DER"])) # detection error rate
+        #print("Train loss: %.2f" % summarize_average(train_stats["loss"]))
+        #print("Valid loss: %.2f" % summarize_average(valid_stats["loss"]))
+        #print("Valid DER: %.2f" % summarize_error_rate(valid_stats["DER"])) # detection error rate
 
 
 train_set = params.train_loader()
-first_x, first_y = next(zip(*train_set))
+first_x, first_y = next(iter(train_set))
 vad_brain = VADBrain(
     modules=[params.rnn, params.lin],
     optimizer=params.optimizer,
@@ -54,7 +68,7 @@ vad_brain = VADBrain(
 )
 vad_brain.fit(range(params.N_epochs), train_set, params.valid_loader())
 test_stats = vad_brain.evaluate(params.test_loader())
-print("Test DER: %.2f" % summarize_error_rate(test_stats["DER"]))
+#print("Test DER: %.2f" % summarize_error_rate(test_stats["DER"]))
 
 
 # For such a small dataset, the PER can be unpredictable.
