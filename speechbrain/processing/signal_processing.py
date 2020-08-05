59c90f890a31cbd4c1b46d341a9121ec9e070c7d
williamaris <william.aris@usherbrooke.ca>
2020-08-04 21:07:50 -0400

    Added utils functions + tested delay and sum

--- speechbrain/processing/signal_processing.py
+++ speechbrain/processing/signal_processing.py
@@ -451,18 +451,18 @@ def delay_sum(xs, steer):
     >>> import speechbrain.processing.processing_utils as pu
     >>> from speechbrain.processing.signal_processing import delay_sum
     >>> mics_pos = torch.tensor([
-            [-0.05, -0.05, 0.00],
-            [-0.05, 0.05, 0.00],
-            [0.05, -0.05, 0.00],
-            [0.05, 0.05, 0.00]
-        ])
+    ...     [-0.05, -0.05, 0.00],
+    ...     [-0.05, 0.05, 0.00],
+    ...     [0.05, -0.05, 0.00],
+    ...     [0.05, 0.05, 0.00]
+    ... ])
     >>> signal_pos = torch.tensor([-0.82918, 0.55279, -0.082918])
     >>> signal, fs = sf.read(
-            'samples/audio_samples/multi_mic/speech_-0.82918_0.55279_-0.082918.flac'
-        )
+    ...     'samples/audio_samples/multi_mic/speech_-0.82918_0.55279_-0.082918.flac'
+    ... )
     >>> noise, _ = sf.read(
-            'samples/audio_samples/multi_mic/noise_0.70225_-0.70225_0.11704.flac'
-        )
+    ...     'samples/audio_samples/multi_mic/noise_0.70225_-0.70225_0.11704.flac'
+    ... )
     >>> signal = torch.tensor(signal).unsqueeze(0)
     >>> noise = torch.tensor(noise).unsqueeze(0)
     >>> signal_with_noise = signal + noise
