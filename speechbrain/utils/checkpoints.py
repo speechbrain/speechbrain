cec08918d47233484cd2b3c5754c6dac3c3cfa41
mirco <mirco.ravabelli@gmail.com>
2020-08-08 11:25:50 -0400

    support model avg in checkpoints.py

--- speechbrain/utils/checkpoints.py
+++ speechbrain/utils/checkpoints.py
@@ -59,6 +59,7 @@ import pathlib
 import inspect
 import shutil
 import logging
+import copy
 
 logger = logging.getLogger(__name__)
 
@@ -537,6 +538,7 @@ class Checkpointer:
         max_key=None,
         min_key=None,
         ckpt_predicate=None,
+        get_all=False,
     ):
         """Picks a particular checkpoint from all available checkpoints.
 
@@ -562,6 +564,8 @@ class Checkpointer:
             See the filter builtin.
             The function is called with Checkpoint namedtuples (see above).
             By default, all checkpoints are considered.
+        get_all: bool, optional
+            If True, returns all the checkpoints found
 
         Returns
         -------
@@ -591,6 +595,9 @@ class Checkpointer:
 
         ckpts = self.list_checkpoints()
         ckpts = list(filter(ckpt_predicate, ckpts))
+
+        if get_all:
+            return ckpts
         if ckpts:
             chosen_ckpt = max(ckpts, key=importance_key)
             return chosen_ckpt
@@ -603,6 +610,7 @@ class Checkpointer:
         max_key=None,
         min_key=None,
         ckpt_predicate=None,
+        get_average=False,
     ):
         """Picks a checkpoint and recovers from that, if one is found.
 
@@ -628,6 +636,9 @@ class Checkpointer:
             See the filter builtin.
             The function is called with Checkpoint namedtuples (see above).
             By default, all checkpoints are considered.
+        get_average : bool, optional
+            If True loads a model whose parameters are computed by averaging
+            all the founded checkpoints.
 
 
         Returns
@@ -638,15 +649,22 @@ class Checkpointer:
             if no Checkpoints exist/remain after filtering
         """
         chosen_ckpt = self.find_checkpoint(
-            importance_key, max_key, min_key, ckpt_predicate
+            importance_key,
+            max_key,
+            min_key,
+            ckpt_predicate,
+            get_all=get_average,
         )
         if chosen_ckpt is not None:
-            self.load_checkpoint(chosen_ckpt)
+            if get_average:
+                self.average_checkpoints(chosen_ckpt, self.recoverables)
+            else:
+                self.load_checkpoint(chosen_ckpt, self.recoverables)
         else:
             logger.info("Would load a checkpoint here, but none found yet.")
         return chosen_ckpt
 
-    def load_checkpoint(self, checkpoint):
+    def load_checkpoint(self, checkpoint, recoverables):
         """Loads the specified checkpoint.
 
         Arguments
@@ -654,7 +672,7 @@ class Checkpointer:
         checkpoint : Checkpoint
             Checkpoint to load
         """
-        self._call_load_hooks(checkpoint)
+        self._call_load_hooks(checkpoint, recoverables)
 
     def list_checkpoints(self):
         """List all checkpoints in the checkpoints directory.
@@ -665,6 +683,71 @@ class Checkpointer:
             list of Checkpoint namedtuple (see above)
         """
         return self._load_checkpoint_extra_data(self._list_checkpoint_dirs())
+
+    def average_checkpoints(self, ckpt_lst, recoverables):
+        """Loads a checkpoint whose parameters are the average of all the
+        models found in the checkpoint directory.
+
+        Arguments
+        ---------
+        ckpt_lst : Checkpoint
+            List of checkpoints found in the checkpoint directory.
+        recoverables : mapping, optional
+            Objects to to recover
+        """
+        # Set all the parameters to zero
+        self._weight_params_checkpoint(recoverables, weight=0)
+
+        # Deep copy of the recoverable objects is used to store
+        # temporarily the weighted parameters
+        weighed_recoverables = copy.deepcopy(recoverables)
+
+        for ckpt in ckpt_lst:
+            self.load_checkpoint(ckpt, weighed_recoverables)
+            self._weight_params_checkpoint(
+                weighed_recoverables, weight=1 / len(ckpt_lst)
+            )
+            self._sum_params_checkpoint(recoverables, weighed_recoverables)
+
+    def _weight_params_checkpoint(self, recoverables, weight=1.0):
+        """Multiplies the parameters by the given weight factor.
+
+        Arguments
+        ---------
+        recoverables : mapping, optional
+             Objects to to recover
+        weight: float
+            Model parameters are scaled by the given weight.
+        """
+        for name, obj in recoverables.items():
+            if hasattr(obj, "state_dict"):
+                with torch.no_grad():
+                    for pname, param in obj.state_dict().items():
+                        param.copy_(param.data * weight)
+
+    def _sum_params_checkpoint(self, recoverables, recoverables_sum):
+        """Sums the parameters of the two recoverable objects in input.
+
+        Arguments
+        ---------
+        recoverables : mapping, optional
+             Objects where the summed parameters are stored
+        recoverables_sum : mapping, optional
+             Objects to sum
+        """
+        objs = zip(recoverables.items(), recoverables_sum.items())
+        for obj, obj_sum in objs:
+            name, obj = obj
+            name, obj_sum = obj_sum
+            if hasattr(obj, "state_dict"):
+                with torch.no_grad():
+                    params = zip(
+                        obj.state_dict().items(), obj_sum.state_dict().items()
+                    )
+                    for param, param_sum in params:
+                        pname, param = param
+                        pname, param_sum = param_sum
+                        param.copy_(param.data + param_sum.data)
 
     # NOTE: * in arglist -> keyword only arguments
     def delete_checkpoints(
@@ -734,12 +817,12 @@ class Checkpointer:
         shutil.rmtree(checkpoint.path)
         logger.info(f"Deleted checkpoint in {checkpoint.path}")
 
-    def _call_load_hooks(self, checkpoint):
+    def _call_load_hooks(self, checkpoint, recoverables):
         # This internal function finds the correct hook to call for every
         # recoverable, and calls it.
         logger.info(f"Loading a checkpoint from {checkpoint.path}")
         end_of_epoch = checkpoint.meta["end-of-epoch"]
-        for name, obj in self.recoverables.items():
+        for name, obj in recoverables.items():
             # NOTE: We want the checkpoint namedtuple to have the paramfile
             # paths for each recoverable.
             # In some rare case, the user can e.g. add a path there manually.
