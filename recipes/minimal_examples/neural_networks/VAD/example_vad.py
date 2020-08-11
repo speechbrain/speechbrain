bba3c65f45f02dbe71d597b15984cd4b6ecbd593
popcornell <cornellsamuele@gmail.com>
2020-08-11 15:14:36 +0200

    working minimal example for VAD

--- recipes/minimal_examples/neural_networks/VAD/example_vad.py
+++ recipes/minimal_examples/neural_networks/VAD/example_vad.py
@@ -5,6 +5,9 @@ from speechbrain.decoders.decoders import undo_padding
 from speechbrain.utils.train_logger import summarize_average
 from speechbrain.utils.train_logger import summarize_error_rate
 import torch
+from speechbrain.utils.classification_metrics import BinaryMetrics
+from speechbrain.data_io.data_io import DataLoaderFactory
+import numpy as np
 
 experiment_dir = os.path.dirname(os.path.abspath(__file__))
 params_file = os.path.join(experiment_dir, "params.yaml")
@@ -15,63 +18,66 @@ with open(params_file) as fin:
 
 
 class VADBrain(sb.core.Brain):
+    def on_training_start(self):
+        self.metrics = BinaryMetrics()
+
     def compute_forward(self, x, train_mode=True, init_params=False, stage=None):
         id, wavs, lens = x
         feats = params.compute_features(wavs, init_params)
         feats = params.mean_var_norm(feats, lens)
         x = params.rnn(feats, init_params=init_params)
-        x = params.lin(x, init_params)
-        outputs = x
+        outputs = params.lin(x, init_params)
 
         return outputs, lens
-
-    def create_targets_mat(self, predictions, targets):
-        # create targets here
-        gt = torch.zeros_like(predictions).to(predictions.device)
-
-        for b in range(len(targets[1])): #kinda hacky
-            tmp = [int(float(x)/0.01) for x in targets[1][b]]
-            tmp = list(zip(tmp[::2], tmp[1::2]))
-            for indxs in tmp:
-                start, stop = indxs
-                gt[b][start:stop] = 1
-
-        return gt
 
     def compute_objectives(self, predictions, targets, stage=True):
         predictions, lens = predictions
 
-        targets = self.create_targets_mat(predictions, targets)
-
+        targets = targets[1].to(predictions.device)
+        predictions = predictions[:, :targets.shape[-1], 0]
         loss = params.compute_cost(torch.nn.BCEWithLogitsLoss(reduction="none"), predictions, targets, lens)
 
-
+        self.metrics.update(torch.sigmoid(predictions), targets)
         # compute DER
-        stats = {"DER": loss} # dummy for now
+        stats = {"loss": loss} # dummy for now
         return loss, stats
-
-
 
     def on_epoch_end(self, epoch, train_stats, valid_stats):
         print("Epoch %d complete" % epoch)
-        #print("Train loss: %.2f" % summarize_average(train_stats["loss"]))
-        #print("Valid loss: %.2f" % summarize_average(valid_stats["loss"]))
-        #print("Valid DER: %.2f" % summarize_error_rate(valid_stats["DER"])) # detection error rate
+        print("Train loss: %.4f" % summarize_average(train_stats["loss"]))
+        print("Precision: %.2f" % self.metrics.get_precision())
+        print("Recall: %.2f" % self.metrics.get_recall())
 
 
-train_set = params.train_loader()
-first_x, first_y = next(iter(train_set))
+def parsing_func(params, string):
+    boundaries = string.split(" ")
+    # we group by two
+    # 0.01 is 10 ms hop size ... IS THERE AN EASY WAY to pass to mfcc the step size ?
+    boundaries =  [int(float(x)/0.01) for x in boundaries]
+    boundaries = list(zip(boundaries[::2], boundaries[1::2]))
+
+    gt = torch.zeros(int(np.ceil(params.example_length*(1/0.01))))
+
+    for indxs in boundaries:
+        start, stop = indxs
+        gt[start:stop] = 1
+
+    return gt
+
+train_set = DataLoaderFactory(params.csv_train, params.N_batch, ["wav", "speech"],
+                              replacements={"$data_folder": params.data_folder},
+                              label_parsing_func = lambda x: parsing_func(params, x))
+
+first_x, first_y = next(iter(train_set()))
 vad_brain = VADBrain(
     modules=[params.rnn, params.lin],
     optimizer=params.optimizer,
     first_inputs=[first_x],
 )
-vad_brain.fit(range(params.N_epochs), train_set, params.valid_loader())
-test_stats = vad_brain.evaluate(params.test_loader())
+vad_brain.fit(range(params.N_epochs), train_set(), train_set())
+test_stats = vad_brain.evaluate(train_set())
 #print("Test DER: %.2f" % summarize_error_rate(test_stats["DER"]))
 
 
-# For such a small dataset, the PER can be unpredictable.
-# Instead, check that at the end of training, the error is acceptable.
 def test_error():
-    assert summarize_average(test_stats["DER"]) < 1.0
+    assert summarize_average(test_stats["loss"]) < 0.01
