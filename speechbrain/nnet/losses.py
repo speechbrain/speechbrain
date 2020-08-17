acae2c2cf8a07c91175cb6ece2655d30c05e6616
cem <csubakan@gmail.com>
2020-08-17 02:10:12 -0400

    adding docstrings, and cleaning up

--- speechbrain/nnet/losses.py
+++ speechbrain/nnet/losses.py
@@ -428,129 +428,24 @@ def compute_masked_loss(
         return label_smoothing * loss_reg + (1 - label_smoothing) * loss
 
 
-def get_si_snr(source, estimate_source, source_lengths):
-    """
-    Args:
-        source: [B, C, T], B is batch size
-        estimate_source: [B, C, T]
-        source_lengths: [B]
-    """
-    source = source.permute(0, 2, 1)
-    estimate_source = estimate_source.permute(0, 2, 1)
-
-    max_snr, perms, max_snr_idx = cal_si_snr_with_pit(
-        source, estimate_source, source_lengths
-    )
-    loss = 0 - torch.mean(max_snr)
-    reorder_estimate_source = reorder_source(
-        estimate_source, perms, max_snr_idx
-    )
-    return loss, max_snr, estimate_source, reorder_estimate_source
-
-
-def cal_si_snr_with_pit(source, estimate_source, source_lengths):
-    """Calculate SI-SNR with PIT training.
-    Args:
-        source: [B, C, T], B is batch size
-        estimate_source: [B, C, T]
-        source_lengths: [B], each item is between [0, T]
-    """
-    EPS = 1e-8
-    assert source.size() == estimate_source.size()
-    B, C, T = source.size()
-    # mask padding position along T
-    mask = get_mask(source, source_lengths)
-    estimate_source *= mask
-
-    # Step 1. Zero-mean norm
-    num_samples = source_lengths.view(-1, 1, 1).float()  # [B, 1, 1]
-    mean_target = torch.sum(source, dim=2, keepdim=True) / num_samples
-    mean_estimate = (
-        torch.sum(estimate_source, dim=2, keepdim=True) / num_samples
-    )
-    zero_mean_target = source - mean_target
-    zero_mean_estimate = estimate_source - mean_estimate
-    # mask padding position along T
-    zero_mean_target *= mask
-    zero_mean_estimate *= mask
-
-    # Step 2. SI-SNR with PIT
-    # reshape to use broadcast
-    s_target = torch.unsqueeze(zero_mean_target, dim=1)  # [B, 1, C, T]
-    s_estimate = torch.unsqueeze(zero_mean_estimate, dim=2)  # [B, C, 1, T]
-    # s_target = <s', s>s / ||s||^2
-    pair_wise_dot = torch.sum(
-        s_estimate * s_target, dim=3, keepdim=True
-    )  # [B, C, C, 1]
-    s_target_energy = (
-        torch.sum(s_target ** 2, dim=3, keepdim=True) + EPS
-    )  # [B, 1, C, 1]
-    pair_wise_proj = pair_wise_dot * s_target / s_target_energy  # [B, C, C, T]
-    # e_noise = s' - s_target
-    e_noise = s_estimate - pair_wise_proj  # [B, C, C, T]
-    # SI-SNR = 10 * log_10(||s_target||^2 / ||e_noise||^2)
-    pair_wise_si_snr = torch.sum(pair_wise_proj ** 2, dim=3) / (
-        torch.sum(e_noise ** 2, dim=3) + EPS
-    )
-    pair_wise_si_snr = 10 * torch.log10(pair_wise_si_snr + EPS)  # [B, C, C]
-
-    # Get max_snr of each utterance
-    # permutations, [C!, C]
-    perms = source.new_tensor(list(permutations(range(C))), dtype=torch.long)
-    # one-hot, [C!, C, C]
-    index = torch.unsqueeze(perms, 2)
-    perms_one_hot = source.new_zeros((*perms.size(), C)).scatter_(2, index, 1)
-    # [B, C!] <- [B, C, C] einsum [C!, C, C], SI-SNR sum of each permutation
-    snr_set = torch.einsum("bij,pij->bp", [pair_wise_si_snr, perms_one_hot])
-    max_snr_idx = torch.argmax(snr_set, dim=1)  # [B]
-    # max_snr = torch.gather(snr_set, 1, max_snr_idx.view(-1, 1))  # [B, 1]
-    max_snr, _ = torch.max(snr_set, dim=1, keepdim=True)
-    max_snr /= C
-    return max_snr, perms, max_snr_idx
-
-
-def reorder_source(source, perms, max_snr_idx):
-    """
-    Args:
-        source: [B, C, T]
-        perms: [C!, C], permutations
-        max_snr_idx: [B], each item is between [0, C!)
-    Returns:
-        reorder_source: [B, C, T]
-    """
-    B, C, *_ = source.size()
-    # [B, C], permutation whose SI-SNR is max of each utterance
-    # for each utterance, reorder estimate source according this permutation
-    max_snr_perm = torch.index_select(perms, dim=0, index=max_snr_idx)
-    # print('max_snr_perm', max_snr_perm)
-    # maybe use torch.gather()/index_select()/scatter() to impl this?
-    reorder_source = torch.zeros_like(source)
-    for b in range(B):
-        for c in range(C):
-            reorder_source[b, c] = source[b, max_snr_perm[b][c]]
-    return reorder_source
-
-
-def get_mask(source, source_lengths):
-    """
-    Args:
-        source: [T, B, C]
-        source_lengths: [B]
-    Returns:
-        mask: [T, B, 1]
-    """
-    T, B, _ = source.size()
-    mask = source.new_ones((T, B, 1))
-    for i in range(B):
-        mask[source_lengths[i] :, i, :] = 0
-    return mask
-
-
 def get_si_snr_with_pitwrapper(source, estimate_source):
     """
     This function wraps si_snr calculation with the speechbrain pit-wrapper
 
-    comment: I am adding this, but unfortunately this option runs out of memory
+    Args:
+        source: [B, T, C],
+            where B is batch size, T is the length of the sources, C is the number of sources
+            the ordering is made so that this loss is compatible with the class PitWrapper
+
+        estimate_source: [B, T, C]
+            the estimated source
+
+    Example:
+    >>> x = torch.arange(600).reshape(3, 100, 2)
+    >>> xhat = x[:, :, (1, 0)]
+    >>> si_snr = -get_si_snr_with_pitwrapper(x, xhat)
+    >>> print(si_snr)
+    tensor(135.2284)
     """
 
     pit_si_snr = PitWrapper(cal_si_snr)
@@ -561,13 +456,24 @@ def get_si_snr_with_pitwrapper(source, estimate_source):
 
 def cal_si_snr(source, estimate_source):
     """Calculate SI-SNR
-    Args:
+    Arguments:
         source: [T, B, C],
-        where B is batch size, T is the length of the sources, C is the number of sources
-        the ordering is made so that this loss is compatible with the class PitWrapper
+            where B is batch size, T is the length of the sources, C is the number of sources
+            the ordering is made so that this loss is compatible with the class PitWrapper
 
         estimate_source: [T, B, C]
-        source_lengths: [B], each item is between [0, T]
+            the estimated source
+
+    Example:
+    >>> import numpy as np
+    >>> x = torch.Tensor([[1, 0], [123, 45], [34, 5], [2312, 421]])
+    >>> xhat = x[:, (1, 0)]
+    >>> x = x.unsqueeze(-1).repeat(1, 1, 2)
+    >>> xhat = xhat.unsqueeze(1).repeat(1, 2, 1)
+    >>> si_snr = -cal_si_snr(x, xhat)
+    >>> print(si_snr)
+    tensor([[[ 25.2142, 144.1789],
+             [130.9283,  25.2142]]])
     """
     EPS = 1e-8
     assert source.size() == estimate_source.size()
@@ -609,3 +515,40 @@ def cal_si_snr(source, estimate_source):
     si_snr = 10 * torch.log10(si_snr_beforelog + EPS)  # [B, C]
 
     return -si_snr.unsqueeze(0)
+
+
+def get_mask(source, source_lengths):
+    """
+    Args:
+        source: [T, B, C]
+        source_lengths: [B]
+    Returns:
+        mask: [T, B, 1]
+
+    Example:
+    ---------
+    >>> source = torch.randn(4, 3, 2)
+    >>> source_lengths = torch.Tensor([2, 1, 4]).int()
+    >>> mask = get_mask(source, source_lengths)
+    >>> print(mask)
+    tensor([[[1.],
+             [1.],
+             [1.]],
+    <BLANKLINE>
+            [[1.],
+             [0.],
+             [1.]],
+    <BLANKLINE>
+            [[0.],
+             [0.],
+             [1.]],
+    <BLANKLINE>
+            [[0.],
+             [0.],
+             [1.]]])
+    """
+    T, B, _ = source.size()
+    mask = source.new_ones((T, B, 1))
+    for i in range(B):
+        mask[source_lengths[i] :, i, :] = 0
+    return mask
