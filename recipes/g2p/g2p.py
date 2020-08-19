5a7408ce21c5528071a00503d0ee9e566ce89160
Loren Lugosch <lugosch@orthanc.ece.mcgill.ca>
2020-08-19 17:12:22 -0400

    hparam tuning; output augmented lexicon

--- recipes/g2p/g2p.py
+++ recipes/g2p/g2p.py
@@ -2,30 +2,22 @@
 import os
 import sys
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import speechbrain as sb
 from speechbrain.data_io.data_io import prepend_bos_token
 from speechbrain.data_io.data_io import append_eos_token
 from speechbrain.decoders.decoders import undo_padding
-from speechbrain.decoders.seq2seq import S2SRNNGreedySearcher
+from speechbrain.decoders.seq2seq import S2SRNNBeamSearcher
 from speechbrain.utils.edit_distance import wer_details_for_batch
 from speechbrain.utils.train_logger import summarize_average
 from speechbrain.utils.train_logger import summarize_error_rate
 import torch
+import numpy as np
 
-# output_folder = os.path.join("results", "g2p")
-# experiment_dir = os.path.dirname(os.path.abspath(__file__))
-# params_file = os.path.join(experiment_dir, "hparams.yaml")
-# overrides = {}
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(current_dir))
 params_file, overrides = sb.core.parse_arguments(sys.argv[1:])
 with open(params_file) as fin:
     hparams = sb.yaml.load_extended_yaml(fin, overrides)
-
-# sb.core.create_experiment_directory(
-#    experiment_directory=output_folder, hyperparams_to_save=params_file,
-# )
 
 # Create experiment directory
 sb.core.create_experiment_directory(
@@ -34,8 +26,15 @@ sb.core.create_experiment_directory(
     overrides=overrides,
 )
 
+modules = torch.nn.ModuleList([
+    hparams.encoder_embed,
+    hparams.encoder_net,
+    hparams.decoder_embed,
+    hparams.decoder_net,
+    hparams.decoder_linear,
+])
 
-searcher = S2SRNNGreedySearcher(
+searcher = S2SRNNBeamSearcher(
     modules=[
         hparams.decoder_embed,
         hparams.decoder_net,
@@ -45,24 +44,47 @@ searcher = S2SRNNGreedySearcher(
     bos_index=hparams.bos,
     eos_index=hparams.eos,
     min_decode_ratio=0,
-    max_decode_ratio=1.0,
+    max_decode_ratio=10.0, # the output may be longer than the input, e.g. "b o x" --> "B AA K S"
+    beam_size=10,
 )
 
-# Create train.csv, dev.csv, test.csv from lexicon.csv
-
-lexicon = pd.read_csv(hparams.input_lexicon)
-train_lexicon, other = train_test_split(
-    lexicon, test_size=0.20, random_state=hparams.seed
+checkpointer = sb.utils.checkpoints.Checkpointer(
+    checkpoints_dir=hparams.save_folder,
+    recoverables={
+        "model": modules,
+        "optimizer": hparams.optimizer,
+        "scheduler": hparams.lr_annealing,
+        "counter": hparams.epoch_counter,
+    },
 )
-valid_lexicon, test_lexicon = train_test_split(
-    other, test_size=0.50, random_state=hparams.seed
-)
-train_lexicon.to_csv(hparams.csv_train, index=False)
-valid_lexicon.to_csv(hparams.csv_valid, index=False)
-test_lexicon.to_csv(hparams.csv_test, index=False)
-
 
 class G2P(sb.core.Brain):
+    def decode_batch(self, batch, init_params=False):
+        x = batch[0]
+        _, graphemes, graphemes_lens = x
+        x_embedded = hparams.encoder_embed(
+            graphemes.long(), init_params=init_params
+        )
+        encoder_out = hparams.encoder_net(x_embedded, init_params=init_params)
+        seq, _ = searcher(encoder_out, graphemes_lens)
+        gg = torch.round(graphemes.shape[1] * graphemes_lens).long()
+        for i in range(len(graphemes)):
+            # remove padding from search results
+            if 0 in seq[i]:
+                seq_no_padding = []
+                for j in range(len(seq[i])):
+                    if seq[i][j] != 0: seq_no_padding += [seq[i][j]]
+                    else: break
+                seq[i] = seq_no_padding
+
+        # Convert indices to labels
+        batch_size = len(graphemes)
+        model_in = [graphemes[i][:gg[i]].long() for i in range(batch_size)]
+        decoded_inputs = [" ".join([hparams.train_loader.label_dict["graphemes"]["index2lab"][l.item()] for l in s_in]) for s_in in model_in]
+        decoded_outputs = [" ".join([hparams.train_loader.label_dict["phonemes"]["index2lab"][l] for l in s_out]) for s_out in seq]
+
+        return decoded_inputs, decoded_outputs
+
     def compute_forward(self, x, y, stage="train", init_params=False):
         _, graphemes, graphemes_lens = x
         _, phonemes, phonemes_lens = y
@@ -71,8 +93,6 @@ class G2P(sb.core.Brain):
         x_embedded = hparams.encoder_embed(
             graphemes.long(), init_params=init_params
         )
-        # print(graphemes_lens)
-        # print(phonemes_lens)
         y_embedded = hparams.decoder_embed(phonemes, init_params=init_params)
         encoder_out = hparams.encoder_net(x_embedded, init_params=init_params)
         h, w = hparams.decoder_net(
@@ -83,12 +103,15 @@ class G2P(sb.core.Brain):
 
         if stage != "train":
             seq, _ = searcher(encoder_out, graphemes_lens)
-            ######
-            # print(graphemes[0])
-            # print(seq[0])
-            # print(outputs.argmax(2)[0])
-            # print(phonemes[0])
-            ######
+            gg = torch.round(graphemes.shape[1] * graphemes_lens).long()
+            for i in range(len(graphemes)):
+                # remove padding from search results
+                if 0 in seq[i]:
+                    seq_no_padding = []
+                    for j in range(len(seq[i])):
+                        if seq[i][j] != 0: seq_no_padding += [seq[i][j]]
+                        else: break
+                    seq[i] = seq_no_padding
             return outputs, seq
 
         return outputs
@@ -140,13 +163,39 @@ class G2P(sb.core.Brain):
         print("Train loss: %.2f" % summarize_average(train_stats["loss"]))
         print("Valid loss: %.2f" % summarize_average(valid_stats["loss"]))
         print("Valid PER: %.2f" % summarize_error_rate(valid_stats["PER"]))
-
+        per = summarize_error_rate(valid_stats["PER"])
+        old_lr, new_lr = hparams.lr_annealing([hparams.optimizer], epoch, per)
+        epoch_stats = {"epoch": epoch, "lr": old_lr}
+        hparams.train_logger.log_stats(epoch_stats, train_stats, valid_stats)
+        checkpointer.save_and_keep_only(meta={"PER": per}, min_keys=["PER"])
 
 train_set = hparams.train_loader()
 valid_set = hparams.valid_loader()
+test_set = hparams.test_loader()
 first_x, first_y = next(iter(train_set))
-# print(len(hparams.train_loader.label_dict["graphemes"]["counts"])) --> 27
-# print(len(hparams.train_loader.label_dict["phonemes"]["counts"])) --> 39
+
+# Add 1 to labels, so that the token for padding =\= the token for a label
+temp = {}
+for key in hparams.train_loader.label_dict["graphemes"]["index2lab"]:
+        index = hparams.train_loader.label_dict["graphemes"]["index2lab"][key]
+        temp[key + 1] = index
+        hparams.train_loader.label_dict["graphemes"]["lab2index"][index] = key + 1
+hparams.train_loader.label_dict["graphemes"]["index2lab"] = temp
+hparams.valid_loader.label_dict["graphemes"]["index2lab"] = hparams.train_loader.label_dict["graphemes"]["index2lab"]
+hparams.valid_loader.label_dict["graphemes"]["lab2index"] = hparams.train_loader.label_dict["graphemes"]["lab2index"]
+hparams.test_loader.label_dict["graphemes"]["index2lab"] = hparams.train_loader.label_dict["graphemes"]["index2lab"]
+hparams.test_loader.label_dict["graphemes"]["lab2index"] = hparams.train_loader.label_dict["graphemes"]["lab2index"]
+
+temp = {}
+for key in hparams.train_loader.label_dict["phonemes"]["index2lab"]:
+        index = hparams.train_loader.label_dict["phonemes"]["index2lab"][key]
+        temp[key + 1] = index
+        hparams.train_loader.label_dict["phonemes"]["lab2index"][index] = key + 1
+hparams.train_loader.label_dict["phonemes"]["index2lab"] = temp
+hparams.valid_loader.label_dict["phonemes"]["index2lab"] = hparams.train_loader.label_dict["phonemes"]["index2lab"]
+hparams.valid_loader.label_dict["phonemes"]["lab2index"] = hparams.train_loader.label_dict["phonemes"]["lab2index"]
+hparams.test_loader.label_dict["phonemes"]["index2lab"] = hparams.train_loader.label_dict["phonemes"]["index2lab"]
+hparams.test_loader.label_dict["phonemes"]["lab2index"] = hparams.train_loader.label_dict["phonemes"]["lab2index"]
 
 # why do I have to list the modules?
 model = G2P(
@@ -162,5 +211,49 @@ model = G2P(
 )
 
 model.fit(range(hparams.N_epochs), train_set, valid_set)
+
 test_stats = model.evaluate(hparams.test_loader())
 print("Test PER: %.2f" % summarize_error_rate(test_stats["PER"]))
+
+# Get pronunciations for OOV words; add to lexicon_augmented.
+# (As in the other dataloaders, we need to change the labels to deal with padding.)
+oov_set = hparams.oov_loader()
+hparams.oov_loader.label_dict["graphemes"]["index2lab"] = hparams.train_loader.label_dict["graphemes"]["index2lab"]
+hparams.oov_loader.label_dict["graphemes"]["lab2index"] = hparams.train_loader.label_dict["graphemes"]["lab2index"]
+ID = []
+duration = []
+graphemes = []
+graphemes_format = []
+graphemes_opts = []
+phonemes = []
+phonemes_format = []
+phonemes_opts = []
+lexicon = pd.read_csv(hparams.input_lexicon)
+current_ID = max(lexicon.ID) + 1
+for batch in oov_set:
+	decoded_inputs, decoded_outputs = model.decode_batch(batch)
+	batch_size = len(decoded_inputs)
+	ID += [i for i in range(current_ID, current_ID + batch_size)]
+	duration += [len(d.split()) for d in decoded_inputs]
+	graphemes += decoded_inputs
+	graphemes_format += ["string"] * batch_size
+	graphemes_opts += [np.nan] * batch_size
+	phonemes += decoded_outputs
+	phonemes_format += ["string"] * batch_size
+	phonemes_opts += [np.nan] * batch_size
+	current_ID += batch_size
+
+augment = pd.DataFrame({
+	"ID" : ID,
+	"duration" : duration,
+	"graphemes" : graphemes,
+	"graphemes_format" : graphemes_format,
+	"graphemes_opts" : graphemes_opts,
+	"phonemes" : phonemes,
+	"phonemes_format" : phonemes_format,
+	"phonemes_opts" : phonemes_opts
+})
+
+lexicon_augmented = pd.concat([lexicon, augment])
+lexicon_augmented.to_csv(hparams.output_lexicon, index=False)
+print("New lexicon generated (%s)." % hparams.output_lexicon)
