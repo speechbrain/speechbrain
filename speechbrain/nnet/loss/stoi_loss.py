Fix floating point precision doctest error with stoi loss

speechbrain/nnet/loss/stoi_loss.py
@@ -114,10 +114,10 @@ def stoi_loss(y_pred_batch, y_true_batch, lens, reduction="mean"):
 
     Example
     -------
-    >>> a = torch.sin(torch.arange(16000, dtype=float)).unsqueeze(0)
+    >>> a = torch.sin(torch.arange(16000, dtype=torch.float32)).unsqueeze(0)
     >>> b = a + 0.001
     >>> -stoi_loss(b, a, torch.ones(1))
-    tensor(0.7740)
+    tensor(0.7762)
     """
 
     y_pred_batch = torch.squeeze(y_pred_batch, dim=-1)