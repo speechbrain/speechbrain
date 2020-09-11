41919054bcd5db357b3f44a229f3c9d09906afce
jerrygood0703 <jerrygood0703@gmail.com>
2020-09-11 16:53:58 +0800

    fix format for sharedgrouplinear

--- speechbrain/nnet/shared_group_linear.py
+++ speechbrain/nnet/shared_group_linear.py
@@ -1,6 +1,3 @@
-
-
-
 import torch
 import torch.nn as nn
 
@@ -11,23 +8,26 @@ class GroupLinearLayer(nn.Module):
     def __init__(self, din, dout, num_blocks):
         super(GroupLinearLayer, self).__init__()
 
-        #gain = 1.0 / math.sqrt(2)
-        #a = gain * math.sqrt(6.0 / (din + dout))
+        # gain = 1.0 / math.sqrt(2)
+        # a = gain * math.sqrt(6.0 / (din + dout))
 
         a = 0.1
-        self.weight = nn.Parameter(torch.FloatTensor(num_blocks,din,dout).uniform_(-a,a))
+        self.weight = nn.Parameter(
+            torch.FloatTensor(num_blocks, din, dout).uniform_(-a, a)
+        )
 
-    def forward(self,x):
-        x = x.permute(1,0,2)
-        x = torch.bmm(x,self.weight)
-        x = x.permute(1,0,2)
+    def forward(self, x):
+        x = x.permute(1, 0, 2)
+        x = torch.bmm(x, self.weight)
+        x = x.permute(1, 0, 2)
 
         return x
+
 
 class SharedGroupLinearLayer(nn.Module):
     """All the parameters are shared using soft attention this layer is used for sharing Q,K,V parameters of MHA"""
 
-    def __init__(self, din, dout, num_blocks, ns = None, bias=True, a=None):
+    def __init__(self, din, dout, num_blocks, ns=None, bias=True, a=None):
         super(SharedGroupLinearLayer, self).__init__()
 
         if ns is None:
@@ -38,44 +38,43 @@ class SharedGroupLinearLayer(nn.Module):
         else:
             n_templates = ns
 
-        print('nb', num_blocks, 'ns', n_templates)
-
-        self.w = nn.ModuleList([nn.Linear(din,dout) for _ in range(0,n_templates)])
+        self.w = nn.ModuleList(
+            [nn.Linear(din, dout) for _ in range(0, n_templates)]
+        )
         self.gll_write = GroupLinearLayer(dout, 16, n_templates)
-        self.gll_read = GroupLinearLayer(din,16,1)
+        self.gll_read = GroupLinearLayer(din, 16, 1)
         self.nb = num_blocks
 
         self.weight = self.w[0].weight
         self.bias = None
 
-    def forward(self,x):
-        T,bsz,_ = x.shape
-        x = x.reshape((x.shape[0]*x.shape[1], self.nb, x.shape[2]//self.nb))
-        #input size (bs,num_blocks,din), required matching num_blocks vs n_templates
+    def forward(self, x):
+        T, bsz, _ = x.shape
+        x = x.reshape((x.shape[0] * x.shape[1], self.nb, x.shape[2] // self.nb))
+        # input size (bs,num_blocks,din), required matching num_blocks vs n_templates
         bs_size = x.shape[0]
         k = x.shape[1]
-        x= x.reshape(k*bs_size,-1)
-        x_read = self.gll_read((x*1.0).reshape((x.shape[0], 1, x.shape[1])))
+        x = x.reshape(k * bs_size, -1)
+        x_read = self.gll_read((x * 1.0).reshape((x.shape[0], 1, x.shape[1])))
         x_next = []
         for layer in self.w:
             x_next_l = layer(x)
             x_next.append(x_next_l)
-        x_next = torch.stack(x_next,1) #(k*bs,n_templates,dout)
+        x_next = torch.stack(x_next, 1)  # (k*bs,n_templates,dout)
 
         x_write = self.gll_write(x_next)
-
 
         sm = nn.Softmax(2)
         att = sm(torch.bmm(x_read, x_write.permute(0, 2, 1)))
 
         x_next = torch.bmm(att, x_next)
 
-        x_next = x_next.mean(dim=1).reshape(bs_size,k,-1)
+        x_next = x_next.mean(dim=1).reshape(bs_size, k, -1)
 
-
-        x_next = x_next.reshape((T,bsz,self.nb*x_next.shape[2]))
+        x_next = x_next.reshape((T, bsz, self.nb * x_next.shape[2]))
 
         return x_next
+
 
 if __name__ == "__main__":
 
@@ -83,19 +82,18 @@ if __name__ == "__main__":
     ns = 2
     din = 512
     dout = 512
-    GLN = SharedGroupLinearLayer(din,dout,nb,ns)
+    GLN = SharedGroupLinearLayer(din, dout, nb, ns)
 
-    #for g in GLN.parameters():
+    # for g in GLN.parameters():
     #    print(g.shape)
 
-    print('params', sum(g.numel() for g in GLN.parameters()))
+    print("params", sum(g.numel() for g in GLN.parameters()))
 
     T = 10
     bs = 10
 
-    x = torch.randn(T,bs,nb*din)
-    print('x shape', x.shape)
-    #x = torch.randn(bs,nb,128)
+    x = torch.randn(T, bs, nb * din)
+    print("x shape", x.shape)
+    # x = torch.randn(bs,nb,128)
 
     print(GLN(x).shape)
-
