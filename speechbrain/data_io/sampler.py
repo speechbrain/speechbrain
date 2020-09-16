b9b26a347c25dd2b973b45792b2f3fc3ce738e1d
Aku Rouhe <aku.rouhe@aalto.fi>
2020-09-16 16:53:10 +0300

    Saveable DataLoader and RandomSampler

---
+++ speechbrain/data_io/sampler.py
@@ -0,0 +1,35 @@
+"""
+PyTorch compatible samplers
+
+These determine the order of iteration through a dataset.
+"""
+import torch
+from torch.utils.data.sampler import RandomSampler
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ReproducibleRandomSampler(RandomSampler):
+    def __init__(self, data_source, seed=563375142, epoch=0, **kwargs):
+        if "generator" in kwargs:
+            MSG = (
+                "Cannot give a separate generator when using "
+                + "ReproducibleRandomSampler"
+            )
+            raise ValueError(MSG)
+        super().__init__(data_source, **kwargs)
+        self.seed = int(seed)
+        self.epoch = epoch
+        self.generator = torch.Generator()
+
+    def set_epoch(self, epoch):
+        """
+        You can also just access self.epoch, but we maintain this interface
+        to mirror torch.utils.data.distributed.DistributedSampler
+        """
+        self.epoch = epoch
+
+    def __iter__(self):
+        self.generator.manual_seed(self.seed + self.epoch)
+        return super().__iter__()
