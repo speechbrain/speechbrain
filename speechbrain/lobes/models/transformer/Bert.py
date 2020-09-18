860915fd9743a666b3c14e058ce3ff724dbf0799
Jianyuan Zhong <jzhong9@u.rochester.edu>
2020-09-18 13:40:22 -0400

    added bert

--- speechbrain/lobes/models/transformer/Bert.py
+++ speechbrain/lobes/models/transformer/Bert.py
@@ -11,7 +11,12 @@ from transformers import BertModel
 from speechbrain.nnet.linear import Linear
 from speechbrain.nnet.containers import Sequential
 from speechbrain.nnet.normalization import LayerNorm
-from speechbrain.lobes.models.transformer.Transformer import NormalizedEmbedding, PositionalEncoding, get_lookahead_mask, get_key_padding_mask
+from speechbrain.lobes.models.transformer.Transformer import (
+    NormalizedEmbedding,
+    PositionalEncoding,
+    get_lookahead_mask,
+    get_key_padding_mask,
+)
 
 
 class Bert(nn.Module):
@@ -45,25 +50,16 @@ class Bert(nn.Module):
     torch.Size([3, 3, 100])
     """
 
-    def __init__(
-        self,
-        vocab,
-        masking_func,
-        d_model=768,
-        dropout=0.1
-    ):
+    def __init__(self, vocab, masking_func, d_model=768, dropout=0.1):
         super().__init__()
 
         self.custom_src_module = BertEmbedding(d_model, vocab, dropout)
         pretrain = BertModel.from_pretrained(
-            'bert-base-uncased',
-            return_dict=False
+            "bert-base-uncased", return_dict=False
         )
         self.bert = pretrain.encoder
         self.output_proj = Sequential(
-            Linear(d_model),
-            LayerNorm(eps=1e-12),
-            Linear(vocab)
+            Linear(d_model), LayerNorm(eps=1e-12), Linear(vocab)
         )
 
         self.masking_func = masking_func
@@ -90,17 +86,13 @@ class Bert(nn.Module):
             src, src_mask = self.masking_func(src, self.training)
 
         src = self.custom_src_module(src, init_params)
-        encoder_out = self.bert(
-            src,
-            attention_mask=src_mask,
-        )
+        encoder_out = self.bert(src, attention_mask=src_mask,)
 
         pred = self.output_proj(encoder_out[0], init_params)
         return pred
 
 
 class BertEmbedding(nn.Module):
-
     def __init__(self, d_model, vocab, dropout=0.1):
         super().__init__()
         self.token_emb = NormalizedEmbedding(d_model, vocab)
@@ -115,7 +107,9 @@ class BertEmbedding(nn.Module):
         return self.drop(x)
 
 
-def make_masks(src, istraining, pad_idx=0, padding_mask=True, token_masking=True, p=0.15):
+def make_masks(
+    src, istraining, pad_idx=0, padding_mask=True, token_masking=True, p=0.15
+):
     src_key_padding_mask = None
     if padding_mask:
         src_key_padding_mask = get_key_padding_mask(src, pad_idx).int()
@@ -124,10 +118,15 @@ def make_masks(src, istraining, pad_idx=0, padding_mask=True, token_masking=True
 
         batch_size, seq_length = src.shape
         seq_ids = torch.arange(seq_length, device=src.device)
-        causal_mask = seq_ids[None, None, :].repeat(batch_size, seq_length, 1) <= seq_ids[None, :, None]
+        causal_mask = (
+            seq_ids[None, None, :].repeat(batch_size, seq_length, 1)
+            <= seq_ids[None, :, None]
+        )
         # causal and attention masks must have same type with pytorch version < 1.3
         causal_mask = causal_mask.to(src_key_padding_mask.dtype)
-        extended_attention_mask = causal_mask[:, None, :, :] * src_key_padding_mask[:, None, None, :]
+        extended_attention_mask = (
+            causal_mask[:, None, :, :] * src_key_padding_mask[:, None, None, :]
+        )
         extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
 
     # if istraining:
