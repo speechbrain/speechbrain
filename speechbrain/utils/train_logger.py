95132b502c63e7afa925c38a8f7500494d40d5e2
ravanelm <mirco.ravanelli@gmail.com>
2020-09-23 12:28:15 -0400

    resolve conflict - part2

--- speechbrain/utils/train_logger.py
+++ speechbrain/utils/train_logger.py
@@ -5,7 +5,6 @@ Authors
  * Peter Plantinga 2020
 """
 import logging
-from speechbrain.utils.edit_distance import wer_summary
 
 logger = logging.getLogger(__name__)
 
@@ -54,13 +53,12 @@ class FileTrainLogger(TrainLogger):
         from a training/validation pass and summarize it to a single scalar.
     """
 
-    def __init__(self, save_file, summary_fns=None):
+    def __init__(self, save_file):
         self.save_file = save_file
-        self.summary_fns = summary_fns or {}
 
     def _item_to_string(self, key, value, dataset=None):
         """Convert one item to string, handling floats"""
-        if isinstance(value, float) and 0.01 < value < 100.0:
+        if isinstance(value, float) and 1.0 < value < 100.0:
             value = f"{value:.2f}"
         elif isinstance(value, float):
             value = f"{value:.2e}"
@@ -89,15 +87,8 @@ class FileTrainLogger(TrainLogger):
             ("valid", valid_stats),
             ("test", test_stats),
         ]:
-            if stats is None:
-                continue
-            summary = {}
-            for stat, value_list in stats.items():
-                if stat in self.summary_fns:
-                    summary[stat] = self.summary_fns[stat](value_list)
-                else:
-                    summary[stat] = summarize_average(value_list)
-            string_summary += " - " + self._stats_to_string(summary, dataset)
+            if stats is not None:
+                string_summary += " - " + self._stats_to_string(stats, dataset)
 
         with open(self.save_file, "a") as fout:
             print(string_summary, file=fout)
@@ -125,7 +116,7 @@ class TensorboardLogger(TrainLogger):
         from torch.utils.tensorboard import SummaryWriter
 
         self.writer = SummaryWriter(self.save_dir)
-        self.global_step = {"train": {}, "valid": {}, "meta": 0}
+        self.global_step = {"train": {}, "valid": {}, "test": {}, "meta": 0}
 
     def log_stats(
         self,
@@ -155,12 +146,3 @@ class TensorboardLogger(TrainLogger):
                     new_global_step = self.global_step[dataset][stat] + 1
                     self.writer.add_scalar(tag, value, new_global_step)
                     self.global_step[dataset][stat] = new_global_step
-
-
-def summarize_average(stat_list):
-    return float(sum(stat_list) / len(stat_list))
-
-
-def summarize_error_rate(stat_list):
-    summary = wer_summary(stat_list)
-    return summary["WER"]
