38cd94dbc2015feec6e6fcab50c55e31898f0f7c
popcornell <cornellsamuele@gmail.com>
2020-09-27 12:37:09 +0200

    read_wav is handled in data_io.py

--- speechbrain/data_io/data_io.py
+++ speechbrain/data_io/data_io.py
@@ -16,8 +16,34 @@ import pickle
 import hashlib
 import multiprocessing as mp
 import csv
+import torchaudio
 
 logger = logging.getLogger(__name__)
+
+
+def read_audio_example(example):
+    """
+    example with {"supervision": {"start" : x "stop": y ... etc}
+                 {"waveforms": {"files": , channels,
+    """
+
+    waveforms = []
+    for f in example["waveforms"]["files"]:
+        if (
+            "start" not in example["supervision"].keys()
+            or "stop" not in example["supervision"].keys()
+        ):
+            tmp = torchaudio.load(f)
+            waveforms.append(tmp)
+        else:
+            num_frames = (
+                example["supervision"]["stop"] - example["supervision"]["start"]
+            )
+            offset = example["supervision"]["start"]
+            tmp, fs = torchaudio.load(f, num_frames=num_frames, offset=offset)
+            waveforms.append(tmp)
+
+    return torch.cat(waveforms, 0)  # torch.cat(waveforms, 0)
 
 
 def convert_index_to_lab(batch, ind2lab):
