3df2bd3f7061d805ad3e7cec415303a88409e61b
popcornell <cornellsamuele@gmail.com>
2020-09-27 12:10:34 +0200

    moved CategoricalEncoder to a encoders.py

--- speechbrain/data_io/utils.py
+++ speechbrain/data_io/utils.py
@@ -1,62 +1,20 @@
 import os
-import torch
-from ruamel import yaml
-from collections.abc import MutableMapping
 from copy import copy
 
 
-class DataCollection(MutableMapping):
-    # TODO not still sure about this
-    def __init__(self, path_to_yaml):
-
-        with open(path_to_yaml, "r") as f:
-            self.datacol = yaml.safe_load(f)
-
-    def __getitem__(self, key):
-        return self.datacol[key]
-
-    def __delitem__(self, key):
-        del self.datacol[key]
-
-    def __setitem__(self, key, value):
-        if key in self:
-            del self[self[key]]
-        if value in self:
-            del self[value]
-        self.datacol[key] = value
-        self.datacol[value] = key
-
-    def __iter__(self):
-        return iter(self.datacol)
-
-    def __len__(self):
-        return len(self.datacol)
-
-    # def __repr__(self):
-    #   return f"{type(self).__name__}({self.datacol})
-
-    def sanity_check(self):
-        dataset_sanity_check(self)
-
-    def to_ASR_format(self):
-        pass
-
-    def filter(self):
-        pass
-
-    def sort(self):
-        pass
-
-    @staticmethod
-    def merge(list_of_datacollections):
-        pass
-
-
 def dataset_sanity_check(dataset_splits):
+    """
+    This function performs sanity check over a DataCollection.
+    STILL WIP BECAUSE WE MIGHT CHANGE THE DATACOLLECTION FORMAT.
 
-    # dataset_structs is a list of dataset_struct
-    # each dataset struct is a dict-based hierarchical annotation
-    # we check for consistency between all of these e.g. between train.yaml, dev.yaml and test.yaml
+    Parameters
+    ----------
+    dataset_splits
+
+    Returns
+    -------
+
+    """
 
     assert len(dataset_splits) > 0, "provided list of dataset splits is empty"
 
@@ -162,71 +120,6 @@ def to_ASR_format(dataset):
     return utterances
 
 
-class CategoricalEncoder:
-    def __init__(self, data_collections: (list, dict), supervision: str):
-        if isinstance(data_collections, dict):
-            data_collections = [data_collections]
-
-        all_labs = set()
-        for data_coll in data_collections:
-            for data_obj_key in data_coll:
-                data_obj = data_coll[data_obj_key]
-                for sup in data_obj["supervision"]:
-                    for sup_key in sup.keys():
-                        if sup_key == supervision:
-                            if isinstance(sup[sup_key], (list, tuple)):
-                                all_labs.update(set(sup[sup_key]))
-                            elif isinstance(sup[sup_key], (str)):
-                                all_labs.add(sup[sup_key])
-                            else:
-                                raise NotImplementedError
-
-        all_labs = sorted(list(all_labs))  # sort alphabetically just in case
-
-        self.lab2indx = {key: index for index, key in enumerate(all_labs)}
-        self.indx2lab = {key: index for key, index in enumerate(all_labs)}
-
-    def encode(self, x, dtype=torch.long):
-        # TODO handle numpy arrays maybe ?
-        if isinstance(x, (tuple, list)):
-            # then we will return
-            labels = torch.zeros(
-                (len(self.lab2indx.keys()), len(x)), dtype=dtype
-            )
-            for i, elem in enumerate(x):
-                labels[self.lab2indx[elem], i] = 1
-        else:
-            labels = torch.zeros((len(self.lab2indx.keys()), 1), dtype=dtype)
-
-        return labels
-
-    def decode(self, x: torch.Tensor):
-        # labels are of shape num_classes or num_classes, lengthofseq
-        if x.ndim == 1:
-            indx = torch.argmax(x)
-            decoded = self.indx2lab[indx]
-            return decoded
-
-        elif x.ndim == 2:
-            decoded = []
-            indexes = torch.argmax(x, 0)
-            for time_step in range(len(indexes)):
-                decoded.append(self.indx2lab[indexes[time_step]])
-            return decoded
-
-        elif x.ndim == 3:  # batched tensor b, classes, steps
-            decoded = []
-            for batch in x.shape[0]:
-                c_batch = []
-                indexes = torch.argmax(x[batch], 0)
-                for time_step in range(len(indexes)):
-                    c_batch.append(self.indx2lab[indexes[time_step]])
-                decoded.append(c_batch)
-            return decoded
-        else:
-            raise NotImplementedError
-
-
 def replace_entries(data_coll, replacements_dict):
 
     for data_obj_key in data_coll:
@@ -256,19 +149,3 @@ def replace_entries(data_coll, replacements_dict):
                     # check if we have to replace it
 
     return data_coll
-
-
-def get_windowed_examples(dataset):
-    """
-    get examples for diarization, vad and other speech labelling applications
-    from long files by using a sliding window with overlap.
-    """
-    pass
-
-
-def filter_supervision(dataset):
-    pass
-
-
-def filter_waveforms(dataset):
-    pass
