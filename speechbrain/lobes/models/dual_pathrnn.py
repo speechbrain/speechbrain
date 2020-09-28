2b82e9263c632b41750a8f03755a06fa1c97d5de
cem <csubakan@gmail.com>
2020-09-28 08:53:03 -0400

    fixing the bug in dptransformer and implemented dprim

--- speechbrain/lobes/models/dual_pathrnn.py
+++ speechbrain/lobes/models/dual_pathrnn.py
@@ -4,6 +4,10 @@ import torch.nn.functional as F
 
 from speechbrain.lobes.models.transformer.Transformer import TransformerEncoder
 from speechbrain.nnet.linear import Linear
+from speechbrain.lobes.models.block_models.modularity import RIM
+
+# from speechbrain.lobes.models.block_models.modularity import SCOFF
+
 
 # from speechbrain.nnet.RNN import LSTM
 
@@ -427,144 +431,6 @@ class Dual_Path_RNN(nn.Module):
         return input
 
 
-class Dual_Path_Transformer(Dual_Path_RNN):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        num_tf_layers=6,
-        num_layers=1,
-        nhead=8,
-        d_ffn=2048,
-        kdim=None,
-        vdim=None,
-        dropout=0.1,
-        activation=nn.ReLU,
-        return_attention=False,
-        num_modules=1,
-        use_group_comm=False,
-        norm="ln",
-        K=200,
-        num_spks=2,
-    ):
-        super(Dual_Path_Transformer, self).__init__(
-            in_channels, out_channels, hidden_channels=64
-        )
-        self.K = K
-        self.num_spks = num_spks
-        self.num_layers = num_layers
-        self.norm = select_norm(norm, in_channels, 3)
-        self.conv1d = nn.Conv1d(in_channels, out_channels, 1, bias=False)
-
-        self.dual_mdl = nn.ModuleList([])
-        for i in range(num_layers):
-            self.dual_mdl.append(
-                Dual_Transformer_Block(
-                    out_channels,
-                    num_layers=num_tf_layers,
-                    nhead=8,
-                    d_ffn=2048,
-                    kdim=None,
-                    vdim=None,
-                    dropout=0.1,
-                    activation=nn.ReLU,
-                    return_attention=False,
-                    num_modules=1,
-                    use_group_comm=False,
-                    norm="ln",
-                )
-            )
-
-        self.conv2d = nn.Conv2d(
-            out_channels, out_channels * num_spks, kernel_size=1
-        )
-        self.end_conv1x1 = nn.Conv1d(out_channels, in_channels, 1, bias=False)
-        self.prelu = nn.PReLU()
-        self.activation = nn.ReLU()
-        # gated output layer
-        self.output = nn.Sequential(
-            nn.Conv1d(out_channels, out_channels, 1), nn.Tanh()
-        )
-        self.output_gate = nn.Sequential(
-            nn.Conv1d(out_channels, out_channels, 1), nn.Sigmoid()
-        )
-
-    def forward(self, x, init_params=True):
-        """
-           x: [B, N, L]
-        """
-        # [B, N, L]
-        x = self.norm(x)
-        # [B, N, L]
-        x = self.conv1d(x)
-        # [B, N, K, S]
-        x, gap = self._Segmentation(x, self.K)
-        # [B, N*spks, K, S]
-        for i in range(self.num_layers):
-            x = self.dual_mdl[i](x, init_params=init_params)
-        x = self.prelu(x)
-        x = self.conv2d(x)
-        # [B*spks, N, K, S]
-        B, _, K, S = x.shape
-        x = x.view(B * self.num_spks, -1, K, S)
-        # [B*spks, N, L]
-        x = self._over_add(x, gap)
-        x = self.output(x) * self.output_gate(x)
-        # [spks*B, N, L]
-        x = self.end_conv1x1(x)
-        # [B*spks, N, L] -> [B, spks, N, L]
-        _, N, L = x.shape
-        x = x.view(B, self.num_spks, N, L)
-        x = self.activation(x)
-        # [spks, B, N, L]
-        x = x.transpose(0, 1)
-
-        return x
-
-
-# class Dual_RNN_model(nn.Module):
-#    '''
-#       model of Dual Path RNN
-#       input:
-#            in_channels: The number of expected features in the input x
-#            out_channels: The number of features in the hidden state h
-#            hidden_channels: The hidden size of RNN
-#            kernel_size: Encoder and Decoder Kernel size
-#            rnn_type: RNN, LSTM, GRU
-#            norm: gln = "Global Norm", cln = "Cumulative Norm", ln = "Layer Norm"
-#            dropout: If non-zero, introduces a Dropout layer on the outputs
-#                     of each LSTM layer except the last layer,
-#                     with dropout probability equal to dropout. Default: 0
-#            bidirectional: If True, becomes a bidirectional LSTM. Default: False
-#            num_layers: number of Dual-Path-Block
-#            K: the length of chunk
-#            num_spks: the number of speakers
-#    '''
-#    def __init__(self, in_channels, out_channels, hidden_channels,
-#                 kernel_size=2, rnn_type='LSTM', norm='ln', dropout=0,
-#                 bidirectional=False, num_layers=4, K=200, num_spks=2):
-#        super(Dual_RNN_model,self).__init__()
-#        self.encoder = Encoder(kernel_size=kernel_size,out_channels=in_channels)
-#        self.separation = Dual_Path_RNN(in_channels, out_channels, hidden_channels,
-#                 rnn_type=rnn_type, norm=norm, dropout=dropout,
-#                 bidirectional=bidirectional, num_layers=num_layers, K=K, num_spks=num_spks)
-#        self.decoder = Decoder(in_channels=in_channels, out_channels=1, kernel_size=kernel_size, stride=kernel_size//2, bias=False)
-#        self.num_spks = num_spks
-#
-#    def forward(self, x):
-#        '''
-#           x: [B, L]
-#        '''
-#        # [B, N, L]
-#        e = self.encoder(x)
-#        # [spks, B, N, L]
-#        s = self.separation(e)
-#        # [B, N, L] -> [B, L]
-#        out = [s[i]*e for i in range(self.num_spks)]
-#        audio = [self.decoder(out[i]) for i in range(self.num_spks)]
-#        return audio
-#
-#
 class Dual_Transformer_Block(nn.Module):
     """
 #            norm: gln = "Global Norm", cln = "Cumulative Norm", ln = "Layer Norm"
@@ -669,3 +535,420 @@ class Dual_Transformer_Block(nn.Module):
         out = inter_rnn + intra_rnn
 
         return out
+
+
+class Dual_Path_Transformer(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        num_tf_layers=6,
+        num_layers=1,
+        nhead=8,
+        d_ffn=2048,
+        kdim=None,
+        vdim=None,
+        dropout=0.1,
+        activation=nn.ReLU,
+        return_attention=False,
+        num_modules=1,
+        use_group_comm=False,
+        norm="ln",
+        K=200,
+        num_spks=2,
+    ):
+        super(Dual_Path_Transformer, self).__init__()
+        self.K = K
+        self.num_spks = num_spks
+        self.num_layers = num_layers
+        self.norm = select_norm(norm, in_channels, 3)
+        self.conv1d = nn.Conv1d(in_channels, out_channels, 1, bias=False)
+
+        self.dual_mdl = nn.ModuleList([])
+        for i in range(num_layers):
+            self.dual_mdl.append(
+                Dual_Transformer_Block(
+                    out_channels,
+                    num_layers=num_tf_layers,
+                    nhead=8,
+                    d_ffn=2048,
+                    kdim=None,
+                    vdim=None,
+                    dropout=0.1,
+                    activation=nn.ReLU,
+                    return_attention=False,
+                    num_modules=1,
+                    use_group_comm=False,
+                    norm="ln",
+                )
+            )
+
+        self.conv2d = nn.Conv2d(
+            out_channels, out_channels * num_spks, kernel_size=1
+        )
+        self.end_conv1x1 = nn.Conv1d(out_channels, in_channels, 1, bias=False)
+        self.prelu = nn.PReLU()
+        self.activation = nn.ReLU()
+        # gated output layer
+        self.output = nn.Sequential(
+            nn.Conv1d(out_channels, out_channels, 1), nn.Tanh()
+        )
+        self.output_gate = nn.Sequential(
+            nn.Conv1d(out_channels, out_channels, 1), nn.Sigmoid()
+        )
+
+    def forward(self, x, init_params=True):
+        """
+           x: [B, N, L]
+        """
+        # [B, N, L]
+        x = self.norm(x)
+        # [B, N, L]
+        x = self.conv1d(x)
+        # [B, N, K, S]
+        x, gap = self._Segmentation(x, self.K)
+        # [B, N*spks, K, S]
+        for i in range(self.num_layers):
+            x = self.dual_mdl[i](x, init_params=init_params)
+        x = self.prelu(x)
+        x = self.conv2d(x)
+        # [B*spks, N, K, S]
+        B, _, K, S = x.shape
+        x = x.view(B * self.num_spks, -1, K, S)
+        # [B*spks, N, L]
+        x = self._over_add(x, gap)
+        x = self.output(x) * self.output_gate(x)
+        # [spks*B, N, L]
+        x = self.end_conv1x1(x)
+        # [B*spks, N, L] -> [B, spks, N, L]
+        _, N, L = x.shape
+        x = x.view(B, self.num_spks, N, L)
+        x = self.activation(x)
+        # [spks, B, N, L]
+        x = x.transpose(0, 1)
+
+        return x
+
+    def _padding(self, input, K):
+        """
+           padding the audio times
+           K: chunks of length
+           P: hop size
+           input: [B, N, L]
+        """
+        B, N, L = input.shape
+        P = K // 2
+        gap = K - (P + L % K) % K
+        if gap > 0:
+            pad = torch.Tensor(torch.zeros(B, N, gap)).type(input.type())
+            input = torch.cat([input, pad], dim=2)
+
+        _pad = torch.Tensor(torch.zeros(B, N, P)).type(input.type())
+        input = torch.cat([_pad, input, _pad], dim=2)
+
+        return input, gap
+
+    def _Segmentation(self, input, K):
+        """
+           the segmentation stage splits
+           K: chunks of length
+           P: hop size
+           input: [B, N, L]
+           output: [B, N, K, S]
+        """
+        B, N, L = input.shape
+        P = K // 2
+        input, gap = self._padding(input, K)
+        # [B, N, K, S]
+        input1 = input[:, :, :-P].contiguous().view(B, N, -1, K)
+        input2 = input[:, :, P:].contiguous().view(B, N, -1, K)
+        input = (
+            torch.cat([input1, input2], dim=3).view(B, N, -1, K).transpose(2, 3)
+        )
+
+        return input.contiguous(), gap
+
+    def _over_add(self, input, gap):
+        """
+           Merge sequence
+           input: [B, N, K, S]
+           gap: padding length
+           output: [B, N, L]
+        """
+        B, N, K, S = input.shape
+        P = K // 2
+        # [B, N, S, K]
+        input = input.transpose(2, 3).contiguous().view(B, N, -1, K * 2)
+
+        input1 = input[:, :, :, :K].contiguous().view(B, N, -1)[:, :, P:]
+        input2 = input[:, :, :, K:].contiguous().view(B, N, -1)[:, :, :-P]
+        input = input1 + input2
+        # [B, N, L]
+        if gap > 0:
+            input = input[:, :, :-gap]
+
+        return input
+
+
+class Dual_RIM_Block(nn.Module):
+    """
+#            norm: gln = "Global Norm", cln = "Cumulative Norm", ln = "Layer Norm"
+    """
+
+    def __init__(
+        self,
+        device,
+        in_channels,
+        out_channels,
+        hidden_channels,
+        num_units,
+        k,
+        rnn_type="GRU",
+        norm="ln",
+        dropout=0.0,
+        num_layers=1,
+        bidirectional=True,
+        version=1,
+        attention_out=32,
+        num_rules=0,
+        rule_time_steps=0,
+    ):
+        super(Dual_RIM_Block, self).__init__()
+        # RNN model
+        self.intra_mdl = RIM(
+            device,
+            input_size=in_channels,
+            hidden_size=hidden_channels,
+            num_units=num_units,
+            k=k,
+            rnn_cell=rnn_type,
+            n_layers=num_layers,
+            bidirectional=bidirectional,
+            version=version,
+            attention_out=attention_out,
+            num_rules=num_rules,
+            rule_time_steps=rule_time_steps,
+            batch_first=True,
+            dropout=dropout,
+        )
+
+        self.inter_mdl = RIM(
+            device,
+            input_size=in_channels,
+            hidden_size=hidden_channels,
+            num_units=num_units,
+            k=k,
+            rnn_cell=rnn_type,
+            n_layers=num_layers,
+            bidirectional=bidirectional,
+            version=version,
+            attention_out=attention_out,
+            num_rules=num_rules,
+            rule_time_steps=rule_time_steps,
+            batch_first=True,
+            dropout=dropout,
+        )
+
+        self.intra_norm = select_norm(norm, out_channels, 4)
+        self.inter_norm = select_norm(norm, out_channels, 4)
+
+        # Linear
+        self.intra_linear = Linear(out_channels)
+        self.inter_linear = Linear(out_channels)
+
+    def forward(self, x, init_params=True):
+        """
+           x: [B, N, K, S]
+           out: [Spks, B, N, K, S]
+        """
+        B, N, K, S = x.shape
+        # intra RNN
+        # [BS, K, N]
+        intra_rnn = x.permute(0, 3, 2, 1).contiguous().view(B * S, K, N)
+        # [BS, K, H]
+        intra_rnn, _ = self.intra_mdl(intra_rnn, init_params=init_params)
+        # intra_rnn = self.intra_rnn(intra_rnn, init_params=init_params)
+        # [BS, K, N]
+        intra_rnn = self.intra_linear(
+            intra_rnn.contiguous().view(B * S * K, -1), init_params=init_params
+        ).view(B * S, K, -1)
+        # [B, S, K, N]
+        intra_rnn = intra_rnn.view(B, S, K, N)
+        # [B, N, K, S]
+        intra_rnn = intra_rnn.permute(0, 3, 2, 1).contiguous()
+        intra_rnn = self.intra_norm(intra_rnn)
+
+        # [B, N, K, S]
+        intra_rnn = intra_rnn + x
+        # out = intra_rnn
+
+        # inter RNN
+        # [BK, S, N]
+        inter_rnn = intra_rnn.permute(0, 2, 3, 1).contiguous().view(B * K, S, N)
+        # [BK, S, H]
+        inter_rnn, _ = self.inter_mdl(inter_rnn, init_params=init_params)
+        # [BK, S, N]
+        inter_rnn = self.inter_linear(
+            inter_rnn.contiguous().view(B * S * K, -1), init_params=init_params
+        ).view(B * K, S, -1)
+        # [B, K, S, N]
+        inter_rnn = inter_rnn.view(B, K, S, N)
+        # [B, N, K, S]
+        inter_rnn = inter_rnn.permute(0, 3, 1, 2).contiguous()
+        inter_rnn = self.inter_norm(inter_rnn)
+        # [B, N, K, S]
+        out = inter_rnn + intra_rnn
+
+        return out
+
+
+class Dual_Path_RIM(nn.Module):
+    def __init__(
+        self,
+        device,
+        in_channels,
+        out_channels,
+        hidden_channels,
+        num_rim_layers=1,
+        num_layers=6,
+        num_units=4,
+        k=4,
+        rnn_type="GRU",
+        norm="ln",
+        dropout=0.0,
+        bidirectional=True,
+        version=1,
+        K=200,
+        num_spks=2,
+    ):
+        super(Dual_Path_RIM, self).__init__()
+        self.K = K
+        self.num_spks = num_spks
+        self.num_layers = num_layers
+        self.norm = select_norm(norm, in_channels, 3)
+        self.conv1d = nn.Conv1d(in_channels, out_channels, 1, bias=False)
+
+        self.dual_mdl = nn.ModuleList([])
+        for i in range(num_layers):
+            self.dual_mdl.append(
+                Dual_RIM_Block(
+                    device,
+                    out_channels,
+                    out_channels,
+                    hidden_channels,
+                    num_units=num_units,
+                    k=k,
+                    rnn_type=rnn_type,
+                    norm=norm,
+                    dropout=dropout,
+                    num_layers=num_rim_layers,
+                    bidirectional=bidirectional,
+                    version=version,
+                )
+            )
+
+        self.conv2d = nn.Conv2d(
+            out_channels, out_channels * num_spks, kernel_size=1
+        )
+        self.end_conv1x1 = nn.Conv1d(out_channels, in_channels, 1, bias=False)
+        self.prelu = nn.PReLU()
+        self.activation = nn.ReLU()
+        # gated output layer
+        self.output = nn.Sequential(
+            nn.Conv1d(out_channels, out_channels, 1), nn.Tanh()
+        )
+        self.output_gate = nn.Sequential(
+            nn.Conv1d(out_channels, out_channels, 1), nn.Sigmoid()
+        )
+
+    def forward(self, x, init_params=True):
+        """
+           x: [B, N, L]
+        """
+        # [B, N, L]
+        x = self.norm(x)
+        # [B, N, L]
+        x = self.conv1d(x)
+        # [B, N, K, S]
+        x, gap = self._Segmentation(x, self.K)
+        # [B, N*spks, K, S]
+        for i in range(self.num_layers):
+            x = self.dual_mdl[i](x, init_params=init_params)
+        x = self.prelu(x)
+        x = self.conv2d(x)
+        # [B*spks, N, K, S]
+        B, _, K, S = x.shape
+        x = x.view(B * self.num_spks, -1, K, S)
+        # [B*spks, N, L]
+        x = self._over_add(x, gap)
+        x = self.output(x) * self.output_gate(x)
+        # [spks*B, N, L]
+        x = self.end_conv1x1(x)
+        # [B*spks, N, L] -> [B, spks, N, L]
+        _, N, L = x.shape
+        x = x.view(B, self.num_spks, N, L)
+        x = self.activation(x)
+        # [spks, B, N, L]
+        x = x.transpose(0, 1)
+
+        return x
+
+    def _padding(self, input, K):
+        """
+           padding the audio times
+           K: chunks of length
+           P: hop size
+           input: [B, N, L]
+        """
+        B, N, L = input.shape
+        P = K // 2
+        gap = K - (P + L % K) % K
+        if gap > 0:
+            pad = torch.Tensor(torch.zeros(B, N, gap)).type(input.type())
+            input = torch.cat([input, pad], dim=2)
+
+        _pad = torch.Tensor(torch.zeros(B, N, P)).type(input.type())
+        input = torch.cat([_pad, input, _pad], dim=2)
+
+        return input, gap
+
+    def _Segmentation(self, input, K):
+        """
+           the segmentation stage splits
+           K: chunks of length
+           P: hop size
+           input: [B, N, L]
+           output: [B, N, K, S]
+        """
+        B, N, L = input.shape
+        P = K // 2
+        input, gap = self._padding(input, K)
+        # [B, N, K, S]
+        input1 = input[:, :, :-P].contiguous().view(B, N, -1, K)
+        input2 = input[:, :, P:].contiguous().view(B, N, -1, K)
+        input = (
+            torch.cat([input1, input2], dim=3).view(B, N, -1, K).transpose(2, 3)
+        )
+
+        return input.contiguous(), gap
+
+    def _over_add(self, input, gap):
+        """
+           Merge sequence
+           input: [B, N, K, S]
+           gap: padding length
+           output: [B, N, L]
+        """
+        B, N, K, S = input.shape
+        P = K // 2
+        # [B, N, S, K]
+        input = input.transpose(2, 3).contiguous().view(B, N, -1, K * 2)
+
+        input1 = input[:, :, :, :K].contiguous().view(B, N, -1)[:, :, P:]
+        input2 = input[:, :, :, K:].contiguous().view(B, N, -1)[:, :, :-P]
+        input = input1 + input2
+        # [B, N, L]
+        if gap > 0:
+            input = input[:, :, :-gap]
+
+        return input
