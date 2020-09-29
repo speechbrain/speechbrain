513bc268c44f0e52a0902ce27717ebfb8695f24b
Peter Plantinga <plantinga.peter@protonmail.com>
2020-09-29 09:55:04 -0400

    Update CNN Transformer Voicebank recipe

--- recipes/Voicebank/enhance/spectral_mask/experiment.py
+++ recipes/Voicebank/enhance/spectral_mask/experiment.py
@@ -15,12 +15,12 @@ class SEBrain(sb.Brain):
     def compute_forward(self, x, stage):
         ids, wavs, lens = x
         wavs, lens = wavs.to(self.device), lens.to(self.device)
-        feats = self.compute_STFT(wavs)
+        feats = self.hparams.compute_STFT(wavs)
         feats = spectral_magnitude(feats, power=0.5)
         feats = torch.log1p(feats)
 
         # mask with "signal approximation (SA)"
-        mask = self.model(feats)
+        mask = self.hparams.model(feats)
         predict_spec = torch.mul(mask, feats)
 
         # Also return predicted wav
@@ -33,16 +33,16 @@ class SEBrain(sb.Brain):
         ids, target_wav, lens = targets
         target_wav, lens = target_wav.to(self.device), lens.to(self.device)
 
-        if hasattr(self, "waveform_target") and self.waveform_target:
-            loss = self.compute_cost(predict_wav, target_wav, lens)
+        if getattr(self.hparams, "waveform_target", False):
+            loss = self.hparams.compute_cost(predict_wav, target_wav, lens)
             self.loss_metric.append(
                 ids, predict_wav, target_wav, lens, reduction="batch"
             )
         else:
-            targets = self.compute_STFT(target_wav)
+            targets = self.hparams.compute_STFT(target_wav)
             targets = spectral_magnitude(targets, power=0.5)
             targets = torch.log1p(targets)
-            loss = self.compute_cost(predict_spec, targets, lens)
+            loss = self.hparams.compute_cost(predict_spec, targets, lens)
             self.loss_metric.append(
                 ids, predict_spec, targets, lens, reduction="batch"
             )
@@ -62,7 +62,9 @@ class SEBrain(sb.Brain):
                 lens = lens * target_wav.shape[1]
                 for name, pred_wav, length in zip(ids, predict_wav, lens):
                     name += ".wav"
-                    enhance_path = os.path.join(self.enhanced_folder, name)
+                    enhance_path = os.path.join(
+                        self.hparams.enhanced_folder, name
+                    )
                     torchaudio.save(
                         enhance_path, predict_wav[: int(length)].cpu(), 16000
                     )
@@ -70,7 +72,7 @@ class SEBrain(sb.Brain):
         return loss
 
     def on_stage_start(self, stage, epoch=None):
-        self.loss_metric = MetricStats(metric=self.compute_cost)
+        self.loss_metric = MetricStats(metric=self.hparams.compute_cost)
         self.stoi_metric = MetricStats(metric=stoi_loss)
 
         # Define function taking (prediction, target) for parallel eval
@@ -97,27 +99,26 @@ class SEBrain(sb.Brain):
             }
 
         if stage == sb.Stage.VALID:
-            if self.use_tensorboard:
+            if self.hparams.use_tensorboard:
                 valid_stats = {
                     "loss": self.loss_metric.scores,
                     "stoi": self.stoi_metric.scores,
                     "pesq": self.pesq_metric.scores,
                 }
-                self.tensorboard_train_logger.log_stats(
+                self.hparams.tensorboard_train_logger.log_stats(
                     {"Epoch": epoch}, self.train_stats, valid_stats
                 )
-            self.train_logger.log_stats(
+            self.hparams.train_logger.log_stats(
                 {"Epoch": epoch},
                 train_stats={"loss": self.train_loss},
                 valid_stats=stats,
             )
-            self.checkpointer.save_and_keep_only(
-                meta=stats, min_keys=["pesq"],
-            )
+            self.checkpointer.save_and_keep_only(meta=stats, min_keys=["pesq"])
 
         if stage == sb.Stage.TEST:
-            self.train_logger.log_stats(
-                {"Test": "stage"}, test_stats=stats,
+            self.hparams.train_logger.log_stats(
+                {"Epoch loaded": self.hparams.epoch_counter.current},
+                test_stats=stats,
             )
 
     def resynthesize(self, predictions, inputs):
@@ -126,7 +127,7 @@ class SEBrain(sb.Brain):
         wavs = wavs.to(self.device)
 
         # Extract noisy phase
-        feats = self.compute_STFT(wavs)
+        feats = self.hparams.compute_STFT(wavs)
         phase = torch.atan2(feats[:, :, :, 1], feats[:, :, :, 0])
         complex_predictions = torch.mul(
             torch.unsqueeze(predictions, -1),
@@ -140,7 +141,7 @@ class SEBrain(sb.Brain):
         )
 
         # Get the predicted waveform
-        pred_wavs = self.compute_ISTFT(complex_predictions)
+        pred_wavs = self.hparams.compute_ISTFT(complex_predictions)
 
         # Normalize the waveform
         abs_max, _ = torch.max(torch.abs(pred_wavs), dim=1, keepdim=True)
@@ -158,44 +159,46 @@ if __name__ == "__main__":
     from voicebank_prepare import prepare_voicebank  # noqa E402
 
     # Load hyperparameters file with command-line overrides
-    params_file, overrides = sb.parse_arguments(sys.argv[1:])
-    with open(params_file) as fin:
-        params = sb.load_extended_yaml(fin, overrides)
+    hparams_file, overrides = sb.parse_arguments(sys.argv[1:])
+    with open(hparams_file) as fin:
+        hparams = sb.load_extended_yaml(fin, overrides)
 
     # Create experiment directory
     sb.create_experiment_directory(
-        experiment_directory=params.output_folder,
-        hyperparams_to_save=params_file,
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file,
         overrides=overrides,
     )
 
-    if params.use_tensorboard:
+    if hparams["use_tensorboard"]:
         from speechbrain.utils.train_logger import TensorboardLogger
 
-        params.modules["tensorboard_train_logger"] = TensorboardLogger(
-            params.tensorboard_logs
+        hparams["hparams"]["tensorboard_train_logger"] = TensorboardLogger(
+            hparams["tensorboard_logs"]
         )
 
     # Create the folder to save enhanced files
-    if not os.path.exists(params.enhanced_folder):
-        os.mkdir(params.enhanced_folder)
+    if not os.path.exists(hparams["enhanced_folder"]):
+        os.mkdir(hparams["enhanced_folder"])
 
     # Prepare data
     prepare_voicebank(
-        data_folder=params.data_folder, save_folder=params.data_folder,
+        data_folder=hparams["data_folder"], save_folder=hparams["data_folder"],
     )
 
-    se_brain = SEBrain(modules=params.modules, optimizers=["optimizer"])
+    se_brain = SEBrain(
+        hparams=hparams["hparams"],
+        opt_class=hparams["opt_class"],
+        checkpointer=hparams["checkpointer"],
+        device=hparams["device"],
+    )
 
     # Load latest checkpoint to resume training
-    se_brain.checkpointer.recover_if_possible()
     se_brain.fit(
-        params.epoch_counter,
-        train_set=params.train_loader(),
-        valid_set=params.valid_loader(),
+        hparams["epoch_counter"],
+        train_set=hparams["train_loader"](),
+        valid_set=hparams["valid_loader"](),
     )
 
     # Load best checkpoint for evaluation
-    se_brain.checkpointer.recover_if_possible(max_key="pesq")
-    print("Epoch loaded:", params.epoch_counter.current)
-    test_stats = se_brain.evaluate(params.test_loader())
+    test_stats = se_brain.evaluate(hparams["test_loader"](), max_key="pesq")
