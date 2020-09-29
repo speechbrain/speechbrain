1a3b7427942a50ef34f4fc32522653f974d160e9
cem <csubakan@gmail.com>
2020-09-29 15:41:45 -0400

    integrated reformer

--- speechbrain/lobes/models/dual_pathrnn.py
+++ speechbrain/lobes/models/dual_pathrnn.py
@@ -7,6 +7,7 @@ from speechbrain.nnet.linear import Linear
 from speechbrain.lobes.models.block_models.modularity import RIM
 
 # from speechbrain.lobes.models.block_models.modularity import SCOFF
+from reformer_pytorch import Reformer
 
 
 # from speechbrain.nnet.RNN import LSTM
@@ -439,6 +440,7 @@ class Dual_Transformer_Block(nn.Module):
     def __init__(
         self,
         out_channels,
+        transformer_type,
         num_layers=6,
         nhead=8,
         d_ffn=2048,
@@ -450,10 +452,11 @@ class Dual_Transformer_Block(nn.Module):
         num_modules=1,
         use_group_comm=False,
         norm="ln",
-        transformer_type="pytorch",
+        reformer_bucket_size=32,
     ):
         super(Dual_Transformer_Block, self).__init__()
         self.transformer_type = transformer_type
+        self.reformer_bucket_size = reformer_bucket_size
 
         if transformer_type == "pytorch":
             encoder_layer = nn.TransformerEncoderLayer(
@@ -469,6 +472,27 @@ class Dual_Transformer_Block(nn.Module):
             )
             self.inter_mdl = nn.TransformerEncoder(
                 encoder_layer, num_layers=num_layers
+            )
+
+        elif transformer_type == "reformer":
+
+            self.intra_mdl = Reformer(
+                dim=out_channels,
+                depth=num_layers,
+                max_seq_len=8192,
+                heads=nhead,
+                lsh_dropout=dropout,
+                causal=False,
+            )
+
+            self.inter_mdl = Reformer(
+                dim=out_channels,
+                depth=num_layers,
+                bucket_size=reformer_bucket_size,
+                max_seq_len=8192,
+                heads=nhead,
+                lsh_dropout=dropout,
+                causal=False,
             )
 
         elif transformer_type == "speechbrain":
@@ -504,6 +528,8 @@ class Dual_Transformer_Block(nn.Module):
                 num_modules,
                 use_group_comm,
             )
+        else:
+            raise ValueError("Unknown Transformer Type!!!")
 
         # Norm
         self.intra_norm = select_norm(norm, out_channels, 4)
@@ -547,6 +573,27 @@ class Dual_Transformer_Block(nn.Module):
         # [BK, S, H]
         if self.transformer_type == "speechbrain":
             inter_rnn = self.inter_mdl(inter_rnn, init_params=init_params)
+        elif self.transformer_type == "reformer":
+            # pad zeros at the end
+            pad_size = (self.reformer_bucket_size * 2) - (
+                inter_rnn.shape[1] % (self.reformer_bucket_size * 2)
+            )
+            device = inter_rnn.device
+            inter_rnn_padded = torch.cat(
+                [
+                    inter_rnn,
+                    torch.zeros(
+                        inter_rnn.size(0), pad_size, inter_rnn.size(-1)
+                    ).to(device),
+                ],
+                dim=1,
+            )
+
+            # apply the model
+            inter_rnn_padded = self.inter_mdl(inter_rnn_padded)
+
+            # get rid of zeros at the end
+            inter_rnn = inter_rnn_padded[:, :-pad_size, :]
         else:
             inter_rnn = self.inter_mdl(inter_rnn)
         # [BK, S, N]
@@ -569,6 +616,7 @@ class Dual_Path_Transformer(nn.Module):
         self,
         in_channels,
         out_channels,
+        transformer_type,
         num_tf_layers=6,
         num_layers=1,
         nhead=8,
@@ -583,7 +631,7 @@ class Dual_Path_Transformer(nn.Module):
         norm="ln",
         K=200,
         num_spks=2,
-        transformer_type="pytorch",
+        reformer_bucket_size=32,
     ):
         super(Dual_Path_Transformer, self).__init__()
         self.K = K
@@ -609,6 +657,7 @@ class Dual_Path_Transformer(nn.Module):
                     use_group_comm=use_group_comm,
                     norm=norm,
                     transformer_type=transformer_type,
+                    reformer_bucket_size=reformer_bucket_size,
                 )
             )
 
