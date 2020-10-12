a3b97f15303a66d4b4c2eb261ec5afb44580197c
mirkobronzi <m.bronzi@gmail.com>
2020-10-12 11:32:00 -0400

    added pos encoding for pytorch transformer

--- speechbrain/lobes/models/dual_pathrnn.py
+++ speechbrain/lobes/models/dual_pathrnn.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -500,6 +502,26 @@ class FastTransformerBlock(nn.Module):
             return self.mdl(x)
 
 
+class PyTorchPositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PyTorchPositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + self.pe[: x.size(0), :]
+        return self.dropout(x)
+
+
 class PytorchTransformerBlock(nn.Module):
     def __init__(
         self,
@@ -509,6 +531,7 @@ class PytorchTransformerBlock(nn.Module):
         d_ffn=2048,
         dropout=0.1,
         activation="relu",
+        use_positional_encoding=True,
     ):
         super(PytorchTransformerBlock, self).__init__()
 
@@ -522,7 +545,14 @@ class PytorchTransformerBlock(nn.Module):
         # cem :this encoder thing has a normalization component. we should look at that probably also.
         self.mdl = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
 
+        if use_positional_encoding:
+            self.pos_encoder = PyTorchPositionalEncoding(out_channels)
+        else:
+            self.pos_encoder = None
+
     def forward(self, x, init_params=False):
+        if self.pos_encoder is not None:
+            x = self.pos_encoder(x)
         return self.mdl(x)
 
 
