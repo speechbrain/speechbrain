7a8da549b15ed40ae929e5b0f8954a8e9b345794
30stomercury <ff936tw@gmail.com>
2020-10-17 19:01:26 +0800

    fix doctests: fix input shape

--- speechbrain/nnet/transducer/transducer_joint.py
+++ speechbrain/nnet/transducer/transducer_joint.py
@@ -30,7 +30,7 @@ class Transducer_joint(nn.Module):
     >>> from speechbrain.nnet.linear import Linear
     >>> input_TN = torch.rand(8, 200, 1, 40)
     >>> input_PN = torch.rand(8, 1, 12, 40)
-    >>> joint_network = Linear(input_shape=input_TN.shape, n_neurons=80)
+    >>> joint_network = Linear(input_size=80, n_neurons=80)
     >>> TJoint = Transducer_joint(joint_network, joint="concat")
     >>> output = TJoint(input_TN, input_PN)
     >>> output.shape
