8fe5a6e511b12d43eb0c52689f2802fe00d10bd2
30stomercury <ff936tw@gmail.com>
2020-10-17 18:41:08 +0800

    fix doctests: pass input size to Linear

--- speechbrain/nnet/transducer/transducer_joint.py
+++ speechbrain/nnet/transducer/transducer_joint.py
@@ -28,10 +28,10 @@ class Transducer_joint(nn.Module):
     -------
     >>> from speechbrain.nnet.transducer.transducer_joint import Transducer_joint
     >>> from speechbrain.nnet.linear import Linear
-    >>> joint_network = Linear(80)
-    >>> TJoint = Transducer_joint(joint_network, joint="concat")
     >>> input_TN = torch.randn((8, 200, 1, 40))
     >>> input_PN = torch.randn((8, 1, 12, 40))
+    >>> joint_network = Linear(input_size=40, 80)
+    >>> TJoint = Transducer_joint(joint_network, joint="concat")
     >>> output = TJoint(input_TN, input_PN, init_params=True)
     >>> output.shape
     torch.Size([8, 200, 12, 80])
