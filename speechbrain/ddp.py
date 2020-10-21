a6d759ad4922f3fcfe75ef5306b14a4ec6703564
Peter Plantinga <plantinga.peter@protonmail.com>
2020-10-21 11:14:44 -0400

    Add helpful messages for running ddp

--- speechbrain/ddp.py
+++ speechbrain/ddp.py
@@ -15,14 +15,21 @@ import sys
 import torch
 import subprocess
 
+print(
+    "Beginning training with ddp. Ensure that multigpu_backend is one of: "
+    "['ddp_nccl', 'ddp_gloo', 'ddp_mpi']"
+)
 original_argslist = list(sys.argv)[1:]
 num_gpus = torch.cuda.device_count()
 original_argslist += [f"--multigpu_count={num_gpus}"]
 workers = []
+
 ddp_logs_folder = "ddp_logs"
+print(f"Creating logs directory in {ddp_logs_folder} for non-root process logs")
 if not os.path.isdir(ddp_logs_folder):
     os.mkdir(ddp_logs_folder)
 
+print(f"Starting {num_gpus} processes")
 for i in range(num_gpus):
     argslist = original_argslist + [f"--device=cuda:{i}", f"--rank={i}"]
     logfile = os.path.join(ddp_logs_folder, f"log_{i}.log")
