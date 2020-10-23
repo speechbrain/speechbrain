ee415850679810d519bc491698af49ad37445dba
popcornell <cornellsamuele@gmail.com>
2020-10-23 12:02:10 +0200

     Conflicts: 	recipes/minimal_examples/neural_networks/ASR_CTC/example_asr_ctc_experiment.py 	recipes/minimal_examples/neural_networks/ASR_CTC/hyperparams.yaml 	speechbrain/data_io/data_io.py 	speechbrain/processing/speech_augmentation.py

--- recipes/minimal_examples/neural_networks/speaker_identification/example_spkid_experiment.py
+++ recipes/minimal_examples/neural_networks/speaker_identification/example_spkid_experiment.py
@@ -2,63 +2,72 @@
 import os
 import torch
 import speechbrain as sb
-from speechbrain.utils.train_logger import summarize_average
-
-experiment_dir = os.path.dirname(os.path.realpath(__file__))
-hyperparams_file = os.path.join(experiment_dir, "hyperparams.yaml")
-data_folder = "../../../../samples/audio_samples/nn_training_samples"
-data_folder = os.path.realpath(os.path.join(experiment_dir, data_folder))
-with open(hyperparams_file) as fin:
-    hyperparams = sb.yaml.load_extended_yaml(fin, {"data_folder": data_folder})
 
 
-class SpkIdBrain(sb.core.Brain):
-    def compute_forward(self, x, stage="train", init_params=False):
+class SpkIdBrain(sb.Brain):
+    def compute_forward(self, x, stage):
         id, wavs, lens = x
-        feats = hyperparams.compute_features(wavs, init_params)
-        feats = hyperparams.mean_var_norm(feats, lens)
+        feats = self.hparams.compute_features(wavs)
+        feats = self.modules.mean_var_norm(feats, lens)
 
-        x = hyperparams.linear1(feats, init_params)
-        x = hyperparams.activation(x)
-        x = hyperparams.linear2(x, init_params)
+        x = self.modules.linear1(feats)
+        x = self.hparams.activation(x)
+        x = self.modules.linear2(x)
         x = torch.mean(x, dim=1, keepdim=True)
-        outputs = hyperparams.softmax(x)
+        outputs = self.hparams.softmax(x)
 
         return outputs, lens
 
-    def compute_objectives(self, predictions, targets, stage="train"):
+    def compute_objectives(self, predictions, targets, stage):
         predictions, lens = predictions
         uttid, spkid, _ = targets
-        loss = hyperparams.compute_cost(predictions, spkid, lens)
+        loss = self.hparams.compute_cost(predictions, spkid, lens)
 
-        stats = {}
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics.append(uttid, predictions, spkid, lens)
 
-        if stage != "train":
-            stats["error"] = hyperparams.compute_error(predictions, spkid, lens)
+        return loss
 
-        return loss, stats
+    def on_stage_start(self, stage, epoch=None):
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics = self.hparams.error_stats()
 
-    def on_epoch_end(self, epoch, train_stats, valid_stats):
-        print("Epoch %d complete" % epoch)
-        print("Train loss: %.2f" % summarize_average(train_stats["loss"]))
-        print("Valid loss: %.2f" % summarize_average(valid_stats["loss"]))
-        print("Valid error: %.2f" % summarize_average(valid_stats["error"]))
-
-
-train_set = hyperparams.train_loader()
-first_x, first_y = next(iter(train_set))
-spk_id_brain = SpkIdBrain(
-    modules=[hyperparams.linear1, hyperparams.linear2],
-    optimizer=hyperparams.optimizer,
-    first_inputs=[first_x],
-)
-spk_id_brain.fit(
-    range(hyperparams.N_epochs), train_set, hyperparams.valid_loader()
-)
-test_stats = spk_id_brain.evaluate(hyperparams.test_loader())
-print("Test error: %.2f" % summarize_average(test_stats["error"]))
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+        if stage == sb.Stage.TRAIN:
+            self.train_loss = stage_loss
+        if stage == sb.Stage.VALID:
+            print("Epoch %d complete" % epoch)
+            print("Train loss: %.2f" % self.train_loss)
+        if stage != sb.Stage.TRAIN:
+            print(stage, "loss: %.2f" % stage_loss)
+            print(
+                stage, "error: %.2f" % self.error_metrics.summarize("average")
+            )
 
 
-# Integration test: ensure we overfit the training data
+def main():
+    experiment_dir = os.path.dirname(os.path.realpath(__file__))
+    hparams_file = os.path.join(experiment_dir, "hyperparams.yaml")
+    data_folder = "../../../../samples/audio_samples/nn_training_samples"
+    data_folder = os.path.realpath(os.path.join(experiment_dir, data_folder))
+    with open(hparams_file) as fin:
+        hparams = sb.load_extended_yaml(fin, {"data_folder": data_folder})
+
+    spk_id_brain = SpkIdBrain(hparams["modules"], hparams["opt_class"], hparams)
+    spk_id_brain.fit(
+        range(hparams["N_epochs"]),
+        hparams["train_loader"](),
+        hparams["valid_loader"](),
+    )
+    spk_id_brain.evaluate(hparams["test_loader"]())
+
+    # Check that model overfits for an integration test
+    assert spk_id_brain.train_loss < 0.2
+
+
+if __name__ == "__main__":
+    main()
+
+
 def test_error():
-    assert spk_id_brain.avg_train_loss < 0.2
+    main()
