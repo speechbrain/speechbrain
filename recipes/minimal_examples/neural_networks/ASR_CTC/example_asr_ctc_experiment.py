8e713c2bfc48b6b297fa4b7aca29d9f72b2fc7e8
popcornell <cornellsamuele@gmail.com>
2020-10-28 00:55:36 +0100

    updated ctc example_asr_ctc_experiment.py

--- recipes/minimal_examples/neural_networks/ASR_CTC/example_asr_ctc_experiment.py
+++ recipes/minimal_examples/neural_networks/ASR_CTC/example_asr_ctc_experiment.py
@@ -1,118 +1,155 @@
 #!/usr/bin/python
 import os
 import speechbrain as sb
-from speechbrain.decoders.ctc import ctc_greedy_decode
-from speechbrain.decoders.decoders import undo_padding
-from speechbrain.utils.edit_distance import wer_details_for_batch
-from speechbrain.utils.train_logger import summarize_average
-from speechbrain.utils.train_logger import summarize_error_rate
+import torch
+
+from speechbrain.data_io.utils import replace_entries
 
 # TODO: Replace local placeholder Dataset class
-from placeholders import ASRMinimalExampleDataset
+from speechbrain.data_io.datasets import SegmentedDataset
 
-# TODO: Replace ASR Dataset transforms:
-from placeholders import FuncPipeline
-from placeholders import torchaudio_load
-from placeholders import split_by_whitespace
-from placeholders import ExampleCategoricalEncoder
-from placeholders import to_int_tensor
+# TODO: Replace ASR Dataset transforms for now no FuncPipeline:
+# from placeholders import FuncPipeline
+from speechbrain.data_io.data_io import read_wav
+from speechbrain.data_io.encoders import CategoricalEncoder
+
+# from placeholders import split_by_whitespace
+# from placeholders import ExampleCategoricalEncoder
+# from placeholders import to_int_tensor
 
 # TODO: Replace label dict creation
-from placeholders import ASR_example_label2ind, ASR_example_ind2label
+# from placeholders import ASR_example_label2ind, ASR_example_ind2label
 
 # TODO: Replace collate fn:
-from placeholders import ASR_example_collation
+from speechbrain.data_io.dataloader import collate_pad
 from speechbrain.data_io.dataloader import SaveableDataLoader
 
-experiment_dir = os.path.dirname(os.path.realpath(__file__))
-hyperparams_file = os.path.join(experiment_dir, "hyperparams.yaml")
-data_folder = "../../../../samples/audio_samples/nn_training_samples"
-data_folder = os.path.realpath(os.path.join(experiment_dir, data_folder))
-with open(hyperparams_file) as fin:
-    # TODO: Data loading back into YAML:
-    hyperparams = sb.yaml.load_extended_yaml(fin)
 
-    # Placeholders:
-    label_encoder = ExampleCategoricalEncoder(
-        label2ind=ASR_example_label2ind, ind2label=ASR_example_ind2label
-    )
-    # TODO: Make proper transforms
-    text_transform = FuncPipeline(
-        split_by_whitespace, label_encoder.encode_list, to_int_tensor
-    )
+class CTCBrain(sb.Brain):
+    def compute_forward(self, x, stage):
 
-    # TODO: Convert minimal example CSV to new YAML format
-    train_data = ASRMinimalExampleDataset(
-        os.path.join(data_folder, "train.csv"),
-        audio_transform=torchaudio_load,
-        text_transform=text_transform,
-    )
-    valid_data = ASRMinimalExampleDataset(
-        os.path.join(data_folder, "dev.csv"),
-        audio_transform=torchaudio_load,
-        text_transform=text_transform,
-    )
-    test_data = ASRMinimalExampleDataset(
-        os.path.join(data_folder, "test.csv"),
-        audio_transform=torchaudio_load,
-        text_transform=text_transform,
-    )
+        wavs, lens = x["waveforms"]
+        # lens now supports multidimensional padding
+        # TEMPORARY PATCH:
+        lens = torch.Tensor([x[-1] for x in lens]).to(wavs.device)
+        assert wavs.size(1) == 1, "multichannel not supported"
+        wavs = wavs.squeeze(1)
 
-# Placeholders:
-train_loader = SaveableDataLoader(
-    train_data, batch_size=hyperparams.N_batch, collate_fn=ASR_example_collation
-)
-valid_loader = SaveableDataLoader(
-    valid_data, batch_size=1, collate_fn=ASR_example_collation
-)
-test_loader = SaveableDataLoader(
-    valid_data, batch_size=1, collate_fn=ASR_example_collation
-)
-
-
-class CTCBrain(sb.core.Brain):
-    def compute_forward(self, x, stage="train", init_params=False):
-        id, wavs, lens = x
-        feats = hyperparams.compute_features(wavs, init_params)
-        feats = hyperparams.mean_var_norm(feats, lens)
-        x = hyperparams.model(feats, init_params=init_params)
-        x = hyperparams.lin(x, init_params)
-        outputs = hyperparams.softmax(x)
+        feats = self.hparams.compute_features(wavs)
+        feats = self.modules.mean_var_norm(feats, lens)
+        x = self.modules.model(feats)
+        x = self.modules.lin(x)
+        outputs = self.hparams.softmax(x)
 
         return outputs, lens
 
-    def compute_objectives(self, predictions, targets, stage="train"):
+    def compute_objectives(self, predictions, batch, stage):
+
         predictions, lens = predictions
-        ids, phns, phn_lens = targets
-        loss = hyperparams.compute_cost(predictions, phns, lens, phn_lens)
+        phns, phn_lens = batch["phones"]
+        ids = batch["id"]
+        # SAME UGLY PATCHES AS BEFORE:
+        phn_lens = torch.Tensor([x[-1] for x in phn_lens]).to(
+            predictions.device
+        )
+        loss = self.hparams.compute_cost(predictions, phns, lens, phn_lens)
 
-        stats = {}
-        if stage != "train":
-            seq = ctc_greedy_decode(predictions, lens, blank_id=-1)
-            phns = undo_padding(phns, phn_lens)
-            stats["PER"] = wer_details_for_batch(ids, phns, seq)
+        if stage != sb.Stage.TRAIN:
+            seq = sb.decoders.ctc_greedy_decode(predictions, lens, blank_id=-1)
+            self.per_metrics.append(ids, seq, phns, target_len=phn_lens)
 
-        return loss, stats
+        return loss
 
-    def on_epoch_end(self, epoch, train_stats, valid_stats):
-        print("Epoch %d complete" % epoch)
-        print("Train loss: %.2f" % summarize_average(train_stats["loss"]))
-        print("Valid loss: %.2f" % summarize_average(valid_stats["loss"]))
-        print("Valid PER: %.2f" % summarize_error_rate(valid_stats["PER"]))
+    def on_stage_start(self, stage, epoch=None):
+        if stage != sb.Stage.TRAIN:
+            self.per_metrics = self.hparams.per_stats()
 
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+        if stage == sb.Stage.TRAIN:
+            self.train_loss = stage_loss
 
-# TODO: Replace first input fetching
-first_x, first_y = ASR_example_collation([train_data[0]])
-ctc_brain = CTCBrain(
-    modules=[hyperparams.model, hyperparams.lin],
-    optimizer=hyperparams.optimizer,
-    first_inputs=[first_x],
-)
-ctc_brain.fit(range(hyperparams.N_epochs), train_loader, valid_loader)
-test_stats = ctc_brain.evaluate(test_loader)
-print("Test PER: %.2f" % summarize_error_rate(test_stats["PER"]))
+        if stage == sb.Stage.VALID and epoch is not None:
+            print("Epoch %d complete" % epoch)
+            print("Train loss: %.2f" % self.train_loss)
+
+        if stage != sb.Stage.TRAIN:
+            print(stage, "loss: %.2f" % stage_loss)
+            print(stage, "PER: %.2f" % self.per_metrics.summarize("error_rate"))
 
 
-# Integration test: check that the model overfits the training data
+def main():
+    experiment_dir = os.path.dirname(os.path.realpath(__file__))
+    hparams_file = os.path.join(experiment_dir, "hyperparams.yaml")
+    data_folder = "../../../../samples/audio_samples/nn_training_samples"
+    data_folder = os.path.realpath(os.path.join(experiment_dir, data_folder))
+    with open(hparams_file) as fin:
+        # TODO: Data loading back into YAML:
+        hparams = sb.yaml.load_extended_yaml(fin)
+
+    with open(os.path.join(data_folder, "train.yaml"), "r",) as f:
+        train_examples = sb.yaml.load_extended_yaml(f)
+    with open(os.path.join(data_folder, "dev.yaml"), "r",) as f:
+        dev_examples = sb.yaml.load_extended_yaml(f)
+    with open(os.path.join(data_folder, "test.yaml"), "r",) as f:
+        test_examples = sb.yaml.load_extended_yaml(f)
+
+    # TODO this can be handled into load_extended yaml in future but requires modifications
+    # to core extended yaml overriding functionality.
+    replacements_dict = {
+        "files": {"DATASET_ROOT": data_folder},
+        "alignment_file": {"ALIGNMENTS_ROOT": data_folder},
+    }
+
+    replace_entries(train_examples, replacements_dict)  # in place substitution
+    replace_entries(dev_examples, replacements_dict)
+    replace_entries(test_examples, replacements_dict)
+
+    # Placeholders:
+    label_encoder = CategoricalEncoder()
+    label_encoder.fit([train_examples, dev_examples, test_examples], "phones")
+    label_encoder.add_elem("<blank>", hparams["blank_index"])
+
+    # TODO: Convert minimal example CSV to new YAML format
+    train_data = SegmentedDataset(
+        train_examples,
+        ["waveforms", "phones"],
+        {"waveforms": read_wav, "phones": label_encoder.encode_int},
+    )
+    valid_data = SegmentedDataset(
+        dev_examples,
+        ["waveforms", "phones"],
+        {"waveforms": read_wav, "phones": label_encoder.encode_int},
+    )
+    test_data = SegmentedDataset(
+        test_examples,
+        ["waveforms", "phones"],
+        {"waveforms": read_wav, "phones": label_encoder.encode_int},
+    )
+
+    # Placeholders:
+    train_loader = SaveableDataLoader(
+        train_data, batch_size=hparams["N_batch"], collate_fn=collate_pad,
+    )
+    valid_loader = SaveableDataLoader(
+        valid_data, batch_size=1, collate_fn=collate_pad
+    )
+    test_loader = SaveableDataLoader(
+        test_data, batch_size=1, collate_fn=collate_pad
+    )
+
+    ctc_brain = CTCBrain(hparams["modules"], hparams["opt_class"], hparams)
+    ctc_brain.fit(
+        range(hparams["N_epochs"]), train_loader, valid_loader,
+    )
+    ctc_brain.evaluate(test_loader)
+
+    # Check if model overfits for integration test
+    assert ctc_brain.train_loss < 3.0
+
+
+if __name__ == "__main__":
+    main()
+
+
 def test_error():
-    assert ctc_brain.avg_train_loss < 3.0
+    main()
