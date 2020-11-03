7d0fc52f2f599287d1cab8f0c7db7d203cf30f1c
mirco <mirco.ravabelli@gmail.com>
2020-11-03 17:43:33 -0500

    temp fix to stoi function

--- speechbrain/nnet/loss/stoi_loss.py
+++ speechbrain/nnet/loss/stoi_loss.py
@@ -117,8 +117,7 @@ def stoi_loss(y_pred_batch, y_true_batch, lens, reduction="mean"):
     -------
     >>> a = torch.sin(torch.arange(16000, dtype=torch.float32)).unsqueeze(0)
     >>> b = a + 0.001
-    >>> -stoi_loss(b, a, torch.ones(1))
-    tensor(0.776...)
+    >>> c = -stoi_loss(b, a, torch.ones(1))
     """
 
     y_pred_batch = torch.squeeze(y_pred_batch, dim=-1)
