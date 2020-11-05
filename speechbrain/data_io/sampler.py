e531dd230336d7f47a7f7f255ecd7cbd72e1c267
popcornell <cornellsamuele@gmail.com>
2020-11-05 17:41:45 +0100

    draft of dynamicbatchsampler

--- speechbrain/data_io/sampler.py
+++ speechbrain/data_io/sampler.py
@@ -4,7 +4,8 @@ PyTorch compatible samplers
 These determine the order of iteration through a dataset.
 """
 import torch
-from torch.utils.data.sampler import RandomSampler
+from torch.utils.data.sampler import RandomSampler, SequentialSampler
+from speechbrain.data_io.datasets import SegmentedDataset
 import logging
 
 logger = logging.getLogger(__name__)
@@ -89,3 +90,75 @@ class ReproducibleRandomSampler(RandomSampler):
     def __iter__(self):
         self.generator.manual_seed(self.seed + self.epoch)
         return super().__iter__()
+
+
+class DynamicBatchSampler(SequentialSampler):
+    def __init__(
+        self,
+        data_source: SegmentedDataset,
+        max_batch_size: int,
+        sorting="descending",
+    ):
+
+        if not isinstance(max_batch_size, int) or max_batch_size <= 0:
+            raise ValueError(
+                "max_batch_size should be a positive integer value, "
+                "got max_batch_size={}".format(max_batch_size)
+            )
+
+        self.sorting = sorting
+        self.max_bsz = max_batch_size
+
+        # sorting data_source
+        if len(data_source.examples) == 0:
+            raise IndexError("Data source must not be empty.")
+
+        examples = data_source.examples
+
+        if not ("length" in examples[examples.keys()[0]].keys()):
+            raise ValueError(
+                "Each example must have a 'length' key containing the length of the example "
+                "in order to be able to sort them and use Dynamic Batching"
+            )
+
+        if self.sorting == "ascending":
+            ex_ids = sorted(
+                data_source.ex_ids, key=lambda x: examples[x]["length"]
+            )
+            min_len = examples[ex_ids[0]]["length"]
+        elif self.sorting == "descending":
+            ex_ids = sorted(
+                data_source.ex_ids,
+                key=lambda x: examples[x]["length"],
+                reverse=True,
+            )
+            min_len = examples[ex_ids[-1]]["length"]
+        else:
+            raise ValueError("Sorting must be either ascending or descending")
+
+        # bucketing data_source examples,
+        t_len = min_len * self.max_bsz
+
+        tot_batches = []
+        c_batch = []
+        c_batch_len = 0
+        for c_id in ex_ids:
+            c_len = examples[c_id]["length"]
+            if c_batch_len == 0 or (c_batch_len + c_len) <= t_len:
+                c_batch.append(c_id)
+                c_batch_len += c_len
+            else:
+                # put it in another batch
+                tot_batches.append(c_batch)
+                c_batch = [c_id]
+                c_batch_len = c_len
+
+        self.bucketed = tot_batches
+
+        super().__init__(data_source)
+
+    def __iter__(self):
+        return iter(self.bucketed)
+
+    def __len__(self) -> int:
+        return len(self.bucketed)
