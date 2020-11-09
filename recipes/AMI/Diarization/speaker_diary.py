12c828b44695b86048e8d2c96fb0f5ae5c7537eb
ravanelm <mirco.ravanelli@gmail.com>
2020-11-09 15:34:39 -0500

    convert to the new arch

--- recipes/AMI/Diarization/speaker_diary.py
+++ recipes/AMI/Diarization/speaker_diary.py
@@ -60,16 +60,16 @@ except ImportError:
     raise ImportError(err_msg)
 
 
-def compute_embeddings(wavs, lens, init_params=False):
+def compute_embeddings(wavs, lens):
     """Definition of the steps for computation of embeddings from the waveforms
     """
     with torch.no_grad():
-        wavs = wavs.to(params.device)
-        feats = params.compute_features(wavs, init_params=init_params)
-        feats = params.mean_var_norm(feats, lens)
-        emb = params.embedding_model(feats, lens, init_params=init_params)
-        emb = params.mean_var_norm_emb(
-            emb, torch.ones(emb.shape[0]).to("cuda:0")
+        wavs = wavs.to(params["device"])
+        feats = params["compute_features"](wavs)
+        feats = params["mean_var_norm"](feats, lens)
+        emb = params["embedding_model"](feats, lens=lens)
+        emb = params["mean_var_norm_emb"](
+            emb, torch.ones(emb.shape[0], device=params["device"])
         )
 
     return emb
@@ -78,9 +78,9 @@ def compute_embeddings(wavs, lens, init_params=False):
 def download_and_pretrain():
     """Downloads pre-trained model
     """
-    save_model_path = params.model_dir + "/emb.ckpt"
-    download_file(params.embedding_file, save_model_path)
-    params.embedding_model.load_state_dict(
+    save_model_path = params["model_dir"] + "/emb.ckpt"
+    download_file(params["embedding_file"], save_model_path)
+    params["embedding_model"].load_state_dict(
         torch.load(save_model_path), strict=True
     )
 
@@ -92,12 +92,14 @@ def embedding_computation_loop(split, set_loader, stat_file):
     # Extract embeddings (skip if already done)
     if not os.path.isfile(stat_file):
 
-        embeddings = numpy.empty(shape=[0, params.emb_dim], dtype=numpy.float64)
+        embeddings = numpy.empty(
+            shape=[0, params["emb_dim"]], dtype=numpy.float64
+        )
         modelset = []
         segset = []
         with tqdm(set_loader, dynamic_ncols=True) as t:
             # different data may have different statistics
-            params.mean_var_norm_emb.count = 0
+            params["mean_var_norm_emb"].count = 0
 
             for wav in t:
                 ids, wavs, lens = wav[0]
@@ -108,7 +110,7 @@ def embedding_computation_loop(split, set_loader, stat_file):
                 segset = segset + seg
 
                 # embedding computation
-                emb = compute_embeddings(wavs, lens).squeeze().cpu().numpy()
+                emb = compute_embeddings(wavs, lens).squeeze(1).cpu().numpy()
                 embeddings = numpy.concatenate((embeddings, emb), axis=0)
 
         modelset = numpy.array(modelset, dtype="|O")
@@ -506,7 +508,7 @@ def do_spec_clustering(diary_obj_eval, out_rttm_file, rec_id, k=4):
     clust_obj = Spec_Cluster(
         n_clusters=k,
         assign_labels="kmeans",
-        random_state=params.seed,
+        random_state=params["seed"],
         affinity="nearest_neighbors",
     )
 
@@ -571,7 +573,9 @@ class Spec_Cluster(SpectralClustering):
 
         # Computation of affinity matrix
         connectivity = kneighbors_graph(
-            X, n_neighbors=params.n_neighbors, include_self=params.include_self
+            X,
+            n_neighbors=params["n_neighbors"],
+            include_self=params["include_self"],
         )
         self.affinity_matrix_ = 0.5 * (connectivity + connectivity.T)
 
@@ -589,9 +593,9 @@ def diarize_dataset(full_csv, split_type, n_lambdas):
     """
 
     # Prepare `spkr_info` only once when Oracle num of speakers is selected
-    if params.oracle_n_spkrs is True:
+    if params["oracle_n_spkrs"] is True:
         full_ref_rttm_file = (
-            params.ref_rttm_dir + "/fullref_ami_" + split_type + ".rttm"
+            params["ref_rttm_dir"] + "/fullref_ami_" + split_type + ".rttm"
         )
         RTTM = []
         with open(full_ref_rttm_file, "r") as f:
@@ -611,7 +615,17 @@ def diarize_dataset(full_csv, split_type, n_lambdas):
     N = str(len(all_rec_ids))
     split = "AMI_" + split_type
     i = 1
-    init_params = True
+
+    # Pretrain model
+    if "https://" in params["embedding_file"]:
+        download_and_pretrain()
+    else:
+        params["embedding_model"].load_state_dict(
+            torch.load(params["embedding_file"]), strict=True
+        )
+
+    # Setting eval modality
+    params["embedding_model"].eval()
 
     for rec_id in all_rec_ids:
 
@@ -621,49 +635,34 @@ def diarize_dataset(full_csv, split_type, n_lambdas):
         msg = "Diarizing %s : %s " % (tag, rec_id)
         logger.info(msg)
 
-        if not os.path.exists(os.path.join(params.embedding_dir, split)):
-            os.makedirs(os.path.join(params.embedding_dir, split))
+        if not os.path.exists(os.path.join(params["embedding_dir"], split)):
+            os.makedirs(os.path.join(params["embedding_dir"], split))
 
         diary_stat_file = os.path.join(
-            params.embedding_dir, split, rec_id + "_xv_stat.pkl"
+            params["embedding_dir"], split, rec_id + "_xv_stat.pkl"
         )
 
         # Prepare a csv for a recording
         new_csv_file = os.path.join(
-            params.embedding_dir, split, rec_id + ".csv"
+            params["embedding_dir"], split, rec_id + ".csv"
         )
         prepare_subset_csv(full_csv, rec_id, new_csv_file)
 
         # Setup a dataloader for above one recording (above csv)
         diary_set = DataLoaderFactory(
             new_csv_file,
-            params.diary_loader_eval.batch_size,
-            params.diary_loader_eval.csv_read,
-            params.diary_loader_eval.sentence_sorting,
+            params["diary_loader_eval"].batch_size,
+            params["diary_loader_eval"].csv_read,
+            params["diary_loader_eval"].sentence_sorting,
         )
 
-        diary_set_loader = diary_set.forward()
+        diary_set_loader = diary_set.forward().get_dataloader()
 
-        # Dir to store embeddings
-        if not os.path.exists(os.path.join(params.embedding_dir, split)):
-            os.makedirs(os.path.join(params.embedding_dir, split))
-
-        if init_params:
-            _, wavs, lens = next(iter(diary_set_loader))[0]
-
-            # Initialize the model and perform pre-training
-            _ = compute_embeddings(wavs, lens, init_params=True)
-
-            # Download models from the web if needed
-            if "https://" in params.embedding_file:
-                download_and_pretrain()
-            else:
-                params.embedding_model.load_state_dict(
-                    torch.load(params.embedding_file), strict=True
-                )
-
-            init_params = False
-            params.embedding_model.eval()
+        # Putting modules on the device
+        params["compute_features"].to(params["device"])
+        params["mean_var_norm"].to(params["device"])
+        params["embedding_model"].to(params["device"])
+        params["mean_var_norm_emb"].to(params["device"])
 
         # Compute Embeddings
         diary_obj_dev = embedding_computation_loop(
@@ -671,12 +670,12 @@ def diarize_dataset(full_csv, split_type, n_lambdas):
         )
 
         # Perform spectral clustering
-        out_rttm_dir = os.path.join(params.sys_rttm_dir, split)
+        out_rttm_dir = os.path.join(params["sys_rttm_dir"], split)
         if not os.path.exists(out_rttm_dir):
             os.makedirs(out_rttm_dir)
         out_rttm_file = out_rttm_dir + "/" + rec_id + ".rttm"
 
-        if params.oracle_n_spkrs is True:
+        if params["oracle_n_spkrs"] is True:
             # Oracle num of speakers
             num_spkrs = get_oracle_num_spkrs(rec_id, spkr_info)
         else:
@@ -713,15 +712,18 @@ def dev_tuner(full_csv, split_type):
     """
 
     DER_list = []
-    for n_lambdas in range(1, params.max_num_spkrs + 1):
+    for n_lambdas in range(1, params["max_num_spkrs"] + 1):
 
         # Process whole dataset for value of n_lambdas
         concate_rttm_file = diarize_dataset(full_csv, split_type, n_lambdas)
 
-        ref_rttm = os.path.join(params.ref_rttm_dir, "fullref_ami_dev.rttm")
+        ref_rttm = os.path.join(params["ref_rttm_dir"], "fullref_ami_dev.rttm")
         sys_rttm = concate_rttm_file
         [MS, FA, SER, DER_] = DER(
-            ref_rttm, sys_rttm, params.ignore_overlap, params.forgiveness_collar
+            ref_rttm,
+            sys_rttm,
+            params["ignore_overlap"],
+            params["forgiveness_collar"],
         )
 
         msg = "[Tuner]: n_lambdas= %d , DER= %s\n" % (
@@ -749,18 +751,18 @@ if __name__ == "__main__":  # noqa: C901
 
     # Create experiment directory
     sb.core.create_experiment_directory(
-        experiment_directory=params.output_folder,
+        experiment_directory=params["output_folder"],
         hyperparams_to_save=params_file,
         overrides=overrides,
     )
 
     # Few more experiment directories (to have cleaner structure)
     exp_dirs = [
-        params.model_dir,
-        params.embedding_dir,
-        params.csv_dir,
-        params.ref_rttm_dir,
-        params.sys_rttm_dir,
+        params["model_dir"],
+        params["embedding_dir"],
+        params["csv_dir"],
+        params["ref_rttm_dir"],
+        params["sys_rttm_dir"],
     ]
     for dir_ in exp_dirs:
         if not os.path.exists(dir_):
@@ -771,26 +773,26 @@ if __name__ == "__main__":  # noqa: C901
         "AMI: Data preparation [Prepares both, the reference RTTMs and the CSVs]"
     )
     prepare_ami(
-        data_folder=params.data_folder,
-        manual_annot_folder=params.manual_annot_folder,
-        save_folder=params.save_folder,
-        split_type=params.split_type,
-        skip_TNO=params.skip_TNO,
-        mic_type=params.mic_type,
-        vad_type=params.vad_type,
-        max_subseg_dur=params.max_subseg_dur,
-        overlap=params.overlap,
+        data_folder=params["data_folder"],
+        manual_annot_folder=params["manual_annot_folder"],
+        save_folder=params["save_folder"],
+        split_type=params["split_type"],
+        skip_TNO=params["skip_TNO"],
+        mic_type=params["mic_type"],
+        vad_type=params["vad_type"],
+        max_subseg_dur=params["max_subseg_dur"],
+        overlap=params["overlap"],
     )
 
     # AMI Dev Set
     full_csv = []
-    with open(params.csv_diary_dev, "r") as csv_file:
+    with open(params["csv_diary_dev"], "r") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         for row in reader:
             full_csv.append(row)
 
     # TUNING for num of lambdas
-    if params.oracle_n_spkrs is False:
+    if params["oracle_n_spkrs"] is False:
         a = time.time()
         n_lambdas = dev_tuner(full_csv, "dev")
         msg = "Tuning completed! Total time spent in tuning = %s seconds\n" % (
@@ -806,10 +808,13 @@ if __name__ == "__main__":  # noqa: C901
 
     # Evaluating on DEV set
     logger.info("Evaluating for AMI Dev. set")
-    ref_rttm = os.path.join(params.ref_rttm_dir, "fullref_ami_dev.rttm")
+    ref_rttm = os.path.join(params["ref_rttm_dir"], "fullref_ami_dev.rttm")
     sys_rttm = out_boundaries
     [MS_dev, FA_dev, SER_dev, DER_dev] = DER(
-        ref_rttm, sys_rttm, params.ignore_overlap, params.forgiveness_collar
+        ref_rttm,
+        sys_rttm,
+        params["ignore_overlap"],
+        params["forgiveness_collar"],
     )
     msg = "AMI Dev set: Diarization Error Rate = %s %%\n" % (
         str(round(DER_dev, 2))
@@ -818,7 +823,7 @@ if __name__ == "__main__":  # noqa: C901
 
     # AMI Eval Set
     full_csv = []
-    with open(params.csv_diary_eval, "r") as csv_file:
+    with open(params["csv_diary_eval"], "r") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         for row in reader:
             full_csv.append(row)
@@ -827,10 +832,13 @@ if __name__ == "__main__":  # noqa: C901
 
     # Evaluating on EVAL set
     logger.info("Evaluating for AMI Eval. set")
-    ref_rttm = os.path.join(params.ref_rttm_dir, "fullref_ami_eval.rttm")
+    ref_rttm = os.path.join(params["ref_rttm_dir"], "fullref_ami_eval.rttm")
     sys_rttm = out_boundaries
     [MS_eval, FA_eval, SER_eval, DER_eval] = DER(
-        ref_rttm, sys_rttm, params.ignore_overlap, params.forgiveness_collar
+        ref_rttm,
+        sys_rttm,
+        params["ignore_overlap"],
+        params["forgiveness_collar"],
     )
     msg = "AMI Eval set: Diarization Error Rate = %s %%\n" % (
         str(round(DER_eval, 2))
