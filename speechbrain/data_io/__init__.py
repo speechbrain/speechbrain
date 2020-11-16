7e253657da1f9833d2b5e913418bb830ea8cee30
popcornell <cornellsamuele@gmail.com>
2020-11-16 12:39:25 +0100

    updated init

--- speechbrain/data_io/__init__.py
+++ speechbrain/data_io/__init__.py
@@ -1,22 +1,7 @@
 """Data loading and dataset preprocessing
 """
 
-from .data_io import (
-    prepend_bos_token,
-    append_eos_token,
-    convert_index_to_lab,
-    relative_time_to_absolute,
-    length_to_mask,
-    merge_char,
-    split_word,
-)
-
-__all__ = [
-    "prepend_bos_token",
-    "append_eos_token",
-    "convert_index_to_lab",
-    "relative_time_to_absolute",
-    "length_to_mask",
-    "merge_char",
-    "split_word",
-]
+from .data_io import to_longTensor, to_floatTensor, to_doubleTensor
+from .datasets import SegmentedDataset
+from .encoders import CategoricalEncoder
+from .dataloader import SaveableDataLoader, collate_pad
