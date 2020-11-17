7e74e8486e6e2c14f6c8d3bdcb52cec1a1701895
popcornell <cornellsamuele@gmail.com>
2020-11-17 16:41:18 +0100

    read_wav instead of placeholder torchaudio_load

--- recipes/minimal_examples/neural_networks/ASR_CTC/example_asr_ctc_experiment.py
+++ recipes/minimal_examples/neural_networks/ASR_CTC/example_asr_ctc_experiment.py
@@ -52,7 +52,7 @@ def main():
     data_folder = os.path.realpath(os.path.join(experiment_dir, data_folder))
     with open(hparams_file) as fin:
         hparams = sb.yaml.load_extended_yaml(fin, {"data_folder": data_folder})
-
+    hparams["train_data"][0]
     ctc_brain = CTCBrain(hparams["modules"], hparams["opt_class"], hparams)
     ctc_brain.fit(
         range(hparams["N_epochs"]),
