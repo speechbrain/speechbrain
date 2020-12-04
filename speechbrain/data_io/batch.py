5a1db59cc4ce01187adcc96835eb9944a23220f2
Aku Rouhe <aku.rouhe@aalto.fi>
2020-12-04 18:27:13 +0200

    Batching

---
+++ speechbrain/data_io/batch.py
@@ -0,0 +1,136 @@
+"""Batch collation
+"""
+import collections
+import torch
+import speechbrain.utils.data_utils
+
+PaddedData = collections.namedtuple("PaddedData", ["data", "lengths"])
+
+
+class PaddedBatch:
+    """Collate_fn when examples are dicts and have variable length sequences.
+
+    Different elements in the examples get matched by key.
+    By default, all torch.Tensor valued elements get padded and support
+    collective pin_memory() and to() calls.
+    Regular Python data types are just collected in a list.
+
+    Arguments
+    ---------
+    examples : list
+        List of example dicts, as produced by Dataloader.
+    padded_keys : list, None, optional
+        List of keys to pad on. If None, pad all torch.Tensors
+    device_prep_keys : list, None, optional
+        Only these keys participate in collective memory pinning and moving with
+        to()
+        If None, defaults to all items with torch.Tensor values.
+    padding_func : callable, optional
+        Called with a list of tensors to be padded together. Needs to return
+        two tensors: the padded data, and another tensor for the data lengths.
+    padding_kwargs : dict, optional
+        Extra kwargs to pass to padding_func. E.G. mode, value
+
+    Example
+    -------
+    >>> batch = PaddedBatch([
+    ...     {"id": "ex1", "foo": torch.Tensor([1.])},
+    ...     {"id": "ex2", "foo": torch.Tensor([2., 1.])}])
+    >>> # Attribute or key-based access:
+    >>> batch.id
+    ['ex1', 'ex2']
+    >>> batch["id"]
+    ['ex1', 'ex2']
+    >>> # torch.Tensors get padded
+    >>> type(batch.foo)
+    <class 'speechbrain.data_io.batch.PaddedData'>
+    >>> batch.foo.data
+    tensor([[1., 0.],
+            [2., 1.]])
+    >>> batch.foo.lengths
+    tensor([0.5000, 1.0000])
+    >>> # Batch supports collective operations:
+    >>> _ = batch.to(dtype=torch.half)
+    >>> batch.foo.data
+    tensor([[1., 0.],
+            [2., 1.]], dtype=torch.float16)
+    >>> batch.foo.lengths
+    tensor([0.5000, 1.0000], dtype=torch.float16)
+
+    """
+
+    def __init__(
+        self,
+        examples,
+        padded_keys=None,
+        device_prep_keys=None,
+        padding_func=speechbrain.utils.data_utils.batch_pad_right,
+        padding_kwargs={},
+    ):
+        self.__keys = examples[0].keys()
+        self.__padded_keys = []
+        self.__device_prep_keys = []
+        for key in self.__keys:
+            values = [example[key] for example in examples]
+            if (padded_keys is not None and key in padded_keys) or (
+                padded_keys is None and isinstance(values[0], torch.Tensor)
+            ):
+                self.__padded_keys.append(key)
+                padded = PaddedData(*padding_func(values, **padding_kwargs))
+                setattr(self, key, padded)
+            else:
+                setattr(self, key, values)
+            if (device_prep_keys is not None and key in device_prep_keys) or (
+                device_prep_keys is None and isinstance(values[0], torch.Tensor)
+            ):
+                self.__device_prep_keys.append(key)
+
+    def __getitem__(self, key):
+        if key in self.__keys:
+            return getattr(self, key)
+        else:
+            raise KeyError(f"Batch doesn't have key: {key}")
+
+    def __iter__(self):
+        """Iterates over the different elements of the batch
+
+        Example
+        -------
+        >>> batch = PaddedBatch([
+        ...     {"id": "ex1", "val": torch.Tensor([1.])},
+        ...     {"id": "ex2", "val": torch.Tensor([2., 1.])}])
+        >>> ids, vals = batch
+        >>> ids
+        ['ex1', 'ex2']
+        """
+        return iter((getattr(self, key) for key in self.__keys))
+
+    def pin_memory(self):
+        """In-place, moves relevant elements to pinned memory."""
+        for key in self.__device_prep_keys:
+            value = getattr(self, key)
+            if isinstance(value, PaddedData):
+                pinned = PaddedData(
+                    value.data.pin_memory(), value.lengths.pin_memory()
+                )
+            else:
+                pinned = value.pin_memory()
+            setattr(self, key, pinned)
+        return self
+
+    def to(self, *args, **kwargs):
+        """In-place move/cast relevant elements.
+
+        Passes all arguments to torch.Tensor.to, see its documentation.
+        """
+        for key in self.__device_prep_keys:
+            value = getattr(self, key)
+            if isinstance(value, PaddedData):
+                moved = PaddedData(
+                    value.data.to(*args, **kwargs),
+                    value.lengths.to(*args, **kwargs),
+                )
+            else:
+                moved = value.to(*args, **kwargs)
+            setattr(self, key, moved)
+        return self
