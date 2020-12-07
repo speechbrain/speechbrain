055c681db548c0b81762e418150b431366392dc8
Peter Plantinga <plantinga.peter@protonmail.com>
2020-12-07 11:03:29 -0500

    Update all recipes to use run_opts

--- recipes/VoxCeleb/SpeakerRec/train_speaker_embeddings.py
+++ recipes/VoxCeleb/SpeakerRec/train_speaker_embeddings.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     from voxceleb_prepare import prepare_voxceleb  # noqa E402
 
     # Load hyperparameters file with command-line overrides
-    hparams_file, overrides = sb.core.parse_arguments(sys.argv[1:])
+    hparams_file, run_opts, overrides = sb.core.parse_arguments(sys.argv[1:])
     with open(hparams_file) as fin:
         hparams = sb.yaml.load_extended_yaml(fin, overrides)
 
@@ -148,6 +148,7 @@ if __name__ == "__main__":
         modules=hparams["modules"],
         opt_class=hparams["opt_class"],
         hparams=hparams,
+        run_opts=run_opts,
         checkpointer=hparams["checkpointer"],
     )
 
