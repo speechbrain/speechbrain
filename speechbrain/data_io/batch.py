0d20fb5777a8b71c0d3840e7bb9426466a8b0713
Aku Rouhe <aku.rouhe@aalto.fi>
2020-12-20 12:16:38 +0200

    Default convert and collate

--- speechbrain/data_io/batch.py
+++ speechbrain/data_io/batch.py
@@ -7,6 +7,11 @@ import collections
 import torch
 import speechbrain.utils.data_utils
 from torch.utils.data._utils.collate import default_convert
+from torch.utils.data._utils.collate import default_collate
+from torch.utils.data._utils.pin_memory import (
+    pin_memory as recursive_pin_memory,
+)
+import collections.abc
 
 PaddedData = collections.namedtuple("PaddedData", ["data", "lengths"])
 
@@ -35,6 +40,12 @@ class PaddedBatch:
         two tensors: the padded data, and another tensor for the data lengths.
     padding_kwargs : dict, optional
         Extra kwargs to pass to padding_func. E.G. mode, value
+    apply_default_convert : bool
+        Whether to apply PyTorch default_convert (numpy to torch recursively,
+        etc.) on all data. Default:True, usually does the right thing.
+    nonpadded_default_collate : bool
+        Whether to apply PyTorch default_collate on values that didn't get padded.
+        Default:True, usually does the right thing.
 
     Example
     -------
@@ -71,20 +82,29 @@ class PaddedBatch:
         device_prep_keys=None,
         padding_func=speechbrain.utils.data_utils.batch_pad_right,
         padding_kwargs={},
+        apply_default_convert=True,
+        nonpadded_default_collate=True,
     ):
         self.__keys = list(examples[0].keys())
         self.__padded_keys = []
         self.__device_prep_keys = []
         for key in self.__keys:
             values = [example[key] for example in examples]
-            values = default_convert(values)
+            # Default convert usually does the right thing (numpy2torch etc.)
+            if apply_default_convert:
+                values = default_convert(values)
             if (padded_keys is not None and key in padded_keys) or (
                 padded_keys is None and isinstance(values[0], torch.Tensor)
             ):
+                # Padding and PaddedData
                 self.__padded_keys.append(key)
                 padded = PaddedData(*padding_func(values, **padding_kwargs))
                 setattr(self, key, padded)
             else:
+                # Default PyTorch collate usually does the right thing
+                # (convert lists of equal sized tensors to batch tensors, etc.)
+                if nonpadded_default_collate:
+                    values = default_collate(values)
                 setattr(self, key, values)
             if (device_prep_keys is not None and key in device_prep_keys) or (
                 device_prep_keys is None and isinstance(values[0], torch.Tensor)
@@ -115,12 +135,7 @@ class PaddedBatch:
         """In-place, moves relevant elements to pinned memory."""
         for key in self.__device_prep_keys:
             value = getattr(self, key)
-            if isinstance(value, PaddedData):
-                pinned = PaddedData(
-                    value.data.pin_memory(), value.lengths.pin_memory()
-                )
-            else:
-                pinned = value.pin_memory()
+            pinned = recursive_pin_memory(value)
             setattr(self, key, pinned)
         return self
 
@@ -131,13 +146,7 @@ class PaddedBatch:
         """
         for key in self.__device_prep_keys:
             value = getattr(self, key)
-            if isinstance(value, PaddedData):
-                moved = PaddedData(
-                    value.data.to(*args, **kwargs),
-                    value.lengths.to(*args, **kwargs),
-                )
-            else:
-                moved = value.to(*args, **kwargs)
+            moved = recursive_to(value, *args, **kwargs)
             setattr(self, key, moved)
         return self
 
@@ -145,3 +154,30 @@ class PaddedBatch:
         """Fetch an item by its position in the batch"""
         key = self.__keys[pos]
         return getattr(self, key)
+
+
+def recursive_to(data, *args, **kwargs):
+    """Moves data to device, or other type, and handles containers
+
+    Very similar to torch.utils.data._utils.pin_memory.pin_memory,
+    but applies .to() instead.
+    """
+    if isinstance(data, torch.Tensor):
+        return data.to(*args, **kwargs)
+    elif isinstance(data, collections.abc.Mapping):
+        return {
+            k: recursive_to(sample, *args, **kwargs)
+            for k, sample in data.items()
+        }
+    elif isinstance(data, tuple) and hasattr(data, "_fields"):  # namedtuple
+        return type(data)(
+            *(recursive_to(sample, *args, **kwargs) for sample in data)
+        )
+    elif isinstance(data, collections.abc.Sequence):
+        return [recursive_to(sample, *args, **kwargs) for sample in data]
+    elif hasattr(data, "to"):
+        return data.to(*args, **kwargs)
+    # What should be done with unknown data?
+    # For now, just return as they are
+    else:
+        return data
