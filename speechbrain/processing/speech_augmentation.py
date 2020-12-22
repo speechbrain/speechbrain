e358b7dfe4aacf139a63fc387419be1697221969
Mirco Ravanelli <mirco.ravanelli@gmail.com>
2020-12-22 17:39:26 -0500

    Update speech_augmentation.py
    set noise_factor=0 by default

--- speechbrain/processing/speech_augmentation.py
+++ speechbrain/processing/speech_augmentation.py
@@ -1015,7 +1015,7 @@ class DropChunk(torch.nn.Module):
         drop_start=0,
         drop_end=None,
         drop_prob=1,
-        noise_factor=1,
+        noise_factor=0,
     ):
         super().__init__()
         self.drop_length_low = drop_length_low
