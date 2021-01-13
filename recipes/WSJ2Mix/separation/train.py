0f3814c9831dfad57db5f4218e1becff6b52696e
ravanelm <mirco.ravanelli@gmail.com>
2021-01-12 20:42:09 -0500

    small fixes in wsjmix

--- recipes/WSJ2Mix/separation/train.py
+++ recipes/WSJ2Mix/separation/train.py
@@ -54,11 +54,7 @@ class Separation(sb.Brain):
         # Add speech distortions
         if stage == sb.Stage.TRAIN:
             with torch.no_grad():
-                if (
-                    self.hparams.use_speedperturb
-                    or self.hparams.use_rand_shift
-                    or self.hparams.use_speedperturb_sameforeachsource
-                ):
+                if self.hparams.use_speedperturb or self.hparams.use_rand_shift:
                     mix, targets = self.add_speed_perturb(targets, mix_lens)
 
                 if self.hparams.use_wavedrop:
@@ -278,21 +274,6 @@ class Separation(sb.Brain):
                     )
                 for i, new_target in enumerate(new_targets):
                     targets[:, :, i] = new_targets[i][:, 0:min_len]
-
-        # this applies the same speed perturb to each source
-        if self.hparams.use_speedperturb_sameforeachsource:
-
-            targets = targets.permute(0, 2, 1)
-            targets = targets.reshape(-1, targets.shape[-1])
-            wav_lens = torch.tensor([targets.shape[-1]] * targets.shape[0]).to(
-                self.device
-            )
-
-            targets = self.hparams.speedperturb(targets, wav_lens)
-            targets = targets.reshape(
-                -1, self.hparams.num_spks, targets.shape[-1]
-            )
-            targets = targets.permute(0, 2, 1)
 
         mix = targets.sum(-1)
         return mix, targets
