d277af49b5930a37abe051757895087d6cc6b9cd
Peter Plantinga <plantinga.peter@protonmail.com>
2021-01-14 18:11:37 -0500

    Update init.py to work as a pip package

--- speechbrain/__init__.py
+++ speechbrain/__init__.py
@@ -2,17 +2,15 @@
 """
 from .core import Stage, Brain, create_experiment_directory, parse_arguments
 from .yaml import load_extended_yaml, resolve_references
-from speechbrain import (
-    alignment,
-    data_io,
-    decoders,
-    lobes,
-    lm,
-    nnet,
-    processing,
-    tokenizers,
-    utils,
-)
+import speechbrain.alignment
+import speechbrain.data_io
+import speechbrain.decoders
+import speechbrain.lobes
+import speechbrain.lm
+import speechbrain.nnet
+import speechbrain.processing
+import speechbrain.tokenizers
+import speechbrain.utils  # noqa
 
 from speechbrain.utils.Accuracy import AccuracyStats
 
@@ -23,16 +21,7 @@ __all__ = [
     "parse_arguments",
     "load_extended_yaml",
     "resolve_references",
-    "alignment",
-    "data_io",
-    "decoders",
-    "lobes",
     "AccuracyStats",
-    "lm",
-    "nnet",
-    "processing",
-    "tokenizers",
-    "utils",
 ]
 
 
