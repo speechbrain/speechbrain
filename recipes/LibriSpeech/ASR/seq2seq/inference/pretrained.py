ee71b8424ecddf9a73e3107efc22c00df7e29338
ravanelm <mirco.ravanelli@gmail.com>
2021-01-16 19:55:59 -0500

    converted direct recipe (timers_and_such)

--- recipes/LibriSpeech/ASR/seq2seq/inference/pretrained.py
+++ recipes/LibriSpeech/ASR/seq2seq/inference/pretrained.py
@@ -10,7 +10,7 @@ Example
 -------
 >>> import torchaudio
 >>> asr_model = ASR()
->>> audio_file='../../../../samples/audio_samples/example2.flac'
+>>> audio_file='../../../../../samples/audio_samples/example2.flac'
 >>> # Make sure your output is sampled at 16 kHz
 >>> wav, fs = torchaudio.load(audio_file)
 >>> wav_lens = torch.tensor([1]).float()
