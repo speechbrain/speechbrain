b4198051cf20b097e2c3df3bf4820bf049366f86
mirco <mirco.ravabelli@gmail.com>
2021-01-19 23:10:00 -0500

    fix failing test

--- speechbrain/lobes/models/MetricGAN_D.py
+++ speechbrain/lobes/models/MetricGAN_D.py
@@ -80,49 +80,60 @@ class MetricGAN_D(nn.Module):
 
         return out
 
-    """
-    def __init__(
-        self,
-        kernel_size=(5,5),
-        base_channels=15,
-        activation=nn.LeakyReLU,
-        ):
-        super().__init__()
 
-        self.IN = InstanceNorm2d(input_size=2, affine=True)
-        self.activation = activation()
-        self.conv1 = Conv2d(in_channels=2, kernel_size=kernel_size, out_channels=base_channels)
-        self.conv2 = Conv2d(in_channels=base_channels, kernel_size=kernel_size, out_channels=base_channels)
-        self.conv3 = Conv2d(in_channels=base_channels, kernel_size=kernel_size, out_channels=base_channels)
-        self.conv4 = Conv2d(in_channels=base_channels, kernel_size=kernel_size, out_channels=base_channels)
-        self.liner1 = Linear(50, input_size=base_channels)
-        self.liner2 = Linear(10, input_size=50)
-        self.liner3 = Linear(1,  input_size=10)
+#    def __init__(
+#        self, kernel_size=(5, 5), base_channels=15, activation=nn.LeakyReLU,
+#    ):
+#        super().__init__()
+#
+#        self.IN = InstanceNorm2d(input_size=2, affine=True)
+#        self.activation = activation()
+#        self.conv1 = Conv2d(
+#            in_channels=2, kernel_size=kernel_size, out_channels=base_channels
+#        )
+#        self.conv2 = Conv2d(
+#            in_channels=base_channels,
+#            kernel_size=kernel_size,
+#            out_channels=base_channels,
+#        )
+#        self.conv3 = Conv2d(
+#            in_channels=base_channels,
+#            kernel_size=kernel_size,
+#            out_channels=base_channels,
+#        )
+#        self.conv4 = Conv2d(
+#            in_channels=base_channels,
+#            kernel_size=kernel_size,
+#            out_channels=base_channels,
+#        )
+#        self.liner1 = Linear(50, input_size=base_channels)
+#        self.liner2 = Linear(10, input_size=50)
+#        self.liner3 = Linear(1, input_size=10)
 
-    def forward(self, x):
-        out = self.IN(x)
 
-        out = self.conv1(out)
-        out = self.activation(out)
-
-        out = self.conv2(out)
-        out = self.activation(out)
-
-        out = self.conv3(out)
-        out = self.activation(out)
-
-        out = self.conv4(out)
-        out = self.activation(out)
-
-        out = torch.mean(out,(1,2))
-
-        out = self.liner1(out)
-        out = self.activation(out)
-
-        out = self.liner2(out)
-        out = self.activation(out)
-
-        out = self.liner3(out)
-
-        return out
-      """
+#    def forward(self, x):
+#        out = self.IN(x)
+#
+#        out = self.conv1(out)
+#        out = self.activation(out)
+#
+#        out = self.conv2(out)
+#        out = self.activation(out)
+#
+#        out = self.conv3(out)
+#        out = self.activation(out)
+#
+#        out = self.conv4(out)
+#        out = self.activation(out)
+#
+#        out = torch.mean(out, (1, 2))
+#
+#        out = self.liner1(out)
+#        out = self.activation(out)
+#
+#        out = self.liner2(out)
+#        out = self.activation(out)
+#
+#        out = self.liner3(out)
+#
+#        return out
