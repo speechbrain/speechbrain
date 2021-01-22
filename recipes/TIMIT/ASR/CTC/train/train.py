64953fdfd83eae95cb4fd769c8e141c77426435c
mirco <mirco.ravabelli@gmail.com>
2021-01-22 18:07:02 -0500

    test on timit ctc

--- recipes/TIMIT/ASR/CTC/train/train.py
+++ recipes/TIMIT/ASR/CTC/train/train.py
@@ -213,6 +213,7 @@ if __name__ == "__main__":
             "data_folder": hparams["data_folder"],
             "splits": ["train", "dev", "test"],
             "save_folder": hparams["data_folder"],
+            "skip_prep": hparams["skip_prep"]
         },
     )
 
