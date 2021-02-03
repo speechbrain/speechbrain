2ed6b6718b731217782c4985187a103917429cc7
Peter Plantinga <plantinga.peter@protonmail.com>
2021-02-03 16:54:46 -0500

    Last fixes

--- recipes/Voicebank/MTL/ASR_enhance/train/train.py
+++ recipes/Voicebank/MTL/ASR_enhance/train/train.py
@@ -2,7 +2,7 @@
 """Recipe for multi-task learning, using seq2seq and enhancement objectives.
 
 To run this recipe, do the following:
-> python train.py hparams/{hyperparameter file} --data_folder /path/to/noisy-vctk
+> python train.py hparams/{config file} --data_folder /path/to/noisy-vctk
 
 There's three provided files for three stages of training:
 > python train.py hparams/pretrain_perceptual.yaml
@@ -19,6 +19,7 @@ Authors
 import os
 import sys
 import torch
+import torchaudio
 import speechbrain as sb
 from pesq import pesq
 from pystoi import stoi
@@ -167,6 +168,14 @@ class ASR_Brain(sb.Brain):
                     lengths=lens,
                 )
 
+                if hasattr(self.hparams, "enh_dir"):
+                    abs_lens = lens * predictions["wavs"].size(1)
+                    for i, uid in enumerate(batch.id):
+                        length = int(abs_lens[i])
+                        wav = predictions["wavs"][i, :length].unsqueeze(0)
+                        path = os.path.join(self.hparams.enh_dir, uid + ".wav")
+                        torchaudio.save(path, wav.cpu(), sample_rate=16000)
+
         # Compute mimic loss
         if self.hparams.mimic_weight > 0:
             clean_embed = self.modules.src_embedding.CNN(clean_feats)
@@ -211,7 +220,7 @@ class ASR_Brain(sb.Brain):
             )
             loss += self.hparams.seq_weight * seq_loss
 
-            if stage != sb.Stage.TRAIN and self.hparams.target_type == "wrd":
+            if stage != sb.Stage.TRAIN and self.hparams.target_type == "words":
                 pred_words = self.tokenizer(
                     predictions["hyps"], task="decode_from_list"
                 )
@@ -308,7 +317,6 @@ class ASR_Brain(sb.Brain):
                 stats_meta={"Epoch loaded": self.hparams.epoch_counter.current},
                 test_stats=stage_stats,
             )
-            self.checkpointer.save_checkpoint(meta=stage_stats, name="avg")
             with open(self.hparams.stats_file, "w") as w:
                 if self.hparams.enhance_weight > 0:
                     w.write("\nstoi stats:\n")
@@ -356,12 +364,18 @@ def dataio_prep(hparams):
     """Creates the datasets and their data processing pipelines"""
 
     # 1. define tokenizer
-    if hparams["target_type"] == "wrd":
+    if hparams["target_type"] == "words":
+        save_model_path = os.path.join(
+            hparams["save_folder"], "1000_unigram.model"
+        )
+        download_file(
+            source=hparams["tok_mdl_file"],
+            dest=save_model_path,
+            replace_existing=True,
+        )
         tokenizer = SentencePiece(
             model_dir=hparams["save_folder"],
             vocab_size=hparams["output_neurons"],
-            csv_train=hparams["train_annotation"],
-            csv_read="wrd",
             model_type=hparams["token_type"],
             character_coverage=hparams["character_coverage"],
         )
@@ -385,7 +399,7 @@ def dataio_prep(hparams):
     @sb.utils.data_pipeline.takes(hparams["target_type"])
     @sb.utils.data_pipeline.provides("tokens_list", *[t for t in token_keys])
     def target_pipeline(target):
-        if hparams["target_type"] == "wrd":
+        if hparams["target_type"] == "words":
             tokens_list = tokenizer.sp.encode_as_ids(target)
             yield tokens_list
         else:
@@ -423,25 +437,9 @@ def dataio_prep(hparams):
         )
 
     # 5. Load or update tokenizer
-    if hparams["target_type"] == "wrd":
-        save_model_path = os.path.join(hparams["save_folder"], "tok_uni.model")
-        save_vocab_path = os.path.join(hparams["save_folder"], "tok_uni.vocab")
+    if hparams["target_type"] == "words":
 
-        if "tok_mdl_file" in hparams:
-            download_file(
-                source=hparams["tok_mdl_file"],
-                dest=save_model_path,
-                replace_existing=True,
-            )
-            tokenizer.sp.load(save_model_path)
-
-        if "tok_voc_file" in hparams:
-            download_file(
-                source=hparams["tok_voc_file"],
-                dest=save_vocab_path,
-                replace_existing=True,
-            )
-            tokenizer.sp.load(save_model_path)
+        tokenizer.sp.load(save_model_path)
 
         if (tokenizer.sp.eos_id() + 1) == (
             tokenizer.sp.bos_id() + 1
@@ -456,7 +454,6 @@ def dataio_prep(hparams):
                 "Desired indexes for special tokens do not agree "
                 "with loaded tokenizer special tokens !"
             )
-
     else:
         tokenizer.update_from_didataset(data["train"], output_key="tokens_list")
         tokenizer.insert_bos_eos(
