5607c8e607ac0f06913e9871e370e97e64f165b8
ravanelm <mirco.ravanelli@gmail.com>
2021-02-05 18:32:50 -0500

    save label encoder

--- recipes/TIMIT/ASR/transducer/train/train.py
+++ recipes/TIMIT/ASR/transducer/train/train.py
@@ -11,6 +11,7 @@ Authors
  * Mirco Ravanelli 2020
  * Ju-Chieh Chou 2020
 """
+import os
 import sys
 import torch
 import logging
@@ -212,8 +213,18 @@ def dataio_prep(hparams):
     sb.dataio.dataset.add_dynamic_item(datasets, text_pipeline)
 
     # 3. Fit encoder:
-    label_encoder.insert_blank(index=hparams["blank_index"])
-    label_encoder.update_from_didataset(train_data, output_key="phn_list")
+    # Load or compute the label encoder
+    label_encoder_file = os.path.join(
+        hparams["save_folder"], "label_encoder.txt"
+    )
+    if os.path.exists(label_encoder_file):
+        label_encoder.load(label_encoder_file)
+    else:
+        label_encoder.insert_blank(index=hparams["blank_index"])
+        label_encoder.update_from_didataset(train_data, output_key="phn_list")
+        label_encoder.save(
+            os.path.join(hparams["save_folder"], "label_encoder.txt")
+        )
 
     # 4. Set output:
     sb.dataio.dataset.set_output_keys(datasets, ["id", "sig", "phn_encoded"])
