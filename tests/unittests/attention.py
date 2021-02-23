7137d08e06006bd8df07d411c8f0736e6cf6a18d
popcornell <cornellsamuele@gmail.com>
2021-02-24 00:36:42 +0100

    added MHA with relative pos encodings as in Conformer and TransformerXL + tests. Need Docs

---
+++ tests/unittests/attention.py
@@ -0,0 +1,25 @@
+import torch
+
+
+def test_rel_pos_MHA():
+
+    from speechbrain.nnet.attention import RelPosMultiHeadAttention
+
+    bsz = 2
+    emb_dim = 4
+    k_len = [12, 10]
+    q_len = [10, 12]
+    bias = [True, False]
+    head_dim = [3, None]
+
+    for kl in k_len:
+        for ql in q_len:
+            for b in bias:
+                for h in head_dim:
+                    relpos = RelPosMultiHeadAttention(
+                        emb_dim, 2, bias=b, head_dim=h
+                    )
+                    q = torch.rand((ql, bsz, emb_dim))
+                    k = torch.rand((kl, bsz, emb_dim))
+                    r = torch.rand((kl, emb_dim))
+                    relpos(q, k, k, r)
