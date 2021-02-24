5b4d26599a00dfc746938029491c8adcf3a0a534
popcornell <cornellsamuele@gmail.com>
2021-02-24 23:46:23 +0100

    simple fetching using git lfs for now.

--- speechbrain/pretrained/fetching.py
+++ speechbrain/pretrained/fetching.py
@@ -1,12 +1,40 @@
 """Downloads or otherwise fetches pretrained models"""
 import huggingface_hub
+import re
+import os
 
 
-def fetch(filename, source):
-    """Fetch a file from Huggingface hub / filesystem / other
+def list_models(user="sb", regex=None):
+
+    regex = re.compile(regex)
+    api = huggingface_hub.HfApi()
+    models = []
+    for m in api.model_list():
+        if m.modelId.startswith(user):
+            if regex is not None:
+                if re.match(regex, m.modelId):
+                    models.append(m)
+            else:
+                models.append(m)
+    return models
+
+
+def _fetch_all_from_hub(model_url, local_dir, **huggingface_kwargs):
+    huggingface_hub.Repository(
+        local_dir, clone_from=model_url, **huggingface_kwargs
+    )
+
+
+def fetch(
+    hub_modelID,
+    local_dir,
+    hparams_file="hyperparams.yaml",
+    **huggingface_kwargs,
+):
     """
-    # In the filesystem the full path would be source/file
-    # If source exists in system, copy or maybe symlink here.
-    # Otherwise look in huggingface hub and cached_download here.
-    huggingface_hub
-    pass
+    """
+    if os.path.exists(local_dir) and os.path.join(local_dir, hparams_file):
+        print("Model already exists locally, loading local copy")
+    else:
+        _fetch_all_from_hub(hub_modelID, local_dir, **huggingface_kwargs)
+    return os.path.join(local_dir, hparams_file)
