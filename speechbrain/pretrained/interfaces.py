b24567bd95cce1b5b8fb49d66063d896e93ca4b9
Peter Plantinga <plantinga.peter@protonmail.com>
2021-02-25 16:08:17 -0500

    Working "Flow" class

--- speechbrain/pretrained/interfaces.py
+++ speechbrain/pretrained/interfaces.py
@@ -2,13 +2,16 @@
 import torch
 import torchaudio
 from types import SimpleNamespace
-from hyperpyyml import load_hyperpyyaml
+from torch.nn import SyncBatchNorm
+from torch.nn import DataParallel as DP
+from hyperpyyaml import load_hyperpyyaml
 from speechbrain.pretrained.fetching import fetch
 from speechbrain.dataio.preprocess import AudioNormalizer
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 
-class Heart:
-    """Inference with learned systems - what is known by Heart
+class Flow:
+    """Takes a trained model and makes predictions on new data.
 
     Arguments
     ---------
@@ -20,16 +23,44 @@ class Heart:
         that is used within the overridden methods. These will
         be accessible via an ``hparams`` attribute, using "dot" notation:
         e.g., self.hparams.model(x).
-    eval_mode : bool
+    run_opts : dict
+        Options parsed from command line. See ``speechbrain.parse_arguments()``.
+        List that are supported here:
+         * device
+         * data_parallel_count
+         * data_parallel_backend
+         * distributed_launch
+         * distributed_backend
+         * jit_module_keys
+    freeze_params : bool
         To freeze (requires_grad=False) parameters or not. Normally in inference
         you want to freeze the params. Also calls .eval() on all modules.
     """
 
     def __init__(
-        self, modules=None, hparams=None, freeze_params=True, device="cpu"
+        self, modules=None, hparams=None, run_opts=None, freeze_params=True
     ):
 
-        self.device = device
+        # Arguments passed via the run opts dictionary. Set a limited
+        # number of these, since some don't apply to inference.
+        run_opt_defaults = {
+            "device": "cpu",
+            "data_parallel_count": -1,
+            "data_parallel_backend": False,
+            "distributed_launch": False,
+            "distributed_backend": "nccl",
+            "jit_module_keys": None,
+        }
+        for arg, default in run_opt_defaults.items():
+            if run_opts is not None and arg in run_opts:
+                setattr(self, arg, run_opts[arg])
+            else:
+                # If any arg from run_opt_defaults exist in hparams and
+                # not in command line args "run_opts"
+                if hparams is not None and arg in hparams:
+                    setattr(self, arg, hparams[arg])
+                else:
+                    setattr(self, arg, default)
 
         # Put modules on the right device, accessible with dot notation
         self.modules = torch.nn.ModuleDict(modules).to(self.device)
@@ -38,28 +69,99 @@ class Heart:
         if hparams is not None:
             self.hparams = SimpleNamespace(**hparams)
 
+        # Prepare modules for computation, e.g. jit
+        self._prepare_modules(freeze_params)
+
+    def _prepare_modules(self, freeze_params):
+        """Prepare modules for computation, e.g. jit.
+
+        Arguments
+        ---------
+        freeze_params : bool
+            Whether to freeze the parameters and call ``eval()``.
+        """
+
+        # Make jit-able
+        self._compile_jit()
+        self._wrap_distributed()
+
         # If we don't want to backprop, freeze the pretrained parameters
         if freeze_params:
             self.modules.eval()
             for p in self.modules.parameters():
                 p.requires_grad = False
 
+    def _compile_jit(self):
+        """Compile requested modules with ``torch.jit.script``."""
+        if self.jit_module_keys is None:
+            return
+
+        for name in self.jit_module_keys:
+            if name not in self.modules:
+                raise ValueError(
+                    "module " + name + " cannot be jit compiled because "
+                    "it is not defined in your hparams file."
+                )
+            module = torch.jit.script(self.modules[name])
+            self.modules[name] = module.to(self.device)
+
+    def _wrap_distributed(self):
+        """Wrap modules with distributed wrapper when requested."""
+        if not self.distributed_launch and not self.data_parallel_backend:
+            return
+        elif self.distributed_launch:
+            for name, module in self.modules.items():
+                if any(p.requires_grad for p in module.parameters()):
+                    # for ddp, all module must run on same GPU
+                    module = SyncBatchNorm.convert_sync_batchnorm(module)
+                    module = DDP(module, device_ids=[self.device])
+                    self.modules[name] = module
+        else:
+            # data_parallel_backend
+            for name, module in self.modules.items():
+                if any(p.requires_grad for p in module.parameters()):
+                    # if distributed_count = -1 then use all gpus
+                    # otherwise, specify the set of gpu to use
+                    if self.data_parallel_count == -1:
+                        module = DP(module)
+                    else:
+                        module = DP(
+                            module,
+                            [i for i in range(self.data_parallel_count)],
+                        )
+                    self.modules[name] = module
+
     @classmethod
     def from_hparams(
-        cls, source, hparams_file="hyperparams.yaml", overrides={}
+        cls,
+        source,
+        hparams_file="hyperparams.yaml",
+        local_path="./data",
+        overrides={},
     ):
         """Fetch and load based from outside source based on HyperPyYAML file
 
         The source can be a location on the filesystem or online/huggingface
 
         The hyperparams file should contain a "modules" key, which is a
-        speechbrain.utils.parameter_transfer.Pretrainer
+        dictionary of torch modules used for computation.
 
         The hyperparams file should contain a "pretrainer" key, which is a
         speechbrain.utils.parameter_transfer.Pretrainer
 
+        Arguments
+        ---------
+        source : str
+            The location to use for finding the model. See
+            ``speechbrain.pretrained.fetching.fetch`` for details.
+        hparams_file : str
+            The name of the hyperparameters file to use for constructing
+            the modules necessary for inference. Must contain two keys:
+            "modules" and "pretrainer", as described.
+        overrides : dict
+            Any changes to make to the hparams file when it is loaded.
         """
-        hparams_local_path = fetch(source, hparams_file)
+        hparams_local_path = fetch(local_path, hparams_file, source)
 
         # Load the modules:
         with open(hparams_local_path) as fin:
@@ -73,7 +175,7 @@ class Heart:
         return cls(hparams["modules"], hparams)
 
 
-class ASRInterface(Heart):
+class ASRInterface(Flow):
     """General interface for Automatic Speech Recognition"""
 
     def __init__(self, *args, **kwargs):
@@ -121,20 +223,20 @@ class EncoderDecoderASR(ASRInterface):
 
     """
 
-    def __init__(self,):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         # TODO: Don't rely on LM!!
         # The tokenizer is the one used by the LM
-        self.tokenizer = self.lm_model.tokenizer
+        self.tokenizer = self.hparams.tokenizer
 
     def encode_batch(self, wavs, wav_lens):
         """Encodes the input audio into a sequence of hidden states"""
         wavs = wavs.float()
         wavs, wav_lens = wavs.to(self.device), wav_lens.to(self.device)
-        feats = self.mod.compute_features(wavs)
-        feats = self.mod.normalize(feats, wav_lens)
-        encoder_out = self.mod.asr_encoder(feats)
+        feats = self.modules.compute_features(wavs)
+        feats = self.modules.normalize(feats, wav_lens)
+        encoder_out = self.modules.asr_encoder(feats)
         return encoder_out
 
     def transcribe_batch(self, wavs, wav_lens):
@@ -142,7 +244,7 @@ class EncoderDecoderASR(ASRInterface):
         with torch.no_grad():
             wav_lens = wav_lens.to(self.device)
             encoder_out = self.encode_batch(wavs, wav_lens)
-            predicted_tokens, scores = self.mod.beam_searcher(
+            predicted_tokens, scores = self.modules.beam_searcher(
                 encoder_out, wav_lens
             )
 
