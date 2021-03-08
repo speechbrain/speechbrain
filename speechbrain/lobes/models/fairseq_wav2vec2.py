fa59eef64f418a138b5eae66a11b67e9de1c63d2
mdhaffar <salima.mdhaffar@univ-lemans.fr>
2021-03-08 14:58:08 +0100

    wave2vec1.0

--- speechbrain/lobes/models/fairseq_wav2vec2.py
+++ speechbrain/lobes/models/fairseq_wav2vec2.py
@@ -72,17 +72,13 @@ class FairseqWav2Vec2(nn.Module):
         if self.freeze:
             model.eval()
 
-    def forward(self, x):
+    def forward(self, wav):
         """Takes an input waveform and return its corresponding wav2vec encoding.
 
         Arguments
         ---------
-        x : torch.Tensor (signal)
-
-        Outputs
-        --------
-        x : torch.Tensor [batch, time, features]
-
+        wav : torch.Tensor (signal)
+            A batch of audio signals to transform to features.
         """
 
         # If freeze is specified, we remove the model from the gradient graph.
@@ -92,7 +88,7 @@ class FairseqWav2Vec2(nn.Module):
 
         # We normalize the input signal if needed.
         if self.normalize:
-            x = F.layer_norm(x, x.shape)
+            x = F.layer_norm(wav, wav.shape)
 
         # Extract wav2vec output
         out = self.model.extract_features(x, padding_mask=None, mask=False)[0]
@@ -100,5 +96,65 @@ class FairseqWav2Vec2(nn.Module):
         # We normalize the output if required
         if self.output_norm:
             out = F.layer_norm(out, out.shape)
+
+        return out
+
+
+class FairseqWav2Vec1(nn.Module):
+    """This lobes enables the integration of fairseq pretrained wav2vec1.0 models.
+
+    Arguments
+    ---------
+    pretrained_path : str
+        Path of the pretrained wav2vec2 model. It can be a url or a local path.
+    save_path : str
+        Path and filename of the downloaded model.
+    output_norm : bool (default: True)
+        If True, a layer_norm (affine) will be applied to the output obtained
+        from the wav2vec model.
+    freeze : bool (default: True)
+        If True, the model is frozen. If False, the model will be trained
+        alongside with the rest of the pipeline.
+
+    Example
+    -------
+    >>> inputs = torch.rand([10, 600])
+    >>> model_url = ""
+    >>> save_path = "models_checkpoints/wav2vec2.pt"
+    >>> model = FairseqWav2Vec2(model_url, save_path)
+    >>> outputs = model(inputs)
+    >>> outputs.shape
+    torch.Size([10, 100, 512])
+    """
+
+    def __init__(
+        self, pretrained_path, save_path, output_norm=True, freeze=True
+    ):
+        super().__init__()
+
+        # Download the pretrained wav2vec1 model. It can be local or online.
+        download_file(pretrained_path, save_path)
+
+        (
+            model,
+            cfg,
+            task,
+        ) = fairseq.checkpoint_utils.load_model_ensemble_and_task(
+            [pretrained_path]
+        )
+        self.model = model
+        self.model = self.model[0]
+
+    def forward(self, wav):
+        """Takes an input waveform and return its corresponding wav2vec encoding.
+
+        Arguments
+        ---------
+        wav : torch.Tensor (signal)
+            A batch of audio signals to transform to features.
+        """
+        out = self.model.feature_extractor(wav)
+        out = self.model.feature_aggregator(out).squeeze(0)
+        out = out.transpose(2, 1)
 
         return out
