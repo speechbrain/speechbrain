979fc6035aebe57fb5d209a300f71de0d0720c90
mirco <mirco.ravabelli@gmail.com>
2021-03-13 11:33:54 -0500

    updated toml and setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 
 setup(
     name="SpeechBrain",
-    version="0.0.1",
+    version="0.5.0",
     description="All-in-one speech toolkit in pure Python and Pytorch",
     author="Mirco Ravanelli & Others",
     author_email="speechbrain@gmail.com",
     packages=["speechbrain"],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
+    url="https://speechbrain.github.io/"
 )
