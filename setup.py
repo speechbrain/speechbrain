e7128ee6cd8e3fba390198846917cb0822d5b869
mirco <mirco.ravabelli@gmail.com>
2021-03-13 13:14:30 -0500

    add project description in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 import setuptools
 from distutils.core import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="speechbrain",
     version="0.5.0",
     description="All-in-one speech toolkit in pure Python and Pytorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Mirco Ravanelli & Others",
     author_email="speechbrain@gmail.com",
     packages=setuptools.find_packages(),
