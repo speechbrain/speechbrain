e732f42be6b597a8b6181f38b10d401332ea699c
mirco <mirco.ravabelli@gmail.com>
2021-03-13 12:16:13 -0500

    update requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import setuptools
 from distutils.core import setup
 
 setup(
@@ -7,7 +8,19 @@ setup(
     description="All-in-one speech toolkit in pure Python and Pytorch",
     author="Mirco Ravanelli & Others",
     author_email="speechbrain@gmail.com",
-    packages=["speechbrain"],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "hyperpyyaml",
+        "joblib",
+        "numpy",
+        "packaging",
+        "scipy",
+        "sentencepiece",
+        "torch",
+        "torchaudio",
+        "tqdm",
+        "huggingface_hub",
+    ],
     python_requires=">=3.7",
     url="https://speechbrain.github.io/",
 )
