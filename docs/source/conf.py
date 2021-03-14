09599c8976bd12ace132f32cfc5ef12f78c960f5
Parcollet Titouan <parcollet.titouan@gmail.com>
2021-03-14 18:21:05 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,7 @@ def run_apidoc(app):
             "--separate",
             "-o",
             "source",
-            os.path.join("../..", "speechbrain"),
+            "speechbrain",
         ]
     )
 
