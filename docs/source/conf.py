89dd7996eceec51a5c14d817c8eccc4c8c06e13a
Parcollet Titouan <parcollet.titouan@gmail.com>
2021-03-14 18:22:42 +0100

    revert changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,7 @@ def run_apidoc(app):
             "--separate",
             "-o",
             "source",
-            "speechbrain",
+            os.path.join("../..", "speechbrain"),,
         ]
     )
 
