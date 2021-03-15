e15f5c6ecd2f72adacceb4f005feb742b9d978a3
Haoxian WANG <haoxian.wang@probayes.com>
2021-03-15 16:39:49 +0100

    fix trailing whitespaces.

--- recipes/DNS/enhance/spectral_map/train.py
+++ recipes/DNS/enhance/spectral_map/train.py
@@ -24,7 +24,7 @@ from speechbrain.utils.torch_audio_backend import get_torchaudio_backend
 torchaudio_backend = get_torchaudio_backend()
 torchaudio.set_audio_backend(
     torchaudio_backend
-) 
+)
 logger = logging.getLogger(__name__)
 
 try:
