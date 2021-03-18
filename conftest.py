1f0457b50f5fe39cbeeddbdc0021617231876e3b
Parcollet Titouan <parcollet.titouan@gmail.com>
2021-03-18 19:10:32 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,3 +3,8 @@ try:
     import numba  # noqa: F401
 except ModuleNotFoundError:
     collect_ignore.append("speechbrain/nnet/loss/transducer_loss.py")
+    
+try:
+    import fairseq  # noqa: F401
+except ModuleNotFoundError:
+    collect_ignore.append("speechbrain/lobes/models/fairseq_wav2vec.py")
