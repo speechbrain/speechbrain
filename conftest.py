942609680a17b4cbd8528d2946a1d5633e1dda27
Titouan <parcollet.titouan@gmail.com>
2021-04-07 16:06:02 +0200

    fix tests

--- conftest.py
+++ conftest.py
@@ -7,3 +7,7 @@ try:
     import fairseq  # noqa: F401
 except ModuleNotFoundError:
     collect_ignore.append("speechbrain/lobes/models/fairseq_wav2vec.py")
+try:
+    from transformers import Wav2Vec2Model  # noqa: F401
+except ModuleNotFoundError:
+    collect_ignore.append("speechbrain/lobes/models/huggingface_wav2vec.py")
