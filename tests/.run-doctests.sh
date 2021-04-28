33ca0e7c3fb03fb69327a0da5d0f2e092dd034f4
mirco <mirco.ravabelli@gmail.com>
2021-04-28 09:30:22 -0400

    restoring grep with updated list of files to skip

--- tests/.run-doctests.sh
+++ tests/.run-doctests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-pytest --doctest-modules speechbrain/
+# We filter out tests that require not mandatory dependencies.
+avoid="transducer_loss.py\|fairseq_wav2vec.py\|huggingface_wav2vec.py"
+git ls-files speechbrain | grep -e "\.py$" | grep -v $avoid | xargs pytest --doctest-modules
