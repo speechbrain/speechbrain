ecb9d79bd430e0dbc6f4985c498e1aaae6aed363
Peter Plantinga <plantinga.peter@protonmail.com>
2021-04-28 10:59:55 -0400

    Add message explaining the somewhat complex approach

--- tests/.run-doctests.sh
+++ tests/.run-doctests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-# We filter out tests that require not mandatory dependencies.
+# To run doctests locally, the easiest approach is to do:
+# > pytest --doctest-modules speechbrain/
+# However, we take this more complex approach to avoid testing files not
+# tracked by git. We filter out tests that require optional dependencies.
 avoid="transducer_loss.py\|fairseq_wav2vec.py\|huggingface_wav2vec.py"
 git ls-files speechbrain | grep -e "\.py$" | grep -v $avoid | xargs pytest --doctest-modules
