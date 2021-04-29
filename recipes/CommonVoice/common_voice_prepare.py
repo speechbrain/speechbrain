e92f376528fd46f481cddde59588d6ff44e9bc92
Jim Regan <jaoregan@tcd.ie>
2021-04-29 09:16:33 +0200

    change lambda to def

--- recipes/CommonVoice/common_voice_prepare.py
+++ recipes/CommonVoice/common_voice_prepare.py
@@ -268,8 +268,8 @@ def create_csv(
             words = re.sub("[^" + letters + "]+", " ", words).upper()
         elif language == "ga-IE":
             # Irish lower() is complicated, but upper() is nondeterministic, so use lowercase
-            pfxuc = lambda a : len(a) >= 2 and a[0] in "tn" and a[1] in "AEIOUÁÉÍÓÚ"
-            galc = lambda w : w.lower() if not pfxuc(w) else w[0] + '-' + w[1:].lower()
+            def pfxuc(a): return len(a) >= 2 and a[0] in "tn" and a[1] in "AEIOUÁÉÍÓÚ"
+            def galc(w): return w.lower() if not pfxuc(w) else w[0] + '-' + w[1:].lower()
             words = re.sub("[^-A-Za-z'ÁÉÍÓÚáéíóú]+", " ", words)
             words = " ".join(map(galc, words.split(" ")))
 
