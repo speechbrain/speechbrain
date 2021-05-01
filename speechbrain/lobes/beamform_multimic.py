f5a1ce78ff4435c01a5086778921430da1ccea81
Nauman Dawalatabad <nauman.daw@gmail.com>
2021-05-01 10:38:08 -0400

    Added DelaySum_Beamformer in lobes

--- speechbrain/lobes/beamform_multimic.py
+++ speechbrain/lobes/beamform_multimic.py
@@ -10,14 +10,26 @@ from speechbrain.processing.multi_mic(
     DelaySum,
 )
 
-class Fbank(torch.nn.Module):
+class DelaySum_Beamformer(torch.nn.Module):
 
-    def __init__(self,
+    def __init__(
+        self,
+        sampling_rate=16000,
     ):
-        super().__init__()
+        self.fs = sampling_rate
+        self.stft = STFT(sample_rate=fs)
+        self.cov = Covariance()
+        self.gccphat = GccPhat()
+        self.delaysum = DelaySum()
+        self.istft = ISTFT(sample_rate=fs)
 
-    def forward(self, wav):
+    def forward(self, mics_signals):
+        with torch.no_grad():
+            Xs = self.stft(mics_signals)
+            XXs = self.cov(Xs)
+            tdoas = self.gccphat(XXs)
+            Ys_ds = self.delaysum(Xs, tdoas)
+            sig = self.istft(Ys_ds)
 
-        pass
-        return beamformed
+        return sig
 
