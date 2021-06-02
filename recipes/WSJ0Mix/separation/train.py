cc5545e3181b1f28bfaf5b97661b3d98b7d6531d
cem <csubakan@gmail.com>
2021-06-01 23:54:43 -0400

    fixed automatic mixed precision

--- recipes/WSJ0Mix/separation/train.py
+++ recipes/WSJ0Mix/separation/train.py
@@ -555,6 +555,7 @@ if __name__ == "__main__":
     hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
     with open(hparams_file) as fin:
         hparams = load_hyperpyyaml(fin, overrides)
+    run_opts["auto_mix_prec"] = hparams["auto_mix_prec"]
 
     # Initialize ddp (useful only for multi-GPU DDP training)
     sb.utils.distributed.ddp_init_group(run_opts)
