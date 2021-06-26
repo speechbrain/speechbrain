Add the output of alignments, enable masking by default, change the number of MELs, fix the use of multi-GPU/data parallelism, remove Python 2 leftovers

recipes/TTS/tacotron2/train.py
@@ -37,7 +37,7 @@ class Tacotron2Brain(sb.Brain, PretrainedModelMixin, ProgressSampleImageMixin):
         self.init_progress_samples()
 
     def compute_forward(self, batch, stage):
-        inputs, y, num_items = batch_to_gpu(batch)
+        inputs, y, num_items = self.batch_to_gpu(batch)
         return self.hparams.model(inputs)  # 1#2#
 
     def fit_batch(self, *args, **kwargs):
@@ -62,14 +62,43 @@ class Tacotron2Brain(sb.Brain, PretrainedModelMixin, ProgressSampleImageMixin):
             A one-element tensor used for backpropagating the gradient.
         """
 
-        inputs, y, num_items = batch_to_gpu(batch)
+        inputs, y, num_items = self.batch_to_gpu(batch)
         mel_target, _ = y
-        mel_out, mel_out_postnet, gate_out, _ = predictions
+        mel_out, mel_out_postnet, gate_out, alignments = predictions
         self.remember_progress_sample(
             target=self._get_sample_data(mel_target),
             output=self._get_sample_data(mel_out),
-            output_postnet=self._get_sample_data(mel_out_postnet))
+            output_postnet=self._get_sample_data(mel_out_postnet),
+            alignments=alignments[0].T)
         return criterion(predictions, y)
+
+    # some helper functoions
+    def batch_to_gpu(self, batch):
+        (
+            text_padded,
+            input_lengths,
+            mel_padded,
+            gate_padded,
+            output_lengths,
+            len_x,
+        ) = batch
+        text_padded = self.to_device(text_padded).long()
+        input_lengths = self.to_device(input_lengths).long()
+        max_len = torch.max(input_lengths.data).item()
+        mel_padded = self.to_device(mel_padded).float()
+        gate_padded = self.to_device(gate_padded).float()
+        output_lengths = self.to_device(output_lengths).long()
+        x = (text_padded, input_lengths, mel_padded, max_len, output_lengths)
+        y = (mel_padded, gate_padded)
+        len_x = torch.sum(output_lengths)
+        return (x, y, len_x)
+
+
+    def to_device(self, x):
+        x = x.contiguous()
+        x = x.to(self.device, non_blocking=True)
+        return x
+
 
     def _get_sample_data(self, raw):
         sample = raw[0]
@@ -126,34 +155,6 @@ class Tacotron2Brain(sb.Brain, PretrainedModelMixin, ProgressSampleImageMixin):
             )
 
 
-# some helper functoions
-def batch_to_gpu(batch):
-    (
-        text_padded,
-        input_lengths,
-        mel_padded,
-        gate_padded,
-        output_lengths,
-        len_x,
-    ) = batch
-    text_padded = to_gpu(text_padded).long()
-    input_lengths = to_gpu(input_lengths).long()
-    max_len = torch.max(input_lengths.data).item()
-    mel_padded = to_gpu(mel_padded).float()
-    gate_padded = to_gpu(gate_padded).float()
-    output_lengths = to_gpu(output_lengths).long()
-    x = (text_padded, input_lengths, mel_padded, max_len, output_lengths)
-    y = (mel_padded, gate_padded)
-    len_x = torch.sum(output_lengths)
-    return (x, y, len_x)
-
-
-def to_gpu(x):
-    x = x.contiguous()
-
-    if torch.cuda.is_available():
-        x = x.cuda(non_blocking=True)
-    return x
 
 
 def criterion(model_output, targets):

speechbrain/lobes/models/synthesis/tacotron2/model.py
@@ -45,7 +45,7 @@ from torch.nn import functional as F
 
 class LinearNorm(torch.nn.Module):
     def __init__(self, in_dim, out_dim, bias=True, w_init_gain="linear"):
-        super(LinearNorm, self).__init__()
+        super().__init__()
         self.linear_layer = torch.nn.Linear(in_dim, out_dim, bias=bias)
 
         torch.nn.init.xavier_uniform_(
@@ -69,7 +69,7 @@ class ConvNorm(torch.nn.Module):
         bias=True,
         w_init_gain="linear",
     ):
-        super(ConvNorm, self).__init__()
+        super().__init__()
         if padding is None:
             assert kernel_size % 2 == 1
             padding = int(dilation * (kernel_size - 1) / 2)
@@ -99,7 +99,7 @@ class LocationLayer(nn.Module):
     def __init__(
         self, attention_n_filters, attention_kernel_size, attention_dim
     ):
-        super(LocationLayer, self).__init__()
+        super().__init__()
         padding = int((attention_kernel_size - 1) / 2)
         self.location_conv = ConvNorm(
             2,
@@ -130,7 +130,7 @@ class Attention(nn.Module):
         attention_location_n_filters,
         attention_location_kernel_size,
     ):
-        super(Attention, self).__init__()
+        super().__init__()
         self.query_layer = LinearNorm(
             attention_rnn_dim, attention_dim, bias=False, w_init_gain="tanh"
         )
@@ -212,7 +212,7 @@ class Attention(nn.Module):
 
 class Prenet(nn.Module):
     def __init__(self, in_dim, sizes):
-        super(Prenet, self).__init__()
+        super().__init__()
         in_sizes = [in_dim] + sizes[:-1]
         self.layers = nn.ModuleList(
             [
@@ -239,7 +239,7 @@ class Postnet(nn.Module):
         postnet_kernel_size,
         postnet_n_convolutions,
     ):
-        super(Postnet, self).__init__()
+        super().__init__()
         self.convolutions = nn.ModuleList()
 
         self.convolutions.append(
@@ -310,7 +310,7 @@ class Encoder(nn.Module):
     def __init__(
         self, encoder_n_convolutions, encoder_embedding_dim, encoder_kernel_size
     ):
-        super(Encoder, self).__init__()
+        super().__init__()
 
         convolutions = []
         for _ in range(encoder_n_convolutions):
@@ -394,7 +394,7 @@ class Decoder(nn.Module):
         p_decoder_dropout,
         early_stopping,
     ):
-        super(Decoder, self).__init__()
+        super().__init__()
         self.n_mel_channels = n_mel_channels
         self.n_frames_per_step = n_frames_per_step
         self.encoder_embedding_dim = encoder_embedding_dim
@@ -962,7 +962,7 @@ class Tacotron2(nn.Module):
             number of convolution layers in postnet
 
         """
-        super(Tacotron2, self).__init__()
+        super().__init__()
         self.mask_padding = mask_padding
         self.n_mel_channels = n_mel_channels
         self.n_frames_per_step = n_frames_per_step