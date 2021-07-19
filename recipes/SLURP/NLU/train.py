54517bdd2e002f4ed737bfef1c1f57435c0cf39d
30stomercury <ff936tw@gmail.com>
2021-07-19 14:24:48 +0800

    Finish SLURP/NLU conversion

--- recipes/SLURP/NLU/train.py
+++ recipes/SLURP/NLU/train.py
@@ -14,6 +14,7 @@ import torch
 import speechbrain as sb
 from hyperpyyaml import load_hyperpyyaml
 from speechbrain.utils.distributed import run_on_main
+from speechbrain.utils.data_utils import undo_padding
 import jsonlines
 import ast
 import pandas as pd
@@ -40,27 +41,23 @@ class SLU(sb.Brain):
         p_seq = self.hparams.log_softmax(logits)
 
         # Compute outputs
-        if (
-            stage == sb.Stage.TRAIN
-            and self.batch_count % show_results_every != 0
-        ):
-            return p_seq, transcript_tokens_lens
-        else:
-            p_tokens, scores = self.hparams.beam_searcher(
+        p_tokens = None
+        if stage != sb.Stage.TRAIN:
+            topk_tokens, topk_lens, _, _ = self.hparams.beam_searcher(
                 encoder_out, transcript_tokens_lens
             )
-            return p_seq, transcript_tokens_lens, p_tokens
+            # Select the best hypothesis
+            best_hyps, best_lens = topk_tokens[:, 0, :], topk_lens[:, 0]
+
+            # Convert best hypothesis to list
+            p_tokens = undo_padding(best_hyps, best_lens)
+
+        return p_seq, transcript_tokens_lens, p_tokens
 
     def compute_objectives(self, predictions, batch, stage):
         """Computes the loss (NLL) given predictions and targets."""
 
-        if (
-            stage == sb.Stage.TRAIN
-            and self.batch_count % show_results_every != 0
-        ):
-            p_seq, transcript_tokens_lens = predictions
-        else:
-            p_seq, transcript_tokens_lens, predicted_tokens = predictions
+        p_seq, transcript_tokens_lens, predicted_tokens = predictions
 
         ids = batch.id
         (
