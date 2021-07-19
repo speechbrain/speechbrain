2a95f722092b2cb04171d7a1e8655c9613d63565
Kamalesh Palanisamy <kamalesh800@gmail.com>
2021-07-19 18:01:19 +0400

    Update music transformer code to solve training issues

---
+++ speechbrain/lobes/models/transformer/MusicTransformer.py
@@ -0,0 +1,364 @@
+"""Music Transformer Implementation 
+
+Authors
+* Kamalesh Palanisamy 2021
+"""
+
+import math
+import torch
+import torch.nn as nn
+import speechbrain as sb
+
+from typing import Optional
+from speechbrain.nnet.activations import Swish
+
+
+class MusicTransformer(nn.Module):
+
+    def __init__(
+        self,
+        d_model=512,
+        nhead=8,
+        num_encoder_layers=6,
+        num_decoder_layers=6,
+        d_ffn=2048,
+        dropout=0.1,
+        max_seq_length=2048,
+        activation=nn.ReLU,
+        custom_src_module=None,
+        custom_tgt_module=None,
+        positional_encoding=True,
+        normalize_before=False,
+        kernel_size: Optional[int] = 31,
+        bias: Optional[bool] = True,
+        encoder_module: Optional[str] = "transformer",
+        conformer_activation: Optional[nn.Module] = Swish,
+    ):
+        super().__init__()
+
+        assert (
+            num_encoder_layers + num_decoder_layers > 0
+        ), "number of encoder layers and number of decoder layers cannot both be 0!"
+
+        if positional_encoding:
+            self.positional_encoding = PositionalEncoding(d_model)
+        
+        # initialize the encoder
+        if num_encoder_layers > 0:
+            if custom_src_module is not None:
+                self.custom_src_module = custom_src_module(d_model)
+            if encoder_module == "transformer":
+                self.encoder = TransformerEncoder(
+                    nhead=nhead,
+                    max_seq_length=max_seq_length,
+                    num_layers=num_encoder_layers,
+                    d_ffn=d_ffn,
+                    d_model=d_model,
+                    dropout=dropout,
+                    activation=activation,
+                    normalize_before=normalize_before,
+                )
+            elif encoder_module == "conformer":
+                self.encoder = ConformerEncoder(
+                    nhead=nhead,
+                    num_layers=num_encoder_layers,
+                    d_ffn=d_ffn,
+                    d_model=d_model,
+                    dropout=dropout,
+                    activation=conformer_activation,
+                    kernel_size=kernel_size,
+                    bias=bias,
+                )
+                assert (
+                    normalize_before
+                ), "normalize_before must be True for Conformer"
+
+                assert (
+                    conformer_activation is not None
+                ), "conformer_activation must not be None"
+
+        # initialize the decoder
+        if num_decoder_layers > 0:
+            if custom_tgt_module is not None:
+                self.custom_tgt_module = custom_tgt_module(d_model)
+
+            self.decoder = TransformerDecoder(
+                num_layers=num_decoder_layers,
+                nhead=nhead,
+                d_ffn=d_ffn,
+                d_model=d_model,
+                dropout=dropout,
+                activation=activation,
+                normalize_before=normalize_before,
+            )
+
+    def forward(self, src):
+
+        pos_encod  = src + self.positional_encoding(src)
+
+        output, _ = self.encoder(pos_encod)
+        output = torch.nn.functional.softmax(output, dim=-1)
+        return output, _
+
+class PositionalEncoding(nn.Module):
+    """This class implements the positional encoding function.
+
+    PE(pos, 2i)   = sin(pos/(10000^(2i/dmodel)))
+    PE(pos, 2i+1) = cos(pos/(10000^(2i/dmodel)))
+
+    Arguments
+    ---------
+    max_len : int
+        Max length of the input sequences (default 2500).
+
+    Example
+    -------
+    >>> a = torch.rand((8, 120, 512))
+    >>> enc = PositionalEncoding(input_size=a.shape[-1])
+    >>> b = enc(a)
+    >>> b.shape
+    torch.Size([1, 120, 512])
+    """
+
+    def __init__(self, input_size, max_len=2500):
+        super().__init__()
+        self.max_len = max_len
+        pe = torch.zeros(self.max_len, input_size, requires_grad=False)
+        positions = torch.arange(0, self.max_len).unsqueeze(1).float()
+        denominator = torch.exp(
+            torch.arange(0, input_size, 2).float()
+            * -(math.log(10000.0) / input_size)
+        )
+
+        pe[:, 0::2] = torch.sin(positions * denominator)
+        pe[:, 1::2] = torch.cos(positions * denominator)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        """
+        Arguments
+        ---------
+        x : tensor
+            Input feature shape (batch, time, fea)
+        """
+        return self.pe[:, : x.size(1)].clone().detach()
+
+class TransformerEncoder(nn.Module):
+    """This class implements the transformer encoder.
+
+    Arguments
+    ---------
+    num_layers : int
+        Number of transformer layers to include.
+    nhead : int
+        Number of attention heads.
+    d_ffn : int
+        Hidden size of self-attention Feed Forward layer.
+    input_shape : tuple
+        Expected shape of an example input.
+    d_model : int
+        The dimension of the input embedding.
+    max_seq_length: int
+        The dimension of the maximum sequence length of the input.
+    kdim : int
+        Dimension for key (Optional).
+    vdim : int
+        Dimension for value (Optional).
+    dropout : float
+        Dropout for the encoder (Optional).
+    input_module: torch class
+        The module to process the source input feature to expected
+        feature dimension (Optional).
+
+    Example
+    -------
+    >>> import torch
+    >>> x = torch.rand((8, 60, 512))
+    >>> net = TransformerEncoder(1, 8, 512, d_model=512)
+    >>> output, _ = net(x)
+    >>> output.shape
+    torch.Size([8, 60, 512])
+    """
+
+    def __init__(
+        self,
+        num_layers,
+        nhead,
+        d_ffn,
+        max_seq_length,
+        input_shape=None,
+        d_model=None,
+        kdim=None,
+        vdim=None,
+        dropout=0.1,
+        activation=nn.ReLU,
+        normalize_before=False,
+    ):
+        super().__init__()
+
+        if input_shape is None and d_model is None:
+            raise ValueError("Expected one of input_shape or d_model")
+
+        if input_shape is not None and d_model is None:
+            if len(input_shape) == 3:
+                msg = "Input shape of the Transformer must be (batch, time, fea). Please revise the forward function in TransformerInterface to handle arbitrary shape of input."
+                raise ValueError(msg)
+            d_model = input_shape[-1]
+
+        self.layers = torch.nn.ModuleList(
+            [
+                TransformerEncoderLayer(
+                    d_ffn=d_ffn,
+                    nhead=nhead,
+                    d_model=d_model,
+                    erdim=max_seq_length,
+                    kdim=kdim,
+                    vdim=vdim,
+                    dropout=dropout,
+                    activation=activation,
+                    normalize_before=normalize_before,
+                )
+                for i in range(num_layers)
+            ]
+        )
+        self.norm = sb.nnet.normalization.LayerNorm(d_model, eps=1e-6)
+
+    def forward(
+        self,
+        src,
+        src_mask: Optional[torch.Tensor] = None,
+        src_key_padding_mask: Optional[torch.Tensor] = None,
+    ):
+        """
+        Arguments
+        ----------
+        src : tensor
+            The sequence to the encoder layer (required).
+        src_mask : tensor
+            The mask for the src sequence (optional).
+        src_key_padding_mask : tensor
+            The mask for the src keys per batch (optional).
+        """
+        output = src
+        attention_lst = []
+        for enc_layer in self.layers:
+            output, attention = enc_layer(
+                output,
+                src_mask=src_mask,
+                src_key_padding_mask=src_key_padding_mask,
+            )
+            attention_lst.append(attention)
+        output = self.norm(output)
+
+        return output, attention_lst
+
+class TransformerEncoderLayer(nn.Module):
+    """This is an implementation of self-attention encoder layer.
+
+    Arguments
+    ----------
+    d_ffn : int
+        Hidden size of self-attention Feed Forward layer.
+    nhead : int
+        Number of attention heads.
+    d_model : int
+        The expected size of the input embedding.
+    reshape : bool
+        Whether to automatically shape 4-d input to 3-d.
+    erdim: int
+        The dimension of the relative positional encoding(Er) matrix.
+    kdim : int
+        Dimension of the key (Optional).
+    vdim : int
+        Dimension of the value (Optional).
+    dropout : float
+        Dropout for the encoder (Optional).
+
+    Example
+    -------
+    >>> import torch
+    >>> x = torch.rand((8, 60, 512))
+    >>> net = TransformerEncoderLayer(512, 8, d_model=512)
+    >>> output = net(x)
+    >>> output[0].shape
+    torch.Size([8, 60, 512])
+    """
+
+    def __init__(
+        self,
+        d_ffn,
+        nhead,
+        erdim,
+        d_model=None,
+        kdim=None,
+        vdim=None,
+        dropout=0.1,
+        activation=nn.ReLU,
+        normalize_before=False,
+    ):
+        super().__init__()
+
+        self.self_att = sb.nnet.attention.MultiheadRelativeAttention(
+            num_heads=nhead, embed_dim=d_model, erdim=erdim, dropout=dropout, kdim=kdim, vdim=vdim,
+        )
+        self.pos_ffn = sb.nnet.attention.PositionalwiseFeedForward(
+            d_ffn=d_ffn,
+            input_size=d_model,
+            dropout=dropout,
+            activation=activation,
+        )
+
+        self.norm1 = sb.nnet.normalization.LayerNorm(d_model, eps=1e-6)
+        self.norm2 = sb.nnet.normalization.LayerNorm(d_model, eps=1e-6)
+        self.dropout1 = torch.nn.Dropout(dropout)
+        self.dropout2 = torch.nn.Dropout(dropout)
+
+        self.normalize_before = normalize_before
+
+    def forward(
+        self,
+        src,
+        src_mask: Optional[torch.Tensor] = None,
+        src_key_padding_mask: Optional[torch.Tensor] = None,
+    ):
+        """
+        Arguments
+        ----------
+        src : tensor
+            The sequence to the encoder layer (required).
+        src_mask : tensor
+            The mask for the src sequence (optional).
+        src_key_padding_mask : tensor
+            The mask for the src keys per batch (optional).
+        """
+        if self.normalize_before:
+            src1 = self.norm1(src)
+        else:
+            src1 = src
+
+        output, self_attn = self.self_att(
+            src1,
+            src1,
+            src1,
+            attn_mask=src_mask,
+            key_padding_mask=src_key_padding_mask,
+        )
+
+        # add & norm
+        src = src + self.dropout1(output)
+        if not self.normalize_before:
+            src = self.norm1(src)
+
+        if self.normalize_before:
+            src1 = self.norm2(src)
+        else:
+            src1 = src
+        output = self.pos_ffn(src1)
+
+        # add & norm
+        output = src + self.dropout2(output)
+        if not self.normalize_before:
+            output = self.norm2(output)
+
+        return output, self_attn
