c474d0a7189a15609c2db950450069bc8e2d6f54
Titouan <parcollet.titouan@gmail.com>
2021-08-17 16:37:26 +0200

    compare len

--- recipes/CommonVoice/common_voice_prepare.py
+++ recipes/CommonVoice/common_voice_prepare.py
@@ -299,7 +299,7 @@ def create_csv(
         chars = " ".join([char for char in chars][:])
 
         # Remove too short sentences (or empty):
-        if len(words) < 3:
+        if len(words.split(" ")) < 3:
             continue
 
         # Composition of the csv_line
