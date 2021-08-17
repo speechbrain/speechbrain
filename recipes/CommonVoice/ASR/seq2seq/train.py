e2259d3e7c9af3c64b98d3059e7a09b3c431c59c
Titouan <parcollet.titouan@gmail.com>
2021-08-17 13:27:51 +0200

    update recipe with new tokenizer

--- recipes/CommonVoice/ASR/seq2seq/train.py
+++ recipes/CommonVoice/ASR/seq2seq/train.py
@@ -173,7 +173,7 @@ class ASR(sb.core.Brain):
 
 
 # Define custom data procedure
-def dataio_prepare(hparams):
+def dataio_prepare(hparams, tokenizer):
     """This function prepares the datasets to be used in the brain class.
     It also defines the data processing pipeline through user-defined functions."""
 
@@ -224,16 +224,6 @@ def dataio_prepare(hparams):
     test_data = test_data.filtered_sorted(sort_key="duration")
 
     datasets = [train_data, valid_data, test_data]
-
-    # defining tokenizer and loading it
-    tokenizer = SentencePiece(
-        model_dir=hparams["save_folder"],
-        vocab_size=hparams["output_neurons"],
-        annotation_train=hparams["train_csv"],
-        annotation_read="wrd",
-        model_type=hparams["token_type"],
-        character_coverage=hparams["character_coverage"],
-    )
 
     # 2. Define audio pipeline:
     @sb.utils.data_pipeline.takes("wav")
@@ -310,8 +300,20 @@ if __name__ == "__main__":
         },
     )
 
+    # Defining tokenizer and loading it
+    tokenizer = SentencePiece(
+        model_dir=hparams["save_folder"],
+        vocab_size=hparams["output_neurons"],
+        annotation_train=hparams["train_csv"],
+        annotation_read="wrd",
+        model_type=hparams["token_type"],
+        character_coverage=hparams["character_coverage"],
+    )
+
     # Create the datasets objects as well as tokenization and encoding :-D
-    train_data, valid_data, test_data, tokenizer = dataio_prepare(hparams)
+    train_data, valid_data, test_data, tokenizer = dataio_prepare(
+        hparams, tokenizer
+    )
 
     # Trainer initialization
     asr_brain = ASR(
