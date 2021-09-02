11f2c52a6619f3d0164fb07821286fd459195fb8
Sung-Lin <sung-lin.yeh@ed.ac.uk>
2021-09-02 12:50:46 -0400

    Fix tests
    Fix test

--- speechbrain/nnet/CNN.py
+++ speechbrain/nnet/CNN.py
@@ -1100,7 +1100,7 @@ class DepthwiseSeparableConv2d(nn.Module):
 
 
 class Mask2d(nn.Module):
-    """This function implements masking for output after 2d convolution, 
+    """This function implements masking for output after 2d convolution,
     pooling layers.
 
     Arguments
@@ -1156,11 +1156,13 @@ class Mask2d(nn.Module):
         if unsqueeze:
             mask = mask.unsqueeze(1)
         if self.padding == "same":
-            mask = mask[:, :, ::self.stride[-1], ::self.stride[-2]] # noqa
+            mask = mask[:, :, :: self.stride[-1], :: self.stride[-2]]
         elif self.padding == "valid":
             time_end = -self.dilation[-1] * (self.kernel_size[-1] - 1)
             freq_end = -self.dilation[-2] * (self.kernel_size[-2] - 1)
-            mask = mask[:, :, :time_end:self.stride[-1], :freq_end:self.stride[-2]] # noqa
+            mask = mask[
+                :, :, : time_end : self.stride[-1], : freq_end : self.stride[-2]
+            ]
         else:
             raise ValueError(
                 "Padding must be 'same' or 'valid'. Got " + self.padding
