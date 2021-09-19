Masking of conv: 1. Fix wrong masking size of valid padding mode 2. Add masking to Conv1d 3. Fix CNN unittests

speechbrain/nnet/CNN.py
@@ -77,7 +77,7 @@ class SincConv(nn.Module):
         stride=1,
         dilation=1,
         padding="same",
-        padding_mode="reflect",
+        padding_mode="constant",
         sample_rate=16000,
         min_low_hz=50,
         min_band_hz=50,
@@ -347,7 +347,7 @@ class Conv1d(nn.Module):
         padding="same",
         groups=1,
         bias=True,
-        padding_mode="reflect",
+        padding_mode="constant",
         skip_transpose=False,
     ):
         super().__init__()
@@ -376,7 +376,7 @@ class Conv1d(nn.Module):
             bias=bias,
         )
 
-    def forward(self, x, masks=None):
+    def forward(self, x, mask=None):
         """Returns the output of the convolution.
 
         Arguments
@@ -417,7 +417,42 @@ class Conv1d(nn.Module):
         if not self.skip_transpose:
             wx = wx.transpose(1, -1)
 
-        return wx
+        if mask is not None:
+            mask = self.compute_mask(mask)
+            wx.masked_fill_(mask, 0.0)
+
+        return wx, mask
+
+    def compute_mask(self, mask):
+        if not self.skip_transpose:
+            mask = mask.transpose(1, -1)
+
+        # Unsqueeze
+        unsqueeze = mask.ndim == 2
+        if unsqueeze:
+            mask = mask.unsqueeze(1)
+
+        length = math.ceil(
+            (mask.size(-1) - (self.dilation * (self.kernel_size - 1)))
+            / self.stride
+        )
+        if self.padding == "same":
+            length = length + 1
+
+        # Subsample mask
+        mask = mask[..., :: self.stride]
+
+        # Make it to (batch, 1, time)
+        mask = mask[:, :1, -length:]
+
+        # Squeeze
+        if unsqueeze:
+            mask = mask.squeeze(1)
+
+        if not self.skip_transpose:
+            mask = mask.transpose(1, -1)
+
+        return mask
 
     def _manage_padding(
         self, x, kernel_size: int, dilation: int, stride: int,
@@ -610,30 +645,28 @@ class Conv2d(nn.Module):
         unsqueeze = mask.ndim == 3
         if unsqueeze:
             mask = mask.unsqueeze(1)
+
+        freq_length = math.ceil(
+            (mask.size(2) - (self.dilation[-2] * (self.kernel_size[-2] - 1)))
+            / self.stride[-2]
+        )
+        time_length = math.ceil(
+            (mask.size(3) - (self.dilation[-1] * (self.kernel_size[-1] - 1)))
+            / self.stride[-1]
+        )
         if self.padding == "same":
-            freq_end = mask.size(2)
-            time_end = mask.size(3)
-        elif self.padding == "valid":
-            freq_end = (
-                mask.size(2)
-                - self.dilation[-2] * (self.kernel_size[-2] - 1)
-                - 1
-            )
-            time_end = (
-                mask.size(3)
-                - self.dilation[-1] * (self.kernel_size[-1] - 1)
-                - 1
-            )
+            freq_length = freq_length + 1
+            time_length = freq_length + 1
 
         # Subsample mask
-        mask = mask[
-            :, :, : freq_end : self.stride[-1], : time_end : self.stride[-2]
-        ]
+        mask = mask[:, :, :: self.stride[-1], :: self.stride[-2]]
 
-        # Make it to (batch, 1, time, freq)
-        mask = mask[:, :1]
-        # Make it to (batch, freq, time, 1)
+        # Make it to (batch, 1, freq, time)
+        mask = mask[:, :1, -freq_length:, -time_length:]
+
+        # Make it to (batch, time, freq, 1)
         mask = mask.transpose(1, -1)
+
         # Squeeze
         if unsqueeze:
             mask = mask.squeeze(1)
@@ -750,7 +783,7 @@ class Conv2dWithConstraint(Conv2d):
         self.max_norm = max_norm
         super(Conv2dWithConstraint, self).__init__(*args, **kwargs)
 
-    def forward(self, x):
+    def forward(self, x, mask=None):
         """Returns the output of the convolution.
 
         Arguments
@@ -762,7 +795,7 @@ class Conv2dWithConstraint(Conv2d):
         self.conv.weight.data = torch.renorm(
             self.conv.weight.data, p=2, dim=0, maxnorm=self.max_norm
         )
-        return super(Conv2dWithConstraint, self).forward(x)
+        return super(Conv2dWithConstraint, self).forward(x, mask)
 
 
 class ConvTranspose1d(nn.Module):

tests/unittests/test_CNN.py
@@ -24,17 +24,17 @@ def test_Conv1d():
     convolve = Conv1d(
         out_channels=1, kernel_size=1, input_shape=input.shape, padding="same"
     )
-    output = convolve(input)
+    output, _ = convolve(input)
     assert input.shape == output.shape
 
     convolve.conv.weight = torch.nn.Parameter(
         torch.tensor([-1]).float().unsqueeze(0).unsqueeze(1)
     )
     convolve.conv.bias = torch.nn.Parameter(torch.tensor([0]).float())
-    output = convolve(input)
+    output, _ = convolve(input)
     assert torch.all(torch.eq(torch.ones(input.shape), output))
 
-    assert torch.jit.trace(convolve, input)
+    assert torch.jit.trace(convolve, [input, input.eq(0.0)])
 
 
 def test_Conv2d():
@@ -68,13 +68,45 @@ def test_Conv2d():
     assert torch.jit.trace(convolve, [input, input.eq(0.0)])
 
 
+def test_Conv1d_with_Mask():
+
+    from speechbrain.nnet.CNN import Conv1d
+
+    sample_length = 31
+    # Padded input with the shape (batch, time, fea)
+    input_padded = torch.rand([2, 65, 40])
+    # The first sample is padded
+    input_padded[0, sample_length:] = 0.0
+    input_nonpadded = input_padded[0, :sample_length].unsqueeze(0)
+    # Create mask
+    input_mask = input_padded.eq(0.0)
+
+    convolve = Conv1d(
+        out_channels=4,
+        input_shape=input_padded.shape,
+        kernel_size=3,
+        stride=2,
+        padding="valid",
+    )
+    convolve.eval()
+    output_padded, mask = convolve(input_padded, mask=input_mask)
+    output_nonpadded, _ = convolve(input_nonpadded)
+    output, mask = convolve(input_padded)
+
+    assert torch.allclose(
+        torch.sum(output_nonpadded[0]), torch.sum(output_padded[0])
+    )
+
+    assert torch.jit.trace(convolve, [input_padded, input_mask])
+
+
 def test_Conv2d_with_Mask():
 
     from speechbrain.nnet.CNN import Conv2d
 
     sample_length = 32
     # Padded input with the shape (batch, time, fea)
-    input_padded = torch.rand([2, 66, 40])
+    input_padded = torch.rand([2, 64, 40])
     # The first sample is padded
     input_padded[0, sample_length:] = 0.0
     input_nonpadded = input_padded[0, :sample_length].unsqueeze(0)
@@ -86,13 +118,18 @@ def test_Conv2d_with_Mask():
         input_shape=input_padded.shape,
         kernel_size=(3, 3),
         stride=(2, 2),
-        padding="same",
+        padding="valid",
     )
     convolve.eval()
-    output_padded, mask = convolve(input_padded, mask=input_padded.eq(0.0))
+    output_padded, mask = convolve(input_padded, mask=input_mask)
     output_nonpadded, _ = convolve(input_nonpadded)
 
-    print(torch.sum(output_nonpadded[0]), torch.sum(output_padded[0]))
-    assert torch.allclose(torch.sum(output_nonpadded[0]), torch.sum(output_padded[0]))
+    assert torch.allclose(
+        torch.sum(output_nonpadded[0]), torch.sum(output_padded[0])
+    )
 
-    assert torch.jit.trace(convolve, [input_padded, input_padded.eq(0.0)])
+    assert torch.jit.trace(convolve, [input_padded, input_mask])
+
+
+test_Conv2d_with_Mask()
+test_Conv1d_with_Mask()