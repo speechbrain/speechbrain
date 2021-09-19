1f8fd069e08059516000e4ec76f567f70e712d45
Sung-Lin <sunglin.yeh@ed.ac.uk>
2021-09-19 02:52:28 -0400

    Avoid the effect of padding on backward direction

--- speechbrain/nnet/RNN.py
+++ speechbrain/nnet/RNN.py
@@ -43,6 +43,8 @@ def pad_packed_sequence(inputs, max_length):
     ---------
     inputs : torch.nn.utils.rnn.PackedSequence
         An input set of sequences to convert to a tensor.
+    max_length: torch.Tensor
+        The max sequence length of the batch, must be specified to support nn.DataParallel.
     """
     outputs, lengths = torch.nn.utils.rnn.pad_packed_sequence(
         inputs, total_length=max_length, batch_first=True
@@ -128,7 +130,7 @@ class RNN(torch.nn.Module):
         if re_init:
             rnn_init(self.rnn)
 
-    def forward(self, x, hx=None, lengths=None):
+    def forward(self, x, hx=None, mask=None):
         """Returns the output of the vanilla RNN.
 
         Arguments
@@ -149,7 +151,11 @@ class RNN(torch.nn.Module):
         self.rnn.flatten_parameters()
 
         # Pack sequence for proper RNN handling of padding
-        if lengths is not None:
+        if mask is not None:
+            if mask.ndim == 4:
+                mask = mask[:, :, 0]
+            lengths = torch.sum(~mask, dim=1).squeeze(-1).cpu()
+            max_length = x.size(1)
             x = pack_padded_sequence(x, lengths)
 
         # Support custom initial state
@@ -159,10 +165,10 @@ class RNN(torch.nn.Module):
             output, hn = self.rnn(x)
 
         # Unpack the packed sequence
-        if lengths is not None:
-            output = pad_packed_sequence(output)
+        if mask is not None:
+            output = pad_packed_sequence(output, max_length)
 
-        return output, hn
+        return (output, hn), mask
 
 
 class LSTM(torch.nn.Module):
@@ -255,17 +261,16 @@ class LSTM(torch.nn.Module):
         if self.reshape:
             if x.ndim == 4:
                 x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3])
-            if mask is not None and mask.ndim == 4:
-                mask = mask[:, :, 0]
 
         # Flatten params for data parallel
         self.rnn.flatten_parameters()
 
         # Pack sequence for proper RNN handling of padding
         if mask is not None:
+            if mask.ndim == 4:
+                mask = mask[:, :, 0]
             lengths = torch.sum(~mask, dim=1).squeeze(-1).cpu()
             max_length = x.size(1)
-            #print(x.shape, lengths, mask.shape)
             x = pack_padded_sequence(x, lengths)
 
         # Support custom initial state
@@ -355,7 +360,7 @@ class GRU(torch.nn.Module):
         if re_init:
             rnn_init(self.rnn)
 
-    def forward(self, x, hx=None, lengths=None):
+    def forward(self, x, hx=None, mask=None):
         """Returns the output of the GRU.
 
         Arguments
@@ -376,7 +381,11 @@ class GRU(torch.nn.Module):
         self.rnn.flatten_parameters()
 
         # Pack sequence for proper RNN handling of padding
-        if lengths is not None:
+        if mask is not None:
+            if mask.ndim == 4:
+                mask = mask[:, :, 0]
+            lengths = torch.sum(~mask, dim=1).squeeze(-1).cpu()
+            max_length = x.size(1)
             x = pack_padded_sequence(x, lengths)
 
         # Support custom initial state
@@ -386,10 +395,10 @@ class GRU(torch.nn.Module):
             output, hn = self.rnn(x)
 
         # Unpack the packed sequence
-        if lengths is not None:
-            output = pad_packed_sequence(output)
+        if mask is not None:
+            output = pad_packed_sequence(output, max_length)
 
-        return output, hn
+        return (output, hn), mask
 
 
 class RNNCell(nn.Module):
@@ -1088,12 +1097,16 @@ class LiGRU(torch.nn.Module):
         if self.reshape:
             if x.ndim == 4:
                 x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3])
+        if mask is not None and mask.ndim == 4:
+            mask = mask[:, :, 0]
 
         # run ligru
-        print("rnn: mask", mask.shape, "x", x.shape)
         output, hh, mask = self._forward_ligru(x, hx=hx, mask=mask)
 
-        return output, hh, mask
+        if mask is not None:
+            output.masked_fill_(mask, 0.0)
+
+        return (output, hh), mask
 
     def _forward_ligru(self, x, hx: Optional[Tensor], mask=None):
         """Returns the output of the vanilla liGRU.
@@ -1224,8 +1237,7 @@ class LiGRU_Layer(torch.nn.Module):
             if mask is None:
                 x_flip = x.flip(1)
             else:
-                lengths = mask.mean(-2).unsqueeze(-1).sum(1)
-                print(lengths.shape, mask.shape, x.shape)
+                lengths = torch.sum(~mask, dim=1).squeeze(-1)
                 x_flip = reverse_padded_sequence(x, lengths, batch_first=True)
             x = torch.cat([x, x_flip], dim=0)
 
@@ -1248,7 +1260,10 @@ class LiGRU_Layer(torch.nn.Module):
 
         if self.bidirectional:
             h_f, h_b = h.chunk(2, dim=0)
-            h_b = h_b.flip(1)
+            if mask is None:
+                h_b = h_b.flip(1)
+            else:
+                h_b = reverse_padded_sequence(h_b, lengths, batch_first=True)
             h = torch.cat([h_f, h_b], dim=2)
 
         return h, mask
@@ -1335,268 +1350,6 @@ class LiGRU_Layer(torch.nn.Module):
                 ).data
 
 
-class QuasiRNNLayer(torch.nn.Module):
-    """Applies a single layer Quasi-Recurrent Neural Network (QRNN) to an
-    input sequence.
-
-    Arguments
-    ---------
-    input_size : int
-        The number of expected features in the input x.
-    hidden_size : int
-        The number of features in the hidden state h. If not specified,
-        the input size is used.
-    zoneout : float
-        Whether to apply zoneout (i.e. failing to update elements in the
-        hidden state) to the hidden state updates. Default: 0.
-    output_gate : bool
-        If True, performs QRNN-fo (applying an output gate to the output).
-        If False, performs QRNN-f. Default: True.
-
-    Example
-    -------
-    >>> import torch
-    >>> model = QuasiRNNLayer(60, 256, bidirectional=True)
-    >>> a = torch.rand([10, 120, 60])
-    >>> b = model(a)
-    >>> b[0].shape
-    torch.Size([10, 120, 512])
-    """
-
-    def __init__(
-        self,
-        input_size,
-        hidden_size,
-        bidirectional,
-        zoneout=0.0,
-        output_gate=True,
-    ):
-        super().__init__()
-
-        self.hidden_size = hidden_size
-        self.zoneout = zoneout
-        self.output_gate = output_gate
-        self.bidirectional = bidirectional
-
-        stacked_hidden = (
-            3 * self.hidden_size if self.output_gate else 2 * self.hidden_size
-        )
-        self.w = torch.nn.Linear(input_size, stacked_hidden, True)
-
-        self.z_gate = nn.Tanh()
-        self.f_gate = nn.Sigmoid()
-        if self.output_gate:
-            self.o_gate = nn.Sigmoid()
-
-    def forgetMult(self, f, x, hidden):
-        # type: (Tensor, Tensor, Optional[Tensor]) -> Tensor # noqa F821
-        """Returns the hidden states for each time step.
-
-        Arguments
-        ---------
-        wx : torch.Tensor
-            Linearly transformed input.
-        """
-        result = []
-        htm1 = hidden
-        hh = f * x
-
-        for i in range(hh.shape[0]):
-            h_t = hh[i, :, :]
-            ft = f[i, :, :]
-            if htm1 is not None:
-                h_t = h_t + (1 - ft) * htm1
-            result.append(h_t)
-            htm1 = h_t
-
-        return torch.stack(result)
-
-    def split_gate_inputs(self, y):
-        # type: (Tensor) -> Tuple[Tensor, Tensor, Optional[Tensor]] # noqa F821
-        if self.output_gate:
-            z, f, o = y.chunk(3, dim=-1)
-        else:
-            z, f = y.chunk(2, dim=-1)
-            o = None
-        return z, f, o
-
-    def forward(self, x, hidden=None):
-        # type: (Tensor, Optional[Tensor]) -> Tuple[Tensor, Tensor] # noqa F821
-        """Returns the output of the QRNN layer.
-
-        Arguments
-        ---------
-        x : torch.Tensor
-            Input to transform linearly.
-        """
-        if x.ndim == 4:
-            # if input is a 4d tensor (batch, time, channel1, channel2)
-            # reshape input to (batch, time, channel)
-            x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3])
-
-        # give a tensor of shape (time, batch, channel)
-        x = x.permute(1, 0, 2)
-        if self.bidirectional:
-            x_flipped = x.flip(0)
-            x = torch.cat([x, x_flipped], dim=1)
-
-        # note: this is equivalent to doing 1x1 convolution on the input
-        y = self.w(x)
-
-        z, f, o = self.split_gate_inputs(y)
-
-        z = self.z_gate(z)
-        f = self.f_gate(f)
-        if o is not None:
-            o = self.o_gate(o)
-
-        # If zoneout is specified, we perform dropout on the forget gates in F
-        # If an element of F is zero, that means the corresponding neuron
-        # keeps the old value
-        if self.zoneout:
-            if self.training:
-                mask = (
-                    torch.empty(f.shape)
-                    .bernoulli_(1 - self.zoneout)
-                    .to(f.get_device())
-                ).detach()
-                f = f * mask
-            else:
-                f = f * (1 - self.zoneout)
-
-        z = z.contiguous()
-        f = f.contiguous()
-
-        # Forget Mult
-        c = self.forgetMult(f, z, hidden)
-
-        # Apply output gate
-        if o is not None:
-            h = o * c
-        else:
-            h = c
-
-        # recover shape (batch, time, channel)
-        c = c.permute(1, 0, 2)
-        h = h.permute(1, 0, 2)
-
-        if self.bidirectional:
-            h_fwd, h_bwd = h.chunk(2, dim=0)
-            h_bwd = h_bwd.flip(1)
-            h = torch.cat([h_fwd, h_bwd], dim=2)
-
-            c_fwd, c_bwd = c.chunk(2, dim=0)
-            c_bwd = c_bwd.flip(1)
-            c = torch.cat([c_fwd, c_bwd], dim=2)
-
-        return h, c[-1, :, :]
-
-
-class QuasiRNN(nn.Module):
-    """This is a implementation for the Quasi-RNN.
-
-    https://arxiv.org/pdf/1611.01576.pdf
-
-    Part of the code is adapted from:
-    https://github.com/salesforce/pytorch-qrnn
-
-    Arguments
-    ---------
-    hidden_size : int
-        The number of features in the hidden state h. If not specified,
-        the input size is used.
-    input_shape : tuple
-        The shape of an example input. Alternatively, use ``input_size``.
-    input_size : int
-        The size of the input. Alternatively, use ``input_shape``.
-    num_layers : int
-        The number of QRNN layers to produce.
-    zoneout : bool
-        Whether to apply zoneout (i.e. failing to update elements in the
-        hidden state) to the hidden state updates. Default: 0.
-    output_gate : bool
-        If True, performs QRNN-fo (applying an output gate to the output).
-        If False, performs QRNN-f. Default: True.
-
-    Example
-    -------
-    >>> a = torch.rand([8, 120, 40])
-    >>> model = QuasiRNN(
-    ...     256, num_layers=4, input_shape=a.shape, bidirectional=True
-    ... )
-    >>> b, _ = model(a)
-    >>> b.shape
-    torch.Size([8, 120, 512])
-    """
-
-    def __init__(
-        self,
-        hidden_size,
-        input_shape=None,
-        input_size=None,
-        num_layers=1,
-        bias=True,
-        batch_first=False,
-        dropout=0,
-        bidirectional=False,
-        **kwargs,
-    ):
-        assert bias is True, "Removing underlying bias is not yet supported"
-        super().__init__()
-
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.bidirectional = bidirectional
-        self.dropout = dropout if dropout > 0 else None
-        self.kwargs = kwargs
-
-        if input_shape is None and input_size is None:
-            raise ValueError("Expected one of input_shape or input_size.")
-
-        # Computing the feature dimensionality
-        if input_size is None:
-            if len(input_shape) > 3:
-                self.reshape = True
-            input_size = torch.prod(torch.tensor(input_shape[2:]))
-
-        layers = []
-        for layer in range(self.num_layers):
-            layers.append(
-                QuasiRNNLayer(
-                    input_size
-                    if layer == 0
-                    else self.hidden_size * 2
-                    if self.bidirectional
-                    else self.hidden_size,
-                    self.hidden_size,
-                    self.bidirectional,
-                    **self.kwargs,
-                )
-            )
-        self.qrnn = torch.nn.ModuleList(layers)
-
-        if self.dropout:
-            self.dropout = torch.nn.Dropout(self.dropout)
-
-    def forward(self, x, hidden=None):
-
-        next_hidden = []
-
-        for i, layer in enumerate(self.qrnn):
-            x, h = layer(x, None if hidden is None else hidden[i])
-
-            next_hidden.append(h)
-
-            if self.dropout and i < len(self.qrnn) - 1:
-                x = self.dropout(x)
-
-        hidden = torch.cat(next_hidden, 0).view(
-            self.num_layers, *next_hidden[0].shape[-2:]
-        )
-
-        return x, hidden
-
-
 def rnn_init(module):
     """This function is used to initialize the RNN weight.
     Recurrent connection: orthogonal initialization.
@@ -1619,6 +1372,9 @@ def rnn_init(module):
 
 
 def reverse_padded_sequence(input, lengths, batch_first=False):
+    # This function is borrowed from:
+    # https://github.com/pytorch/pytorch/commit/1b65ed072aa6908ee1266bd1e21275aa0b742a3e
+
     # type: (Tensor, Tensor, bool) -> Tensor
     r"""Reverses sequences according to their lengths.
     ``input`` should have size ``T x B x *`` if ``batch_first`` is False, or
@@ -1654,17 +1410,19 @@ def reverse_padded_sequence(input, lengths, batch_first=False):
     max_length, batch_size, *trailing_dims = input.shape
 
     if len(lengths) != batch_size:
-        raise ValueError('lengths has batch size {} while input has batch '
-                         'size {}'.format(len(lengths), batch_size))
+        raise ValueError(
+            "lengths has batch size {} while input has batch "
+            "size {}".format(len(lengths), batch_size)
+        )
 
     # Compute the new (i.e. reversed) indices
     tiled_indices = torch.arange(max_length, device=lengths.device)
     tiled_indices = tiled_indices.view(
-            (max_length, 1) + (1,) * len(trailing_dims))
+        (max_length, 1) + (1,) * len(trailing_dims)
+    )
     tiled_indices = tiled_indices.expand_as(input)
 
-    tiled_lengths = lengths.view(
-            (1, batch_size) + (1,) * len(trailing_dims))
+    tiled_lengths = lengths.view((1, batch_size) + (1,) * len(trailing_dims))
     tiled_lengths = tiled_lengths.expand_as(input)
 
     new_tiled_indices = (tiled_lengths - tiled_indices - 1).clamp(min=0)
