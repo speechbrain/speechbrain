f6df5a74674d5a9312ea4de12a6013cce78a1c7e
Aku Rouhe <aku.rouhe@aalto.fi>
2021-11-03 18:24:23 +0200

    Add default checkpoint hooks for Torch Schedulers

--- speechbrain/utils/checkpoints.py
+++ speechbrain/utils/checkpoints.py
@@ -160,10 +160,14 @@ def torch_parameter_transfer(obj, path, device):
 DEFAULT_LOAD_HOOKS = {
     torch.nn.Module: torch_recovery,
     torch.optim.Optimizer: torch_recovery,
+    torch.optim.lr_scheduler._LRScheduler: torch_recovery,
+    torch.optim.lr_scheduler.ReduceLROnPlateau: torch_recovery,
 }
 DEFAULT_SAVE_HOOKS = {
     torch.nn.Module: torch_save,
     torch.optim.Optimizer: torch_save,
+    torch.optim.lr_scheduler._LRScheduler: torch_save,
+    torch.optim.lr_scheduler.ReduceLROnPlateau: torch_save,
 }
 DEFAULT_TRANSFER_HOOKS = {
     torch.nn.Module: torch_parameter_transfer,
