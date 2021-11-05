0feb943ee0086fe066cae80db33510f075743a41
Mingshuang Luo <37799481+luomingshuang@users.noreply.github.com>
2021-11-05 22:52:28 +0800

    Update download_lm.py

--- recipes/LibriSpeech/ASR/wfst/local/download_lm.py
+++ recipes/LibriSpeech/ASR/wfst/local/download_lm.py
@@ -40,53 +40,9 @@ import os
 import shutil
 from pathlib import Path
 
+from lhotse.utils import urlretrieve_progress
 from tqdm.auto import tqdm
 
-def tqdm_urlretrieve_hook(t):
-    """Wraps tqdm instance.
-    Don't forget to close() or __exit__()
-    the tqdm instance once you're done with it (easiest using `with` syntax).
-    Example
-    -------
-    >>> from urllib.request import urlretrieve
-    >>> with tqdm(...) as t:
-    ...     reporthook = tqdm_urlretrieve_hook(t)
-    ...     urlretrieve(..., reporthook=reporthook)
-    Source: https://github.com/tqdm/tqdm/blob/master/examples/tqdm_wget.py
-    """
-    last_b = [0]
-
-    def update_to(b=1, bsize=1, tsize=None):
-        """
-        b  : int, optional
-            Number of blocks transferred so far [default: 1].
-        bsize  : int, optional
-            Size of each block (in tqdm units) [default: 1].
-        tsize  : int, optional
-            Total size (in tqdm units). If [default: None] or -1,
-            remains unchanged.
-        """
-        if tsize not in (None, -1):
-            t.total = tsize
-        displayed = t.update((b - last_b[0]) * bsize)
-        last_b[0] = b
-        return displayed
-
-    return update_to
-
-def urlretrieve_progress(url, filename=None, data=None, desc=None):
-    """
-    Works exactly like urllib.request.urlretrieve, but attaches a tqdm hook to display
-    a progress bar of the download.
-    Use "desc" argument to display a user-readable string that informs what is being downloaded.
-    """
-    from urllib.request import urlretrieve
-    with tqdm(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc=desc) as t:
-        reporthook = tqdm_urlretrieve_hook(t)
-        return urlretrieve(url=url, 
-                           filename=filename, 
-                           reporthook=reporthook, 
-                           data=data)
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -133,6 +89,12 @@ if __name__ == "__main__":
         "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
     )
 
+    logging.basicConfig(format=formatter, level=logging.INFO)
+
+    args = get_args()
+    logging.info(f"out_dir: {args.out_dir}")
+
+    main(out_dir=args.out_dir)
     logging.basicConfig(format=formatter, level=logging.INFO)
 
     args = get_args()
