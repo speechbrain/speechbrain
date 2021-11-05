db7cdacaa3f16b75b2646672778f4984dd56151f
Abdel HEBA <wahab.heba@gmail.com>
2021-11-05 11:26:03 +0100

    add missing function

--- recipes/LibriSpeech/ASR/wfst/local/download_lm.py
+++ recipes/LibriSpeech/ASR/wfst/local/download_lm.py
@@ -42,6 +42,38 @@ from pathlib import Path
 
 from tqdm.auto import tqdm
 
+def tqdm_urlretrieve_hook(t):
+    """Wraps tqdm instance.
+    Don't forget to close() or __exit__()
+    the tqdm instance once you're done with it (easiest using `with` syntax).
+    Example
+    -------
+    >>> from urllib.request import urlretrieve
+    >>> with tqdm(...) as t:
+    ...     reporthook = tqdm_urlretrieve_hook(t)
+    ...     urlretrieve(..., reporthook=reporthook)
+    Source: https://github.com/tqdm/tqdm/blob/master/examples/tqdm_wget.py
+    """
+    last_b = [0]
+
+    def update_to(b=1, bsize=1, tsize=None):
+        """
+        b  : int, optional
+            Number of blocks transferred so far [default: 1].
+        bsize  : int, optional
+            Size of each block (in tqdm units) [default: 1].
+        tsize  : int, optional
+            Total size (in tqdm units). If [default: None] or -1,
+            remains unchanged.
+        """
+        if tsize not in (None, -1):
+            t.total = tsize
+        displayed = t.update((b - last_b[0]) * bsize)
+        last_b[0] = b
+        return displayed
+
+    return update_to
+
 def urlretrieve_progress(url, filename=None, data=None, desc=None):
     """
     Works exactly like urllib.request.urlretrieve, but attaches a tqdm hook to display
