b6c8a9eed24d7230108e835650ac6c6ce82f9462
Titouan <parcollet.titouan@gmail.com>
2021-11-12 21:10:07 +0100

    mask indices

--- speechbrain/lobes/models/huggingface_wav2vec.py
+++ speechbrain/lobes/models/huggingface_wav2vec.py
@@ -346,10 +346,14 @@ class HuggingFaceWav2Vec2Pretrain(nn.Module):
         sequence_length = self.model._get_feat_extract_output_lengths(
             raw_sequence_length
         )
-        mask_time_indices = _compute_mask_indices(
-            (batch_size, sequence_length),
-            mask_prob=self.mask_prob,
-            mask_length=self.mask_length,
+        mask_time_indices = torch.tensor(
+            _compute_mask_indices(
+                (batch_size, sequence_length),
+                mask_prob=self.mask_prob,
+                mask_length=self.mask_length,
+            ),
+            device=wav.device,
+            dtype=torch.bool,
         )
 
         return (
