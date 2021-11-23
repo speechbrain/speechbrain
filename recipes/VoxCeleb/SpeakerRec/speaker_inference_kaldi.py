b40cf9518601373e8cee59156ffb6c8a30908c0a
underdogliu <553473428@qq.com>
2021-11-23 13:50:21 +0200

    ckptdir is useless

--- recipes/VoxCeleb/SpeakerRec/speaker_inference_kaldi.py
+++ recipes/VoxCeleb/SpeakerRec/speaker_inference_kaldi.py
@@ -88,12 +88,10 @@ if __name__ == "__main__":
     # load directories
     parser = argparse.ArgumentParser()
     parser.add_argument("--datadir", type=str, required=True)
-    parser.add_argument("--ckptdir", type=str, required=True)
     parser.add_argument("--outdir", type=str, required=True)
 
     args = parser.parse_args()
     datadir = args.datadir
-    ckptdir = args.ckptdir
     outdir = args.outdir
 
     # Logger setup
