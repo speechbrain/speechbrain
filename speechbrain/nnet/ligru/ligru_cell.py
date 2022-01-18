f73b0b7f0f002f5db73b0fd75a0b161c12af0425
Adel Moumen <adelmoumen.pro@gmail;com>
2022-01-18 18:20:06 +0100

    add doc

--- speechbrain/nnet/ligru/ligru_cell.py
+++ speechbrain/nnet/ligru/ligru_cell.py
@@ -7,11 +7,10 @@ Authors
  * Adel Moumen 2022
 """
 
-from tokenize import String
 import torch
-from torch import Tensor
 import torch.autograd as autograd
 import torch.nn as nn 
+
 try:
     import cupy as cp
 except ImportError:
@@ -28,18 +27,22 @@ except ImportError:
     err_msg += "=============================\n"
     raise ImportError(err_msg)
 
-_preamble_relu = '''
+_preamble_gradient_relu = '''
 template <typename T> __device__ T gradient_activation_hcand(T x) { return (x > 0. ? 1. : 0.); }
 '''
 
-_preamble_leaky_relu = '''
+_preamble_gradient_leaky_relu = '''
 template <typename T> __device__ T gradient_activation_hcand(T x) { 
     T negative_slope = 1e-2;  
     return (x > 0. ? 1. : negative_slope); 
 }
 '''
 
-def transform_tensor_to_cupy(x: Tensor):
+_preamble_gradient_tanh = '''
+template <typename T> __device__ T gradient_activation_hcand(T x) { return 1 - x * x; }
+'''
+
+def transform_tensor_to_cupy(x):
     """Transform a PyTorch Tensor located on device="cuda" to a CuPy array. 
     
     Argument
@@ -50,15 +53,17 @@ def transform_tensor_to_cupy(x: Tensor):
 
 
 class _ligru_cell_cupy(autograd.Function):
-    """
-    This class redefine the backpropagation of a LiGRU cell and implement the backward using CuPy. 
+    """This class redefine the backpropagation of a LiGRU cell and implement the backward using CuPy. 
     By doing so, we speed up the training by a factor of 2x in comparison to the original implementation. 
     """
 
     @staticmethod
-    def forward(ctx, wx: Tensor, u: Tensor, ht: Tensor, drop_mask: Tensor, act: nn.Module) -> Tensor:
-        """Returns the hidden states for each time step and save the intermediates results for the backward.
+    def forward(ctx, wx, u, ht, drop_mask, act):
+        """Compute the hidden states over each time step and save the intermediates results for the backward.
 
+        The utilisation of Tanh in the LiGRU cell is not recommended because it increase the instability and can leads to 
+        nan values in the gradients.
+        
         Arguments
         ---------
         wx : torch.Tensor
@@ -71,9 +76,10 @@ class _ligru_cell_cupy(autograd.Function):
             Dropout mask. 
         act : nn.Module
             Activation Function. 
-            Only two possibilities for now:
+            Only three possibilities for now:
                 1) ReLU,
-                2) Leaky ReLU with slope_parameter of 1e-2 (default parameter of PyTorch).
+                2) Leaky ReLU with slope_parameter of 1e-2 (default parameter of PyTorch),
+                3) Tanh. 
         """
         
         # save values for backward
@@ -83,6 +89,7 @@ class _ligru_cell_cupy(autograd.Function):
         save_at        = []
         h_init         = ht
 
+        # iterate over each timesteps
         for k in range(wx.shape[1]):
             
             gates  = wx[:, k] + ht @ u.T
@@ -96,6 +103,7 @@ class _ligru_cell_cupy(autograd.Function):
             update_gate.append(zt)
             save_at.append(at)
         
+        # stacks values
         ht    = torch.stack(hiddens, dim=1) 
         zt    = torch.stack(update_gate, dim=1)
         at    = torch.stack(save_at, dim=1)
@@ -106,29 +114,42 @@ class _ligru_cell_cupy(autograd.Function):
 
         return ht
 
-
-
     @staticmethod
     def backward(ctx, grad_out):
-        '''
-        '''
+        """Run the backward phase of the forward call defined above. This implementation
+        is dependant of CuPy. 
+
+        Arguments
+        ---------
+        ctx : context variable
+        grad_out : torch.Tensor
+        """
         h_init, u, wx, zt, at, ht, hcand, drop_mask,  = ctx.saved_tensors
 
         activation_function = ctx.activation_function
 
-        # we need to reshape our h_init tensor  
+        # we need to reshape our h_init tensor if h_init doesnt match the shape.
         if h_init.shape[0] != ht[:, 0].shape[0]:
             h_init = h_init.repeat(ht[:, 0].shape[0], 1)
             
+        # Tensor -> Cuda CuPy Array
         h_init, u, wx, zt, at, ht, hcand, drop_mask, grad_out, = (transform_tensor_to_cupy(x) for x in [h_init, u, wx, zt, at, ht, hcand, drop_mask, grad_out])
 
+        # allocate memory space 
         dwx     = cp.empty_like(wx)
         du      = cp.zeros_like(u)
         dh      = cp.empty_like(h_init)
         dh_prev = cp.zeros_like(h_init)
         idx     = dwx.shape[2] // 2
 
-        _preamble = _preamble_leaky_relu if activation_function.__class__.__name__  == "LeakyReLU" else _preamble_relu
+        # find the activation function and load the appropriate preamble 
+        activation_function_name = activation_function.__class__.__name__
+        if activation_function_name  == "LeakyReLU":
+            preamble_gradient = _preamble_gradient_leaky_relu    
+        elif activation_function_name  == "tanh":
+            preamble_gradient = _preamble_gradient_tanh    
+        else:
+            preamble_gradient = _preamble_gradient_relu
 
         _ligru_cell_backward_kernel = cp.ElementwiseKernel(
             'T grad_out, T dh_prev, T zt, T at, T drop_mask, T ht, T hcand',
@@ -136,13 +157,15 @@ class _ligru_cell_cupy(autograd.Function):
             '''
             dh = grad_out + dh_prev;
             T temp = (1 - zt) * dh;
-            dat = (at > 0. ? 1. : 0.) * drop_mask * temp;
+            dat = gradient_activation_hcand(at) * drop_mask * temp;
             dzt = (ht - hcand) * temp * zt;
             grad_dh_prev = dh * zt; 
             ''',
             '_ligru_cell_backward_kernel', 
-            preamble=_preamble)
+            preamble=preamble_gradient
+        )
 
+        # iterate over each timesteps in reversed
         for t in reversed(range(dwx.shape[1])):
 
             ht_ = h_init if t - 1 < 0 else ht[:, t - 1]
@@ -162,19 +185,6 @@ class _ligru_cell_cupy(autograd.Function):
             )
 
             dh_prev = dh_prev + dwx[:, t].dot(u)
-            du     += dwx[:, t].T.dot(ht_ )   
+            du     += dwx[:, t].T.dot(ht_)   
 
         return torch.from_dlpack(dwx), torch.from_dlpack(du), torch.from_dlpack(dh), None, None
-
-if __name__ == "__main__":
-    torch.manual_seed(22)
-    w = torch.randn(16, 7, 256, device="cuda", requires_grad=True)
-    u = torch.randn(256, 128, device="cuda", requires_grad=True)
-    ht = torch.randn(16, 128, device="cuda", requires_grad=True)
-    drop_mask = torch.randn(16, 128, device="cuda")
-    act = nn.ReLU()
-
-    out = _ligru_cell_cupy.apply(w, u, ht, drop_mask, act)
-    out.sum().backward()
-
-    print(w.grad)
