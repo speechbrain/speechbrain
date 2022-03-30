aecf7602008af1afcd29f15ee8c035b4a2025148
Titouan <parcollet.titouan@gmail.com>
2022-03-30 17:13:37 +0200

    Fix the logger (DDP compliance) + resumability

--- speechbrain/utils/train_logger.py
+++ speechbrain/utils/train_logger.py
@@ -2,9 +2,14 @@
 
 Authors
  * Peter Plantinga 2020
+ * Titouan Parcollet 2021
 """
+import os
 import logging
+import torch
 import ruamel.yaml
+from speechbrain.utils.distributed import run_on_main
+from speechbrain.utils import checkpoints
 
 logger = logging.getLogger(__name__)
 
@@ -99,8 +104,12 @@ class FileTrainLogger(TrainLogger):
             logger.info(string_summary)
 
 
+@checkpoints.register_checkpoint_hooks
 class TensorboardLogger(TrainLogger):
     """Logs training information in the format required by Tensorboard.
+    If you wish your loader to be resumable i.e., the logging does not restart
+    from the global step 0 every time, you must create a SpeechBrain checkpointer
+    that will automatically save the corresponding global steps.
 
     Arguments
     ---------
@@ -114,15 +123,34 @@ class TensorboardLogger(TrainLogger):
 
     def __init__(self, save_dir):
         self.save_dir = save_dir
+        self.writer = None
+        self.global_step = {"train": {}, "valid": {}, "test": {}, "meta": 0}
+
+    def prepare_tensorboard_logger(self, purge_step=None):
+        """Used to create the dir of the tensorboard logs compliant with DDP
+        Indeed, the init function is call before DDP is initialised. Hence,
+        the SummaryWriter would try to create the save_folder with all processes,
+        leading to a R/W error. The SummaryWriter must be initialised after DDP.
+
+        Arguments
+        ---------
+        purge_step : int
+            When logging crashes at step T + X and restart at step T,
+            any events whose global_step larger or equal to T will be purged and
+            hidden from TensorBoard.
+        """
+        if not os.path.isdir(self.save_dir):
+            run_on_main(os.makedirs, args=[self.save_dir])
 
         # Raises ImportError if TensorBoard is not installed
         from torch.utils.tensorboard import SummaryWriter
 
-        self.writer = SummaryWriter(self.save_dir)
-        self.global_step = {"train": {}, "valid": {}, "test": {}, "meta": 0}
+        self.purge_step = purge_step
+        self.writer = SummaryWriter(self.save_dir, purge_step=purge_step)
 
     def log_stats(
         self,
+        global_step,
         stats_meta,
         train_stats=None,
         valid_stats=None,
@@ -130,6 +158,7 @@ class TensorboardLogger(TrainLogger):
         verbose=False,
     ):
         """See TrainLogger.log_stats()"""
+
         self.global_step["meta"] += 1
         for name, value in stats_meta.items():
             self.writer.add_scalar(name, value, self.global_step["meta"])
@@ -144,6 +173,9 @@ class TensorboardLogger(TrainLogger):
             for stat, value_list in stats.items():
                 if stat not in self.global_step[dataset]:
                     self.global_step[dataset][stat] = 0
+                elif self.global_step[dataset][stat] > self.purge_step:
+                    self.global_step[dataset][stat] = self.purge_step
+
                 tag = f"{stat}/{dataset}"
 
                 # Both single value (per Epoch) and list (Per batch) logging is supported
@@ -157,6 +189,33 @@ class TensorboardLogger(TrainLogger):
                     new_global_step = self.global_step[dataset][stat] + 1
                     self.writer.add_scalar(tag, value, new_global_step)
                     self.global_step[dataset][stat] = new_global_step
+
+    @checkpoints.mark_as_saver
+    def save(self, path):
+        data = {"global_step": self.global_step}
+        torch.save(data, path)
+
+    @checkpoints.mark_as_loader
+    def load(self, path, end_of_epoch=False, device=None):
+        del end_of_epoch  # Unused in this class
+        del device
+        data = torch.load(path)
+        self.global_step = data["global_step"]
+
+        # Remove all the logged event up until purge step
+        # This is usefull in case of resuming (we restart the log from the last
+        # epoch and not the last log that might be newer than the epoch)
+        for dict in self.global_step:
+            if dict != "meta":
+                for subdict in self.global_step[dict]:
+                    if self.global_step[dict][subdict] > self.purge_step:
+                        self.global_step[dict][subdict] = self.purge_step
+            else:
+                if self.global_step["meta"] > self.purge_step:
+                    self.global_step["meta"] = self.purge_step
+
+        if self.global_step["meta"] > self.purge_step:
+            self.global_step["meta"] = self.purge_step
 
 
 class WandBLogger(TrainLogger):
