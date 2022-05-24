cce57327ca874bff0017bc31209142a17c99e41e
Gabriel Oexle <gabriel@assemblyai.com>
2022-05-25 00:06:43 +0200

    toch.equal

--- tests/unittests/test_interfaces.py
+++ tests/unittests/test_interfaces.py
@@ -1,3 +1,5 @@
+import torch
+
 def test_load_audio(tmpdir):
     from speechbrain.pretrained.interfaces import Pretrained
     verification = Pretrained.from_hparams(
@@ -5,4 +7,4 @@ def test_load_audio(tmpdir):
         savedir = tmpdir.mkdir("savedir") )
     audio1 = verification.load_audio("samples/voxceleb_samples/wav/id10002/xTV-jFAUKcw/00001.wav")
     audio2 = verification.load_audio("samples/voxceleb_samples/wav/id10001/1zcIwhmdeo4/00001.wav")
-    assert not (audio1 == audio2).all()
+    assert not torch.equal(audio1, audio2)
