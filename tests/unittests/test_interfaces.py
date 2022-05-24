b8991b0d18139f29c82d6a7b53c1fd714e63824f
Gabriel Oexle <gabriel@assemblyai.com>
2022-05-25 00:10:01 +0200

    black

--- tests/unittests/test_interfaces.py
+++ tests/unittests/test_interfaces.py
@@ -1,10 +1,17 @@
 import torch
 
+
 def test_load_audio(tmpdir):
     from speechbrain.pretrained.interfaces import Pretrained
+
     verification = Pretrained.from_hparams(
-        source = "speechbrain/spkrec-ecapa-voxceleb",
-        savedir = tmpdir.mkdir("savedir") )
-    audio1 = verification.load_audio("samples/voxceleb_samples/wav/id10002/xTV-jFAUKcw/00001.wav")
-    audio2 = verification.load_audio("samples/voxceleb_samples/wav/id10001/1zcIwhmdeo4/00001.wav")
+        source="speechbrain/spkrec-ecapa-voxceleb",
+        savedir=tmpdir.mkdir("savedir"),
+    )
+    audio1 = verification.load_audio(
+        "samples/voxceleb_samples/wav/id10002/xTV-jFAUKcw/00001.wav"
+    )
+    audio2 = verification.load_audio(
+        "samples/voxceleb_samples/wav/id10001/1zcIwhmdeo4/00001.wav"
+    )
     assert not torch.equal(audio1, audio2)
