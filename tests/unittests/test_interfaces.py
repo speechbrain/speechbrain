b75370f34d50bddee0811ab903fdb666505e4475
Gabriel Oexle <gabriel@assemblyai.com>
2022-05-25 10:21:02 +0200

    update unit test

--- tests/unittests/test_interfaces.py
+++ tests/unittests/test_interfaces.py
@@ -1,17 +1,27 @@
+from pathlib import Path
+
 import torch
+from hyperpyyaml import load_hyperpyyaml
 
 
 def test_load_audio(tmpdir):
     from speechbrain.pretrained.interfaces import Pretrained
 
-    verification = Pretrained.from_hparams(
-        source="speechbrain/spkrec-ecapa-voxceleb",
-        savedir=tmpdir.mkdir("savedir"),
+    hparams_file = (
+        Path("tests")
+        / "integration"
+        / "neural_networks"
+        / "ASR_seq2seq"
+        / "hyperparams.yaml"
     )
-    audio1 = verification.load_audio(
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin)
+
+    pretrained = Pretrained(hparams["modules"], hparams)
+    audio1 = pretrained.load_audio(
         "samples/voxceleb_samples/wav/id10002/xTV-jFAUKcw/00001.wav"
     )
-    audio2 = verification.load_audio(
+    audio2 = pretrained.load_audio(
         "samples/voxceleb_samples/wav/id10001/1zcIwhmdeo4/00001.wav"
     )
     assert not torch.equal(audio1, audio2)
