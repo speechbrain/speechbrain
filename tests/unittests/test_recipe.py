2d32c534496cfa4845a29cb36d9792c401382309
Mirco Ravanelli <mirco.ravanelli@gmail.com>
2022-06-10 18:35:30 -0400

    check if all files in the csv file exist

--- tests/unittests/test_recipe.py
+++ tests/unittests/test_recipe.py
@@ -3,7 +3,7 @@
 Authors
  * Mirco Ravanelli 2022
 """
-
+import os
 from speechbrain.utils.data_utils import get_all_files, get_list_from_csv
 
 
@@ -47,3 +47,30 @@ def test_recipe_list(
         )
 
     assert len(diff_lst) == 0
+
+
+def test_recipe_files(
+    recipe_csvfile="tests/recipes.csv",
+    fields=["Script_file", "Hparam_file", "Data_prep_file", "Readme_file"],
+):
+    """This test checks if the files listed in the recipe csv file exist.
+
+    Arguments
+    ---------.
+    recipe_csvfile: path
+        Path of the csv recipe file.
+    fields: list
+        Fields of the csv recipe file to check.
+    """
+    check = True
+    for field in fields:
+        lst = get_list_from_csv(recipe_csvfile, field=field)
+        lst = filter(None, lst)
+        for file in lst:
+            if not (os.path.exists(file.strip())):
+                print(
+                    "\tWARNING: The file %s listed in %s does not exist!"
+                    % (file, recipe_csvfile)
+                )
+                check = False
+    assert check
