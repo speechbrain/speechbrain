9bac815b22b353416701731f3a6c7f5c48146de4
prometheus <46483626+ddavidebb@users.noreply.github.com>
2022-06-16 10:46:00 +0200

    Fixed issue in leave-one-session-out loop

--- recipes/MOABB/run_experiments_BNCI2014001.sh
+++ recipes/MOABB/run_experiments_BNCI2014001.sh
@@ -16,7 +16,8 @@ for target_session_idx in {0..1}; do
 for target_subject_idx in {0..8}; do
    ((i=i%nparallel)); ((i++==0)) && wait
         python3 train.py 'EEGNet_BNCI2014001.yaml' --data_folder $data_folder \
-        --target_subject_idx $target_session_idx --data_iterator_name 'leave-one-session-out'
+        --target_subject_idx $target_subject_idx --target_session_idx $target_session_idx \
+        --data_iterator_name 'leave-one-session-out'
 done
 )
 wait
