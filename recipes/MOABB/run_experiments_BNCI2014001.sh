6be844dabbc06db0c9e793dad6aceb16cc1aadb2
prometheus <46483626+ddavidebb@users.noreply.github.com>
2022-06-16 21:02:01 +0200

    Fixed run_experiment issue

--- recipes/MOABB/run_experiments_BNCI2014001.sh
+++ recipes/MOABB/run_experiments_BNCI2014001.sh
@@ -5,22 +5,26 @@ data_folder='/path/to/MOABB_datasets'
 (
 for target_subject_idx in {0..8}; do
    ((i=i%nparallel)); ((i++==0)) && wait
-        python3 train.py 'EEGNet_BNCI2014001.yaml' --data_folder $data_folder \
-        --target_subject_idx $target_subject_idx --data_iterator_name 'leave-one-subject-out'
+        python3 train.py 'hparams/EEGNet_BNCI2014001.yaml' --data_folder $data_folder \
+        --target_subject_idx $target_subject_idx --target_session_idx 0 --data_iterator_name 'leave-one-subject-out'
 done
 )
 # LEAVE-ONE-SESSION-OUT
-for target_session_idx in {0..1}; do
+(
+for target_subject_idx in {0..8}; do
+   ((i=i%nparallel)); ((i++==0)) && wait
+        python3 train.py 'hparams/EEGNet_BNCI2014001.yaml' --data_folder $data_folder \
+        --target_subject_idx $target_subject_idx --target_session_idx 0 \
+        --data_iterator_name 'leave-one-session-out'
+done
+)
 
 (
 for target_subject_idx in {0..8}; do
    ((i=i%nparallel)); ((i++==0)) && wait
-        python3 train.py 'EEGNet_BNCI2014001.yaml' --data_folder $data_folder \
-        --target_subject_idx $target_subject_idx --target_session_idx $target_session_idx \
+        python3 train.py 'hparams/EEGNet_BNCI2014001.yaml' --data_folder $data_folder \
+        --target_subject_idx $target_subject_idx --target_session_idx 1 \
         --data_iterator_name 'leave-one-session-out'
 done
 )
-wait
-
-done
 
