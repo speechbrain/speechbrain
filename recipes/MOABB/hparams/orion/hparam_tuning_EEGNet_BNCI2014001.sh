a224c5efe2f01845bf0b37779a1ddcf3e0ab046d
prometheus <46483626+ddavidebb@users.noreply.github.com>
2022-09-29 00:48:50 +0200

    Restored data augmentation and updated EEGNet_BNCI2014001 yaml and sh scripts

--- recipes/MOABB/hparams/orion/hparam_tuning_EEGNet_BNCI2014001.sh
+++ recipes/MOABB/hparams/orion/hparam_tuning_EEGNet_BNCI2014001.sh
@@ -2,10 +2,10 @@
 
 HPOPT_EXPERIMENT_NAME=$1 #random_seach_EEGNet_BNCI2014001 # name of the orion experiment
 OUTPUT_FOLDER=$2 # results/EEGNet_BNCI2014001_seed_variability_moabb
-HPOPT_CONFIG_FILE=hparams/orion/hparams_random_search.yaml  #hparam file for orion
+HPOPT_CONFIG_FILE=hparams/orion/hparams_tpe.yaml  #hparam file for orion
 
 export _MNE_FAKE_HOME_DIR='/network/scratch/r/ravanelm/' # change with your own folder (needed for mne)
-export ORION_DB_ADDRESS=/network/scratch/r/ravanelm/random_seach_EEGNet_BNCI2014001.pkl # This is the database where orion will save the results
+export ORION_DB_ADDRESS=/network/scratch/r/ravanelm/tpe_EEGNet_BNCI2014001.pkl # This is the database where orion will save the results
 export ORION_DB_TYPE=PickledDB
 
 # Running orion
@@ -16,35 +16,29 @@ orion hunt -n $HPOPT_EXPERIMENT_NAME -c $HPOPT_CONFIG_FILE --exp-max-trials=250 
 	/localscratch/eeg_data $OUTPUT_FOLDER 1 1 'random_seed' 1 acc valid_metrics.pkl false true \
   --number_of_epochs~"uniform(100, 1000,discrete=True)" \
 	--avg_models~"uniform(1, 20,discrete=True)" \
+	--step_size~"uniform(50,150,discrete=True)" \
+	--batch_size~"uniform(32,128,discrete=True)" \
+	--lr~"loguniform(1e-5, 1e-1)" \
+	--n_steps_channel_selection~"uniform(1, 3,discrete=True)" \
 	--tmax~"uniform(1.0, 4.0)" \
-	--fmax~"choices([20,30,40,50])" \
-	--lr~"choices([0.01,0.005, 0.001,0.0005, 0.0001])" \
-	--test_with~"choices(['last','best'])" \
-	--batch_size~"choices([32,64,128])" \
-	--step_size~"choices([50,100,150])" \
-	--label_smoothing~"uniform(0.00, 0.075)" \
+	--fmax~"uniform(10, 50,discrete=True)" \
 	--cnn_temporal_kernels~"uniform(4, 64,discrete=True)" \
 	--cnn_temporal_kernelsize~"uniform(8, 64,discrete=True)" \
-	--cnn_spatial_depth_multiplier~"choices([1,2,3,4])" \
-	--cnn_spatial_pool~"uniform(2, 8,discrete=True)" \
-	--cnn_spatial_max_norm~"uniform(0.1, 2)" \
-	--cnn_septemporal_depth_multiplier~"choices([1,2,3,4])" \
+	--cnn_spatial_depth_multiplier~"uniform(1, 4,discrete=True)" \
+	--cnn_spatial_pool~"uniform(1, 4,discrete=True)" \
+	--cnn_septemporal_depth_multiplier~"uniform(1, 4,discrete=True)" \
 	--cnn_septemporal_kernelsize~"uniform(4, 32,discrete=True)" \
-	--cnn_septemporal_pool~"uniform(2, 8,discrete=True)" \
-	--cnn_pool_type~"choices(['avg','max'])" \
-	--dense_max_norm~"uniform(0.1, 2)" \
+	--cnn_septemporal_pool~"uniform(1, 8,discrete=True)" \
 	--dropout~"uniform(0.0, 0.50)" \
-	--activation_type~"choices(['elu', 'relu', 'leaky_relu'])" \
-	--snr_pink_low~"uniform(-5.0, 5.0)" \
+  --dims_to_normalize~"uniform(1, 2,discrete=True)" \
+	--snr_pink_low~"uniform(0.0, 15.0)" \
 	--snr_pink_high~"uniform(15.0, 30.0)" \
-  --snr_white_low~"uniform(5.0, 15.0)" \
+  --snr_white_low~"uniform(0.0, 15.0)" \
   --snr_white_high~"uniform(15.0, 30.0)" \
   --snr_muscular_low~"uniform(0.0, 15.0)" \
   --snr_muscular_high~"uniform(15.0, 30.0)" \
-	--repeat_augment~"choices([1,2,3])" \
-  --n_augmentations~"uniform(1, 10,discrete=True)" \
-  --dims_to_normalize~"choices([1, 2])"
-
+	--repeat_augment~"uniform(1, 3,discrete=True)" \
+  --n_augmentations~"uniform(1, 10,discrete=True)"
 
 
 
--- recipes/MOABB/hparams/orion/hparam_tuning_EEGNet_EPFLP300.sh
+++ recipes/MOABB/hparams/orion/hparam_tuning_EEGNet_EPFLP300.sh
@@ -2,10 +2,10 @@
 
 HPOPT_EXPERIMENT_NAME=$1 #random_seach_EEGNet_EPFLP300 # name of the orion experiment
 OUTPUT_FOLDER=$2 # results/MOABB/EEGNet_EPFLP300_seed_variability
-HPOPT_CONFIG_FILE=hparams/orion/hparams_random_search.yaml  #hparam file for orion
+HPOPT_CONFIG_FILE=hparams/orion/hparams_tpe.yaml  #hparam file for orion
 
-#export _MNE_FAKE_HOME_DIR='' # change with your own folder (needed for mne)
-export ORION_DB_ADDRESS=/home/dborra/Documents/codes/speechbrain-2/recipes/MOABB/results/random_search_EEGNet_EPFLP300.pkl # This is the database where orion will save the results
+export _MNE_FAKE_HOME_DIR='/network/scratch/r/ravanelm/' # change with your own folder (needed for Mirco)
+export ORION_DB_ADDRESS=/network/scratch/r/ravanelm/tpe_EEGNet_EPFLP300.pkl # This is the database where orion will save the results
 export ORION_DB_TYPE=PickledDB
 
 # Running orion
@@ -13,10 +13,11 @@ cd ../..
 #'random_seed'
 orion hunt -n $HPOPT_EXPERIMENT_NAME -c $HPOPT_CONFIG_FILE --exp-max-trials=250  \
 	./run_experiments_seed_variability.sh hparams/EEGNet_EPFLP300_hparam_search.yaml \
-	/home/dborra/Documents/data $OUTPUT_FOLDER 1 1 'random_seed' 1 f1 valid_metrics.pkl false true \
+	/localscratch/eeg_data $OUTPUT_FOLDER 1 1 'random_seed' 1 f1 valid_metrics.pkl false true \
   --number_of_epochs~"uniform(100, 1000,discrete=True)" \
 	--avg_models~"uniform(1, 20,discrete=True)" \
-	--step_size~"uniform(50,100,discrete=True)" \
+	--step_size~"uniform(50,150,discrete=True)" \
+	--batch_size~"uniform(32,128,discrete=True)" \
 	--lr~"loguniform(1e-5, 1e-1)" \
 	--n_steps_channel_selection~"uniform(1, 3,discrete=True)" \
 	--fmax~"uniform(10, 50,discrete=True)" \
@@ -28,4 +29,12 @@ orion hunt -n $HPOPT_EXPERIMENT_NAME -c $HPOPT_CONFIG_FILE --exp-max-trials=250 
 	--cnn_septemporal_kernelsize~"uniform(4, 32,discrete=True)" \
 	--cnn_septemporal_pool~"uniform(1, 8,discrete=True)" \
 	--dropout~"uniform(0.0, 0.50)" \
-  --dims_to_normalize~"uniform(1, 2,discrete=True)"
+  --dims_to_normalize~"uniform(1, 2,discrete=True)" \
+	--snr_pink_low~"uniform(0.0, 15.0)" \
+	--snr_pink_high~"uniform(15.0, 30.0)" \
+  --snr_white_low~"uniform(0.0, 15.0)" \
+  --snr_white_high~"uniform(15.0, 30.0)" \
+  --snr_muscular_low~"uniform(0.0, 15.0)" \
+  --snr_muscular_high~"uniform(15.0, 30.0)" \
+	--repeat_augment~"uniform(1, 3,discrete=True)" \
+  --n_augmentations~"uniform(1, 10,discrete=True)"
