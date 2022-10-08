95111a85d2d56076633cbd2113eee7f52ab6782c
flexthink <flexthink@users.noreply.github.com>
2022-10-08 18:34:53 -0400

    Diffusion: Implement stats, metrics and Tensorboard

--- recipes/AudioMNIST/diffusion/train.py
+++ recipes/AudioMNIST/diffusion/train.py
@@ -13,6 +13,7 @@ Authors
  * Artem Ploujnikov 2022
 """
 import datasets
+import logging
 import math
 import sys
 import torch
@@ -20,9 +21,12 @@ import speechbrain as sb
 import os
 from hyperpyyaml import load_hyperpyyaml
 from torchaudio import functional as AF
-from speechbrain.dataio.dataio import write_audio
+from speechbrain.dataio.dataio import length_to_mask, write_audio
 from speechbrain.utils import data_utils
 from speechbrain.utils.train_logger import plot_spectrogram
+from speechbrain.utils.data_utils import unsqueeze_as
+
+logger = logging.getLogger(__name__)
 
 
 # Brain class for speech enhancement training
@@ -50,7 +54,7 @@ class DiffusionBrain(sb.Brain):
         batch = batch.to(self.device)
 
         # Compute features, embeddings, and predictions
-        feats, lens = self.prepare_features(batch.sig, stage)
+        feats, lens = self.prepare_features(batch, stage)
 
         pred, noise, noisy_sample = self.modules.diffusion.train_sample(feats, lens=lens)
 
@@ -58,21 +62,53 @@ class DiffusionBrain(sb.Brain):
 
     def fit_batch(self, batch):
         """Train the parameters given a single batch in input"""
-        result = super().fit_batch(batch)
+        loss = super().fit_batch(batch)
         self.hparams.lr_annealing(self.optimizer)
-        return result
+        if (
+            self.hparams.enable_train_metrics
+            and self.hparams.use_tensorboard
+            and (
+                self.step == 1
+                or
+                self.step % self.hparams.train_log_interval == 0
+            )
+        ):
+            self.log_batch()
+        return loss
 
-    def prepare_features(self, wavs, stage):
+    def log_batch(self):
+        loss_stats = self.loss_metric.summarize()
+        data_mean_stats = self.data_mean_metric.summarize()
+        data_std_stats = self.data_std_metric.summarize()
+        data_min_stats = self.data_min_metric.summarize()
+        data_max_stats = self.data_max_metric.summarize()
+        stats = {
+            "loss": loss_stats["average"],
+            "data_mean": data_mean_stats["average"],
+            "data_mean_min": data_mean_stats["min_score"],
+            "data_mean_max": data_mean_stats["max_score"],
+            "data_std": data_std_stats["average"],
+            "data_std_min": data_std_stats["min_score"],
+            "data_std_max": data_std_stats["max_score"],
+            "data_min": data_min_stats["min_score"],
+            "data_max": data_max_stats["max_score"],
+        }
+        self.hparams.tensorboard_train_logger.log_stats(
+            stats_meta={"step": self.step},
+            train_stats=stats
+        )
+
+    def prepare_features(self, batch, stage):
         """Prepare the features for computation, including augmentation.
 
         Arguments
         ---------
-        wavs : tuple
-            Input signals (tensor) and their relative lengths (tensor).
+        batch: PaddedData
+            An input batch
         stage : sb.Stage
             The current stage of training.
         """
-        wavs, lens = wavs
+        wavs, lens = batch.sig
 
         # Compute features
         feats = self.modules.compute_features(wavs)
@@ -102,27 +138,17 @@ class DiffusionBrain(sb.Brain):
         )
 
         # Min Level Norm
-        feats = self.modules.min_level_norm(feats)
+        feats_raw = self.modules.min_level_norm(feats)
 
         # Global Norm
-        feats = self.modules.global_norm(feats, lens)
+        feats = self.modules.global_norm(feats_raw, lens)
 
-        # Add noise
-        #        noise = torch.randn_like(feats)
-
-        # Match padding on the noise
-        #        max_len = feats.size(2)
-        #        mask = length_to_mask(lens * max_len, max_len).bool()
-        #        mask = mask.unsqueeze(1).unsqueeze(-1)
-        #        mask = mask.expand(*feats.shape)
-        #        noise[:, :, :, self.hparams.spec_n_mels :] = 0.0
-        #        noise[~mask] = 0.0
-
-        #        feats_noisy = self.hparams.noise_scheduler.add_noise(
-        #            feats, noise, timesteps
-        #        )
-        #        feats_noisy[:, :, :, self.hparams.spec_n_mels :] = 0.0
-        #        feats_noisy[~mask] = -1.0
+        # Compute metrics
+        if self.hparams.enable_train_metrics:
+            max_len = feats.size(2)
+            mask = length_to_mask(lens * max_len, max_len)[:, None, :, None].bool()
+            for metric in self.data_metrics:
+                metric.append(batch.file_name, feats_raw, mask=mask)                
         tail = time_dim % self.hparams.downsample_factor
         if tail > 0:
             feats[:, :, -tail:, :] = 0.0
@@ -161,21 +187,21 @@ class DiffusionBrain(sb.Brain):
 
         return loss
 
-    def generate_samples(self, epoch_sample_path):
-        """Generates samples from diffusion
-        
-        epochs_sample_path: str
-            the path to the samples for the current epoch
+    def generate_samples(self):
+        """Generates spectrogram and (optionally) audio samples using the
+        denoising diffusion model
         """
         samples = self.generate_spectrograms()
-        self.save_spectrograms(samples, epoch_sample_path)
 
+        wav = None
         if self.hparams.eval_generate_audio:
             wav = self.generate_audio(samples)
-            self.save_audio(wav, epoch_sample_path)
+        
+        return samples, wav
 
     def generate_spectrograms(self):
         """Generates sample spectrograms"""
+        return torch.randn(self.hparams.eval_num_samples, 1, 80, 80)
         sample = self.modules.diffusion.sample(
             (
                 self.hparams.eval_num_samples,
@@ -205,14 +231,34 @@ class DiffusionBrain(sb.Brain):
             self.save_spectrogram_sample(sample, spec_file_name)
 
     def save_spectrogram_sample(self, sample, file_name):
-        """Saves a single spectrogram sample
-        
+        """Saves a single spectrogram sample as an image
+
+
+        Arguments
+        ---------
+        sample: torch.Tensor
+            a single generated spectrogram (2D tensor)
+        file_name: str
+            the destination file name
+
         """
         fig = plot_spectrogram(sample)
         if fig is not None:
             fig.savefig(file_name)
 
     def generate_audio(self, samples):
+        """Generates audio from spectrogram samples using a vocoder
+
+        Arguments
+        ---------
+        samples: torch.Tensor
+            a batch of generated spectrograms
+
+        Returns
+        -------
+        audio: torch.Tensor
+            generated audio for the samples (vocoder output)
+        """
         vocoder_in = samples[:, :, : self.hparams.spec_n_mels]
         vocoder_in = vocoder_in.transpose(-1, -2)
         vocoder_in = self.modules.min_level_norm.denormalize(vocoder_in)
@@ -224,12 +270,30 @@ class DiffusionBrain(sb.Brain):
         return self.vocoder(vocoder_in)
 
     def save_audio(self, wav, epoch_sample_path):
+        """Saves a batch of audio samples
+
+        wav: torch.Tensor
+            a batch of audio samples
+
+        epoch_sample_path: str
+            the destination directory
+        """
         wav_sample_path = os.path.join(epoch_sample_path, "wav")
         if not os.path.exists(wav_sample_path):
             os.makedirs(wav_sample_path)
         for idx, sample in enumerate(wav):
             wav_file_name = os.path.join(wav_sample_path, f"sample_{idx}.wav")
             self.save_audio_sample(sample.squeeze(0), wav_file_name)
+
+    def compute_sample_metrics(self, samples):
+        """Computes metrics (mean/std) on samples
+        
+        Arguments
+        ---------
+        """
+        sample_ids = torch.arange(1, len(samples) + 1)
+        self.sample_mean_metric.append(sample_ids, samples)
+        self.sample_std_metric.append(sample_ids, samples)
 
     def save_audio_sample(self, sample, file_name):
         write_audio(file_name, sample, self.hparams.sample_rate_tgt)
@@ -250,6 +314,44 @@ class DiffusionBrain(sb.Brain):
         self.loss_metric = sb.utils.metric_stats.MetricStats(
             metric=sb.nnet.losses.mse_loss
         )
+        if self.hparams.enable_train_metrics:
+            self.data_mean_metric = sb.utils.metric_stats.MetricStats(
+                metric=masked_mean
+            )
+            self.data_std_metric = sb.utils.metric_stats.MetricStats(
+                metric=masked_std
+            )
+            self.data_min_metric = sb.utils.metric_stats.MetricStats(
+                metric=masked_min
+            )
+            self.data_max_metric = sb.utils.metric_stats.MetricStats(
+                metric=masked_max
+            )
+            self.data_metrics = [
+                self.data_mean_metric,
+                self.data_std_metric,
+                self.data_min_metric,
+                self.data_max_metric
+            ]
+
+        self.sample_mean_metric = sb.utils.metric_stats.MetricStats(
+            metric=masked_mean
+        )
+        self.sample_std_metric = sb.utils.metric_stats.MetricStats(
+            metric=masked_std
+        )
+        self.sample_min_metric = sb.utils.metric_stats.MetricStats(
+            metric=masked_min
+        )
+        self.sample_max_metric = sb.utils.metric_stats.MetricStats(
+            metric=masked_max
+        )
+        self.sample_metrics = [
+            self.sample_mean_metric,
+            self.sample_std_metric,
+            self.sample_min_metric,
+            self.sample_max_metric
+        ]
         if stage == sb.Stage.VALID and not hasattr(self, "vocoder"):
             self.vocoder = self.hparams.vocoder()
 
@@ -297,10 +399,58 @@ class DiffusionBrain(sb.Brain):
             )
 
         if stage != sb.Stage.TRAIN:
-            epoch_sample_path = os.path.join(
-                self.hparams.sample_folder, str(epoch)
-            )
-            self.generate_samples(epoch_sample_path)
+            samples, wav = self.generate_samples()
+            self.log_epoch(samples, wav, epoch, stage)
+
+    def log_epoch(self, samples, wav, epoch, stage):
+        """Saves end-of-epoch logs
+        
+        Arguments
+        ---------
+        samples: torch.Tensor
+            Generated spectrograms
+        wav: torch.Tensor
+            Generated audio waveforms
+        epoch: int
+            the epoch number
+        
+        """
+        epoch_sample_path = os.path.join(
+            self.hparams.sample_folder, str(epoch)
+        )
+        self.save_spectrograms(samples, epoch_sample_path)
+        sample_ids = torch.arange(1, len(samples) + 1)
+        for metric in self.sample_metrics:
+            metric.append(sample_ids, samples)
+        if wav is not None:
+            self.save_audio(wav, epoch_sample_path)
+        if self.hparams.use_tensorboard:
+            sample_mean_stats = self.sample_mean_metric.summarize()
+            sample_std_stats = self.sample_std_metric.summarize()
+            sample_min_stats = self.sample_min_metric.summarize()
+            sample_max_stats = self.sample_max_metric.summarize()
+            stats = {
+                "sample_mean": sample_mean_stats["average"],
+                "sample_mean_min": sample_mean_stats["min_score"],
+                "sample_mean_max": sample_mean_stats["max_score"],
+                "sample_std": sample_std_stats["average"],
+                "sample_std_min": sample_std_stats["min_score"],
+                "sample_std_max": sample_std_stats["max_score"],
+                "sample_min": sample_min_stats["min_score"],
+                "sample_max": sample_max_stats["max_score"]
+            }
+            if self.hparams.use_tensorboard:                
+                stats_args = {f"{stage.name.lower()}_stats": stats}                
+                self.hparams.tensorboard_train_logger.log_stats(
+                    stats_meta={"step": self.step},
+                    **stats_args
+                )
+                for sample in samples:
+                    self.hparams.tensorboard_train_logger.log_figure("spectrogram", sample)
+                if wav is not None:
+                    for wav_sample in wav:
+                        self.hparams.tensorboard_train_logger.writer.add_audio("audio", wav_sample)
+
 
 
 DATASET_SPLITS = ["train", "valid", "test"]
@@ -349,9 +499,152 @@ def dataio_prep(hparams):
             dynamic_items=[audio_pipeline],
             output_keys=["file_name", "sig", "digit", "speaker_id"],
         )
-#        datasets_splits[split_id].data_ids = datasets_splits[split_id].data_ids[:2000]
+
+    train_split = datasets_splits["train"]
+    data_count = None
+    if hparams["overfit_test"]:
+        sample_count = hparams["overfit_test_sample_count"]
+        epoch_data_count = hparams["overfit_test_epoch_data_count"]
+        num_repetitions = math.ceil(epoch_data_count / sample_count)
+        overfit_samples = train_split.data_ids[:sample_count] * num_repetitions
+        overfit_samples = overfit_samples[:epoch_data_count]
+        train_split.data_ids = overfit_samples
+    elif hparams["train_data_count"] is not None:
+        data_count = hparams["train_data_count"]
+        train_split.data_ids = train_split.data_ids[:data_count]
 
     return datasets_splits
+
+
+def non_batch_dims(sample):
+    """Returns all dimensons of the specified tensor
+    except the batch dimension
+    
+    Arguments
+    ---------
+    sample: torch.Tensor
+        an arbitrary tensor
+
+    Returns
+    -------
+    dims: list
+        a list of dimensions
+    """
+    return list(range(1, sample.dim()))
+
+
+def masked_mean(sample, mask=None):
+    """A metric function that computes the mean of each sample, excluding
+    padding
+    
+    Arguments
+    ---------
+    samples: torch.Tensor
+        a tensor of spectrograms
+        
+    mask: torch.Tensor
+        a length mask
+        
+    Returns
+    -------
+    result: torch.Tensor
+        a tensor fo means
+    """
+    if mask is None:
+        mask = torch.ones_like(sample).bool()
+    dims = non_batch_dims(sample)
+    return (sample * mask).sum(dim=dims) / mask.expand_as(sample).sum(dim=dims)
+
+
+def masked_std(sample, mask=None):
+    """A metric function that computes the standard deviation of each
+    sample, excluding padding
+    
+    Arguments
+    ---------
+    samples: torch.Tensor
+        a tensor of spectrograms
+        
+    mask: torch.Tensor
+        a length mask
+        
+    Returns
+    -------
+    result: torch.Tensor
+        a tensor fo means
+    """
+    if mask is None:
+        mask = torch.ones_like(sample).bool()
+    dims = non_batch_dims(sample)
+    mean = unsqueeze_as(masked_mean(sample, mask), sample)
+    diff_sq = ((sample - mean) * mask) ** 2
+    return (diff_sq.sum(dim=dims) / (mask.expand_as(diff_sq).sum(dim=dims) - 1)).sqrt()
+
+
+def masked_min(sample, mask=None):
+    """A metric function that computes the minimum of each sample
+    
+    Arguments
+    ---------
+    samples: torch.Tensor
+        a tensor of spectrograms
+        
+    mask: torch.Tensor
+        a length mask
+        
+    Returns
+    -------
+    result: torch.Tensor
+        a tensor fo means
+    """
+    if mask is None:
+        mask = torch.ones_like(sample).bool()  
+    dims = non_batch_dims(sample)
+    return sample.masked_fill(~mask, torch.inf).amin(dim=dims)
+
+
+def masked_max(sample, mask=None):
+    """A metric function that computes the minimum of each sample
+    
+    Arguments
+    ---------
+    samples: torch.Tensor
+        a tensor of spectrograms
+        
+    mask: torch.Tensor
+        a length mask
+        
+    Returns
+    -------
+    result: torch.Tensor
+        a tensor fo means
+    """
+    if mask is None:
+        mask = torch.ones_like(sample).bool()  
+    dims = non_batch_dims(sample)
+    return sample.masked_fill(~mask, -torch.inf).amax(dim=dims)
+
+
+def check_tensorboard(hparams):
+    """Checks whether Tensorboard is enabled and initializes the logger if it is
+
+    Arguments
+    ---------
+    hparams: dict
+        the hyperparameter dictionary
+    """
+    if hparams["use_tensorboard"]:
+        try:
+            from speechbrain.utils.train_logger import TensorboardLogger
+
+            hparams["tensorboard_train_logger"] = TensorboardLogger(
+                hparams["tensorboard_logs"]
+            )
+        except ImportError:
+            logger.warning(
+                "Could not enable TensorBoard logging - TensorBoard is not available"
+            )
+            hparams["use_tensorboard"] = False
 
 
 # Recipe begins!
@@ -367,6 +660,9 @@ if __name__ == "__main__":
     with open(hparams_file) as fin:
         hparams = load_hyperpyyaml(fin, overrides)
 
+    # Check whether Tensorboard is available and enabled
+    check_tensorboard(hparams)    
+
     # Create experiment directory
     sb.create_experiment_directory(
         experiment_directory=hparams["output_folder"],
@@ -378,7 +674,7 @@ if __name__ == "__main__":
     datasets = dataio_prep(hparams)
 
     # Initialize the Brain object to prepare for mask training.
-    spk_id_brain = DiffusionBrain(
+    diffusion_brain = DiffusionBrain(
         modules=hparams["modules"],
         opt_class=hparams["opt_class"],
         hparams=hparams,
@@ -390,8 +686,8 @@ if __name__ == "__main__":
     # necessary to update the parameters of the model. Since all objects
     # with changing state are managed by the Checkpointer, training can be
     # stopped at any point, and will be resumed on next call.
-    spk_id_brain.fit(
-        epoch_counter=spk_id_brain.hparams.epoch_counter,
+    diffusion_brain.fit(
+        epoch_counter=diffusion_brain.hparams.epoch_counter,
         train_set=datasets["train"],
         valid_set=datasets["valid"],
         train_loader_kwargs=hparams["dataloader_options"],
@@ -399,7 +695,7 @@ if __name__ == "__main__":
     )
 
     # Load the best checkpoint for evaluation
-    test_stats = spk_id_brain.evaluate(
+    test_stats = diffusion_brain.evaluate(
         test_set=datasets["test"],
         min_key="error",
         test_loader_kwargs=hparams["dataloader_options"],
