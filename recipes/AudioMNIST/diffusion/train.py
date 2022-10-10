0dec211892c2c58ec9d0f02dc8dc880ded169dbf
flexthink <flexthink@users.noreply.github.com>
2022-10-10 00:33:58 -0400

    Diffusion: fixes, additional logging

--- recipes/AudioMNIST/diffusion/train.py
+++ recipes/AudioMNIST/diffusion/train.py
@@ -21,6 +21,7 @@ import speechbrain as sb
 import os
 from hyperpyyaml import load_hyperpyyaml
 from torchaudio import functional as AF
+from torchaudio import transforms
 from speechbrain.dataio.dataio import length_to_mask, write_audio
 from speechbrain.utils import data_utils
 from speechbrain.utils.train_logger import plot_spectrogram
@@ -58,10 +59,14 @@ class DiffusionBrain(sb.Brain):
 
         pred, noise, noisy_sample = self.modules.diffusion.train_sample(feats, lens=lens)
 
-        return pred, noise, noisy_sample
+        #NOTE: lens can change because of the additional padding needed to account
+        #NOTE: for downsampling
+        return pred, noise, noisy_sample, lens
 
     def fit_batch(self, batch):
         """Train the parameters given a single batch in input"""
+        if self.reference_batch is None:
+            self.reference_batch = batch
         loss = super().fit_batch(batch)
         self.hparams.lr_annealing(self.optimizer)
         if (
@@ -115,10 +120,6 @@ class DiffusionBrain(sb.Brain):
         feats = feats.transpose(-1, -2)
         feats = feats.unsqueeze(1)
 
-        # Randomly initialize time steps for each image
-        timesteps = torch.randint(
-            0, self.hparams.train_timesteps, (len(feats),), device=self.device
-        )
 
         # UNet downsamples features in multiples of 2. Reshape to ensure
         # there are no mismatched tensors due to ambiguity
@@ -130,18 +131,30 @@ class DiffusionBrain(sb.Brain):
         desired_feats_dim = (
             math.ceil(feats_dim / self.hparams.downsample_factor)
             * self.hparams.downsample_factor
-        )
+        )        
         feats, _ = data_utils.pad_right_to(
             feats,
             (batch_dim, channel_dim, desired_time_dim, desired_feats_dim),
-            value=self.hparams.min_level_db,
+            value=self.hparams.pad_level_db,
         )
+        tail = time_dim % self.hparams.downsample_factor
+        if tail > 0:
+            feats[:, :, -tail:, :] = self.hparams.pad_level_db
+        
+        # Adjust lengths to the new dimenson, post-padding
+        lens = lens * (time_dim / desired_time_dim)
+
 
         # Min Level Norm
         feats_raw = self.modules.min_level_norm(feats)
 
         # Global Norm
-        feats = self.modules.global_norm(feats_raw, lens)
+        mask_value = self.modules.min_level_norm(
+            torch.tensor(self.hparams.pad_level_db, device=feats_raw.device))
+        
+        feats = self.modules.global_norm(
+            feats_raw, lens,
+            mask_value=mask_value)
 
         # Compute metrics
         if self.hparams.enable_train_metrics:
@@ -149,9 +162,6 @@ class DiffusionBrain(sb.Brain):
             mask = length_to_mask(lens * max_len, max_len)[:, None, :, None].bool()
             for metric in self.data_metrics:
                 metric.append(batch.file_name, feats_raw, mask=mask)                
-        tail = time_dim % self.hparams.downsample_factor
-        if tail > 0:
-            feats[:, :, -tail:, :] = 0.0
 
         return feats, lens
 
@@ -172,9 +182,8 @@ class DiffusionBrain(sb.Brain):
         loss : torch.Tensor
             A one-element tensor used for backpropagating the gradient.
         """
-        _, lens = batch.sig
 
-        preds, noise, noisy_sample = predictions
+        preds, noise, noisy_sample, lens = predictions
 
         loss = self.hparams.compute_cost(
             preds.squeeze(1), noise.squeeze(1), length=lens
@@ -212,17 +221,17 @@ class DiffusionBrain(sb.Brain):
         sample = self.modules.global_norm.denormalize(sample)
         return sample
 
-    def save_spectrograms(self, samples, epoch_sample_path):
+    def save_spectrograms(self, samples, path):
         """Saves sample spectrograms to filesystem files
         
         Arguments
         ---------
         samples: torch.Tensor
             a tensor of sample spectrograms
-        epoch_sample_path
+        path: str
             ths path to samples for a given epoch
         """
-        spec_sample_path = os.path.join(epoch_sample_path, "spec")
+        spec_sample_path = os.path.join(path, "spec")
         if not os.path.exists(spec_sample_path):
             os.makedirs(spec_sample_path)
         for idx, sample in enumerate(samples):
@@ -268,16 +277,16 @@ class DiffusionBrain(sb.Brain):
         vocoder_in = vocoder_in.squeeze(1)
         return self.vocoder(vocoder_in)
 
-    def save_audio(self, wav, epoch_sample_path):
+    def save_audio(self, wav, path):
         """Saves a batch of audio samples
 
         wav: torch.Tensor
             a batch of audio samples
 
-        epoch_sample_path: str
+        path: str
             the destination directory
         """
-        wav_sample_path = os.path.join(epoch_sample_path, "wav")
+        wav_sample_path = os.path.join(path, "wav")
         if not os.path.exists(wav_sample_path):
             os.makedirs(wav_sample_path)
         for idx, sample in enumerate(wav):
@@ -351,8 +360,18 @@ class DiffusionBrain(sb.Brain):
             self.sample_min_metric,
             self.sample_max_metric
         ]
-        if stage == sb.Stage.VALID and not hasattr(self, "vocoder"):
+        if stage == sb.Stage.TRAIN:
+            self.modules.global_norm.unfreeze()
+        else:
+            self.modules.global_norm.freeze()
+        
+        if (
+            (self.hparams.enable_reference_samples or stage != sb.Stage.TRAIN)
+            and not hasattr(self, "vocoder")
+        ):
             self.vocoder = self.hparams.vocoder()
+        self.reference_batch = None
+        self.reference_samples_neeed = False
 
     def on_stage_end(self, stage, stage_loss, epoch=None):
         """Gets called at the end of an epoch.
@@ -397,9 +416,35 @@ class DiffusionBrain(sb.Brain):
                 test_stats=stats,
             )
 
+        if stage == sb.Stage.TRAIN and self.hparams.enable_reference_samples:
+            self.generate_reference_samples(self.reference_batch)
+
         if stage != sb.Stage.TRAIN:
             samples, wav = self.generate_samples()
             self.log_epoch(samples, wav, epoch, stage)
+
+
+    def generate_reference_samples(self, batch):
+        """Generate an audio sample from one of the spectrograms 
+        using the same normalization techniques
+        
+        Arguments
+        ---------
+        batch: speechbrain.dataio.batch.PaddedBatch
+            a batch of audio
+        """
+        feats, lens = self.prepare_features(batch, sb.Stage.VALID)
+        feats = self.modules.global_norm.denormalize(feats)
+        wav = self.generate_audio(feats)
+        self.log_samples(
+            spectrogram_samples=feats,
+            wav_samples=wav,
+            lens=lens,
+            key_prefix="reference_"
+        )
+        ref_sample_path = os.path.join(self.hparams.sample_folder, "ref")
+        self.save_spectrograms(feats, ref_sample_path)
+        self.save_audio(wav, path=ref_sample_path)
 
     def log_epoch(self, samples, wav, epoch, stage):
         """Saves end-of-epoch logs
@@ -438,17 +483,53 @@ class DiffusionBrain(sb.Brain):
                 "sample_min": sample_min_stats["min_score"],
                 "sample_max": sample_max_stats["max_score"]
             }
-            if self.hparams.use_tensorboard:                
-                stats_args = {f"{stage.name.lower()}_stats": stats}                
-                self.hparams.tensorboard_train_logger.log_stats(
-                    stats_meta={"step": self.step},
-                    **stats_args
-                )
-                for sample in samples:
-                    self.hparams.tensorboard_train_logger.log_figure("spectrogram", sample)
-                if wav is not None:
-                    for wav_sample in wav:
-                        self.hparams.tensorboard_train_logger.writer.add_audio("audio", wav_sample)
+            stats_args = {f"{stage.name.lower()}_stats": stats}                
+            self.hparams.tensorboard_train_logger.log_stats(
+                stats_meta={"step": self.step},
+                **stats_args
+            )
+            self.log_samples(
+                spectrogram_samples=samples, wav_samples=wav)
+
+    def log_samples(
+        self,
+        spectrogram_samples=None,
+        wav_samples=None,
+        key_prefix=None,
+        lens=None
+    ):
+        """Logs a set of audio and spectrogram samples
+        
+        Arguments
+        ---------
+        spectrogram_samples: torch.Tensor
+            a tensor of spectrogram samples
+
+        wav_samples: torch.Tensor
+            a tensor of audio samples
+
+        """
+        if key_prefix is None:
+            key_prefix = ''
+        if lens is None:
+            lens = torch.ones(
+                len(spectrogram_samples),
+                device=spectrogram_samples.device)
+        if self.hparams.use_tensorboard:
+            for sample in spectrogram_samples:
+                self.hparams.tensorboard_train_logger.log_figure(
+                    f"{key_prefix}spectrogram", sample)
+            if wav_samples is not None:
+                max_len = wav_samples.size(-1)
+                lens_full = (lens * max_len).int()
+                for wav_sample, sample_len in zip(wav_samples, lens_full):
+                    self.tb_writer.add_audio(
+                        f"{key_prefix}audio", wav_sample[:, :sample_len])
+    @property
+    def tb_writer(self):
+        """Returns the raw TensorBoard logger writer"""
+        return self.hparams.tensorboard_train_logger.writer
+
 
 
 
@@ -475,12 +556,18 @@ def dataio_prep(hparams):
         to the appropriate DynamicItemDataset object.
     """
 
+    resample = transforms.Resample(
+        orig_freq=hparams["sample_rate_src"],
+        new_freq=hparams["sample_rate_tgt"]
+    )
+
     # Define audio pipeline
     @sb.utils.data_pipeline.takes("file_name")
     @sb.utils.data_pipeline.provides("sig")
     def audio_pipeline(wav):
         """Load the signal, and output it"""
         sig = sb.dataio.dataio.read_audio(wav)
+        sig = resample(sig)
         return sig
 
     # Define datasets. We also connect the dataset with the data processing
