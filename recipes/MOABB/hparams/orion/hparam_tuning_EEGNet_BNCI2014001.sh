ce17bc5a79d898cfc2c8a0badb5ef63cf1845a25
prometheus <46483626+ddavidebb@users.noreply.github.com>
2022-10-21 23:56:22 +0200

    Reduced hparam space BNCI2014001

--- recipes/MOABB/hparams/orion/hparam_tuning_EEGNet_BNCI2014001.sh
+++ recipes/MOABB/hparams/orion/hparam_tuning_EEGNet_BNCI2014001.sh
@@ -15,15 +15,13 @@ orion hunt -n $HPOPT_EXPERIMENT_NAME -c $HPOPT_CONFIG_FILE --exp-max-trials=250 
 	./run_experiments_seed_variability.sh hparams/EEGNet_BNCI2014001_seed_variability.yaml \
 	/localscratch/eeg_data $OUTPUT_FOLDER 1 1 'random_seed' 1 acc valid_metrics.pkl false true \
   --number_of_epochs~"uniform(100, 1000,discrete=True)" \
-	--avg_models~"uniform(1, 20,discrete=True)" \
+	--avg_models~"uniform(1, 15,discrete=True)" \
 	--step_size~"choices([50,100,150])" \
 	--batch_size~"choices([32,64,128])" \
 	--lr~"choices([0.01,0.005, 0.001,0.0005, 0.0001])" \
-	--n_steps_channel_selection~"uniform(1, 3,discrete=True)" \
 	--tmax~"uniform(1.0, 4.0)" \
 	--fmin~"uniform(0.1, 5)" \
-	--fmax~"uniform(10, 50)" \
-	--dims_to_normalize~"uniform(1, 2,discrete=True)" \
+	--fmax~"uniform(20, 50)" \
 	--cnn_temporal_kernels~"uniform(4, 64,discrete=True)" \
 	--cnn_temporal_kernelsize~"uniform(8, 64,discrete=True)" \
 	--cnn_spatial_depth_multiplier~"uniform(1, 4,discrete=True)" \
@@ -32,12 +30,8 @@ orion hunt -n $HPOPT_EXPERIMENT_NAME -c $HPOPT_CONFIG_FILE --exp-max-trials=250 
 	--cnn_septemporal_kernelsize~"uniform(4, 32,discrete=True)" \
 	--cnn_septemporal_pool~"uniform(1, 8,discrete=True)" \
 	--dropout~"uniform(0.0, 0.50)" \
-	--snr_pink_low~"uniform(0.0, 15.0)" \
-	--snr_pink_high~"uniform(15.0, 30.0)" \
-  --snr_white_low~"uniform(0.0, 15.0)" \
-  --snr_white_high~"uniform(15.0, 30.0)" \
-  --snr_muscular_low~"uniform(0.0, 15.0)" \
-  --snr_muscular_high~"uniform(15.0, 30.0)" \
+	--snr_low~"uniform(0.0, 15.0)" \
+	--snr_high~"uniform(15.0, 30.0)" \
 	--repeat_augment~"uniform(1, 3,discrete=True)" \
   --n_augmentations~"uniform(0, 10,discrete=True)"
 
