5a36fe258b867dc86e1638dc2e5c187f7d573e2c
Martin Kocour <martinkocour1102@gmail.com>
2022-11-08 17:17:03 -0500

    Add config, solve problem with padding

--- speechbrain/processing/dynamic_mixing.py
+++ speechbrain/processing/dynamic_mixing.py
@@ -16,7 +16,34 @@ import numbers
 import random
 import warnings
 import uuid
-import pyloudnorm # WARNING: External dependency
+import pyloudnorm  # WARNING: External dependency
+
+from dataclasses import dataclass
+from typing import Optional, Union
+
+
+@dataclass(kw_only=True)
+class DynamicMixingConfig:
+    num_spkrs: Union[int, list] = 2
+    overlap_ratio: Union[int, list] = 1.0
+    audio_norm: bool = True
+    audio_min_loudness: float = -33.0
+    audio_max_loudness: float = -25.0
+    audio_max_amp: float = 0.9
+
+    @classmethod
+    def from_hparams(cls, hparams):
+        config = {}
+        for fld in fields(cls):
+            config[fld.name] = hparams.get(fld.name, fld.default)
+        return cls(**config)
+
+    def __post_init__(self):
+        if isinstance(self.num_spkrs, int):
+            self.num_spkrs = [self.num_spkrs]
+
+        if isinstance(self.overlap_ratio, numbers.Real):
+            self.overlap_ratio = [self.overlap_ratio]
 
 
 class DynamicMixingDataset(torch.utils.data.Dataset):
@@ -34,34 +61,25 @@ class DynamicMixingDataset(torch.utils.data.Dataset):
     ...         'spkr': 'Frodo',
     ...     }
     ... ]
-    >>> dm_dataset = DynamicMixixingDataset.from_didataset(data, "wav_file", "spkr")
+    >>> config = DynamicMixingConfig.from_hparams(hparams)
+    >>> dm_dataset = DynamicMixixingDataset.from_didataset(data, config, "wav_file", "spkr")
     >>> mixture, spkrs, ratios, sources = dm_dataset.generate()
 
     Arguments
     ---------
     spkr_files : dict
-    num_spkrs : Union[list, range, int], optional
-    overlap_ratio : Union[list, range, int], optional
-    normalize_audio: bool, optional
+    config: DynamicMixingConfig
     """
 
-    def __init__(
-        self, spkr_files, num_spkrs=2, overlap_ratio=1.0, normalize_audio=True
-    ):
-        if isinstance(num_spkrs, int):
-            num_spkrs = [num_spkrs]
-
-        if isinstance(overlap_ratio, numbers.Real):
-            overlap_ratio = [overlap_ratio]
-
-        if len(spkr_files.keys()) < max(num_spkrs):
+    def __init__(self, spkr_files, config):
+        if len(spkr_files.keys()) < max(config.num_spkrs):
             raise ValueError(
                 f"Expected at least {num_spkrs} spkrs in spkr_files"
             )
 
-        self.num_spkrs = num_spkrs
-        self.overlap_ratio = overlap_ratio
-        self.normalize_audio = normalize_audio
+        self.num_spkrs = config.num_spkrs
+        self.overlap_ratio = config.overlap_ratio
+        self.normalize_audio = config.audio_norm
         self.spkr_files = spkr_files
 
         tmp_file = next(iter(spkr_files.values()))[0]
@@ -71,14 +89,19 @@ class DynamicMixingDataset(torch.utils.data.Dataset):
         if self.normalize_audio:
             self.meter = pyloudnorm.Meter(self.sampling_rate)
 
+        if min(config.num_spkrs) <= 0:
+            lengths = map(torchaudio.info, iter(self._all_files()))
+
+        self.config = config
+
     @classmethod
-    def from_didataset(cls, dataset, wav_key=None, spkr_key=None, **kwargs):
+    def from_didataset(cls, dataset, config, wav_key=None, spkr_key=None):
         if wav_key is None:
             raise ValueError("Provide valid wav_key for dataset item")
 
         if spkr_key is None:
             files = [d[wav_key] for d in dataset]
-            return cls.from_wavs(files, **kwargs)
+            return cls.from_wavs(files, config)
         else:
             spkr_files = {}
             for d in dataset:
@@ -86,10 +109,10 @@ class DynamicMixingDataset(torch.utils.data.Dataset):
                     d[spkr_key], []
                 ).append(d[wav_key])
 
-            return cls(spkr_files, **kwargs)
+            return cls(spkr_files, config)
 
     @classmethod
-    def from_wavs(cls, wav_file_list, **kwargs):
+    def from_wavs(cls, wav_file_list, config):
         spkr_files = {}
         spkr = 0
         # we assume that each wav is coming from different spkr
@@ -97,10 +120,10 @@ class DynamicMixingDataset(torch.utils.data.Dataset):
             spkr_files[f"spkr{spkr}"] = [wavfile]
             spkr += 1
 
-        return cls(spkr_files, **kwargs)
+        return cls(spkr_files, config)
 
     def generate(self, wavfile=None):
-        n_spkrs = np.random.choice(self.num_spkrs)
+        n_spkrs = np.random.choice(self.config.num_spkrs)
         if n_spkrs <= 0:
             # TODO: how long mixture?
             raise NotImplementedError("Expect at least 1 source")
@@ -112,41 +135,71 @@ class DynamicMixingDataset(torch.utils.data.Dataset):
         for spkr in mix_spkrs:
             src_file = np.random.choice(self.spkr_files[spkr])
             src_audio, fs = torchaudio.load(src_file)
+            src_audio = src_audio[0]  # Support only single channel
 
             if fs != self.sampling_rate:
                 raise RuntimeError(
                     f"{self.sampling_rate} Hz sampling rate expected, but found {fs}"
                 )
 
-            if self.normalize_audio:
-                src_audio = normalize(src_audio.squeeze().numpy(), self.meter)
-                src_audio = src_audio.unsqueeze(0)
-
+            src_audio = self.__prepare_source__(src_audio)
             sources.append(src_audio)
 
-        sources = sorted(sources, key=lambda x: x.size(1), reverse=True)
+        sources = sorted(sources, key=lambda x: x.size(0), reverse=True)
         mixture = sources[0]  # longest audio
         overlap_ratios = []
         padded_sources = []
         for i in range(1, len(sources)):
             src = sources[i]
-            ratio = np.random.choice(self.overlap_ratio)
-            overlap_samples = int(src.size(1) * ratio)
-            mixture, padded_tmp = mix(src, mixture, overlap_samples)
-            overlap_ratios.append((ratio, overlap_samples))
-            if len(padded_sources) == 0:
-                padded_sources.append(padded_tmp[1].unsqueeze(0))
-            padded_sources.append(
-                padded_tmp[0].unsqueeze(0)
-            )  # padded sources are returned in same order
+            ratio = np.random.choice(self.config.overlap_ratio)
+            overlap_samples = int(src.size(0) * ratio)
 
-        # TODO: do some post-porcessing of the mixture, e.g. replace zeros with small noise
+            mixture, padded_tmp, paddings = mix(src, mixture, overlap_samples)
+            # padded sources are returned in same order
+            overlap_ratios.append((ratio, overlap_samples))
+
+            # for sources>2 previous padded_sources are shorter
+            padded_sources = __pad_sources__(
+                padded_sources,
+                [paddings[1] for _ in range(len(padded_sources))],
+            )
+            if len(padded_sources) == 0:
+                padded_sources.append(padded_tmp[1])
+
+            padded_sources.append(padded_tmp[0])
+        mixture, padded_source = self.__postprocess__(mixture, padded_sources)
         if wavfile:
             torchaudio.save(mixture, wavfile)
         return mixture, mix_spkrs, overlap_ratios, padded_sources
 
+    def __prepare_source__(self, source):
+        if self.normalize_audio:
+            source = normalize(
+                source,
+                self.meter,
+                self.config.audio_min_loudness,
+                self.config.audio_max_loudness,
+                self.config.audio_max_amp,
+            )
+        # TODO: Random gain
+        # TODO: add reverb
+        # TODO: add noise
+        return source
+
+    def __postprocess__(self, mixture, sources):
+        # modify gain
+        mix_max_amp = mixture.abs().max().item()
+        gain = 1.0
+        if mix_max_amp > self.config.audio_max_amp:
+            gain = self.config.audio_max_amp / mix_max_amp
+
+        mixture = gain * mixture
+        sources = map(lambda src: gain * src, sources)
+        # TODO: replace zeros with small noise
+        return mixture, sources
+
     def __len__(self):
-        return sum(map(len, self.spkr_files.values())) # dict of lists
+        return sum(map(len, self.spkr_files.values()))  # dict of lists
 
     def __getitem__(self, idx):
         # TODO: Refactor completly
@@ -156,13 +209,27 @@ class DynamicMixingDataset(torch.utils.data.Dataset):
 
         if idx is None:
             idx = uuid.uuid4()
-        mix_id = str(idx) + '_' + '-'.join(spkrs) + '_overlap' + '-'.join(map(lambda x: f"{x[0]:.2f}", ratios))
+        mix_id = (
+            str(idx)
+            + "_"
+            + "-".join(spkrs)
+            + "_overlap"
+            + "-".join(map(lambda x: f"{x[0]:.2f}", ratios))
+        )
         # "id", "mix_sig", "s1_sig", "s2_sig", "s3_sig", "noise_sig"
-        return mix_id, mix.squeeze(), srcs[0].squeeze(), srcs[1].squeeze(), torch.zeros(mix.size(1)), torch.zeros(mix.size(1))
+        return (
+            mix_id,
+            mix,
+            srcs[0],
+            srcs[1],
+            torch.zeros(mix.size(0)),
+            torch.zeros(mix.size(0)),
+        )
 
 
-def normalize(audio, meter, MIN_LOUDNESS=-33, MAX_LOUDNESS=-25, MAX_AMP=0.9):
-    """This function normalizes the audio signals for loudness"""
+def normalize(audio, meter, min_loudness=-33, max_loudness=-25, max_amp=0.9):
+    """This function normalizes the loudness of audio signal"""
+    audio = audio.numpy()
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         c_loudness = meter.integrated_loudness(audio)
@@ -172,21 +239,20 @@ def normalize(audio, meter, MIN_LOUDNESS=-33, MAX_LOUDNESS=-25, MAX_AMP=0.9):
         #    )
         # else:
         #    target_loudness = random.uniform(MIN_LOUDNESS, MAX_LOUDNESS)
-        target_loudness = random.uniform(MIN_LOUDNESS, MAX_LOUDNESS)
+        target_loudness = random.uniform(min_loudness, max_loudness)
         signal = pyloudnorm.normalize.loudness(
             audio, c_loudness, target_loudness
         )
 
         # check for clipping
         if np.max(np.abs(signal)) >= 1:
-            signal = signal * MAX_AMP / np.max(np.abs(signal))
+            signal = signal * max_amp / np.max(np.abs(signal))
 
     return torch.from_numpy(signal)
 
 
-def mix(src1, src2, overlap_samples, channel_first=True):
+def mix(src1, src2, overlap_samples):
     """Mix two audio samples"""
-    src1, src2 = src1.squeeze(), src2.squeeze()
     n_diff = len(src1) - len(src2)
     swapped = False
     if n_diff >= 0:
@@ -200,61 +266,60 @@ def mix(src1, src2, overlap_samples, channel_first=True):
     n_long = len(longer_src)
     n_short = len(shorter_src)
 
+    paddings = []
     if overlap_samples >= n_short:
         # full overlap
         #
-        # short |              ++++++++++++              |
-        # long  |----------------------------------------|
-        # sum   |--------------++++++++++++--------------|
-        #        <--offset---->            <---padding-->
+        # short |        ++++++        |
+        # long  |----------------------|
+        # sum   |--------++++++--------|
+        #        <-lpad->      <-rpad->
         #
-        offset = np.random.choice(range(n_diff)) if n_diff > 0 else 0
-        padding_len = n_diff - offset
-        src1 = torch.cat(
-            (torch.zeros(offset), shorter_src, torch.zeros(padding_len))
-        )
-        src2 = longer_src
+        lpad = np.random.choice(range(n_diff)) if n_diff > 0 else 0
+        rpad = n_diff - offset
+        paddings = [(lpad, rpad), (0, 0)]
     elif overlap_samples > 0:
         # partial overlap
         #
-        # short | +++++++             |
-        # long  |    -----------------|
-        # sum   | +++++++-------------|
+        # short | +++++++       |
+        # long  |    -----------|
+        # sum   | +++++++-------|
         #
         start_short = np.random.choice([True, False])  # start with short
         n_total = n_long + n_short - overlap_samples
         if start_short:
-            src1 = torch.cat((shorter_src, torch.zeros(n_total - n_short)))
-            src2 = torch.cat((torch.zeros(n_total - n_long), longer_src))
+            paddings = [(0, n_total - n_short), (n_total - n_long, 0)]
         else:
-            src1 = torch.cat((torch.zeros(n_total - n_short), shorter_src))
-            src2 = torch.cat((longer_src, torch.zeros(n_total - n_long)))
+            paddings = [(n_total - n_short, 0), (0, n_total - n_long)]
     else:
         # no-overlap
         #
-        # short | +++++            |
-        # long  |         ---------|
-        # sum   | +++++   ---------|
+        # short | +++++         |
+        # long  |         ------|
+        # sum   | +++++   ------|
         #
         sil_between = abs(overlap_samples)
         start_short = np.random.choice([True, False])  # start with short
         if start_short:
-            src1 = torch.cat((shorter_src, torch.zeros(sil_between + n_long)))
-            src2 = torch.cat((torch.zeros(sil_between + n_short), longer_src))
+            paddings = [(0, sil_between + n_long), (sil_between + n_short, 0)]
         else:
-            src1 = torch.cat((torch.zeros(sil_between + n_long), shorter_src))
-            src2 = torch.cat((longer_src, torch.zeros(sil_between + n_short)))
-    try:
-        sources = (
-            torch.stack((src2, src1)) if swapped else torch.stack((src1, src2))
-        )
-        mixture = torch.sum(sources, dim=0)
-    except Exception as e:
-        print(e)
-        import pdb
+            paddings = [(sil_between + n_long, 0), (0, sil_between + n_short)]
 
-        pdb.set_trace()
+    assert len(paddings) == 2
+    src1, src2 = __pad_sources__([shorter_src, longer_src], paddings)
+    sources = (
+        torch.stack((src2, src1)) if swapped else torch.stack((src1, src2))
+    )
+    if swapped:
+        paddings.reverse()
 
-    if channel_first:
-        mixture = mixture.unsqueeze(0)
-    return mixture, sources
+    mixture = torch.sum(sources, dim=0)
+    return mixture, sources, paddings
+
+
+def __pad_sources__(sources, paddings):
+    result = []
+    for src, (lpad, rpad) in zip(sources, paddings):
+        nsrc = torch.cat((torch.zeros(lpad), src, torch.zeros(rpad)))
+        result.append(nsrc)
+    return result
