58ddefb988fa9fa6410e25323e06846ffc790d57
Paissan <francescopaissan@Paissans-MacBook-Air.local>
2022-11-08 08:24:25 -0500

    wip - NMF training with speechbrain's data loading

---
+++ recipes/ESC50/classification/train_nmf.py
@@ -0,0 +1,145 @@
+import os
+import sys
+import torch
+import torchaudio
+import speechbrain as sb
+from hyperpyyaml import load_hyperpyyaml
+from speechbrain.utils.distributed import run_on_main
+from esc50_prepare import prepare_esc50
+from sklearn.metrics import confusion_matrix
+import numpy as np
+from confusion_matrix_fig import create_cm_fig
+
+import librosa
+from librosa.core import stft
+import scipy.io.wavfile as wavf
+from tqdm import tqdm
+
+def dataio_prep(hparams):
+    "Creates the datasets and their data processing pipelines."
+
+    data_audio_folder = hparams["audio_data_folder"]
+    config_sample_rate = hparams["sample_rate"]
+    label_encoder = sb.dataio.encoder.CategoricalEncoder()
+    # TODO  use SB implementation but need to make sure it give the same results as PyTorch
+    # resampler = sb.processing.speech_augmentation.Resample(orig_freq=latest_file_sr, new_freq=config_sample_rate)
+    hparams["resampler"] = torchaudio.transforms.Resample(
+        new_freq=config_sample_rate
+    )
+
+
+    # 2. Define audio pipeline:
+    @sb.utils.data_pipeline.takes("wav")
+    @sb.utils.data_pipeline.provides("sig")
+    def audio_pipeline(wav):
+        """Load the signal, and pass it and its length to the corruption class.
+        This is done on the CPU in the `collate_fn`."""
+
+        wave_file = data_audio_folder + "/{:}".format(wav)
+
+        sig, read_sr = torchaudio.load(wave_file)
+
+        # If multi-channels, downmix it to a mono channel
+        sig = torch.squeeze(sig)
+        if len(sig.shape) > 1:
+            sig = torch.mean(sig, dim=0)
+
+        # Convert sample rate to required config_sample_rate
+        if read_sr != config_sample_rate:
+            # Re-initialize sampler if source file sample rate changed compared to last file
+            if read_sr != hparams["resampler"].orig_freq:
+                hparams["resampler"] = torchaudio.transforms.Resample(
+                    orig_freq=read_sr, new_freq=config_sample_rate
+                )
+            # Resample audio
+            sig = hparams["resampler"].forward(sig)
+
+        # the librosa version
+        fs, inp_audio = wavf.read(wave_file)
+        inp_audio = inp_audio.astype(np.float32)
+        inp_audio = inp_audio / inp_audio.max()
+        # if self.noise:
+        #     energy_signal = (inp_audio ** 2).mean()
+        #     noise = np.random.normal(0, 0.05, inp_audio.shape[0])
+        #     energy_noise = (noise ** 2).mean()
+        #     const = np.sqrt(energy_signal / energy_noise)
+        #     noise = const * noise
+        #     inp_audio = inp_audio + noise
+
+        return torch.from_numpy(inp_audio)
+
+    # 3. Define label pipeline:
+    @sb.utils.data_pipeline.takes("class_string")
+    @sb.utils.data_pipeline.provides("class_string", "class_string_encoded")
+    def label_pipeline(class_string):
+        yield class_string
+        class_string_encoded = label_encoder.encode_label_torch(class_string)
+        yield class_string_encoded
+
+    # Define datasets. We also connect the dataset with the data processing
+    # functions defined above.
+    datasets = {}
+    data_info = {
+        "train": hparams["train_annotation"],
+        "valid": hparams["valid_annotation"],
+        "test": hparams["test_annotation"],
+    }
+    for dataset in data_info:
+        datasets[dataset] = sb.dataio.dataset.DynamicItemDataset.from_json(
+            json_path=data_info[dataset],
+            replacements={"data_root": hparams["data_folder"]},
+            dynamic_items=[audio_pipeline, label_pipeline],
+            output_keys=["id", "sig", "class_string_encoded"],
+        )
+
+    # Load or compute the label encoder (with multi-GPU DDP support)
+    # Please, take a look into the lab_enc_file to see the label to index
+    # mappinng.
+    lab_enc_file = os.path.join(hparams["save_folder"], "label_encoder.txt")
+    label_encoder.load_or_create(
+        path=lab_enc_file,
+        from_didatasets=[datasets["train"]],
+        output_key="class_string",
+    )
+
+    return datasets, label_encoder
+
+if __name__ == "__main__":
+    hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+
+    # Load hyperparameters file with command-line overrides
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin, overrides)
+    
+    run_on_main(
+        prepare_esc50,
+        kwargs={
+            "data_folder": hparams["data_folder"],
+            "audio_data_folder": hparams["audio_data_folder"],
+            "save_json_train": hparams["train_annotation"],
+            "save_json_valid": hparams["valid_annotation"],
+            "save_json_test": hparams["test_annotation"],
+            "train_fold_nums": hparams["train_fold_nums"],
+            "valid_fold_nums": hparams["valid_fold_nums"],
+            "test_fold_nums": hparams["test_fold_nums"],
+            "skip_manifest_creation": hparams["skip_manifest_creation"],
+        },
+    )
+
+    datasets, _ = dataio_prep(hparams)
+    nmf_model = hparams["modules"]["nmf"]
+
+    for element in tqdm(datasets["train"]):
+        # print(element["sig"].shape[0] / hparams["sample_rate"])
+
+        Xs = stft(element["sig"].cpu().numpy(), n_fft=1024, hop_length=512)
+        Xs = np.log(1 + np.abs(Xs))
+
+        # print(Xs.shape)
+        # input()
+
+        nmf_model.sparse_fit(torch.Tensor(Xs).T)
+    
+    torch.save(nmf_model, "nmf_decoder.pt")
+
+    
