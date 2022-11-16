dd2b219f5881256b606d1948dad3321e214d9328
prometheus <46483626+ddavidebb@users.noreply.github.com>
2022-11-16 08:50:17 +0100

    EPFLP300 hparam tuning scripts

--- recipes/MOABB/hparams/orion/hparam_tuning_EEGNet_EPFLP300.sh
+++ recipes/MOABB/hparams/orion/hparam_tuning_EEGNet_EPFLP300.sh
@@ -14,27 +14,19 @@ cd ../..
 orion hunt -n $HPOPT_EXPERIMENT_NAME -c $HPOPT_CONFIG_FILE --exp-max-trials=250  \
 	./run_experiments_seed_variability.sh hparams/EEGNet_EPFLP300_hparam_search.yaml \
 	/localscratch/eeg_data $OUTPUT_FOLDER 1 1 'random_seed' 1 f1 valid_metrics.pkl false true \
-  --number_of_epochs~"uniform(100, 1000,discrete=True)" \
-	--avg_models~"uniform(1, 20,discrete=True)" \
-	--step_size~"uniform(50,150,discrete=True)" \
-	--batch_size~"uniform(32,128,discrete=True)" \
-	--lr~"loguniform(1e-5, 1e-1)" \
-	--n_steps_channel_selection~"uniform(1, 3,discrete=True)" \
-	--fmax~"uniform(10, 50,discrete=True)" \
-	--cnn_temporal_kernels~"uniform(4, 64,discrete=True)" \
-	--cnn_temporal_kernelsize~"uniform(8, 64,discrete=True)" \
-	--cnn_spatial_depth_multiplier~"uniform(1, 4,discrete=True)" \
-	--cnn_spatial_pool~"uniform(1, 4,discrete=True)" \
-	--cnn_septemporal_depth_multiplier~"uniform(1, 4,discrete=True)" \
-	--cnn_septemporal_kernelsize~"uniform(4, 32,discrete=True)" \
-	--cnn_septemporal_pool~"uniform(1, 8,discrete=True)" \
-	--dropout~"uniform(0.0, 0.50)" \
-  --dims_to_normalize~"uniform(1, 2,discrete=True)" \
-	--snr_pink_low~"uniform(0.0, 15.0)" \
-	--snr_pink_high~"uniform(15.0, 30.0)" \
-  --snr_white_low~"uniform(0.0, 15.0)" \
-  --snr_white_high~"uniform(15.0, 30.0)" \
-  --snr_muscular_low~"uniform(0.0, 15.0)" \
-  --snr_muscular_high~"uniform(15.0, 30.0)" \
-	--repeat_augment~"uniform(1, 3,discrete=True)" \
-  --n_augmentations~"uniform(1, 10,discrete=True)"
+  --number_of_epochs~"uniform(250, 1000, discrete=True)" \
+  --avg_models~"uniform(1, 15,discrete=True)" \
+  --batch_size_exponent~"uniform(4, 6,discrete=True)" \
+  --lr~"choices([0.01, 0.005, 0.001, 0.0005, 0.0001])" \
+  --fmin~"uniform(0.1, 5, precision=2)" \
+  --fmax~"uniform(20.0, 50.0, precision=3)" \
+  --n_steps_channel_selection~"uniform(1, 3,discrete=True)" \
+  --cnn_temporal_kernels~"uniform(4, 64,discrete=True)" \
+  --cnn_temporal_kernelsize~"uniform(24, 62,discrete=True)" \
+  --cnn_spatial_depth_multiplier~"uniform(1, 4,discrete=True)" \
+  --cnn_septemporal_point_kernels_ratio_~"uniform(0, 8, discrete=True)" \
+  --cnn_septemporal_kernelsize_~"uniform(3, 24,discrete=True)" \
+  --cnn_septemporal_pool~"uniform(1, 8,discrete=True)" \
+  --dropout~"uniform(0.0, 0.5)" \
+  --repeat_augment~"uniform(0, 2,discrete=True)" \
+  --idx_combination_augmentations~"uniform(1, 15,discrete=True)"
