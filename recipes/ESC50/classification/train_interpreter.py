80fef00e8beed4d0ee6e4125295e905ab8656ebf
Cem Subakan <csubakan@gmail.com>
2022-11-21 08:48:35 -0500

    removed for loops and divide by all components in mask creation

--- recipes/ESC50/classification/train_interpreter.py
+++ recipes/ESC50/classification/train_interpreter.py
@@ -70,7 +70,7 @@ class InterpreterESC50Brain(sb.core.Brain):
         # cut the length of psi
         psi_out = psi_out[:, :, : X_stft_power.shape[1]]
 
-        # cem: do we need this here? 
+        # cem: do we need this here?
         reconstructed = self.hparams.nmf(
             psi_out
         )  #  generate log-mag spectrogram
@@ -88,15 +88,11 @@ class InterpreterESC50Brain(sb.core.Brain):
         psi_out = psi_out.squeeze()
         z = self.modules.theta.hard_att(psi_out).squeeze()
         theta_c_w = self.modules.theta.classifier[0].weight[pred_cl]
-        
+
         # some might be negative, relevance of component
-        r_c_x = (
-            theta_c_w * z / torch.abs(theta_c_w * z).max()
-        )  
+        r_c_x = theta_c_w * z / torch.abs(theta_c_w * z).max()
         # define selected components
-        L = torch.arange(r_c_x.shape[0])[
-            r_c_x > 0.2
-        ].tolist()  
+        L = torch.arange(r_c_x.shape[0])[r_c_x > 0.2].tolist()
 
         # get the log power spectra, this is needed as NMF is trained on log-power spectra
         X_stft_power_log = (
@@ -110,7 +106,7 @@ class InterpreterESC50Brain(sb.core.Brain):
         #     X_k = nmf_dictionary[:, k].unsqueeze(1) @ psi_out[k, :].unsqueeze(0)
         #     sum_X_k += X_k
         #     X_ks[i] = X_k
-        # cem : for the denominator we need to sum over all K, not just the selected ones. 
+        # cem : for the denominator we need to sum over all K, not just the selected ones.
         X_withselected = nmf_dictionary[:, L] @ psi_out[L, :]
         Xhat = nmf_dictionary @ psi_out
 
@@ -122,8 +118,13 @@ class InterpreterESC50Brain(sb.core.Brain):
         # get back to the standard stft
         X_int = torch.exp(X_int) - 1
 
-        # add the phase of the original audio 
-        X_int_wphase = (X_int.permute(1, 0).unsqueeze(0) * torch.exp(1j*X_stft_phase)).cpu().numpy().squeeze()
+        # add the phase of the original audio
+        X_int_wphase = (
+            (X_int.permute(1, 0).unsqueeze(0) * torch.exp(1j * X_stft_phase))
+            .cpu()
+            .numpy()
+            .squeeze()
+        )
 
         # invert back to time domain (cem: need to check if this is the exact same SB istft)
         # I am being lazy by using numpy here for istft as it supports complex numbers directly
@@ -158,7 +159,7 @@ class InterpreterESC50Brain(sb.core.Brain):
             x_int,
             self.hparams.sample_rate,
         )
-        
+
     def compute_forward(self, batch, stage):
         """Computation pipeline based on a encoder + sound classifier.
         Data augmentation and environmental corruption are applied to the
@@ -196,7 +197,14 @@ class InterpreterESC50Brain(sb.core.Brain):
             self.n_augment = len(wavs_aug_tot)
             lens = torch.cat([lens] * self.n_augment)
 
-        elif stage == sb.Stage.VALID and (self.hparams.epoch_counter.current % self.hparams.interpret_period) == 0:
+        elif (
+            stage == sb.Stage.VALID
+            and (
+                self.hparams.epoch_counter.current
+                % self.hparams.interpret_period
+            )
+            == 0
+        ):
             # save some samples
             self.interpret_batch(batch)
 
