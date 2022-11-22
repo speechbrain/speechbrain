28a9e1c72fe08723f6c713a9c0ec80aeb4b3680d
BenoitWang <wangyingzhi666@gmail.com>
2022-11-22 23:47:52 +0100

    add LJSpeech diffwave vocoder recipe

--- recipes/LJSpeech/TTS/README.md
+++ recipes/LJSpeech/TTS/README.md
@@ -30,12 +30,37 @@ To run this recipe, go into the "vocoder/hifi_gan/" folder and run:
 python train.py hparams/train.yaml --data_folder /path/to/LJspeech
 ```
 
-Training takes about X hours on an nvidia RTX8000.
+Training takes about 10 minutes/epoch on an nvidia RTX8000.
 
 The training logs are available [here](https://drive.google.com/drive/folders/19sLwV7nAsnUuLkoTu5vafURA9Fo2WZgG?usp=sharing)
 
 You can find the pre-trained model with an easy-inference function on [HuggingFace](https://huggingface.co/speechbrain/tts-hifigan-ljspeech).
 
+# DiffWave (Vocoder)
+The subfolder "vocoder/diffwave/" contains the [Diffwave](https://arxiv.org/pdf/2009.09761.pdf) vocoder.
+
+DiffWave is a versatile diffusion model for audio synthesis, which produces high-fidelity audio in different waveform generation tasks, including neural vocoding conditioned on mel spectrogram, class-conditional generation, and unconditional generation.
+
+Here it serves as a vocoder that converts a spectrogram into a waveform.
+
+To run this recipe, go into the "vocoder/diffwave/" folder and run:
+
+```
+python train.py hparams/train.yaml --data_folder /path/to/LJspeech
+```
+
+The scripts will output the results to <output_folder>/samples, for every training epoch
+
+We suggest using tensorboard_logger by setting `use_tensorboard: True` in the yaml file, thus Tensorboard should be installed.
+
+
+For inference, by setting `fast_sampling: True` , a fast sampling can be realized by passing user-defined variance schedules. According to the paper, high-quality audios can be generated with only 6 steps.
+
+Training takes about 10 minutes/epoch on an nvidia RTX8000.
+
+The training logs are available [here](to add)
+
+You can find the pre-trained model with an easy-inference function on [HuggingFace](to add).
 
 # **About SpeechBrain**
 - Website: https://speechbrain.github.io/
---
+++ speechbrain/lobes/models/DiffWave.py
@@ -0,0 +1,369 @@
+"""
+Neural network modules for DIFFWAVE: A VERSATILE DIFFUSION MODEL FOR
+AUDIO SYNTHESIS
+
+For more details: https://arxiv.org/pdf/2009.09761.pdf
+
+Authors
+ * Yingzhi WANG 2022
+"""
+
+# This code uses a significant portion of the LMNT implementation, even though it
+# has been modified and enhanced
+
+# https://github.com/lmnt-com/diffwave/blob/master/src/diffwave/model.py
+# *****************************************************************************
+# Copyright 2020 LMNT, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from speechbrain.nnet.CNN import Conv1d
+from speechbrain.nnet import linear
+from math import sqrt
+
+
+Linear = linear.Linear
+# To do: an SB wrapper for ConvTranspose2d
+ConvTranspose2d = nn.ConvTranspose2d
+
+
+@torch.jit.script
+def silu(x):
+    """sigmoid linear unit activation function
+    """
+    return x * torch.sigmoid(x)
+
+
+class DiffusionEmbedding(nn.Module):
+    """Embeds the diffusion step into an input vector of DiffWave
+
+    Arguments
+    ---------
+    max_steps: int
+        total difussion steps
+    """
+
+    def __init__(self, max_steps):
+        super().__init__()
+        self.register_buffer(
+            "embedding", self._build_embedding(max_steps), persistent=False
+        )
+        self.projection1 = Linear(input_size=128, n_neurons=512)
+        self.projection2 = Linear(input_size=512, n_neurons=512)
+
+    def forward(self, diffusion_step):
+        """forward function of diffusion step embedding
+
+        Arguments
+        ---------
+        diffusion_step:
+            which step of diffusion to execute
+        Returns
+        -------
+        diffusion step embedding: tensor [bs, 512]
+        """
+        if diffusion_step.dtype in [torch.int32, torch.int64]:
+            x = self.embedding[diffusion_step]
+        else:
+            x = self._lerp_embedding(diffusion_step)
+        x = self.projection1(x)
+        x = silu(x)
+        x = self.projection2(x)
+        x = silu(x)
+        return x
+
+    def _lerp_embedding(self, t):
+        """Deals with the cases where diffusion_step is not int
+
+        Arguments
+        ---------
+        t: which step of diffusion to execute
+        """
+        low_idx = torch.floor(t).long()
+        high_idx = torch.ceil(t).long()
+        low = self.embedding[low_idx]
+        high = self.embedding[high_idx]
+        return low + (high - low) * (t - low_idx)
+
+    def _build_embedding(self, max_steps):
+        """Build embeddings in a designed way
+
+        Arguments
+        ---------
+        max_steps: int
+            total difussion steps
+        """
+        steps = torch.arange(max_steps).unsqueeze(1)  # [T,1]
+        dims = torch.arange(64).unsqueeze(0)  # [1,64]
+        table = steps * 10.0 ** (dims * 4.0 / 63.0)  # [T,64]
+        table = torch.cat([torch.sin(table), torch.cos(table)], dim=1)
+        return table
+
+
+class SpectrogramUpsampler(nn.Module):
+    """Upsampler for spectrograms with Transposed Conv
+    Only the upsamling is done here, the layer-specific Conv can be found
+    in residual bloack to map the mel bands into 2× residual channels
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = ConvTranspose2d(
+            1, 1, [3, 32], stride=[1, 16], padding=[1, 8]
+        )
+        self.conv2 = ConvTranspose2d(
+            1, 1, [3, 32], stride=[1, 16], padding=[1, 8]
+        )
+
+    def forward(self, x):
+        """Upsamples spectrograms 256 times to match the length of audios
+        Hop length should be 256 when extracting mel spectrograms
+
+        Arguments
+        ---------
+        x: torch.tensor
+            input mel spectrogram [bs, 80, mel_len]
+        Returns
+        -------
+        upsampled spectrogram [bs, 80, mel_len*256]
+        """
+        x = torch.unsqueeze(x, 1)
+        x = self.conv1(x)
+        x = F.leaky_relu(x, 0.4)
+        x = self.conv2(x)
+        x = F.leaky_relu(x, 0.4)
+        x = torch.squeeze(x, 1)
+        return x
+
+
+class ResidualBlock(nn.Module):
+    """
+    Residual Block with dilated convolution
+
+    Arguments
+    ---------
+    n_mels:
+        input mel channels of conv1x1 for conditional vocoding task
+    residual_channels:
+        channels of audio convolution
+    dilation:
+        dilation cycles of audio convolution
+    uncond:
+        conditional/unconditional generation
+    """
+
+    def __init__(self, n_mels, residual_channels, dilation, uncond=False):
+        super().__init__()
+        self.dilated_conv = Conv1d(
+            in_channels=residual_channels,
+            out_channels=2 * residual_channels,
+            kernel_size=3,
+            dilation=dilation,
+            skip_transpose=True,
+            padding="same",
+            conv_init="kaiming",
+        )
+        self.diffusion_projection = Linear(
+            input_size=512, n_neurons=residual_channels
+        )
+
+        # conditional model
+        if not uncond:
+            self.conditioner_projection = Conv1d(
+                in_channels=n_mels,
+                out_channels=2 * residual_channels,
+                kernel_size=1,
+                skip_transpose=True,
+                padding="same",
+                conv_init="kaiming",
+            )
+        # unconditional model
+        else:
+            self.conditioner_projection = None
+
+        self.output_projection = Conv1d(
+            in_channels=residual_channels,
+            out_channels=2 * residual_channels,
+            kernel_size=1,
+            skip_transpose=True,
+            padding="same",
+            conv_init="kaiming",
+        )
+
+    def forward(self, x, diffusion_step, conditioner=None):
+        """
+        forward function of Residual Block
+
+        Arguments
+        ---------
+        x: torch.tensor
+            input sample [bs, 1, time]
+        diffusion_step: int
+            which step of diffusion to execute
+        conditioner: torch.tensor
+            the condition used for conditional generation
+        Returns
+        -------
+        residual output [bs, residual_channels, time]
+        a skip of residual branch [bs, residual_channels, time]
+        """
+        assert (
+            conditioner is None and self.conditioner_projection is None
+        ) or (
+            conditioner is not None and self.conditioner_projection is not None
+        )
+
+        diffusion_step = self.diffusion_projection(diffusion_step).unsqueeze(-1)
+        y = x + diffusion_step
+        if self.conditioner_projection is None:  # using a unconditional model
+            y = self.dilated_conv(y)
+        else:
+            conditioner = self.conditioner_projection(conditioner)
+            # for inference make sure that they have the same length
+            # conditioner = conditioner[:, :, y.shape[-1]]
+            y = self.dilated_conv(y) + conditioner
+
+        gate, filter = torch.chunk(y, 2, dim=1)
+        y = torch.sigmoid(gate) * torch.tanh(filter)
+
+        y = self.output_projection(y)
+        residual, skip = torch.chunk(y, 2, dim=1)
+        return (x + residual) / sqrt(2.0), skip
+
+
+class DiffWave(nn.Module):
+    """
+    DiffWave Model with dilated residual blocks
+
+    Arguments
+    ---------
+    input_channels:
+        input mel channels of conv1x1 for conditional vocoding task
+    residual_layers:
+        number of residual blocks
+    residual_channels:
+        channels of audio convolution
+    dilation_cycle_length:
+        dilation cycles of audio convolution
+    total_steps:
+        total steps of diffusion
+    unconditional:
+        conditional/unconditional generation
+    """
+
+    def __init__(
+        self,
+        input_channels,
+        residual_layers,
+        residual_channels,
+        dilation_cycle_length,
+        total_steps,
+        unconditional=False,
+    ):
+        super().__init__()
+        self.input_channels = input_channels
+        self.residual_layers = residual_layers
+        self.residual_channels = residual_channels
+        self.dilation_cycle_length = dilation_cycle_length
+        self.unconditional = unconditional
+        self.total_steps = total_steps
+        self.input_projection = Conv1d(
+            in_channels=1,
+            out_channels=self.residual_channels,
+            kernel_size=1,
+            skip_transpose=True,
+            padding="same",
+            conv_init="kaiming",
+        )
+        self.diffusion_embedding = DiffusionEmbedding(self.total_steps)
+
+        if self.unconditional:  # use unconditional model
+            self.spectrogram_upsampler = None
+        else:
+            self.spectrogram_upsampler = SpectrogramUpsampler()
+
+        self.residual_layers = nn.ModuleList(
+            [
+                ResidualBlock(
+                    self.input_channels,
+                    self.residual_channels,
+                    2 ** (i % self.dilation_cycle_length),
+                    uncond=self.unconditional,
+                )
+                for i in range(self.residual_layers)
+            ]
+        )
+        self.skip_projection = Conv1d(
+            in_channels=self.residual_channels,
+            out_channels=self.residual_channels,
+            kernel_size=1,
+            skip_transpose=True,
+            padding="same",
+            conv_init="kaiming",
+        )
+        self.output_projection = Conv1d(
+            in_channels=self.residual_channels,
+            out_channels=1,
+            kernel_size=1,
+            skip_transpose=True,
+            padding="same",
+            conv_init="zero",
+        )
+
+    def forward(self, audio, diffusion_step, spectrogram=None):
+        """
+        DiffWave forward function
+
+        Arguments
+        ---------
+        audio:
+            input gaussian sample
+        residual_layers:
+            number of residual blocks
+        residual_channels:
+            channels of audio convolution
+        dilation_cycle_length:
+            dilation cycles of audio convolution
+        total_steps:
+            total steps of diffusion
+        unconditional:
+            conditional/unconditional generation
+        Returns
+        -------
+        predicted noise [bs, 1, time]
+        """
+        assert (spectrogram is None and self.spectrogram_upsampler is None) or (
+            spectrogram is not None and self.spectrogram_upsampler is not None
+        )
+
+        x = self.input_projection(audio)
+        x = F.relu(x)
+
+        diffusion_step = self.diffusion_embedding(diffusion_step)
+        if self.spectrogram_upsampler:  # use conditional model
+            spectrogram = self.spectrogram_upsampler(spectrogram)
+
+        skip = None
+        for layer in self.residual_layers:
+            x, skip_connection = layer(x, diffusion_step, spectrogram)
+            skip = skip_connection if skip is None else skip_connection + skip
+
+        x = skip / sqrt(len(self.residual_layers))
+        x = self.skip_projection(x)
+        x = F.relu(x)
+        x = self.output_projection(x)
+        return x
