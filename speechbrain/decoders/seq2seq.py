a7c4e44c3176a699cbaac3cd90afc66817b9f7d3
Adel Moumen <adelmoumen.pro@gmail.com>
2022-11-28 13:39:40 +0100

    moved utils function to another file

--- speechbrain/decoders/seq2seq.py
+++ speechbrain/decoders/seq2seq.py
@@ -8,6 +8,7 @@ Authors
  * Sung-Lin Yeh 2020
 """
 import torch
+from speechbrain.decoders.utils import inflate_tensor, mask_by_condition
 
 # TODO: when scoring on words, in the scorer, add a function to None that can be used to do some preprocessing on the words
 
@@ -1134,65 +1135,3 @@ class S2STransformerBeamSearcher(S2SBeamSearcher):
         pred, attn = self.model.decode(memory, enc_states)
         prob_dist = self.softmax(self.fc(pred) / self.temperature)
         return prob_dist[:, -1, :], memory, attn
-
-
-def inflate_tensor(tensor, times, dim):
-    """This function inflates the tensor for times along dim.
-
-    Arguments
-    ---------
-    tensor : torch.Tensor
-        The tensor to be inflated.
-    times : int
-        The tensor will inflate for this number of times.
-    dim : int
-        The dim to be inflated.
-
-    Returns
-    -------
-    torch.Tensor
-        The inflated tensor.
-
-    Example
-    -------
-    >>> tensor = torch.Tensor([[1,2,3], [4,5,6]])
-    >>> new_tensor = inflate_tensor(tensor, 2, dim=0)
-    >>> new_tensor
-    tensor([[1., 2., 3.],
-            [1., 2., 3.],
-            [4., 5., 6.],
-            [4., 5., 6.]])
-    """
-    return torch.repeat_interleave(tensor, times, dim=dim)
-
-
-def mask_by_condition(tensor, cond, fill_value):
-    """This function will mask some element in the tensor with fill_value, if condition=False.
-
-    Arguments
-    ---------
-    tensor : torch.Tensor
-        The tensor to be masked.
-    cond : torch.BoolTensor
-        This tensor has to be the same size as tensor.
-        Each element represents whether to keep the value in tensor.
-    fill_value : float
-        The value to fill in the masked element.
-
-    Returns
-    -------
-    torch.Tensor
-        The masked tensor.
-
-    Example
-    -------
-    >>> tensor = torch.Tensor([[1,2,3], [4,5,6]])
-    >>> cond = torch.BoolTensor([[True, True, False], [True, False, False]])
-    >>> mask_by_condition(tensor, cond, 0)
-    tensor([[1., 2., 0.],
-            [4., 0., 0.]])
-    """
-    tensor = torch.where(
-        cond, tensor, torch.Tensor([fill_value]).to(tensor.device)
-    )
-    return tensor
