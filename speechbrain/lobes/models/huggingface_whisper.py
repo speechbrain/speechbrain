0a22f79477d49a130f6355a295c6cb05ec8ddee4
Adel Moumen <adelmoumen.pro@gmail.com>
2022-11-29 09:40:43 +0100

    doc + naming

--- speechbrain/lobes/models/huggingface_whisper.py
+++ speechbrain/lobes/models/huggingface_whisper.py
@@ -90,29 +90,38 @@ class HuggingFaceWhisper(nn.Module):
                 for param in self.model.encoder.parameters():
                     param.requires_grad = False
 
-    def forward(self, wav, tokens=None):
+    def forward(self, wav, decoder_input_ids=None):
         """Perform mel transformation and one step of the whisper (encoder-decoder).
 
         Arguments
         ---------
         wav : torch.Tensor (signal)
             A batch of audio signals to transform to features.
-        tokens : torch.Tensor
-            A batch of whisper decoder input ids. This is only necessary if the decoder is used.
+        decoder_input_ids : torch.Tensor
+            This is necessary if we want to use the decoder.
+
+            A batch of decoder inputs tokens.
+            The first tokens need to dictacte the behavior of the decoder.
+            It needs to start with the bos_token, the language token,
+            the task token, and finally the timestamp token.
+
+            Please refer to the whisper paper for more details or go to the
+            seq2seq2.py file in SpeechBrain to see how to generate the tokens
+            with Greedy Search and/or Beam Search.
         """
         if self.freeze:
             with torch.no_grad():
                 out_encoder = self.forward_encoder(wav)
                 if self.encoder_only:
                     return out_encoder
-                out = self.forward_decoder(out_encoder, tokens)
+                out = self.forward_decoder(out_encoder, decoder_input_ids)
                 return out
         else:
             if self.encoder_only:
                 return self.forward_encoder(wav)
             else:
                 out_encoder = self.forward_encoder(wav)
-                return self.forward_decoder(out_encoder, tokens)
+                return self.forward_decoder(out_encoder, decoder_input_ids)
 
     def forward_encoder(self, wav):
         """Perform one step of the whisper encoder.
@@ -143,18 +152,25 @@ class HuggingFaceWhisper(nn.Module):
             numpy_wav, return_tensors="pt", sampling_rate=self.sampling_rate
         ).input_features.to(wav.device)
 
-    def forward_decoder(self, audio_features, tokens):
+    def forward_decoder(self, audio_features, decoder_input_ids):
         """Perform one step of the whisper decoder.
         Arguments
         ---------
         audio_features : torch.Tensor
             A batch of audio features (mel + whisper encoding).
-        tokens : torch.Tensor (TO DO: ARE MORE INFO IT S NOT CLEAR)
-            A batch of whisper decoder input ids.
+        decoder_input_ids : torch.Tensor
+            A batch of decoder inputs tokens.
+            The first tokens need to dictacte the behavior of the decoder.
+            It needs to start with the bos_token, the language token,
+            the task token, and finally the timestamp token.
+
+            Please refer to the whisper paper for more details or go to the
+            seq2seq2.py file in SpeechBrain to see how to generate the tokens
+            with Greedy Search and/or Beam Search.
         """
         output_states = self.model.decoder(
             encoder_hidden_states=audio_features,
-            input_ids=tokens,
+            input_ids=decoder_input_ids,
             output_attentions=self.output_attentions,
         )
 
