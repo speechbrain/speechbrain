2adf24260af18d1666d056afcacc9e97fd677079
Luca Della Libera <luca.dellalib@gmail.com>
2022-12-13 11:51:43 -0500

    Make CustomPaddedBatch pickleable

--- recipes/CommonVoice/continual-learning/whisper/train_ft.py
+++ recipes/CommonVoice/continual-learning/whisper/train_ft.py
@@ -235,6 +235,23 @@ def dataio_prepare(hparams, tokenizer):
     return train_data, valid_data, test_data
 
 
+class CustomPaddedBatch(PaddedBatch):
+    def __init__(self, examples, hparams, *args, **kwargs):
+        for k in ["sig", "tokens_bos", "tokens_eos", "tokens"]:
+            max_len = max([len(x[k]) for x in examples])
+            pad_value = 0.0
+            if k in ["tokens_bos", "tokens"]:
+                pad_value = hparams["whisper"].tokenizer.pad_token_id
+            elif k == "tokens_eos":
+                pad_value = hparams["ignore_index"]
+            for example in examples:
+                x = example[k]
+                example[k] = torch.nn.functional.pad(
+                    x, [0, max_len - len(x)], value=pad_value
+                )
+        super().__init__(examples, *args, **kwargs)
+
+
 def test():
     # Command-line interface
     hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
@@ -276,24 +293,16 @@ def test():
     # NB: This tokenizer corresponds to the one used for Whisper
     asr_brain.tokenizer = tokenizer
 
-    class CustomPaddedBatch(PaddedBatch):
-        def __init__(self, examples, *args, **kwargs):
-            for k in ["sig", "tokens_bos", "tokens_eos", "tokens"]:
-                max_len = max([len(x[k]) for x in examples])
-                pad_value = 0.0
-                if k in ["tokens_bos", "tokens"]:
-                    pad_value = hparams["whisper"].tokenizer.pad_token_id
-                elif k == "tokens_eos":
-                    pad_value = hparams["ignore_index"]
-                for example in examples:
-                    x = example[k]
-                    example[k] = torch.nn.functional.pad(
-                        x, [0, max_len - len(x)], value=pad_value
-                    )
-            super().__init__(examples, *args, **kwargs)
-
-    hparams["train_dataloader_kwargs"]["collate_fn"] = CustomPaddedBatch
-    hparams["valid_dataloader_kwargs"]["collate_fn"] = CustomPaddedBatch
+    hparams["train_dataloader_kwargs"][
+        "collate_fn"
+    ] = lambda examples, *args, **kwargs: PaddedBatch(
+        examples, hparams, *args, **kwargs
+    )
+    hparams["valid_dataloader_kwargs"][
+        "collate_fn"
+    ] = lambda examples, *args, **kwargs: PaddedBatch(
+        examples, hparams, *args, **kwargs
+    )
 
     # Test on old locales
     for locale in hparams["old_locales"]:
@@ -366,24 +375,16 @@ def train():
     # NB: This tokenizer corresponds to the one used for Whisper
     asr_brain.tokenizer = tokenizer
 
-    class CustomPaddedBatch(PaddedBatch):
-        def __init__(self, examples, *args, **kwargs):
-            for k in ["sig", "tokens_bos", "tokens_eos", "tokens"]:
-                max_len = max([len(x[k]) for x in examples])
-                pad_value = 0.0
-                if k in ["tokens_bos", "tokens"]:
-                    pad_value = hparams["whisper"].tokenizer.pad_token_id
-                elif k == "tokens_eos":
-                    pad_value = hparams["ignore_index"]
-                for example in examples:
-                    x = example[k]
-                    example[k] = torch.nn.functional.pad(
-                        x, [0, max_len - len(x)], value=pad_value
-                    )
-            super().__init__(examples, *args, **kwargs)
-
-    hparams["train_dataloader_kwargs"]["collate_fn"] = CustomPaddedBatch
-    hparams["valid_dataloader_kwargs"]["collate_fn"] = CustomPaddedBatch
+    hparams["train_dataloader_kwargs"][
+        "collate_fn"
+    ] = lambda examples, *args, **kwargs: PaddedBatch(
+        examples, hparams, *args, **kwargs
+    )
+    hparams["valid_dataloader_kwargs"][
+        "collate_fn"
+    ] = lambda examples, *args, **kwargs: PaddedBatch(
+        examples, hparams, *args, **kwargs
+    )
 
     # Train on new locales
     for locale in hparams["new_locales"]:
