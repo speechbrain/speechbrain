a844e7a8c391217ba64aa4d4e9859861b4b906d6
Titouan Parcollet <parcollet.titouan@gmail.com>
2023-01-02 17:08:16 +0000

    go

--- recipes/LibriSpeech/ASR/transducer/train.py
+++ recipes/LibriSpeech/ASR/transducer/train.py
@@ -33,7 +33,7 @@ Authors
 import os
 import sys
 import torch
-from torch.nn import GELU
+from torch.nn import LeakyReLU
 import logging
 import speechbrain as sb
 from speechbrain.utils.distributed import run_on_main
@@ -78,11 +78,11 @@ class ASR(sb.Brain):
 
         src = self.modules.CNN(feats)
         x = self.modules.enc(src, wav_lens, pad_idx=self.hparams.pad_index)
-        x = GELU()(self.modules.proj_enc(x))
+        x = LeakyReLU()(self.modules.proj_enc(x))
 
         e_in = self.modules.emb(tokens_with_bos)
         h, _ = self.modules.dec(e_in)
-        h = GELU()(self.modules.proj_dec(h))
+        h = LeakyReLU()(self.modules.proj_dec(h))
 
         # Joint network
         # add labelseq_dim to the encoder tensor: [B,T,H_enc] => [B,T,1,H_enc]
