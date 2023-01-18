5cbdeb72074a87b32e6beb116f187c03a668a7da
GaelleLaperriere <80116464+GaelleLaperriere@users.noreply.github.com>
2023-01-18 19:48:54 +0100

    add comment

--- speechbrain/utils/edit_distance.py
+++ speechbrain/utils/edit_distance.py
@@ -479,6 +479,7 @@ def wer_details_by_utterance(
         # Compute edits for this utterance
         table = op_table(ref_tokens, hyp_tokens)
         ops = count_ops(table)
+        # Take into account "" outputs as empty
         if ref_tokens[0] == "" and hyp_tokens[0] == "":
             num_ref_tokens = 0
         else:
