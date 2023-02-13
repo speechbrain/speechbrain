a4857aee6af3d3d0078d93df15a64dce3af3f009
GaelleLaperriere <laperriere.gaelle@gmail.com>
2023-02-13 15:32:56 +0100

    tests

--- tests/.run-recipe-tests.sh
+++ tests/.run-recipe-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -c 'from speechbrain.utils.recipe_tests import run_recipe_tests; print("TEST FAILED!") if not(run_recipe_tests(run_opts="--device=cuda")) else print("TEST PASSED")'
+python -c 'from tests.utils.recipe_tests import run_recipe_tests; print("TEST FAILED!") if not(run_recipe_tests(run_opts="--device=cuda")) else print("TEST PASSED")'
--- tests/.run-url-checks.sh
+++ tests/.run-url-checks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install requests
-python -c 'from speechbrain.utils.check_url import check_links; print("TEST FAILED!") if not(check_links()) else print("TEST PASSED!")'
+python -c 'from tests.utils.check_url import check_links; print("TEST FAILED!") if not(check_links()) else print("TEST PASSED!")'
