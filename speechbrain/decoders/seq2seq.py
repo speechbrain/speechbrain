e4e73d0d1bf5b3b83938a312bb7308cf96605b2e
Adel Moumen <adelmoumen.pro@gmail.com>
2023-02-17 15:11:41 +0100

    remove todo

--- speechbrain/decoders/seq2seq.py
+++ speechbrain/decoders/seq2seq.py
@@ -10,8 +10,6 @@ Authors
 import torch
 from speechbrain.decoders.utils import inflate_tensor, mask_by_condition
 
-# TODO: when scoring on words, in the scorer, add a function to None that can be used to do some preprocessing on the words
-
 
 class AlivedHypotheses(torch.nn.Module):
     """ This class handle the data for the hypotheses during the decoding.
