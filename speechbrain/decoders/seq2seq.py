46b1ba9d576b28ad0c6bfe22a25ad5497c893a01
Adel Moumen <adelmoumen.pro@gmail.com>
2023-02-23 16:02:50 +0100

    whisper beam search + fix n_out

--- speechbrain/decoders/seq2seq.py
+++ speechbrain/decoders/seq2seq.py
@@ -8,7 +8,7 @@ Authors
  * Sung-Lin Yeh 2020
 """
 import torch
-from speechbrain.decoders.utils import inflate_tensor, mask_by_condition
+from speechbrain.decoders.utils import inflate_tensor, mask_by_condition, _update_mem
 
 
 class AlivedHypotheses(torch.nn.Module):
@@ -175,6 +175,13 @@ class S2SBaseSearcher(torch.nn.Module):
     def change_max_decoding_length(self, min_decode_steps, max_decode_steps):
         """set the minimum/maximum length the decoder can take."""
         return min_decode_steps, max_decode_steps
+    
+    def set_n_out(self):
+        """set the number of output tokens. 
+        Overrides this function if the fc layer is embedded 
+        in the model, e.g., Whisper.
+        """
+        self.n_out = self.fc.w.out_features
 
 class S2SGreedySearcher(S2SBaseSearcher):
     """ This class implements the general forward-pass of
@@ -721,7 +728,8 @@ class S2SBeamSearcher(S2SBaseSearcher):
         self.device = enc_states.device
         self.batch_size = enc_states.shape[0]
         self.n_bh = self.batch_size * self.beam_size
-        self.n_out = self.fc.w.out_features
+
+        self.set_n_out()
 
         memory, scorer_memory = self._update_reset_memory(enc_states, enc_lens)
 
@@ -1221,12 +1229,7 @@ class S2STransformerBeamSearcher(S2SBeamSearcher):
 
     def forward_step(self, inp_tokens, memory, enc_states, enc_lens):
         """Performs a step in the implemented beamsearcher."""
-        if memory is None:
-            memory = torch.empty(
-                inp_tokens.size(0), 0, device=inp_tokens.device
-            )
-        # Append the predicted token of the previous step to existing memory.
-        memory = torch.cat([memory, inp_tokens.unsqueeze(1)], dim=-1)
+        memory = _update_mem(memory, inp_tokens)
         pred, attn = self.model.decode(memory, enc_states)
         prob_dist = self.softmax(self.fc(pred) / self.temperature)
         return prob_dist[:, -1, :], memory, attn
@@ -1323,10 +1326,7 @@ class S2SWhisperGreedySearch(S2SGreedySearcher):
 
     def forward_step(self, inp_tokens, memory, enc_states, enc_lens):
         """Performs a step in the implemented beamsearcher."""
-        if memory is None:
-            memory = inp_tokens.unsqueeze(1)
-        else:
-            memory = torch.cat([memory, inp_tokens.unsqueeze(1)], dim=-1)
+        memory = _update_mem(inp_tokens, memory)
 
         # WARNING: the max_decode_ratio need to be under 448 because
         #  of positinal encoding
@@ -1341,3 +1341,120 @@ class S2SWhisperGreedySearch(S2SGreedySearcher):
             int(self.min_decode_ratio * self.max_length),
             int(self.max_decode_ratio * self.max_length),
         )
+
+class S2SWhisperBeamSearch(S2SBeamSearcher):
+    """This class implements the beam search decoding
+    for Whisper neural nets made by OpenAI in
+    https://cdn.openai.com/papers/whisper.pdf.
+    Arguments
+    ---------
+    module : list with the followings one:
+        model : torch.nn.Module
+            A whisper model. It should have a decode() method.
+        ctc_lin : torch.nn.Module (optional)
+            A linear output layer for CTC.
+    language_token : int
+        The token to use for language.
+    bos_token : int
+        The token to use for beginning of sentence.
+    task_token : int
+        The token to use for task.
+    timestamp_token : int
+        The token to use for timestamp.
+    max_length : int
+        The maximum decoding steps to perform.
+        The Whisper model has a maximum length of 448.
+    **kwargs
+        Arguments to pass to S2SBeamSearcher
+    """
+
+    def __init__(
+        self,
+        module,
+        temperature=1.0,
+        language_token=50259,
+        bos_token=50258,
+        task_token=50359,
+        timestamp_token=50363,
+        max_length=448,
+        **kwargs,
+    ):
+        super(S2SWhisperBeamSearch, self).__init__(**kwargs)
+
+        self.model = module[0]
+
+        self.softmax = torch.nn.LogSoftmax(dim=-1)
+
+        self.temperature = temperature
+
+        self.decoder_input_tokens = None
+        self.language_token = language_token  # default language is english
+        self.bos_token = bos_token  # always this value
+        self.task_token = task_token  # default task is transcribe
+        self.timestamp_token = timestamp_token  # default is notimestamp
+
+        self.max_length = max_length - 3  # -3 for [bos, language, task]
+
+    def set_language_token(self, language_token):
+        """set the language token to use for the decoder input."""
+        self.language_token = language_token
+
+    def set_bos_token(self, bos_token):
+        """set the bos token to use for the decoder input."""
+        self.bos_token = bos_token
+
+    def set_task_token(self, task_token):
+        """set the task token to use for the decoder input."""
+        self.task_token = task_token
+
+    def set_timestamp_token(self, timestamp_token):
+        """set the timestamp token to use for the decoder input."""
+        self.timestamp_token = timestamp_token
+        # need to reset bos_index too as timestamp_token is the first
+        # inp_token and need to be the first so that the first input gave
+        # to the model is [bos, language, task, timestamp] (order matters).
+        self.bos_index = self.timestamp_token
+
+    def change_max_decoding_length(self, min_decode_steps, max_decode_steps):
+        """set the minimum/maximum length the decoder can take."""
+        return (
+            int(self.min_decode_ratio * self.max_length),
+            int(self.max_decode_ratio * self.max_length),
+        )
+
+    def set_decoder_input_tokens(self, decoder_input_tokens):
+        """decoder_input_tokens are the tokens used as input to the decoder.
+        They are directly taken from the tokenizer.prefix_tokens attribute.
+        decoder_input_tokens = [bos_token, language_token, task_token, timestamp_token]
+        """
+        self.set_bos_token(decoder_input_tokens[0])
+        self.set_language_token(decoder_input_tokens[1])
+        self.set_task_token(decoder_input_tokens[2])
+        self.set_timestamp_token(decoder_input_tokens[3])
+
+        # bos will be timestamp in our case.
+        self.decoder_input_tokens = [
+            self.bos_token,
+            self.language_token,
+            self.task_token,
+        ]
+
+    def reset_mem(self, batch_size, device):
+        """This method set the first tokens to be decoder_input_tokens during search."""
+        return torch.tensor([self.decoder_input_tokens] * batch_size).to(device)
+
+    def permute_mem(self, memory, index):
+        """Permutes the memory."""
+        memory = torch.index_select(memory, dim=0, index=index)
+        return memory
+    
+    def set_n_out(self):
+        """set the number of output tokens."""
+        self.n_out = self.model.model.decoder.embed_tokens.weight.shape[0]
+
+    def forward_step(self, inp_tokens, memory, enc_states, enc_lens):
+        """Performs a step in the implemented beamsearcher."""
+        memory = _update_mem(inp_tokens, memory)
+        dec_out, attn, = self.model.forward_decoder(enc_states, memory)
+        log_probs = self.softmax(dec_out[:, -1] / self.temperature)
+        return log_probs, memory, attn
