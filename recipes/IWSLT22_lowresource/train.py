b80a24a1ec7edb39fc77ef5cff56664687e4d54f
Adel Moumen <adelmoumen.pro@gmail.com>
2023-02-24 07:47:36 +0100

    iwslt22_lowresource

--- recipes/IWSLT22_lowresource/train.py
+++ recipes/IWSLT22_lowresource/train.py
@@ -14,6 +14,7 @@ from speechbrain.utils.distributed import run_on_main
 from hyperpyyaml import load_hyperpyyaml
 from sacremoses import MosesDetokenizer
 
+from speechbrain.utils.data_utils import undo_padding
 
 # Define training procedure
 class ST(sb.core.Brain):
@@ -48,9 +49,22 @@ class ST(sb.core.Brain):
         hyps = None
         if stage == sb.Stage.VALID:
             # the output of the encoder (enc) is used for valid search
-            hyps, _ = self.hparams.valid_search(src.detach(), wav_lens)
+            topk_tokens, topk_lens, _, _ = self.hparams.valid_search(src.detach(), wav_lens)
+
+            # Select the best hypothesis
+            best_hyps, best_lens = topk_tokens[:, 0, :], topk_lens[:, 0]
+
+            # Convert best hypothesis to list
+            hyps = undo_padding(best_hyps, best_lens)
+
         elif stage == sb.Stage.TEST:
-            hyps, _ = self.hparams.test_search(src.detach(), wav_lens)
+            topk_tokens, topk_lens, _, _ = self.hparams.test_search(src.detach(), wav_lens)
+
+            # Select the best hypothesis
+            best_hyps, best_lens = topk_tokens[:, 0, :], topk_lens[:, 0]
+
+            # Convert best hypothesis to list
+            hyps = undo_padding(best_hyps, best_lens)
 
         return p_seq, wav_lens, hyps
 
