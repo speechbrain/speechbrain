1050519c0a4f49652b6d3e127245ae5bcfd8ec76
salah-zaiem <zaiemsalah@gmail.com>
2023-03-23 04:20:21 +0100

    reformatted downsampler file

--- speechbrain/lobes/downsampling.py
+++ speechbrain/lobes/downsampling.py
@@ -1,12 +1,11 @@
 import torch
-import torchaudio
 import torchaudio.transforms as T
 from speechbrain.nnet.CNN import Conv1d
 from speechbrain.nnet.pooling import Pooling1d
 
-class Downsampler(torch.nn.Module):
 
-    def forward(self, x) :
+class Downsampler(torch.nn.Module):
+    def forward(self, x):
         return self.downsampler(x)
 
 
@@ -14,27 +13,39 @@ class SignalDownsampler(Downsampler):
     def __init__(self, downsampling_factor, initial_sampling_rate):
         super().__init__()
         self.downsampling_factor = downsampling_factor
-        self.target_ds_rate = int(initial_sampling_rate/downsampling_factor)
-        self.downsampler = T.Resample(initial_sampling_rate, self.target_ds_rate, dtype=torch.float32)
+        self.target_ds_rate = int(initial_sampling_rate / downsampling_factor)
+        self.downsampler = T.Resample(
+            initial_sampling_rate, self.target_ds_rate, dtype=torch.float32
+        )
+
 
 class Conv1DDownsampler(Downsampler):
-    def __init__(self,downsampling_factor, kernel_size):
+    def __init__(self, downsampling_factor, kernel_size):
         super().__init__()
-        self.kernel_size =kernel_size
+        self.kernel_size = kernel_size
         self.downsampling_factor = downsampling_factor
-        self.downsampler = Conv1d(stride=self.downsampling_factor,
-                                padding="valid", kernel_size = self.kernel_size,
-                                out_channels=1, input_shape = [None, None])
+        self.downsampler = Conv1d(
+            stride=self.downsampling_factor,
+            padding="valid",
+            kernel_size=self.kernel_size,
+            out_channels=1,
+            input_shape=[None, None],
+        )
 
 
 class PoolingDownsampler(Downsampler):
-    def __init__(self, downsampling_factor, kernel_size, padding=0,pool_type="avg"):
+    def __init__(
+        self, downsampling_factor, kernel_size, padding=0, pool_type="avg"
+    ):
         super().__init__()
         self.kernel_size = kernel_size
         self.padding = padding
         self.pool_type = pool_type
         self.downsampling_factor = downsampling_factor
-        self.downsampler = Pooling1d(stride=self.downsampling_factor,
-                                    padding=self.padding, kernel_size = self.kernel_size,
-                                    input_dims=3, pool_type=self.pool_type)
-
+        self.downsampler = Pooling1d(
+            stride=self.downsampling_factor,
+            padding=self.padding,
+            kernel_size=self.kernel_size,
+            input_dims=3,
+            pool_type=self.pool_type,
+        )
