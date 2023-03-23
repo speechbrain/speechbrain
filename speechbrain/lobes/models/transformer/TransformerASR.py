216a74d9764e016a1556d453601ea31456921e35
Titouan Parcollet <parcollet.titouan@gmail.com>
2023-03-23 15:10:59 +0000

    GL understanding THE FUCK IS GOING ON

--- speechbrain/lobes/models/transformer/TransformerASR.py
+++ speechbrain/lobes/models/transformer/TransformerASR.py
@@ -244,13 +244,12 @@ class TransformerASR(TransformerInterface):
 
         tgt = self.custom_tgt_module(tgt)
         if self.attention_type == "RelPosMHAXL":
-            # we use fixed positional encodings in the decoder
+            # use standard sinusoidal pos encoding in decoder
             tgt = tgt + self.positional_encoding_decoder(tgt)
-            # pos_embs_target = self.positional_encoding(tgt)
             pos_embs_encoder = None  # self.positional_encoding(src)
             pos_embs_target = None
         elif self.positional_encoding_type == "fixed_abs_sine":
-            tgt = tgt + self.positional_encoding(tgt)  # add the encodings here
+            tgt = tgt + self.positional_encoding(tgt)
             pos_embs_target = None
             pos_embs_encoder = None
 
