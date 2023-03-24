fix mixed precision

speechbrain/core.py
@@ -920,7 +920,7 @@ class Brain:
         should_step = self.step % self.grad_accumulation_factor == 0
         # Managing automatic mixed precision
         if self.auto_mix_prec:
-            with torch.autocast():
+            with torch.autocast(device_type=torch.device(self.device).type):
                 outputs = self.compute_forward(batch, Stage.TRAIN)
 
             # Losses are excluded from mixed precision to avoid instabilities
@@ -938,7 +938,7 @@ class Brain:
                 self.optimizer_step += 1
         else:
             with torch.autocast(
-                device_type=self.device,
+                device_type=torch.device(self.device).type,
                 dtype=torch.bfloat16,
                 enabled=self.bfloat16_mix_prec,
             ):