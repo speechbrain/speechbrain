67a98924f9b9d182cbeb73ade4ab9e93238c74bd
Adel Moumen <adelmoumen.pro@gmail.com>
2023-03-25 16:48:25 +0100

    docstring example

--- speechbrain/pretrained/interfaces.py
+++ speechbrain/pretrained/interfaces.py
@@ -2881,13 +2881,13 @@ class WhisperASR(Pretrained):
     (transcribe()) to transcribe speech. The given YAML must contains the fields
     specified in the *_NEEDED[] lists.
 
-    # Example
-    # -------
-    # >>> from speechbrain.pretrained.interfaces import foreign_class
-    # >>> tmpdir = getfixture("tmpdir")
-    # >>> asr_model = WhisperASR.from_hparams(source="speechbrain/asr-whisper-large-v2-commonvoice-fr", savedir=tmpdir,)
-    # >>> asr_model.transcribe_file("tests/samples/example2.wav")
-    # """
+    Example
+    -------
+    >>> from speechbrain.pretrained.interfaces import foreign_class
+    >>> tmpdir = getfixture("tmpdir")
+    >>> asr_model = WhisperASR.from_hparams(source="speechbrain/asr-whisper-large-v2-commonvoice-fr", savedir=tmpdir,)
+    >>> asr_model.transcribe_file("tests/samples/example2.wav")
+    """
 
     HPARAMS_NEEDED = ["language"]
     MODULES_NEEDED = ["whisper", "decoder"]
