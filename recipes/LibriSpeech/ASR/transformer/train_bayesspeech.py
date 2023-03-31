57b605957d68fa2c36cb84a28b8e771be4036441
Luca Della Libera <luca.dellalib@gmail.com>
2023-03-31 13:02:17 -0400

    Fix checkpointing

--- recipes/LibriSpeech/ASR/transformer/train_bayesspeech.py
+++ recipes/LibriSpeech/ASR/transformer/train_bayesspeech.py
@@ -496,12 +496,7 @@ if __name__ == "__main__":
         parameters,
     )
     hparams["model"] = torch.nn.ModuleList(
-        [
-            hparams["CNN"],
-            hparams["Transformer"],
-            hparams["seq_lin"],
-            hparams["ctc_lin"],
-        ]
+        [hparams["CNN"], hparams["seq_lin"], hparams["ctc_lin"],]
     )
     hparams["test_search"].modules = hparams["valid_search"].modules = [
         hparams["Transformer"],
@@ -509,7 +504,9 @@ if __name__ == "__main__":
         hparams["ctc_lin"],
     ]
     hparams["checkpointer"].recoverables["model"] = hparams["model"]
-    breakpoint()
+    hparams["checkpointer"].add_recoverable(
+        "Transformer", hparams["Transformer"],
+    )
     # ###################################################################
 
     # Trainer initialization
