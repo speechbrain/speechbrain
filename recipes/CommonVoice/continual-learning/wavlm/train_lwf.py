Fix gradient checkpointing

recipes/CommonVoice/continual-learning/wavlm/train_lwf.py
@@ -43,7 +43,7 @@ class ASR(sb.Brain):
 
         # Forward encoder + projection
         if self.hparams.gradient_checkpointing:
-            tokens.requires_grad_()
+            wavs.requires_grad_()
             logits = torch.utils.checkpoint.checkpoint(
                 self.modules.wavlm, wavs, wav_lens,
             )

recipes/CommonVoice/continual-learning/whisper/train_lwf.py
@@ -50,7 +50,7 @@ class ASR(sb.Brain):
 
         # Forward encoder + decoder
         if self.hparams.gradient_checkpointing:
-            bos_tokens.requires_grad_()
+            wavs.requires_grad_()
             enc_out, logits, _ = torch.utils.checkpoint.checkpoint(
                 self.modules.whisper, wavs, bos_tokens,
             )

recipes/CommonVoice/continual-learning/whisper/train_pnn.py
@@ -50,7 +50,7 @@ class ASR(sb.Brain):
 
         # Forward encoder + decoder
         if self.hparams.gradient_checkpointing:
-            bos_tokens.requires_grad_()
+            wavs.requires_grad_()
             enc_out, logits, _ = torch.utils.checkpoint.checkpoint(
                 self.modules.whisper, wavs, bos_tokens,
             )
@@ -360,7 +360,7 @@ def train(hparams, run_opts):
         ]
 
         # Unfreeze embedding layer
-        hparams["whisper"].model.decoder.embed_tokens.requires_grad_()
+        hparams["whisper"].model.decoder.embed_wavs.requires_grad_()
 
         # Log total number of tokens
         logging.info(