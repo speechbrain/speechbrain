8fc69b3a450904d400fa648d9264653cf94bf017
Adel Moumen <adelmoumen.pro@gmail.com>
2023-06-15 14:51:32 +0200

    pre-commit + docstring

--- speechbrain/utils/optimizers.py
+++ speechbrain/utils/optimizers.py
@@ -5,12 +5,26 @@ Authors
 """
 
 
-def rm_vector_weight_decay(
-    modules,
-):
+def rm_vector_weight_decay(modules):
+    """Put vectors in a parameter group without weight decay
+
+    Takes in a list of modules and separates their parameters into two parameter groups,
+    which can be passed to a PyTorch Optimizer class. Vector parameters get weight_decay overridden to zero.
+    This is particularly useful for biases and norms, which we expect to deviate from zero. Other vectors as parameters are also likely not meant to be pushed toward zero.
+
+    Arguments
+    ---------
+    modules : torch.ModuleList, torch.Module
+        Torch modules to operate on
+
+    Returns
+    -------
+    list
+        The parameter groups in the Pytorch Optimizer specification format.
+    """
     decay = []
     no_decay = []
-    for name, param in modules.named_parameters():
+    for _, param in modules.named_parameters():
         if not param.requires_grad:
             continue
         if len(param.shape) == 1:
