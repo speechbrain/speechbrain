56b0421c9dd83b7c53b64442992c9bc1bd567188
Adel Moumen <88119391+Adel-Moumen@users.noreply.github.com>
2023-06-15 14:42:59 +0200

    Update speechbrain/utils/optimizers.py
    Co-authored-by: Aku Rouhe <akurouhe@hotmail.com>

--- speechbrain/utils/optimizers.py
+++ speechbrain/utils/optimizers.py
@@ -5,19 +5,19 @@ Authors
 """
 
 
-def rm_weight_decay_bias_and_norm_params(
-    modules, weight_decay,
+def rm_vector_weight_decay(
+    modules,
 ):
     decay = []
     no_decay = []
     for name, param in modules.named_parameters():
         if not param.requires_grad:
             continue
-        if len(param.shape) == 1 and ("bias" in name or "norm" in name):
+        if len(param.shape) == 1:
             no_decay.append(param)
         else:
             decay.append(param)
     return [
         {"params": no_decay, "weight_decay": 0.0},
-        {"params": decay, "weight_decay": weight_decay},
+        {"params": decay},
     ]
