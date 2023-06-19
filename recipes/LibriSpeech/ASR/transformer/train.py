3c06180544b56640c5b3dc62f0b018e6fa00bbdc
Titouan Parcollet <t.parcollet@samsung.com>
2023-06-19 13:51:07 +0000

    Reach 2.0 on conformer and retrain all

--- recipes/LibriSpeech/ASR/transformer/train.py
+++ recipes/LibriSpeech/ASR/transformer/train.py
@@ -350,7 +350,7 @@ def dataio_prepare(hparams):
     def audio_pipeline_train(wav):
         # Speed Perturb is done here so it is multi-threaded with the
         # workers of the dataloader (faster).
-        if hparams["speed_perturb"]:
+        if hasattr(hparams, "speed_perturb"):
             sig = sb.dataio.dataio.read_audio(wav)
 
             sig = hparams["speed_perturb"](sig.unsqueeze(0)).squeeze(0)
