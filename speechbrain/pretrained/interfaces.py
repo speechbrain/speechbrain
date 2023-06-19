f52a030f3cb6c63bb8f63685a42b2cd18e4baeed
Adel Moumen <adelmoumen.pro@gmail.com>
2023-06-19 17:49:58 +0200

    precommit

--- speechbrain/pretrained/interfaces.py
+++ speechbrain/pretrained/interfaces.py
@@ -1457,7 +1457,14 @@ class VAD(Pretrained):
         # Fix edge cases (when a speech starts in the last frames)
         if (prob_th == 1).nonzero().shape[0] % 2 == 1:
             prob_th = torch.cat(
-                (prob_th, torch.Tensor([1.0]).unsqueeze(0).unsqueeze(2).to(self.device)), dim=1
+                (
+                    prob_th,
+                    torch.Tensor([1.0])
+                    .unsqueeze(0)
+                    .unsqueeze(2)
+                    .to(self.device),
+                ),
+                dim=1,
             )
 
         # Where prob_th is 1 there is a change
