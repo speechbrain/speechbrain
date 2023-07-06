70d70449328341ff728a7b56dfb037b0db2baf14
Adel Moumen <adelmoumen.pro@gmail.com>
2023-07-06 19:40:37 +0200

    starting to fill the obj

--- speechbrain/decoders/ctc/prefix_beam_search.py
+++ speechbrain/decoders/ctc/prefix_beam_search.py
@@ -19,28 +19,44 @@ class CTCPrefixBeamSearch(CTCBaseSearcher):
     def __init__(self, blank_index, vocab_list, kenlm_model_path=None, unigrams=None, space_index=-1, beam_width=100, beam_prune_logp=-10, token_prune_min_logp=-5, history_prune=True, topk=1):
         super().__init__(blank_index, vocab_list, space_index, kenlm_model_path, unigrams, beam_width, beam_prune_logp, token_prune_min_logp, history_prune, topk)
     
-    def _get_new_beam(self, new_prefix, last_token, last_token_index, beams, p=None, previous_beam=None):
+    def _get_new_beam(self, new_prefix, new_token, new_token_index, beams, p=None, previous_beam=None):
         for beam in beams:
             if beam.text == new_prefix:
                 if p and p > beam.p:
                     beam.p = p 
-                beam.last_token = last_token
-                beam.last_token_index = last_token_index
+                beam.last_token = new_token
+                beam.last_token_index = new_token_index
                 return beam 
         
-        new_beam = CTCBeam(
-            text=new_prefix,
-            next_word="",
-            partial_word="",
-            last_token=last_token,
-            last_token_index=last_token_index,
-            text_frames=[],
-            partial_frames=(-1, -1),
-            score=-math.inf,
-            score_ctc=-math.inf,
-            lm_score=0.0,
-            p_b=-math.inf,
-        )
+        if new_token_index == self.space_index:
+            new_beam = CTCBeam(
+                text=new_prefix,
+                next_word=beam.partial_word,
+                partial_word="",
+                last_token=new_token,
+                last_token_index=new_token_index,
+                text_frames=[],
+                partial_frames=(-1, -1),
+                score=-math.inf,
+                score_ctc=-math.inf,
+                lm_score=0.0,
+                p_b=-math.inf,
+            )     
+            print("partial wrd = ", beam.partial_word)       
+        else: 
+            new_beam = CTCBeam(
+                text=new_prefix,
+                next_word="",
+                partial_word=beam.partial_word,
+                last_token=new_token,
+                last_token_index=new_token_index,
+                text_frames=[],
+                partial_frames=(-1, -1),
+                score=-math.inf,
+                score_ctc=-math.inf,
+                lm_score=0.0,
+                p_b=-math.inf,
+            )
         beams.append(new_beam)
         if previous_beam:
             new_beam.p = previous_beam.p
@@ -96,10 +112,11 @@ class CTCPrefixBeamSearch(CTCBaseSearcher):
                         n_p_nb = np.logaddexp(n_p_nb, beam.score_ctc + p_token)
                     new_beam.n_p_nb = n_p_nb 
 
-                    # max number of beams to consider during this step as an heuristic ? 
             for beam in beams:
                 beam.step()
 
             beams = sorted(beams, key=lambda x: x.score, reverse=True)[:self.beam_width]
-
+        exit()
+        print(beams)
+        exit()
         return beams
