9c227305baa7e29fa84724e51b22e58d397ac655
fpaissan <me@francescopaissan.it>
2023-07-21 19:02:35 +0200

    passing docstrings

--- speechbrain/lobes/models/CRDNN.py
+++ speechbrain/lobes/models/CRDNN.py
@@ -302,7 +302,7 @@ class reCNN_Block(sb.nnet.containers.Sequential):
     >>> block = reCNN_Block(input_shape=inputs.shape, channels=32)
     >>> outputs = block(inputs)
     >>> outputs.shape
-    torch.Size([10, 15, 30, 32])
+    torch.Size([10, 13, 30, 32])
     """
 
     def __init__(
