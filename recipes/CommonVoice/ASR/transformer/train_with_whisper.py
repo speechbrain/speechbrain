183ac61da0dd2f6b141d43d7ea7f9cadb5dba39c
Adel Moumen <adelmoumen.pro@gmail.com>
2023-07-22 19:52:29 +0200

    cv

--- recipes/CommonVoice/ASR/transformer/train_with_whisper.py
+++ recipes/CommonVoice/ASR/transformer/train_with_whisper.py
@@ -307,15 +307,15 @@ if __name__ == "__main__":
     # We dynamicaly add the tokenizer to our brain class.
     # NB: This tokenizer corresponds to the one used for Whisper.
     asr_brain.tokenizer = tokenizer
-    if hparams["test_only"] is False:
-        # Training
-        asr_brain.fit(
-            asr_brain.hparams.epoch_counter,
-            train_data,
-            valid_data,
-            train_loader_kwargs=hparams["train_loader_kwargs"],
-            valid_loader_kwargs=hparams["valid_loader_kwargs"],
-        )
+
+    # Training
+    asr_brain.fit(
+        asr_brain.hparams.epoch_counter,
+        train_data,
+        valid_data,
+        train_loader_kwargs=hparams["train_loader_kwargs"],
+        valid_loader_kwargs=hparams["valid_loader_kwargs"],
+    )
 
     # Testing
     asr_brain.hparams.wer_file = hparams["output_folder"] + "/wer_test.txt"
