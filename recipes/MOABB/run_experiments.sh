a7fbdabac64ed63311aa7c61fc059177ff9243ab
Mirco Ravanelli <mirco.ravanelli@gmail.com>
2023-08-03 15:53:23 -0400

    refactor run_experiments.sh

--- recipes/MOABB/run_experiments.sh
+++ recipes/MOABB/run_experiments.sh
@@ -1,28 +1,83 @@
 #!/bin/bash
 
-# Script to run leave-one-subject-out and/or leave-one-session-out training, optionally with multiple seeds
+###########################################################
+# Script to run leave-one-subject-out and/or leave-one-session-out training, optionally with multiple seeds.
+# This script loops over the different subjects and sessions and trains different models. 
+# At the end, the final performance is computed with the aggregate_results.py script that provides the average performance.
+#
 # Example:
-# ./run_experiments.sh hparams/EEGNet_BNCI2014001.yaml /localscratch/eeg_data /localscratch/eeg_pickled_data results/EEGNet_BNCI2014001 9 2 1986 1 acc valid_metrics.pkl false true --number_of_epochs=2
+# ./run_experiments.sh --hparams=hparams/MotorImagery/BNCI2014001/EEGNet.yaml --data_folder=eeg_data --cached_data_folder=eeg_pickled_data \
+#   --output_folder=results/MotorImagery/BNCI2014001/EEGNet --nsbj=9 --nsess=2 --seed=1986 --nruns=2 --eval_metric=acc --metric_file=valid_metrics.pkl \
+#   --do_leave_one_subject_out=false --do_leave_one_session_out=true --number_of_epochs=2 --device='cpu'
+#
+# Please, see the README.md file for more info
+#
+# Authors:
+# - Mirco Ravanelli (2023)
+# - Davide Borra (2023)
+###########################################################
 
-hparams=$1
-data_folder=$2 #MOABB data folder
-cached_data_folder=$3
-output_folder=$4 #results folder
-nsbj=$5
-nsess=$6
-nruns=$8
-eval_metric=$9 #acc,loss,f1
-metric_file=${10} # test_metrics.pkl or valid_metrics.pkl (use valid_metrics for hyperparameter tuning)
-do_leave_one_subject_out=${11}
-do_leave_one_session_out=${12}
+# Initialize variables
+hparams=""
+data_folder=""
+cached_data_folder=""
+output_folder=""
+nsbj=""
+nsess=""
+seed=""
+nruns=""
+eval_metric=""
+metric_file=""
+do_leave_one_subject_out=""
+do_leave_one_session_out=""
+additional_flags=""
 
-to_download=false
+# Function to print argument descriptions and exit
+print_argument_descriptions() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  --hparams=hparams_path            Hparam YAML file"
+    echo "  --data_folder=data_folder_path    Data folder path"
+    echo "  --cached_data_folder=cache_path   Cached data folder path"
+    echo "  --output_folder=output_path       Output folder path"
+    echo "  --nsbj=num_subjects               Number of subjects"
+    echo "  --nsess=num_sessions              Number of sessions"
+    echo "  --seed=random_seed                Seed (random if not specified)"
+    echo "  --nruns=num_runs                  Number of runs"
+    echo "  --eval_metric=metric              Evaluation metric (e.g., acc or f1)"
+    echo "  --metric_file=metric_file_path    File where to store the eval metrics"
+    echo "  --do_leave_one_subject_out=flag   Runs leave one subject out training "
+    echo "  --do_leave_one_session_out=flag   Runs one session out training"
+    exit 1
+}
 
-if [ $7 = "random_seed" ]; then
-    seed_init=$RANDOM
-else
-    seed_init=$7
+# Parse command-line arguments
+for arg in "$@"; do
+    case "$arg" in
+        --hparams=*) hparams="${arg#*=}" ;;
+        --data_folder=*) data_folder="${arg#*=}" ;;
+        --cached_data_folder=*) cached_data_folder="${arg#*=}" ;;
+        --output_folder=*) output_folder="${arg#*=}" ;;
+        --nsbj=*) nsbj="${arg#*=}" ;;
+        --nsess=*) nsess="${arg#*=}" ;;
+        --seed=*) seed="${arg#*=}" ;;
+        --nruns=*) nruns="${arg#*=}" ;;
+        --eval_metric=*) eval_metric="${arg#*=}" ;;
+        --metric_file=*) metric_file="${arg#*=}" ;;
+        --do_leave_one_subject_out=*) do_leave_one_subject_out="${arg#*=}" ;;
+        --do_leave_one_session_out=*) do_leave_one_session_out="${arg#*=}" ;;
+        *) additional_flags+="$arg " ;;
+    esac
+done
+
+# Check for required arguments
+if [ -z "$hparams" ] || [ -z "$data_folder" ] || [ -z "$cached_data_folder" ] || [ -z "$output_folder" ] || [ -z "$nsbj" ] || [ -z "$nsess" ] || [ -z "$nruns" ] || [ -z "$eval_metric" ] || [ -z "$metric_file" ] || [ -z "$do_leave_one_subject_out" ] || [ -z "$do_leave_one_session_out" ]; then
+    echo "ERROR: Missing required arguments! Please provide all required options."
+    print_argument_descriptions
 fi
+
+# Manage Seed (optional argument)
+seed="${seed:-$RANDOM}"
 
 echo "hparams file : $hparams"
 echo "Data folder: $data_folder"
@@ -37,59 +92,45 @@ mkdir -p $output_folder\_seed\_$seed_init
 mkdir -p $data_folder
 mkdir -p $cached_data_folder
 
-seed=$seed_init
+# Function to run the training experiment
+run_experiment() {
+  local data_iterator_name="$1"
+  local target_session_idx="$2"
 
-# RUN MULTIPLE EXPERIMENTS (with different seeds)
+  for target_subject_idx in $(seq 0 1 $(( nsbj - 1 ))); do
+    echo "Subject $target_subject_idx"
+    python train.py $hparams --seed=$seed --data_folder=$data_folder --cached_data_folder=$cached_data_folder --output_folder=$output_folder/$seed \
+      --target_subject_idx=$target_subject_idx --target_session_idx=$target_session_idx \
+      --data_iterator_name="$data_iterator_name" $additional_flags
+  done
+}
+
+# Run multiple training experiments (with different seeds)
 for i in $(seq 0 1 $(( nruns - 1 ))); do
   echo $seed
-  
+
   # LEAVE-ONE-SUBJECT-OUT
-  if [ "$do_leave_one_subject_out" = true ]
-  then
-
-      for target_subject_idx in $(seq 0 1 $(( nsbj - 1 ))); do
-          echo "Subject $target_subject_idx"
-          python train.py $hparams --seed=$seed --data_folder=$data_folder --cached_data_folder=$cached_data_folder --output_folder=$output_folder\_seed\_$seed_init/$seed\
-          --target_subject_idx=$target_subject_idx --target_session_idx=0 \
-          --data_iterator_name='leave-one-subject-out' --to_download=$to_download --to_prepare=false "${@:13}"
-
-         # Data already downloaded
-         to_download=False 
-      done
+  if [ "$do_leave_one_subject_out" = true ]; then
+    run_experiment "leave-one-subject-out" 0
   fi
-  
+
   # LEAVE-ONE-SESSION-OUT
-  if [ "$do_leave_one_session_out" = true ]
-  then
-
-      # Loop over sessions
-      for j in $(seq 0 1 $(( nsess - 1 ))); do
-          for target_subject_idx in $(seq 0 1 $(( nsbj - 1 ))); do
-              echo "Subject $target_subject_idx"
-              python train.py $hparams --seed=$seed --data_folder=$data_folder --cached_data_folder=$cached_data_folder --output_folder=$output_folder\_seed\_$seed_init/$seed\
-              --target_subject_idx=$target_subject_idx --target_session_idx=$j \
-              --data_iterator_name='leave-one-session-out' --to_download=$to_download --to_prepare=false "${@:13}"
-      
-              # Data already downloaded
-              to_download=False
-      done
-
-  done
+  if [ "$do_leave_one_session_out" = true ]; then
+    # Loop over sessions
+    for j in $(seq 0 1 $(( nsess - 1 ))); do
+      run_experiment "leave-one-session-out" $j
+    done
   fi
 
-  # Store results
-  python parse_results.py $output_folder\_seed\_$seed_init/$seed $metric_file $eval_metric | tee -a  $output_folder\_seed\_$seed_init/$seed\_results.txt
-  
-  # Changing random seed
+
+  # Store the results
+  python utils/parse_results.py $output_folder/$seed $metric_file $eval_metric | tee -a  $output_folder/$seed\_results.txt
+
+  # Changing Random seed
   seed=$((seed+1))
-  
 done
 
-# Aggregate results + notify them to Orion (if needed for hyperparameter tuning)
-echo 'Final Results'
-python aggregate_results.py $output_folder\_seed\_$seed_init $eval_metric | tee -a  $output_folder\_seed\_$seed_init/aggregated_performance.txt
 
-
-
-
+echo 'Final Results (Performance Aggregation)'
+python utils/aggregate_results.py $output_folder $eval_metric | tee -a  $output_folder/$aggregated_performance.txt
 
