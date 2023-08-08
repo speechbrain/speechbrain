91c27e56f5382efaaf6796874b81c8589ad7ac43
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2023-08-08 02:36:01 +0100

    Add k2_ctc, an implementation of CTC by k2. Besides, add ctc_k2_align.py containing an essential function for alignment given log_probs, targets, input_lens, and target_lens.

--- speechbrain/alignment/__init__.py
+++ speechbrain/alignment/__init__.py
@@ -1,2 +1,3 @@
 """Tools for aligning transcripts and speech signals
 """
+from . import ctc_k2_align
--- speechbrain/nnet/losses.py
+++ speechbrain/nnet/losses.py
@@ -2,6 +2,8 @@
 Losses for training neural networks.
 
 Authors
+ * Zeyu Zhao 2023
+ * Georgios Karakasidis 2023
  * Mirco Ravanelli 2020
  * Samuele Cornell 2020
  * Hwidong Na 2020
@@ -292,6 +294,65 @@ def ctc_loss(
         return loss.view(N, -1).sum(1) / target_lens.view(N, -1).sum(1)
     else:
         return loss
+
+
+def k2_ctc(log_probs, targets, input_lens, target_lens, reduction="mean"):
+    """CTC loss implemented with k2. Please make sure that k2 has been installed properly.
+    Note that the blank index must be 0 in this implementation.
+
+    Arguments
+    ---------
+    predictions : torch.Tensor
+        Predicted tensor, of shape [batch, time, chars].
+    targets : torch.Tensor
+        Target tensor, without any blanks, of shape [batch, target_len]
+    input_lens : torch.Tensor
+        Length of each utterance.
+    target_lens : torch.Tensor
+        Length of each target sequence.
+    reduction : str
+        What reduction to apply to the output. 'mean', 'sum', 'none'.
+        See k2.ctc_loss for 'mean', 'sum', 'none'.
+    """
+    import k2
+    input_lens = (input_lens * log_probs.shape[1]).round().int()
+    target_lens = (target_lens * targets.shape[1]).round().int()
+    batch_size = log_probs.shape[0]
+
+    max_token_id = log_probs.shape[-1] - 1
+    ctc_topo = k2.ctc_topo(max_token_id, modified=False, device=log_probs.device)
+
+    # convert targets to k2.FsaVec
+    labels = [targets[i, :target_lens[i]].tolist() for i in range(len(target_lens))]
+    label_fsas = k2.linear_fsa(labels, device=log_probs.device)
+
+    labels_fsas_with_self_loops = k2.remove_epsilon_and_add_self_loops(label_fsas)
+
+    labels_fsas_with_self_loops = k2.arc_sort(labels_fsas_with_self_loops)
+
+    graph = k2.compose(ctc_topo, labels_fsas_with_self_loops, treat_epsilons_specially=False)
+
+    assert graph.requires_grad is False
+
+    supervision_segments = torch.tensor(
+        [[i, 0, input_lens[i]] for i in range(batch_size)],
+        device="cpu",
+        dtype=torch.int32,
+    )
+
+    dense_fsa_vec = k2.DenseFsaVec(
+        log_probs,
+        supervision_segments,
+    )
+
+    loss = k2.ctc_loss(
+        decoding_graph=graph,
+        dense_fsa_vec=dense_fsa_vec,
+        output_beam=20,
+        reduction=reduction,
+        target_lengths=target_lens,
+    )
+    return loss
 
 
 def l1_loss(
