a87ce647ce9a757c5b7afa13b694b7bc9ddf8a6f
prometheus <46483626+ddavidebb@users.noreply.github.com>
2023-08-13 21:20:58 +0200

    Revert changes in recipes/MOABB/run_hparam_optimization.sh, updated yaml files to disable data augmentation in hparam tuning step 1

--- recipes/MOABB/run_hparam_optimization.sh
+++ recipes/MOABB/run_hparam_optimization.sh
@@ -333,7 +333,6 @@ function extract_best_params() {
 
 # Running hparam tuning (loop over multiple steps)
 step_id=1
-augment_flag="--repeat_augment 0" # no data augmentation in hparam tuning step 1
 hparams_step=$hparams
 pattern="@orion_step1:"
 opt_flags=$(get_flag "$hparams_step" "$pattern")
@@ -352,9 +351,7 @@ while [ -n "$opt_flags" ]; do
     output_folder_step="$output_folder"/step"$step_id"
     mkdir -p $output_folder_step
     exp_name_step="$exp_name"_step"$step_id"
-    if [ $step_id -gt 1 ]; then
-        augment_flag="--repeat_augment 1" # enable data augmentation in hparam tuning step >1
-    fi
+
     echo
     echo "**********************************************************************************************"
     echo "Running hparam tuning (step $step_id)..."
@@ -372,7 +369,7 @@ while [ -n "$opt_flags" ]; do
 
 
     # Appending the optimization flags
-    orion_hunt_command="$orion_hunt_command $augment_flag $opt_flags"
+    orion_hunt_command="$orion_hunt_command $opt_flags"
 
     echo $orion_hunt_command &> "$output_folder_step/orion_hunt_command.txt"
 
