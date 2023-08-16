02faebb3c8a671bf5ceb04b1a06ed06516d19211
poonehmousavi <moosavi.pooneh@gmail.com>
2023-08-15 23:42:16 -0400

    precomit changes

--- speechbrain/pretrained/interfaces.py
+++ speechbrain/pretrained/interfaces.py
@@ -3238,7 +3238,7 @@ class WhisperASR(Pretrained):
         predicted_words, predicted_tokens = self.transcribe_batch(
             batch, rel_length
         )
-        return ' '.join(predicted_words[0])
+        return " ".join(predicted_words[0])
 
     def encode_batch(self, wavs, wav_lens):
         """Encodes the input audio into a sequence of hidden states
