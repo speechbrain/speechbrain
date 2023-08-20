ed6ca3fcfcb3e6bf6f500c48595c78ee775a828c
Chaanks <jarod8405@gmail.com>
2023-08-20 16:26:00 +0200

    Add S2UT interface

--- speechbrain/pretrained/interfaces.py
+++ speechbrain/pretrained/interfaces.py
@@ -3195,6 +3195,12 @@ class UnitHIFIGAN(Pretrained):
     ---------
     hparams
         Hyperparameters (from HyperPyYAML)
+    Example
+    -------
+    >>> tmpdir_vocoder = getfixture('tmpdir') / "vocoder"
+    >>> hifi_gan = UnitHIFIGAN.from_hparams(source="chaanks/unit-hifigan-hubert-en-l6_k100", savedir=tmpdir_vocoder)
+    >>> codes = torch.rand(100)
+    >>> waveform = hifi_gan.decode_unit(codes)
     """
 
     HPARAMS_NEEDED = ["generator"]
@@ -3209,9 +3215,7 @@ class UnitHIFIGAN(Pretrained):
         Arguments
         ---------
         units: torch.tensor
-            Batch of discrete units [batch, units]
-        spk: torch.tensor
-            Batch of spks [batch, spk_dim]
+            Batch of discrete units [batch, codes]
         Returns
         -------
         waveforms: torch.tensor
@@ -3230,15 +3234,11 @@ class UnitHIFIGAN(Pretrained):
         Arguments
         ---------
         units: torch.tensor
-            units: [time]
+            codes: [time]
         Returns
         -------
         waveform: torch.tensor
             waveform [1, time]
-        audio can be saved by:
-        >>> waveform = torch.rand(1, 666666)
-        >>> sample_rate = 22050
-        >>> torchaudio.save("test.wav", waveform, sample_rate)
         """
         # Prepare for inference by removing the weight norm
         if self.first_call:
@@ -3866,3 +3866,114 @@ class PIQAudioInterpreter(Pretrained):
     def forward(self, wavs, wav_lens=None):
         """Runs the classification"""
         return self.interpret_batch(wavs, wav_lens)
+
+
+class EncoderDecoderS2UT(Pretrained):
+    """A ready-to-use Encoder Decoder for speech-to-unit translation model
+
+    The class can be used  to  run the entire encoder-decoder S2UT model
+    (translate_file()) to translate speech. The given YAML must contains the fields
+    specified in the *_NEEDED[] lists.
+
+    Example
+    -------
+    >>> from speechbrain.pretrained import EncoderDecoderS2UT
+    >>> tmpdir = getfixture("tmpdir")
+    >>> s2ut_model = EncoderDecoderS2UT.from_hparams(source="chaanks/transformer-s2ut-fr-en-hubert-k100", savedir=tmpdir,) # doctest: +SKIP
+    >>> s2ut_model.translate_file("chaanks/transformer-s2ut-fr-en-hubert-k100/example-fr.wav") # doctest: +SKIP
+    """
+
+    HPARAMS_NEEDED = ["sample_rate"]
+    MODULES_NEEDED = ["encoder", "decoder"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sample_rate = self.hparams.sample_rate
+
+    def translate_file(self, path):
+        """Translates the given audiofile into a sequence speech unit.
+
+        Arguments
+        ---------
+        path : str
+            Path to audio file which to translate.
+
+        Returns
+        -------
+        int[]
+            The audiofile translation produced by this speech-to-unit translationmodel.
+        """
+        info = torchaudio.info(path)
+        audio = speechbrain.dataio.dataio.read_audio(path)
+        audio = torchaudio.transforms.Resample(
+            info.sample_rate, self.sample_rate,
+        )(audio)
+        audio = audio.to(self.device)
+        # Fake a batch:
+        batch = audio.unsqueeze(0)
+        rel_length = torch.tensor([1.0])
+        predicted_tokens = self.translate_batch(batch, rel_length)
+        return predicted_tokens[0]
+
+    def encode_batch(self, wavs, wav_lens):
+        """Encodes the input audio into a sequence of hidden states
+
+        The waveforms should already be in the model's desired format.
+        You can call:
+        ``normalized = EncoderDecoderS2UT.normalizer(signal, sample_rate)``
+        to get a correctly converted signal in most cases.
+
+        Arguments
+        ---------
+        wavs : torch.tensor
+            Batch of waveforms [batch, time, channels].
+        wav_lens : torch.tensor
+            Lengths of the waveforms relative to the longest one in the
+            batch, tensor of shape [batch]. The longest one should have
+            relative length 1.0 and others len(waveform) / max_length.
+            Used for ignoring padding.
+
+        Returns
+        -------
+        torch.tensor
+            The encoded batch
+        """
+        wavs = wavs.float()
+        wavs, wav_lens = wavs.to(self.device), wav_lens.to(self.device)
+        encoder_out = self.mods.encoder(wavs, wav_lens)
+        return encoder_out
+
+    def translate_batch(self, wavs, wav_lens):
+        """Translates the input audio into a sequence of words
+
+        The waveforms should already be in the model's desired format.
+        You can call:
+        ``normalized = EncoderDecoderS2UT.normalizer(signal, sample_rate)``
+        to get a correctly converted signal in most cases.
+
+        Arguments
+        ---------
+        wavs : torch.tensor
+            Batch of waveforms [batch, time, channels].
+        wav_lens : torch.tensor
+            Lengths of the waveforms relative to the longest one in the
+            batch, tensor of shape [batch]. The longest one should have
+            relative length 1.0 and others len(waveform) / max_length.
+            Used for ignoring padding.
+
+        Returns
+        -------
+        list
+            Each waveform in the batch translated.
+        tensor
+            Each predicted token id.
+        """
+        with torch.no_grad():
+            wav_lens = wav_lens.to(self.device)
+            encoder_out = self.encode_batch(wavs, wav_lens)
+            predicted_tokens, _ = self.mods.decoder(encoder_out, wav_lens)
+        return predicted_tokens
+
+    def forward(self, wavs, wav_lens):
+        """Runs full translation"""
+        return self.encode_batch(wavs, wav_lens)
