1013c717a8c77d1acdb8d982ce54cfb684cdb2da
Sylvain de Langen <sdelang@sdelang.fr>
2023-08-30 10:57:47 +0200

    Revert change to custom_tgt_module; patching model instead

--- speechbrain/lobes/models/transformer/TransformerASR.py
+++ speechbrain/lobes/models/transformer/TransformerASR.py
@@ -167,10 +167,9 @@ class TransformerASR(TransformerInterface):
             ),
             torch.nn.Dropout(dropout),
         )
-        if num_decoder_layers > 0:
-            self.custom_tgt_module = ModuleList(
-                NormalizedEmbedding(d_model, tgt_vocab)
-            )
+        self.custom_tgt_module = ModuleList(
+            NormalizedEmbedding(d_model, tgt_vocab)
+        )
 
         # reset parameters using xavier_normal_
         self._init_params()
