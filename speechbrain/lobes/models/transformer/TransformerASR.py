757ba452f6daa8d093b0eb4ce2494d99fbc27b35
Titouan Parcollet/Embedded AI /SRUK/Engineer/Samsung Electronics <t.parcollet@samsung.com>
2023-10-23 11:12:04 +0000

    clean logging

--- speechbrain/lobes/models/transformer/TransformerASR.py
+++ speechbrain/lobes/models/transformer/TransformerASR.py
@@ -205,7 +205,6 @@ class TransformerASR(TransformerInterface):
         tgt = self.custom_tgt_module(tgt)
 
         if self.attention_type == "RelPosMHAXL":
-            # use standard sinusoidal pos encoding in decoder
             tgt = tgt + self.positional_encoding_decoder(tgt)
             pos_embs_encoder = None  # self.positional_encoding(src)
             pos_embs_target = None
@@ -279,7 +278,6 @@ class TransformerASR(TransformerInterface):
 
         tgt = self.custom_tgt_module(tgt)
         if self.attention_type == "RelPosMHAXL":
-            # we use fixed positional encodings in the decoder
             tgt = tgt + self.positional_encoding_decoder(tgt)
             pos_embs_encoder = None  # self.positional_encoding(src)
             pos_embs_target = None
