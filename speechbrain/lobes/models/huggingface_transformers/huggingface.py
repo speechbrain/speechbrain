85350dbe3e76f80dd6a86c800c54fde639f0bb13
Adel Moumen <88119391+Adel-Moumen@users.noreply.github.com>
2023-10-31 21:20:50 +0100

    Fix missing file / import in huggingface_transformers (#2224)
    * init/imports
    
    * comment
    
    * add partial import
    
    * wav2vec -> wav2vec2
    
    * fix ci

--- speechbrain/lobes/models/huggingface_transformers/huggingface.py
+++ speechbrain/lobes/models/huggingface_transformers/huggingface.py
@@ -29,23 +29,14 @@ from torch import nn
 from huggingface_hub import model_info
 from speechbrain.pretrained.fetching import fetch
 from speechbrain.dataio.dataio import length_to_mask
-
-
-# We check if transformers is installed.
-try:
-    from transformers import (
-        AutoConfig,
-        AutoTokenizer,
-        AutoFeatureExtractor,
-        AutoModelForPreTraining,
-        AutoModel,
-        AutoModelWithLMHead,
-    )
-
-except ImportError:
-    MSG = "Please install transformers from HuggingFace to use wav2vec2 / Hubert\n"
-    MSG += "E.G. run: pip install transformers"
-    raise ImportError(MSG)
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    AutoFeatureExtractor,
+    AutoModelForPreTraining,
+    AutoModel,
+    AutoModelWithLMHead,
+)
 
 logger = logging.getLogger(__name__)
 
