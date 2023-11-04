5e249c9dfb137dbe4901db8be24450813d203f1b
flexthink <flexthink@users.noreply.github.com>
2023-11-04 12:23:45 -0400

    Add wrappers for Encodec and Vocos from Huggingface

---
+++ speechbrain/lobes/models/huggingface_transformers/encodec.py
@@ -0,0 +1,96 @@
+"""This lobe enables the integration of huggingface pretrained encodec.
+
+Repository: https://huggingface.co/docs/transformers/v4.31.0/en/model_doc/encodec
+Paper: https://arxiv.org/abs/2210.13438
+
+Authors
+ * Artem Ploujnikov 2023
+"""
+
+from torch import nn
+from speechbrain.dataio.dataio import length_to_mask
+
+try:
+    from transformers import EncodecModel
+except ImportError:
+    MSG = "Please install transformers from HuggingFace to use Encodec\n"
+    MSG += "E.G. run: pip install transformers"
+    raise ImportError(MSG)
+
+
+DEFAULT_SAMPLE_RATE = 24000
+
+
+class Encodec(nn.Module):
+    """An wrapper for the HuggingFace encodec model
+
+    Arguments
+    ---------
+    source : str
+        a HuggingFace repository identifier or a path
+    save_path : str
+        the location where the pretrained model will be saved
+    sample_rate : int
+        the audio sampling rate
+    freeze : bool
+        whether the model will be frozen (e.g. not trainable if used
+        as part of training another model)"""
+
+    def __init__(
+        self, source, save_path=None, sample_rate=None, freeze=True,
+    ):
+        super().__init__()
+        self.source = source
+        self.save_path = save_path
+        self.freeze = freeze
+        self.model = EncodecModel.from_pretrained(source, cache_dir=save_path)
+        if not sample_rate:
+            sample_rate = DEFAULT_SAMPLE_RATE
+        self.sample_rate = sample_rate
+        if self.freeze:
+            for param in self.model.parameters():
+                param.requires_grad = False
+
+    def forward(self, inputs, lengths):
+        return self.encode(inputs, lengths)
+
+    def encode(self, inputs, length):
+        """Encodes the input audio as tokens
+
+        Arguments
+        ---------
+        inputs : torch.Tensor
+            a (Batch X Samples) tensor of audio
+        length : torch.Tensor
+            a tensor of relative lengths
+
+        Returns
+        -------
+        tokens : torch.Tensor
+            a (Batch X Tokens) tensor of audio tokens
+        """
+        max_len = inputs.size(1)
+        mask = length_to_mask(length * max_len, max_len)
+        result = self.model.encode(inputs, mask)
+        return result["audio_codes"].squeeze(0).transpose(-1, -2)
+
+    def decode(self, tokens, length):
+        """Decodes audio from tokens
+
+        Arguments
+        ---------
+        tokens : torch.Tensor
+            a tensor of tokens
+        length : torch.Tensor
+            a tensor of relative lengths
+
+        Returns
+        -------
+        audio : torch.Tensor
+            the audio
+        """
+        max_len = tokens.size(1)
+        mask = length_to_mask(length * max_len, max_len)
+        return self.model.decode(
+            tokens.unsqueeze(0).transpose(-1, -2), [None], mask
+        )
---
+++ speechbrain/lobes/models/huggingface_transformers/vocos.py
@@ -0,0 +1,87 @@
+"""This lobe enables the integration of huggingface pretrained
+Vocos model.
+
+Repository: https://huggingface.co/charactr/vocos-encodec-24khz
+Paper: https://arxiv.org/pdf/2306.00814.pdf
+
+Authors
+ * Artem Ploujnikov 2023
+"""
+
+import torch
+from torch import nn
+from speechbrain.dataio.dataio import length_to_mask
+
+try:
+    from vocos import Vocos as VocosModel
+except ImportError:
+    MSG = "Please install vocos to use the Vocos model\n"
+    MSG += "E.G. run: pip install vocos"
+    raise ImportError(MSG)
+
+
+DEFAULT_SAMPLE_RATE = 24000
+BANDWIDTHS = [1.5, 3.0, 6.0, 12.0]
+
+
+class Vocos(nn.Module):
+    """An wrapper for the HuggingFace Vocos model
+
+    Arguments
+    ---------
+    source : str
+        a HuggingFace repository identifier or a path
+    revision : str
+        the model revision
+    bandwidth : int
+        the bandwidth values
+        Supported:
+        1.5, 3.0, 6.0, 12.0
+    freeze : bool
+        whether or not parameters should be
+        frozen
+    """
+
+    def __init__(
+        self, source, revision=None, bandwidth=1.5, freeze=True,
+    ):
+        super().__init__()
+        self.source = source
+        self.model = VocosModel.from_pretrained(source, revision)
+        self.freeze = freeze
+        self.bandwidth = bandwidth
+        self.bandwidth_id = (
+            (torch.tensor(BANDWIDTHS) - bandwidth).abs().argmin().item()
+        )
+        if self.freeze:
+            for param in self.model.parameters():
+                param.requires_grad = False
+
+    def forward(self, inputs, lengths):
+        """Converts vocodec tokens to audio
+
+        Arguments
+        ---------
+        inputs : torch.Tensor
+            A tensor of Vocodec tokens
+        lengths : torch.Tensor
+            A 1-D tensor of relative lengths
+
+        Returns
+        -------
+        wavs : torch.Tensor
+            A (Batch x Length) tensor of raw waveforms
+        lengths : torch.Tensor
+            Relative lengths
+        """
+        features = self.model.codes_to_features(inputs.permute(2, 0, 1))
+        wavs = self.model.decode(
+            features,
+            bandwidth_id=torch.tensor(
+                [self.bandwidth_id], device=inputs.device
+            ),
+        )
+        mask = length_to_mask(
+            lengths * wavs.size(1), max_len=wavs.size(1), device=wavs.device
+        )
+        return wavs * mask, lengths
