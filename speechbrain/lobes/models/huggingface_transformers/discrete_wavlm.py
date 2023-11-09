628fab1f61f848debe90a303a528eba9b5120d05
poonehmousavi <moosavi.pooneh@gmail.com>
2023-11-08 19:17:52 -0500

    add discrete wavlm, wav2vec

--- speechbrain/lobes/models/huggingface_transformers/discrete_wavlm.py
+++ speechbrain/lobes/models/huggingface_transformers/discrete_wavlm.py
@@ -1,4 +1,4 @@
-"""This lobe enables the integration of pretrained discrete Hubert.
+"""This lobe enables the integration of pretrained discrete wavLM.
 
 Reference: https://arxiv.org/abs/2006.11477
 Reference: https://arxiv.org/abs/1904.05862
@@ -15,29 +15,26 @@ import torch
 from huggingface_hub import hf_hub_download
 import joblib
 
-from speechbrain.lobes.models.huggingface_transformers.hubert import HuBERT
+from speechbrain.lobes.models.huggingface_transformers.wavlm import WavLM
 
 logger = logging.getLogger(__name__)
 
 
-class DiscreteHuBERT(HuBERT):
+class DiscreteWavLM(WavLM):
     """This lobe enables the integration of HuggingFace and SpeechBrain
-    pretrained Discrete HuBERT models.
+    pretrained Discrete WavLM models.
 
-    Source paper HuBERT: https://arxiv.org/abs/2106.07447
+    Source paper WavLM: https://arxiv.org/abs/2110.13900
     Transformer from HuggingFace needs to be installed:
     https://huggingface.co/transformers/installation.html
 
     The model can be used as a fixed Discrete feature extractor or can be finetuned. It
     will download automatically the model from HuggingFace or use a local path.
 
-    For now, HuggingFace's HuBERT and WavLM model can be loaded using the exact code for Wav2Vec2 model.
-    For this reason, HuBERT and WavLM can be fine inheriting the Wav2Vec2 class.
-
     Arguments
     ---------
     source : str
-        HuggingFace hub name: e.g "facebook/hubert-base-ls960"
+        HuggingFace hub name: e.g "microsoft/wavlm-large"
     save_path : str
         Path (dir) of the downloaded model.
     kmeans_repo_id : str
@@ -48,20 +45,20 @@ class DiscreteHuBERT(HuBERT):
         Path (dir) of the downloaded kmenas model.
     output_norm : bool (default: True)
         If True, a layer_norm (affine) will be applied to the output obtained
-        from the HuBERT model.
+        from the WavLM model.
     freeze : bool (default: True)
         If True, the model is frozen. If False, the model will be trained
         alongside with the rest of the pipeline.
     freeze_feature_extractor :  bool (default: False)
         When freeze = False and freeze_feature_extractor True, the featue_extractor module of the model is Frozen. If False
-        all the HuBERT model will be trained including featue_extractor module.
+        all the WavLM model will be trained including featue_extractor module.
     apply_spec_augment : bool (default: False)
         If True, the model will apply spec augment on the output of feature extractor
-        (inside huggingface Hubert Model() class).
+        (inside huggingface WavLM Model() class).
         If False, the model will not apply spec augment. We set this to false to prevent from doing it twice.
     output_all_hiddens : bool (default: True)
         If True, the forward function outputs the hidden states from all transformer layers.
-        For example facebook/hubert-base-ls960 has 12 transformer layers and the output is of shape (13, B, T, C),
+        For example microsoft/wavlm-large  has 12 transformer layers and the output is of shape (13, B, T, C),
         where a projection of the CNN output is added to the beginning.
         If False, the forward function outputs the hidden states only from the last transformer layer.
     ssl_layer_num : (int) (default: -1)
@@ -72,13 +69,13 @@ class DiscreteHuBERT(HuBERT):
     -------
     >>> import torch
     >>> inputs = torch.rand([10, 600])
-    >>> model_hub = "facebook/hubert-base-ls960"
+    >>> model_hub = "microsoft/wavlm-large"
     >>> save_path = "savedir"
     >>> ssl_layer_num = -1
     >>> kmeans_repo_id = "speechbrain/SSL_Quantization"
-    >>> kmeans_filename = "Librispeech_hubert_kmeans_100.pt"
+    >>> kmeans_filename = "Librispeech_wavlm_kmeans_100.pt"
     >>> kmeans_cache_dir="savedir"
-    >>> model = DiscreteHuBERT(model_hub, save_path,freeze = True,ssl_layer_num=ssl_layer_num,kmeans_repo_id=kmeans_repo_id, kmeans_filename=kmeans_filename, kmeans_cache_dir=kmeans_cache_dir)
+    >>> model = DiscreteWavLM(model_hub, save_path,freeze = True,ssl_layer_num=ssl_layer_num,kmeans_repo_id=kmeans_repo_id, kmeans_filename=kmeans_filename, kmeans_cache_dir=kmeans_cache_dir)
     >>> embs, tokens = model(inputs)
     >>> embs.shape
     torch.Size([10, 1, 768])
