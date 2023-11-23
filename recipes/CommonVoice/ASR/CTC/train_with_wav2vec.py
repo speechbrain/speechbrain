99ab7ade8bf269deee5a125416d479af7b47aa0a
Adel Moumen <adelmoumen.pro@gmail.com>
2023-11-23 14:51:48 +0100

    fix space_token

--- recipes/CommonVoice/ASR/CTC/train_with_wav2vec.py
+++ recipes/CommonVoice/ASR/CTC/train_with_wav2vec.py
@@ -353,6 +353,7 @@ if __name__ == "__main__":
         prune_history=hparams["prune_history"],
         topk=hparams["topk"],
         kenlm_model_path=hparams.get("kenlm_model_path"),
+        space_token="▁",
     )
 
     # Training
