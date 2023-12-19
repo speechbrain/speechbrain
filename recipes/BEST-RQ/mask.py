f084239b5792baf8e993e5a57b84bc4de459a09a
whettenr <whettenrmw@gmail.com>
2023-12-19 11:39:52 +0100

    add in bestrq recipies

---
+++ recipes/BEST-RQ/mask.py
@@ -0,0 +1,52 @@
+import numpy as np
+import random
+import torch
+from speechbrain.utils.data_utils import batch_pad_right
+
+
+def compute_mask(shape, sample_lens, mask_prob, mask_length):
+    min_sample_len = min(sample_lens)
+
+    # int always floors the float number so adding + random.random() 
+    # makes it 50% change of rounding up and 50% of rounding down
+    num_mask = int(mask_prob * min_sample_len + random.random())
+
+    # make sure there is at least 1 mask 
+    if num_mask == 0:
+        num_mask = 1
+
+    permutation = torch.randperm(min_sample_len//mask_length) * mask_length
+    selected_indices = permutation[:num_mask]
+    selected_indices, _ = selected_indices.sort()
+
+    # get next for frames
+    idx = []
+    for  i in selected_indices:
+        idx.append(torch.arange(start=i, end=i+mask_length))
+    idx = torch.cat(idx)
+
+    return idx
+
+
+def brq_mask_collate_fn(samples_lst, get_out_len_fn, mask_prob, mask_length, n_mels):
+    wav_lst, latent_length_lst = [], []
+    ids = []
+    for sample in samples_lst:
+        ids.append(sample["id"])
+        sig = sample["sig"]
+        wav_lst.append(sig)
+        latent_length = get_out_len_fn(torch.as_tensor(sig.size(-1)))
+        latent_length_lst.append(latent_length.item())
+    bs = len(wav_lst)
+    wavs_padded, wav_lens = batch_pad_right(wav_lst)
+
+    batch_time_len = max(latent_length_lst)
+    batch_time_len
+    mask = compute_mask(
+        (bs, batch_time_len, n_mels), latent_length_lst, mask_prob, mask_length
+    )
+    return (
+        torch.as_tensor(wavs_padded),
+        torch.as_tensor(wav_lens),
+        torch.as_tensor(mask),
+    )
---
+++ recipes/BEST-RQ/train.py
@@ -0,0 +1,381 @@
+#!/usr/bin/env python3
+"""Recipe for pretraining BestRQ (TODO CITATION).
+See config file for model definition.
+See the readme of the recipe for advice on the pretraining that may appear
+a bit challenging depending on your available resources.
+
+To run this recipe call python train_best_rq.py best_rq.yaml --find_unused_parameters --max_grad_norm 0.0
+
+Authors
+    * Ryan Whetten 2023
+"""
+
+import logging
+import sys
+import time
+from functools import partial
+
+import speechbrain as sb
+import torch
+import torch.nn.functional as F
+from torch.nn.parallel import DistributedDataParallel
+from hyperpyyaml import load_hyperpyyaml
+
+from speechbrain import Stage
+from speechbrain.utils.distributed import run_on_main
+from speechbrain.dataio.dataloader import SaveableDataLoader
+from speechbrain.dataio.sampler import DynamicBatchSampler
+from mask import brq_mask_collate_fn
+
+logger = logging.getLogger(__name__)
+
+class BestRQBrain(sb.core.Brain):
+    
+    def compute_forward(self, batch, stage):
+        """Computes forward pass through BestRQ model and returns encoded and
+        target embeddings as well as other metrics of interest.
+        """
+        # get batch and mask
+        wavs, wav_lens, mask = batch
+        wavs, wav_lens, mask = (
+            wavs.to(self.device),
+            wav_lens.to(self.device),
+            mask.to(self.device),
+        )
+       ############### START ##############
+        ### get fbanks and normalize
+        feats = self.hparams.compute_features(wavs)
+        current_epoch = self.hparams.epoch_counter.current
+        feats = self.modules.normalize(feats, wav_lens, epoch=current_epoch)
+
+        B, T, C = feats.shape
+        divis_by = self.hparams.pad_to_divisible_by
+
+
+        #### pad features
+        current_dim_size = T
+        dim_to_pad = 1  # Pad along the second dimension (i.e. time)
+
+        # Calculate the amount of padding needed to make the tensor divisible by 4
+        current_dim_size = feats.shape[dim_to_pad]
+        padding_needed = (4 - (current_dim_size % 4)) % 4  # Ensure positive padding
+
+        # Define the padding
+        padding = [0, 0, 0, 0, 0, 0]  # Initialize padding for all dimensions
+        padding[dim_to_pad * 2] = padding_needed  # Set padding for the chosen dimension
+
+        # add in padding to features and mask
+        feats = torch.nn.functional.pad(feats, padding)
+        # print('features w/padding: ', feats.shape)
+
+        # normalized = self.modules.layernorm(feats)
+        # get targets from quantizer
+        targets = self.modules.Quantizer(feats.view(B, feats.shape[1]//divis_by, -1))
+        # print('targets: ', targets.shape)
+
+        ### augment data
+        if stage == sb.Stage.TRAIN:
+            if hasattr(self.hparams, "augmentation"):
+                feats = self.hparams.augmentation(feats)
+                
+        # generate random noise
+        noise = torch.normal(
+            mean=self.hparams.noise_mean, 
+            std=self.hparams.noise_std, 
+            size=(B, mask.shape[0], C), 
+            device=self.device
+        )
+        # replace with random noise
+        feats[:,mask,:] = noise
+
+        #### convolutions
+        src = self.modules.CNN(feats)
+        # print('after cnn: ', src.shape)
+
+        ##### transformer
+        enc_out = self.modules.wrapper(src, wav_lens) # only use encoder
+        # print('enc out: ', enc_out.shape)
+
+        ##### linear
+        logits = self.modules.linear(enc_out)
+        # print('linear layer out: ', logits.shape)
+
+        mask_idx = mask[::divis_by] // divis_by
+        logits[:,mask_idx,:]
+        targets[:,mask_idx].shape
+
+        if  not torch.isfinite(logits).all():
+            print('src: ', torch.isfinite(src).all())
+            print('enc: ', torch.isfinite(enc_out).all())
+            print('logits: ', torch.isfinite(logits).all())
+            print('targets: ', torch.isfinite(targets).all())
+        ##### get masked region
+        logits = logits[:,mask_idx,:]
+        targets = targets[:,mask_idx]
+        B, T, C = logits.shape
+        return logits.view(B * T, C), targets.view(B*T)
+    
+
+    def compute_objectives(self, predictions, batch, stage):
+        pred, targets = predictions
+        if stage != sb.Stage.TRAIN and sb.utils.distributed.if_main_process():
+            predicted_classes = torch.argmax(pred, dim=-1)
+            correct_predictions = (predicted_classes == targets)
+            accuracy = correct_predictions.sum().item() / len(correct_predictions)
+            self.acc_metric.append(accuracy)
+
+        return F.cross_entropy(pred, targets)
+
+    def fit_batch(self, batch):
+
+        should_step = self.step % self.grad_accumulation_factor == 0
+        # Managing automatic mixed precision
+        if self.auto_mix_prec:
+            with torch.autocast(torch.device(self.device).type):
+                outputs = self.compute_forward(batch, sb.Stage.TRAIN)
+
+            # Losses are excluded from mixed precision to avoid instabilities
+            loss = self.compute_objectives(outputs, batch, sb.Stage.TRAIN)
+            with self.no_sync(not should_step):
+                self.scaler.scale(
+                    loss / self.grad_accumulation_factor
+                ).backward()
+            if should_step:
+                self.scaler.unscale_(self.optimizer)
+                
+                if self.check_gradients(loss):
+                    self.scaler.step(self.optimizer)
+
+                self.scaler.update()
+                self.zero_grad()
+                self.optimizer_step += 1
+                self.hparams.noam_annealing(self.optimizer)
+        else:
+            if self.bfloat16_mix_prec:
+                with torch.autocast(
+                    device_type=torch.device(self.device).type,
+                    dtype=torch.bfloat16,
+                ):
+                    outputs = self.compute_forward(batch, sb.Stage.TRAIN)
+                    loss = self.compute_objectives(
+                        outputs, batch, sb.Stage.TRAIN
+                    )
+            else:
+                outputs = self.compute_forward(batch, sb.Stage.TRAIN)
+                loss = self.compute_objectives(outputs, batch, sb.Stage.TRAIN)
+            with self.no_sync(not should_step):
+                (loss / self.grad_accumulation_factor).backward()
+            if should_step:
+                if self.check_gradients(loss):
+                    self.optimizer.step()
+                self.zero_grad()
+                self.optimizer_step += 1
+                self.hparams.noam_annealing(self.optimizer)
+
+        self.on_fit_batch_end(batch, outputs, loss, should_step)
+        return loss.detach().cpu()
+
+    def on_fit_batch_end(self, batch, outputs, loss, should_ste):
+        """ Called after fit_batch(), updates learning rate and does per-step logging. """
+        
+        # Perform step-wise logging
+        if (
+            hasattr(self.hparams, "log_interval")
+            and self.optimizer_step % self.hparams.log_interval == 0
+        ):
+
+            # Create a dictionary and fill it with everything we
+            # want to log such as contrastive loss, diversity loss,
+            # learning rate etc.
+            log_dct = {}
+
+            current_lr = self.optimizer.param_groups[0]["lr"]
+            log_dct["steps"] = self.optimizer_step
+            log_dct["lr"] = current_lr
+            log_dct["avg_loss"] = self.avg_train_loss
+
+            if hasattr(self, "time_last_log"):
+                run_time_since_last_log = time.time() - self.time_last_log
+                log_dct["run_time"] = run_time_since_last_log
+            self.time_last_log = time.time()
+
+            if sb.utils.distributed.if_main_process():
+                self.hparams.train_steps_logger.log_stats(stats_meta=log_dct,)
+
+    # def evaluate_batch(self, batch, stage):
+    #     """ Returns accuracy on contrastive objective. """
+    #     out = self.compute_forward(batch, stage=stage)
+    #     objectives = self.compute_objectives(out, batch, stage=stage)
+    #     return objectives["backprop_loss"].detach().cpu()
+
+    def on_stage_start(self, stage, epoch):
+        """Gets called at the beginning of each epoch"""
+        if stage != sb.Stage.TRAIN:
+            self.acc_metric = []
+
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+
+        stage_stats = {"loss": stage_loss}
+        if stage == sb.Stage.TRAIN:
+            self.train_stats = stage_stats
+
+        if stage == sb.Stage.VALID:
+            if self.acc_metric:
+
+                stage_stats["accuracy"] = sum(self.acc_metric) / len(
+                    self.acc_metric
+                )
+
+            self.hparams.train_stage_logger.log_stats(
+                stats_meta={
+                    "epoch": epoch,
+                    "steps": self.optimizer_step,
+                    "lr": self.optimizer.param_groups[0]["lr"],
+                },
+                train_stats=self.train_stats,
+                valid_stats=stage_stats,
+            )
+
+            self.checkpointer.save_and_keep_only(
+                end_of_epoch=True,
+                num_to_keep=4,
+                meta={"valid_loss": stage_loss},
+            )
+
+
+def dataio_prepare(hparams):
+    data_folder = hparams["data_folder"]
+
+    train_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["train_csv"], replacements={"data_root": data_folder},
+    )
+
+    # We remove longer and shorter files from the train.
+    train_data = train_data.filtered_sorted(
+        sort_key="duration",
+        key_max_value={"duration": hparams["avoid_if_longer_than"]},
+        key_min_value={"duration": hparams["avoid_if_shorter_than"]},
+    )
+
+    valid_data = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=hparams["valid_csv"], replacements={"data_root": data_folder},
+    )
+
+    datasets = [train_data, valid_data]
+
+    def get_output_lengths(input_lengths):
+        """ Function to get the output length of the feature extractor this is
+            necessery to compute the masks of BestRQ.
+        """
+        sr = hparams["sample_rate"]
+        hop_length = hparams["hop_length"]
+
+        return (input_lengths // (sr*hop_length / 1000) + 1).to(torch.long)
+
+    @sb.utils.data_pipeline.takes("wav")
+    @sb.utils.data_pipeline.provides("sig")
+    def audio_pipeline(wav):
+        # Speed Perturb is done here so it is multi-threaded with the
+        # workers of the dataloader (faster).
+        if "speed_perturb" in hparams:
+            sig = sb.dataio.dataio.read_audio(wav)
+
+            sig = hparams["speed_perturb"](sig.unsqueeze(0)).squeeze(0)
+        else:
+            sig = sb.dataio.dataio.read_audio(wav)
+        return sig
+
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline)
+    sb.dataio.dataset.set_output_keys(datasets, ["id", "sig"])
+
+    # We create the DynamicBatch Sampler
+    train_sampler = DynamicBatchSampler(
+        train_data,
+        hparams["seconds_per_batch"],
+        num_buckets=hparams["train_num_buckets"],
+        length_func=lambda x: x["duration"],
+        batch_ordering="random",
+        shuffle=True,
+    )
+
+    # We define the custom collation function that is necessary for best-rq to
+    # generate masks.
+    brq_mask_collate_fn_partial = partial(
+        brq_mask_collate_fn,
+        get_out_len_fn=get_output_lengths,
+        mask_prob=hparams["mask_prob"],
+        mask_length=hparams["mask_length"],
+        n_mels=hparams["n_mels"],
+    )
+
+    train_loader_kwargs = {
+        "batch_sampler": train_sampler,
+        "collate_fn": brq_mask_collate_fn_partial,
+        "num_workers": hparams["train_dataloader_options"]["num_workers"],
+        "pin_memory": True,
+    }
+
+    valid_loader = SaveableDataLoader(
+        valid_data,
+        collate_fn=brq_mask_collate_fn_partial,
+        num_workers=hparams["test_dataloader_options"]["num_workers"],
+        batch_size=hparams["test_dataloader_options"]["batch_size"],
+        pin_memory=True,
+    )
+
+    return train_data, valid_loader, train_loader_kwargs
+
+def main():
+    logger.setLevel(logging.INFO)
+    hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
+
+    sb.utils.distributed.ddp_init_group(run_opts)
+
+    with open(hparams_file) as fin:
+        hparams = load_hyperpyyaml(fin, overrides)
+    hparams.update(run_opts)
+
+    sb.create_experiment_directory(
+        experiment_directory=hparams["output_folder"],
+        hyperparams_to_save=hparams_file,
+        overrides=overrides,
+    )
+
+    from librispeech_prepare import prepare_librispeech
+
+    run_on_main(
+        prepare_librispeech,
+        kwargs={
+            "data_folder": hparams["data_folder"],
+            "tr_splits": hparams["train_splits"],
+            "dev_splits": hparams["dev_splits"],
+            "te_splits": hparams["test_splits"],
+            "save_folder": hparams["output_folder"],
+            "merge_lst": hparams["train_splits"],
+            "merge_name": "train.csv",
+            "skip_prep": hparams["skip_prep"],
+        },
+    )
+
+    # Part that matters starts here.
+    train_dataset, valid_loader, train_loader_kwargs = dataio_prepare(hparams)
+
+    brain = BestRQBrain(
+        modules=hparams["modules"],
+        opt_class=hparams["optimizer"],
+        hparams=hparams,
+        run_opts=run_opts,
+        checkpointer=hparams["checkpointer"],
+    )
+    # with torch.autograd.detect_anomaly():
+    brain.fit(
+        brain.hparams.epoch_counter,
+        train_dataset,
+        valid_loader,
+        train_loader_kwargs=train_loader_kwargs,
+        progressbar=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
