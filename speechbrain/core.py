274ed6c8fd446ebe8463989c9a78e627b4ddcfac
Adel Moumen <88119391+Adel-Moumen@users.noreply.github.com>
2024-01-23 11:15:29 +0100

    remove scaler from ckpt if in bf16 mode (#2347)
    * remove scaler from ckpt if in bf16 mode
    
    * precommit

--- speechbrain/core.py
+++ speechbrain/core.py
@@ -762,7 +762,11 @@ class Brain:
         elif "cuda" in self.device and self.precision in ["fp16", "bf16"]:
             self.use_amp = True
 
-        if self.use_amp and self.checkpointer is not None:
+        if (
+            self.use_amp
+            and self.checkpointer is not None
+            and self.precision != "bf16"
+        ):
             self.checkpointer.add_recoverable("scaler", self.scaler)
 
         # List parameter count for the user
