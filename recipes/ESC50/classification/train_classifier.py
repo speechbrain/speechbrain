ce9a0382c5a821861894a462ac291832f84a2f26
fpaissan <me@francescopaissan.it>
2024-02-01 00:09:33 +0100

    roar with random baseline

--- recipes/ESC50/classification/train_classifier.py
+++ recipes/ESC50/classification/train_classifier.py
@@ -89,6 +89,8 @@ class ESC50Brain(sb.core.Brain):
                         ).squeeze().view(-1, 1, 1, 1)
                 int_ = (int_ >= thr).float().squeeze(1)
 
+                int_ = torch.rand_like(int_)
+
                 # basically mask out
                 net_input = (1 - int_) * net_input[:, :int_.shape[1], :]
 
