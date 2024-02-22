cc0c118d6e32fa0e10fb14ca48362da608603acb
Sylvain de Langen <sdelang@sdelang.fr>
2024-02-22 10:02:12 +0100

    wip

---
+++ speechbrain/lobes/models/flair/__init__.py
@@ -0,0 +1,10 @@
+try:
+    import flair
+except ImportError as e:
+    raise ImportError(
+        f"Failed to import flair: {e}\n"
+        f"Please install flair e.g. using `pip install flair`.\n"
+        f"For more details, see https://github.com/flairNLP/flair"
+    )
+
+from . import postagger
---
+++ speechbrain/lobes/models/flair/postagger.py
@@ -0,0 +1,13 @@
+"""Models and tooling for part-of-speech tagging
+
+Authors
+* Sylvain de Langen 2024
+"""
+
+
+class FlairPOSTagger:
+    """
+    Part-of-speech tagger using the flair toolkit.
+    """
+
+    
