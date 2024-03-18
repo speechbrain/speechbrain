8ae360bdab42b44ae0612a4b067e8170926f36a5
Adel Moumen <adelmoumen.pro@gmail.com>
2024-03-18 14:18:52 +0100

    HF DL support

--- recipes/GigaSpeech/gigaspeech_prepare.py
+++ recipes/GigaSpeech/gigaspeech_prepare.py
@@ -16,6 +16,7 @@ import csv
 from dataclasses import dataclass
 import functools
 from speechbrain.utils.parallel import parallel_map
+import speechbrain as sb
 
 logger = logging.getLogger(__name__)
 
@@ -75,6 +76,7 @@ def prepare_gigaspeech(
     json_file: str = "GigaSpeech.json",
     skip_prep: bool = False,
     convert_opus_to_wav: bool = True,
+    download_with_HF: bool = False,
 ) -> None:
     """ Prepare the csv files for GigaSpeech dataset.
 
@@ -107,6 +109,12 @@ def prepare_gigaspeech(
         If True, the data preparation will be skipped, and the function will return immediately.
     convert_opus_to_wav : bool, optional
         If True, the opus files will be converted to wav files.
+    download_with_HF : bool, optional
+        If True, the dataset will be downloaded using the Hugging Face datasets library.
+        We highly recommend using this option if you are based in the EU or US as it will
+        be faster and more reliable than the official host. Make sure to read the
+        instructions on how to get the dataset from Hugging Face here:
+        https://huggingface.co/datasets/speechcolab/gigaspeech
 
     Returns
     -------
@@ -134,9 +142,11 @@ def prepare_gigaspeech(
 
     # Setting output paths
     save_output = {}
+    train_split = ""
     for split in splits:
         if split in TRAIN_SUBSET:
             save_output[split] = output_train
+            train_split = split
         else:
             if split == "DEV":
                 save_output[split] = output_dev
@@ -150,22 +160,38 @@ def prepare_gigaspeech(
     else:
         logger.info("Starting data preparation...")
 
-    # check that the data folder contains the GigaSpeech dataset
-    check_gigaspeech_folders(data_folder, json_file)
+    if download_with_HF:
+        from datasets import load_dataset
 
-    logger.info(f"Starting reading {json_file}.")
-    with open(json_file, "r") as f:
-        info = json.load(f)
-    logger.info(f"Reading {json_file} done.")
+        hf_dataset = load_dataset(
+            "dataset.py",
+            train_split.lower(),
+            trust_remote_code=True,
+            cache_dir=data_folder,
+            data_dir=data_folder,
+        )
+        for split, output in save_output.items():
+            logger.info(f"Starting creating {output} using {split} split.")
+            HF_create_csv(
+                output, hf_dataset[split], split,
+            )
+    else:
+        # check that the data folder contains the GigaSpeech dataset
+        check_gigaspeech_folders(data_folder, json_file)
 
-    for split, output in save_output.items():
-        logger.info(f"Starting creating {output} using {split} split.")
-        create_csv(output, info, data_folder, split, convert_opus_to_wav)
+        logger.info(f"Starting reading {json_file}.")
+        with open(json_file, "r") as f:
+            info = json.load(f)
+        logger.info(f"Reading {json_file} done.")
+
+        for split, output in save_output.items():
+            logger.info(f"Starting creating {output} using {split} split.")
+            create_csv(output, info, data_folder, split, convert_opus_to_wav)
     logger.info("Data preparation completed!")
 
 
 def process_line(
-    audio: json, data_folder: str, split: str, convert_opus_to_wav: bool
+    audio: json, data_folder: str, split: str, convert_opus_to_wav: bool,
 ) -> list:
     """
     Process the audio line and return the utterances for the given split.
@@ -297,6 +323,127 @@ def create_csv(
     logger.info(
         f"Total duration of {split} split: {round(total_duration / 3600, 2)} Hours"
     )
+
+
+def HF_create_csv(csv_file: str, hf_dataset, split: str,) -> None:
+    """
+    Create a CSV file based on the info in the GigaSpeech JSON file and filter the data based on the split.
+
+    Parameters
+    ----------
+    csv_file : str
+        The path to the CSV file to be created.
+    info : dict
+        The GigaSpeech JSON file content.
+    data_folder : str
+        The path to the GigaSpeech dataset.
+    split : str
+        The split to be used for filtering the data.
+
+    Returns
+    -------
+    None
+    """
+    total_duration = 0.0
+    nb_samples = 0
+
+    line_processor = functools.partial(HF_process_line,)
+
+    csv_file_tmp = csv_file + ".tmp"
+    with open(csv_file_tmp, mode="w", encoding="utf-8") as csv_f:
+        csv_writer = csv.writer(
+            csv_f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        header = [
+            "ID",
+            "audio_id",
+            "audio_path",
+            "speaker",
+            "begin_time",
+            "end_time",
+            "duration",
+            "text",
+        ]
+        csv_writer.writerow(header)
+
+        for row in parallel_map(line_processor, hf_dataset, chunk_size=1024):
+            if row is None:
+                continue
+
+            csv_writer.writerow(
+                [
+                    row.utt_id,
+                    row.audio_id,
+                    row.audio_path,
+                    row.speaker,
+                    str(row.begin_time),
+                    str(row.end_time),
+                    str(row.duration),
+                    row.text,
+                ]
+            )
+
+            total_duration += row.duration
+            nb_samples += 1
+
+    os.replace(csv_file_tmp, csv_file)
+
+    print(f"{csv_file} succesfully created!")
+    print(f"Number of samples in {split} split: {nb_samples}")
+    print(
+        f"Total duration of {split} split: {round(total_duration / 3600, 2)} Hours"
+    )
+
+
+def HF_process_line(row,) -> list:
+    """
+    Process the audio line and return the utterances for the given split.
+
+    Parameters
+    ----------
+    row: dict
+        The audio line to be processed.
+
+    Returns
+    -------
+    list
+        The list of utterances for the given split.
+    """
+    audio_path = os.path.join(row["audio"]["path"])
+
+    assert os.path.isfile(audio_path), f"File not found: {audio_path}"
+
+    # check reading the audio file ; HF may have some corrupted files
+    try:
+        _ = sb.dataio.dataio.read_audio(audio_path)
+    except Exception as e:
+        logger.error(f"Failed reading {audio_path}: {e}")
+        return None
+
+    text = preprocess_text(row["text"])
+    if text:
+        utt_id = row["segment_id"]
+        audio_id = row["audio_id"]
+        audio_path = row["audio"]["path"]
+        speaker = row["speaker"]
+        begin_time = float(row["begin_time"])
+        end_time = float(row["end_time"])
+        duration = end_time - begin_time
+
+        row = GigaSpeechRow(
+            utt_id=utt_id,
+            audio_id=audio_id,
+            audio_path=audio_path,
+            speaker=speaker,
+            begin_time=begin_time,
+            end_time=end_time,
+            duration=duration,
+            text=text,
+        )
+
+        return row
+    else:
+        return None
 
 
 def convert_opus2wav(audio_opus_path):
