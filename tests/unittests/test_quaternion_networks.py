149ac806959d132b69512fbe7800c62dff04f21f
Drew Wagner <drewarthurwagner@gmail.com>
2024-03-19 09:07:57 -0400

    Test QPooling2d

--- tests/unittests/test_quaternion_networks.py
+++ tests/unittests/test_quaternion_networks.py
@@ -8,10 +8,14 @@ def test_QLinear(device):
     inputs = torch.rand((1, 2, 4), device=device)
 
     # Test bias is correctly zeroed
-    qlin_no_bias = QLinear(n_neurons=1, input_shape=inputs.shape, bias=False)
+    qlin_no_bias = QLinear(
+        n_neurons=1, input_shape=inputs.shape, bias=False
+    ).to(device)
     assert (qlin_no_bias.bias == 0).all().item()
 
-    qlin_with_bias = QLinear(n_neurons=4, input_shape=inputs.shape, bias=True)
+    qlin_with_bias = QLinear(
+        n_neurons=4, input_shape=inputs.shape, bias=True
+    ).to(device)
     assert (qlin_with_bias.bias == 0).all().item()
 
     # Test output shape is correct
@@ -33,3 +37,45 @@ def test_QLinear(device):
     assert torch.all(torch.eq(inputs, outputs))
 
     assert torch.jit.trace(qlin_with_bias, inputs)
+
+
+def test_QPooling2d(device):
+    from speechbrain.nnet.quaternion_networks.q_pooling import QPooling2d
+
+    input = (
+        torch.tensor(
+            [
+                [[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0]],
+                [[0, 0, 0, 4], [3, 3, 0, 0], [0, 5, 0, 0]],
+            ],
+            device=device,
+        )
+        .float()
+        .unsqueeze(0)
+    )
+    pool = QPooling2d("max", (2, 3)).to(device)
+    output = pool(input)
+
+    # Max pool by magnitude
+    assert torch.all(torch.eq(output, input[:, 1, 2]))  # [0, 5, 0, 0]
+    assert output.shape == (1, 1, 1, 4)
+
+    pool = QPooling2d("max", (1, 3)).to(device)
+    output = pool(input)
+    assert torch.all(
+        torch.eq(output, input[:, :, [2]])
+    )  # [0,0,3,0] and [0,5,0,0]
+    assert output.shape == (1, 2, 1, 4)
+
+    pool = QPooling2d("avg", (2, 3)).to(device)
+    output = pool(input)
+
+    # Component-wise average:
+    assert torch.all(torch.eq(output, input.mean((1, 2))))
+
+    pool = QPooling2d("avg", (1, 3)).to(device)
+    output = pool(input)
+
+    assert torch.all(torch.eq(output, input.mean(2).unsqueeze(2)))
+
+    assert torch.jit.trace(pool, input)
