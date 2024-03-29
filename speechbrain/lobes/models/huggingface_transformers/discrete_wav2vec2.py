11b981cc6cfdd0d82eeae95383374b7e6af196d4
asu <sdelang@sdelang.fr>
2024-03-29 14:31:11 +0100

    skip broken doctests due to ssl model unavailability

--- speechbrain/lobes/models/huggingface_transformers/discrete_wav2vec2.py
+++ speechbrain/lobes/models/huggingface_transformers/discrete_wav2vec2.py
@@ -76,11 +76,11 @@ class DiscreteWav2Vec2(Wav2Vec2):
     >>> kmeans_repo_id = "speechbrain/SSL_Quantization"
     >>> kmeans_filename = "LibriSpeech_wav2vec_k128_L7.pt"
     >>> kmeans_cache_dir="savedir"
-    >>> model = DiscreteWav2Vec2(model_hub, save_path,freeze = True,ssl_layer_num=ssl_layer_num,kmeans_repo_id=kmeans_repo_id, kmeans_filename=kmeans_filename, kmeans_cache_dir=kmeans_cache_dir)
-    >>> embs, tokens = model(inputs)
-    >>> embs.shape
+    >>> model = DiscreteWav2Vec2(model_hub, save_path,freeze = True,ssl_layer_num=ssl_layer_num,kmeans_repo_id=kmeans_repo_id, kmeans_filename=kmeans_filename, kmeans_cache_dir=kmeans_cache_dir)  # doctest: +SKIP
+    >>> embs, tokens = model(inputs)  # doctest: +SKIP
+    >>> embs.shape  # doctest: +SKIP
     torch.Size([10, 1, 1024])
-    >>> tokens.shape
+    >>> tokens.shape  # doctest: +SKIP
     torch.Size([10, 1])
     """
 
