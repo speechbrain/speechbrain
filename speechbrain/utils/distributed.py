8ea3100d5704d6295cb4b4b47bc2a164242738e4
Adel Moumen <adelmoumen.pro@gmail.com>
2024-04-13 16:01:07 +0200

    fix LOCAL_RANK to be RANK in if_main_process

--- speechbrain/utils/distributed.py
+++ speechbrain/utils/distributed.py
@@ -75,15 +75,15 @@ def run_on_main(
 
 def if_main_process():
     """Checks if the current process is the main local process and authorized to run
-    I/O commands. In DDP mode, the main local process is the one with LOCAL_RANK == 0.
-    In standard mode, the process will not have `LOCAL_RANK` Unix var and will be
+    I/O commands. In DDP mode, the main local process is the one with `RANK == 0`.
+    In standard mode, the process will not have `RANK` Unix var and will be
     authorized to run the I/O commands.
     """
-    if "LOCAL_RANK" in os.environ:
-        if os.environ["LOCAL_RANK"] == "":
+    if "RANK" in os.environ:
+        if os.environ["RANK"] == "":
             return False
         else:
-            if int(os.environ["LOCAL_RANK"]) == 0:
+            if int(os.environ["RANK"]) == 0:
                 return True
             return False
     return True
