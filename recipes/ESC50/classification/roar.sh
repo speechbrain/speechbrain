9080e7fe64e878b51d31ccf0aac7cdf030e4c1e8
Francesco Paissan <46992226+fpaissan@users.noreply.github.com>
2024-04-18 02:49:51 +0900

    cleaning up classification as well

--- recipes/ESC50/classification/roar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for rth in 0.2 0.4 0.6 0.8
-do
-	EXPERIMENT_NAME=cnn14_stft_roarth_$rth
-	TS_SOCKET=/tmp/gpu1 CUDA_VISIBLE_DEVICES=1 ts python -Wignore train_classifier.py hparams/roar_cnn14.yaml --experiment_name $EXPERIMENT_NAME \
-    	--add_wham_noise True --seed 2 \
-    	--n_mels 80 --roar_th $rth
-done
-
