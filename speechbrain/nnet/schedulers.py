d086cde84ef1bb7357059ea08c95584b9bafe50b
shucongzhang <104781888+shucongzhang@users.noreply.github.com>
2024-04-18 12:05:50 +0100

    CTC-only training recipes for LibriSpeech (code from Samsung AI Cambridge) (#2290)
    CTC-only pre-training of conformer and branchformer.
    
    ---------
    
    Co-authored-by: Shucong Zhang/Embedded AI /SRUK/Engineer/Samsung Electronics <s1.zhang@sruk-ccn4.eu.corp.samsungelectronics.net>
    Co-authored-by: Adel Moumen <adelmoumen.pro@gmail.com>
    Co-authored-by: Adel Moumen <88119391+Adel-Moumen@users.noreply.github.com>
    Co-authored-by: Parcollet Titouan <titouan.parcollet@univ-avignon.fr>

--- speechbrain/nnet/schedulers.py
+++ speechbrain/nnet/schedulers.py
@@ -5,6 +5,7 @@ Authors
  * Mirco Ravanelli 2020
  * Peter Plantinga 2020
  * Loren Lugosch 2020
+ * Shucong Zhang 2023
 """
 
 import logging
@@ -663,6 +664,108 @@ class NoamIntervalScheduler:
             if self.n_steps > self.anneal_steps[i]:
                 lr_scale = lr_scale * self.anneal_rates[i]
         return lr_scale
+
+    @checkpoints.mark_as_saver
+    def save(self, path):
+        """Saves the current metrics on the specified path."""
+        data = {"losses": self.losses, "n_steps": self.n_steps}
+        torch.save(data, path)
+
+    @checkpoints.mark_as_loader
+    def load(self, path, end_of_epoch=False, device=None):
+        """Loads the needed information."""
+        del end_of_epoch  # Unused in this class
+        del device
+        data = torch.load(path)
+        self.losses = data["losses"]
+        self.n_steps = data["n_steps"]
+
+
+@checkpoints.register_checkpoint_hooks
+class LinearNoamScheduler:
+    """The is an implementation of the extended Noam scheduler in the Squeezeformer paper.
+    Reference: https://arxiv.org/pdf/2206.00888.pdf
+
+    Note: this scheduler anneals the lr at each update of the model's weight,
+    and n_steps must be saved for restarting.
+
+    Arguments
+    ---------
+    lr_initial : float
+        Initial learning rate (i.e. the lr used at epoch 0).
+    n_warmup_steps : int
+        number of warm-up steps.
+    n_keep_steps : int
+        after warmp-up steps, number of steps that the lr is kept unchanged.
+
+    Example
+    -------
+    >>> from speechbrain.nnet.linear import Linear
+    >>> inp_tensor = torch.rand([1,660,3])
+    >>> model = Linear(input_size=3, n_neurons=4)
+    >>> optim = torch.optim.Adam(model.parameters(), lr=1)
+    >>> output = model(inp_tensor)
+    >>> scheduler =LinearNoamScheduler(optim.param_groups[0]["lr"], 2, 2)
+    >>> curr_lr,next_lr=scheduler(optim)
+    >>> optim.param_groups[0]["lr"]
+    0.5
+    >>> curr_lr,next_lr=scheduler(optim)
+    >>> optim.param_groups[0]["lr"]
+    1.0
+    >>> curr_lr,next_lr=scheduler(optim)
+    >>> optim.param_groups[0]["lr"]
+    1.0
+    >>> curr_lr,next_lr=scheduler(optim)
+    >>> optim.param_groups[0]["lr"]
+    1.0
+    >>> curr_lr,next_lr=scheduler(optim)
+    >>> optim.param_groups[0]["lr"]
+    0.6666666666666666
+    """
+
+    def __init__(self, lr_initial, n_warmup_steps, n_keep_steps):
+        self.lr_initial = lr_initial
+        self.n_warmup_steps = n_warmup_steps
+        self.n_keep_steps = n_keep_steps
+        self.current_lr = lr_initial
+        self.losses = []
+        self.n_steps = 0
+
+    def __call__(self, opt):
+        """
+        Arguments
+        ---------
+        opt : optimizer
+            The optimizer to update using this scheduler.
+
+        Returns
+        -------
+        current_lr : float
+            The learning rate before the update.
+        lr : float
+            The learning rate after the update.
+        """
+        self.n_steps += 1
+
+        current_lr = opt.param_groups[0]["lr"]
+
+        lr = self.lr_initial * self._get_lr_scale()
+
+        # Changing the learning rate within the optimizer
+        for param_group in opt.param_groups:
+            param_group["lr"] = lr
+
+        self.current_lr = current_lr
+        return current_lr, lr
+
+    def _get_lr_scale(self):
+        n_steps, n_warmup_steps = self.n_steps, self.n_warmup_steps
+        if n_steps < n_warmup_steps:
+            return (n_steps + 0.0) / n_warmup_steps
+        elif n_steps < self.n_keep_steps + n_warmup_steps:
+            return 1.0
+        else:
+            return n_warmup_steps / (n_steps - self.n_keep_steps)
 
     @checkpoints.mark_as_saver
     def save(self, path):
