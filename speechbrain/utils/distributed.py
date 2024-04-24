e3c48afa2d6f4497aa42d518db59ec1cb72acb9d
Adel Moumen <adelmoumen.pro@gmail.com>
2024-04-24 14:31:34 +0200

    docstring

--- speechbrain/utils/distributed.py
+++ speechbrain/utils/distributed.py
@@ -77,8 +77,7 @@ def run_on_main(
 def if_main_process():
     """Checks if the current process is the main process and authorized to run
     I/O commands. The main process is the one with `RANK == 0`. In standard mode,
-    the process will not have `RANK`/`LOCAL_RANK` Unix var and will be
-    authorized to run the I/O commands.
+    the process will not have `RANK` Unix var and will be authorized to run the I/O commands.
     """
     if "RANK" in os.environ:
         if os.environ["RANK"] == "":
