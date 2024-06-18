a2d63a55a8b991018a47e54b78fc1586fccbd851
Ilias MAOUDJ <ilias.maoudj@gmail.com>
2024-06-18 16:44:37 +0200

    FIX prepare file

--- recipes/SEP-28k/sep28k_prepare.py
+++ recipes/SEP-28k/sep28k_prepare.py
@@ -11,19 +11,22 @@ Adapted from the ESC50 and Urbansound8k recipe.
 import logging
 import os
 import shutil
+
 import pandas as pd
-from tqdm import tqdm
 import requests
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
+
 def download_dropbox(url, podcast):
-    headers = {'user-agent': 'Wget/1.16 (linux-gnu)'}
+    headers = {"user-agent": "Wget/1.16 (linux-gnu)"}
     r = requests.get(url, stream=True, headers=headers)
-    with open(podcast, 'wb') as f:
-        for chunk in tqdm(r.iter_content(chunk_size=1024)): 
+    with open(podcast, "wb") as f:
+        for chunk in tqdm(r.iter_content(chunk_size=1024)):
             if chunk:
                 f.write(chunk)
+
 
 def download_sep28k(data_path):
     """
@@ -38,22 +41,33 @@ def download_sep28k(data_path):
     if not os.path.exists(temp_path):
         os.mkdir(temp_path)
     if not os.path.exists(os.path.join(data_path, "SEP28k-data.zip")):
-        print(f"SEP-28k is missing. We are now downloading it. Be patient, the total size is 1.9GB. Takes 1,995,736 iterations.")
-        print(f"**** NOW DOWNLOADING zip file *******")
-        download_dropbox("https://www.dropbox.com/scl/fi/rpavffri0odb2g25bxy58/sep28k_clips.zip?rlkey=zfxpdrek642pxu0rj64qid7gh&st=xum8yxm1&dl=0", 
-                         f"{temp_path}/SEP28k-data.zip")
-    if not os.path.exists(os.path.join(data_path, "SEP-28k-Extended_clips.csv")):
-        print(f"**** NOW DOWNLOADING csv file *******")
-        download_dropbox("https://www.dropbox.com/scl/fi/amzp62bpj8zqo21kpmoqy/SEP-28k-Extended_clips.csv?rlkey=5ehd8wv1q2gyz32m2pyynlcn2&st=cb76g1r4&dl=0",
-                         f"{temp_path}/SEP-28k-Extended_clips.csv",
-            )
+        print(
+            "SEP-28k is missing. We are now downloading it. Be patient, the total size is 1.9GB. Takes 1,995,736 iterations."
+        )
+        print("**** NOW DOWNLOADING zip file *******")
+        download_dropbox(
+            "https://www.dropbox.com/scl/fi/rpavffri0odb2g25bxy58/sep28k_clips.zip?rlkey=zfxpdrek642pxu0rj64qid7gh&st=xum8yxm1&dl=0",
+            f"{temp_path}/SEP28k-data.zip",
+        )
+    if not os.path.exists(
+        os.path.join(data_path, "SEP-28k-Extended_clips.csv")
+    ):
+        print("**** NOW DOWNLOADING csv file *******")
+        download_dropbox(
+            "https://www.dropbox.com/scl/fi/amzp62bpj8zqo21kpmoqy/SEP-28k-Extended_ \
+            clips.csv?rlkey=5ehd8wv1q2gyz32m2pyynlcn2&st=cb76g1r4&dl=0",
+            f"{temp_path}/SEP-28k-Extended_clips.csv",
+        )
     files = os.listdir(temp_path)
     for fl in files:
         shutil.move(os.path.join(temp_path, fl), data_path)
     shutil.rmtree(os.path.join(temp_path))
     if not os.path.exists(os.path.join(data_path, "SEP28k-data")):
-        shutil.unpack_archive(os.path.join(data_path, "SEP28k-data.zip"), data_path)
+        shutil.unpack_archive(
+            os.path.join(data_path, "SEP28k-data.zip"), data_path
+        )
     print(f"SEP-28k is downloaded in {data_path}")
+
 
 def prepare_sep28k(data_folder, split_type="SEP28k-E"):
     """
@@ -70,20 +84,12 @@ def prepare_sep28k(data_folder, split_type="SEP28k-E"):
 
     df = pd.read_csv(f"{data_folder}/SEP-28k-Extended_clips.csv")
     df["ID"] = df.index
-    df_train = df[df[split_type]=="train"]
-    df_valid = df[df[split_type]=="dev"]
-    df_test = df[df[split_type]=="test"]
+    df_train = df[df[split_type] == "train"]
+    df_valid = df[df[split_type] == "dev"]
+    df_test = df[df[split_type] == "test"]
 
     df_train.to_csv(f"{data_folder}/{split_type}_train.csv")
     df_valid.to_csv(f"{data_folder}/{split_type}_valid.csv")
     df_test.to_csv(f"{data_folder}/{split_type}_test.csv")
-    df_all = pd.concat([df_train,df_valid,df_test])
+    df_all = pd.concat([df_train, df_valid, df_test])
     df_all.to_csv(f"{data_folder}/{split_type}_clean.csv")
-
-
-import argparse
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', dest='path', type=str, default="sep28k_data")
-    args = parser.parse_args()
-    prepare_sep28k(args.path)
