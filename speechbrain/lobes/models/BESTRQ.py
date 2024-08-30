51802ce88bcaee540899601979ab856678dacdac
Titouan Parcollet/Embedded AI /SRUK/Engineer/Samsung Electronics <t.parcollet@sruk-ccn4.eu.corp.samsungelectronics.net>
2024-08-30 15:21:50 +0100

    precommit

--- speechbrain/lobes/models/BESTRQ.py
+++ speechbrain/lobes/models/BESTRQ.py
@@ -6,37 +6,40 @@ Authors
 * Titouan Parcollet 2025
 """
 
-import numpy as np
 import random
+
 import torch
+
 from speechbrain.utils.data_utils import batch_pad_right
 
 
 def compute_mask(shape, sample_lens, mask_prob, mask_length):
     min_sample_len = min(sample_lens)
 
-    # int always floors the float number so adding + random.random() 
+    # int always floors the float number so adding + random.random()
     # makes it 50% change of rounding up and 50% of rounding down
     num_mask = int(mask_prob * min_sample_len + random.random())
 
-    # make sure there is at least 1 mask 
+    # make sure there is at least 1 mask
     if num_mask == 0:
         num_mask = 1
 
-    permutation = torch.randperm(min_sample_len//mask_length) * mask_length
+    permutation = torch.randperm(min_sample_len // mask_length) * mask_length
     selected_indices = permutation[:num_mask]
     selected_indices, _ = selected_indices.sort()
 
     # get next for frames
     idx = []
-    for  i in selected_indices:
-        idx.append(torch.arange(start=i, end=i+mask_length))
+    for i in selected_indices:
+        idx.append(torch.arange(start=i, end=i + mask_length))
     idx = torch.cat(idx)
 
     return idx
 
 
-def brq_mask_collate_fn(samples_lst, get_out_len_fn, mask_prob, mask_length, n_mels):
+def brq_mask_collate_fn(
+    samples_lst, get_out_len_fn, mask_prob, mask_length, n_mels
+):
     wav_lst, latent_length_lst = [], []
     ids = []
     for sample in samples_lst:
