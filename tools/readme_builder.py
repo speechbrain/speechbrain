08ce614dcf910c7edc6f96c92bb364c76cd2c7c4
asu <sdelang@sdelang.fr>
2024-08-30 10:16:24 +0200

    Fix formatting in the PERFORMANCE.md builder

--- tools/readme_builder.py
+++ tools/readme_builder.py
@@ -63,13 +63,13 @@ def create_table(fid_w, csv_file):
         if not recipes_task:
             continue  # Skip empty task
 
-        print(f"### {task} \n", file=fid_w)
+        print(f"### {task}\n", file=fid_w)
 
         performance_dict = extract_name_value_pairs(
             recipes_task[0]["performance"]
         )
         performance_metrics = performance_dict.keys()
-        performance_metrics = " | ".join(performance_metrics) + " | "
+        performance_metrics = " | ".join(performance_metrics) + " |"
 
         print(
             f"| Model | Checkpoints | HuggingFace | {performance_metrics}",
@@ -82,7 +82,7 @@ def create_table(fid_w, csv_file):
 
         for recipe in recipes_task:
             performance_dict = extract_name_value_pairs(recipe["performance"])
-            performance_values = " | ".join(performance_dict.values()) + " | "
+            performance_values = " | ".join(performance_dict.values()) + " |"
 
             str_res = (
                 f'[here]({recipe["Result_url"]})'
@@ -143,9 +143,10 @@ if __name__ == "__main__":
         args.recipe_info_dir, match_and=[".csv"], exclude_or=["~"]
     )
 
-    header = """# SpeechBrain Performance Report
-    This document provides an overview of the performance achieved on key datasets and tasks supported by SpeechBrain.
-    """
+    header = """\
+# SpeechBrain Performance Report
+This document provides an overview of the performance achieved on key datasets and tasks supported by SpeechBrain.
+"""
 
     print(header, file=file_w)
 
