c2a5b7d8d1c129b56e2ab5cf1e46d1018f165648
Ryan Whetten <whettenrmw@gmail.com>
2024-09-03 12:34:51 +0200

    BEST-RQ implementation (#2309)
    * added weighted encoder wrapper
    
    * Sync before and after deleting to prevent another process writing at the same time
    
    * add layer dropout option in conformer
    
    * Remove unneeded if_main_process that conflicts with barrier in delete checkpoint
    
    * Add env variable to signify single-threaded execution
    
    * add output_hidden_states and layerdrop in branchformer
    
    * Fix wrong call to run_on_main in timit recipe
    
    * Fix bug in run_on_main logic
    
    * add output_hidden_states to TransformerEncoder
    
    * add wrapper for using the hidden units
    
    * fix bug in main process function
    
    * add wrappers
    
    * add in bestrq recipies
    
    * update distriibuted
    
    * update train.py
    
    * moved files to librispeech folder, removed unnecessary parameters, add readme, fixed error with learning rate scheduler for new version of speechbrain
    
    * change skip_prep to false
    
    * fix typo in readme
    
    * update brq fine tuning to work with speechbrain version 1.0
    
    * move masking to lobes
    
    * renaming
    
    * cleaning old naming
    
    * fixes and clean
    
    * update transformer interfaces
    
    * revert hidden states capability from here
    
    * revert hidden states capability from here
    
    * revert hidden states capability from here
    
    * update quantiser doc
    
    * update d_model to have 95M parameter model
    
    * quick merge
    
    * fix CTC recipe 1
    
    * Adding update to CTC recipe, cleaningÃ
    
    * precommit
    
    * precommit
    
    * precommit again
    
    * The CI god is angry$
    
    * add recipe test ssl
    
    * add recipe test ssl
    
    * add quantizer and linear into checkpointer
    
    * update ctc recipe
    
    * fix recipe testing
    
    * add docstring for masking
    
    * quantiser docstring
    
    * fix CI
    
    * remove useless parameters
    
    * wtf happened
    
    * add documentation for return type
    
    * Bruh, what?
    
    * wFix the stoopid error
    
    * bruh has broken brain, he stoooopid
    
    * address Asu's comments
    
    * address Asu's comments
    
    * add docstring for hidden_states
    
    ---------
    
    Co-authored-by: Peter Plantinga <plantinga.peter@protonmail.com>
    Co-authored-by: Titouan Parcollet/Embedded AI /SRUK/Engineer/Samsung Electronics <t.parcollet@sruk-ccn4.eu.corp.samsungelectronics.net>

--- speechbrain/nnet/quantisers.py
+++ speechbrain/nnet/quantisers.py
@@ -8,6 +8,7 @@ Authors
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.linalg import vector_norm
 
 
 class GumbelVectorQuantizer(nn.Module):
@@ -121,3 +122,53 @@ class GumbelVectorQuantizer(nn.Module):
         x = x.view(bsz, tsz, -1)
         result["x"] = x
         return result
+
+
+class RandomProjectionQuantizer(nn.Module):
+    """Vector quantization using a projection and a randomly initialised codebook
+    this is useful for models like BEST-RQ for instance.
+
+    The output is the indices of the closest code in the codebook for each
+    time step of the input.
+
+    ref: https://arxiv.org/pdf/2202.01855
+
+    Arguments
+    ---------
+    input_dim: int
+        Input dimension (channels).
+    cb_dim: int
+        Size of each code in the codebook.
+    cb_vocab: int
+        Number of codes in the codebook
+
+    Example
+    -------
+    >>> quantiser = RandomProjectionQuantizer(16, 16, 32)
+    >>> inputs = torch.rand(10, 12, 16)
+    >>> output = quantiser(inputs)
+    >>> output.shape
+    torch.Size([10, 12])
+    """
+
+    def __init__(self, input_dim, cb_dim, cb_vocab):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.cb_dim = cb_dim
+        self.cb_vocab = cb_vocab
+
+        # Section 3.1 "projection matrix A use Xavier initialization"
+        P_init = torch.empty((input_dim, cb_dim))
+        self.register_buffer("P", nn.init.xavier_uniform_(P_init))
+
+        # normalize random matrix for codebook
+        self.register_buffer("CB", F.normalize(torch.randn(cb_vocab, cb_dim)))
+
+    def forward(self, x):
+        """Forward the latent vector to obtain a quantised output"""
+
+        x = F.normalize(x @ self.P)
+        return vector_norm(
+            (self.CB.unsqueeze(1) - x.unsqueeze(1)), dim=-1
+        ).argmin(dim=1)
