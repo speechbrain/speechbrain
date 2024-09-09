b62929a02aae0ce316ac8a6ccf51de62dc3d652f
Peter Plantinga <plantinga.peter@protonmail.com>
2024-09-09 16:30:43 -0400

    Fix docstring errors in LJSpeech

--- recipes/LJSpeech/TTS/vocoder/hifigan_discrete/extract_code.py
+++ recipes/LJSpeech/TTS/vocoder/hifigan_discrete/extract_code.py
@@ -67,6 +67,15 @@ def skip(splits, save_folder, conf):
     Detects if the ljspeech data_extraction has been already done.
     If the extraction has been done, we can skip it.
 
+    Arguments
+    ---------
+    splits : list
+        List of splits to check for existence.
+    save_folder : str
+        Path to folder containing prepared data.
+    conf : dict
+        Loaded configuration options.
+
     Returns
     -------
     bool
@@ -143,6 +152,10 @@ def extract_ljspeech(
         LjSpeech dataset sample rate
     skip_extract: Bool
         If True, skip extraction.
+
+    Returns
+    -------
+    None
 
     Example
     -------
