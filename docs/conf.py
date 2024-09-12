9018207991f5c4845bddaebd2040aedccce1684b
asu <sdelang@sdelang.fr>
2024-09-12 15:01:36 +0200

    Exclude sphinx from `build` directory

--- docs/conf.py
+++ docs/conf.py
@@ -105,7 +105,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_apidoc_templates"]
+exclude_patterns = ["_apidoc_templates", "build"]
 
 # Make backticks behave as inline code blocks rather than italics
 default_role = "code"
