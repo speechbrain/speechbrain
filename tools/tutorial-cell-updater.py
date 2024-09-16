613f720fb4ba6093d0ba627e8e6a7db426ddc256
asu <sdelang@sdelang.fr>
2024-09-16 14:38:28 +0200

    Add footer insertion support to tutorial cell updater

--- tools/tutorial-cell-updater.py
+++ tools/tutorial-cell-updater.py
@@ -17,6 +17,9 @@ logging.basicConfig(level=logging.INFO)
 with open("tutorials/notebook-header.md", "r", encoding="utf-8") as header_file:
     HEADER_CONTENTS = header_file.read()
 
+with open("tutorials/notebook-footer.md", "r", encoding="utf-8") as footer_file:
+    FOOTER_CONTENTS = footer_file.read()
+
 
 def find_first_cell_with_tag(
     cell_list: list, tag_to_find: str
@@ -68,6 +71,24 @@ def update_header_cell(header_cell: dict, tutorial_path: str):
     )
 
 
+def update_footer_cell(footer_cell: dict):
+    """Updates the content of a jupyter cell from the footer template.
+    
+    Arguments
+    ---------
+    footer_cell: dict
+        Footer cell in dict format as loaded from JSON
+    """
+
+    footer_cell.update(
+        {
+            "cell_type": "markdown",
+            "metadata": {"id": "sb_auto_footer", "tags": ["sb_auto_footer"]},
+            "source": FOOTER_CONTENTS.splitlines(True)
+        }
+    )
+
+
 def update_notebook(fname: str):
     """Updates the tagged programmatically updated cells, see:
 
@@ -89,6 +110,7 @@ def update_notebook(fname: str):
         nb = json.load(f)
 
         cells = nb["cells"]
+
         header_cell = find_first_cell_with_tag(cells, "sb_auto_header")
         if header_cell is None:
             logging.info("Header not found; creating")
@@ -96,6 +118,14 @@ def update_notebook(fname: str):
             header_cell = cells[0]
 
         update_header_cell(header_cell, tutorial_path)
+
+        footer_cell = find_first_cell_with_tag(cells, "sb_auto_footer")
+        if footer_cell is None:
+            logging.info("Footer not found; creating")
+            cells.append({})
+            footer_cell = cells[-1]
+
+        update_footer_cell(footer_cell)
 
     with open(fname, "w") as wf:
         json.dump(nb, wf, indent=1, ensure_ascii=False)
