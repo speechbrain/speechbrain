9a585ccc72b908c2402bc60a7dd1f3c18746e65b
asu <sdelang@sdelang.fr>
2024-09-16 16:19:42 +0200

    Rework doc landing page
    License stuff and references is fine and all, but it occupies a lot of space. There is now much fewer text, which makes it a lot more obvious where to find tutorials, and outlines the structureof documentation quite better.
    
    Also added sphinx_design for the license/reference dropdowns, which looks pretty fine

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
     "sphinx_copybutton",
+    "sphinx_design",
     "recommonmark",
     # chose myst-nb over nbsphinx is annoying because of the pandoc dependency
     # of the latter, which needs to be installed system-wide or through conda
