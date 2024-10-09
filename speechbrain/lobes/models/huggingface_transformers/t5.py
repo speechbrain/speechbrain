4237621b7d2d60488650fe4d345c3471b8207543
asu <sdelang@sdelang.fr>
2024-10-09 15:58:11 +0200

    Update T5 docstrings to pass linting

--- speechbrain/lobes/models/huggingface_transformers/t5.py
+++ speechbrain/lobes/models/huggingface_transformers/t5.py
@@ -29,9 +29,11 @@ class T5(HFTransformersInterface):
         HuggingFace hub name: e.g "facebook/wav2vec2-large-lv60"
     save_path : str
         save directory of the downloaded model.
-    freeze : bool (default: True)
-        If True, the model is frozen. If False, the model will be trained
+    freeze : bool (default: False)
+        If True, the entire model is frozen. If False, the model will be trained
         alongside with the rest of the pipeline.
+    freeze_encoder : bool (default: False)
+        If False, the encoder weights are frozen.
     """
 
     def __init__(
@@ -49,7 +51,9 @@ class T5(HFTransformersInterface):
             for param in self.model.encoder.parameters():
                 param.requires_grad = False
 
-    def forward_encoder(self, input_ids: Optional[torch.LongTensor] = None):
+    def forward_encoder(
+        self, input_ids: Optional[torch.LongTensor] = None
+    ) -> torch.Tensor:
         """
         Performs the forward pass of T5's encoder.
 
@@ -57,6 +61,11 @@ class T5(HFTransformersInterface):
         ---------
         input_ids : torch.LongTensor
             A batch of input token ids. The input is supposed to already be padded.
+
+        Returns
+        -------
+        torch.Tensor
+            Final layer hidden state of the encoder.
         """
         if self.freeze_encoder:
             with torch.no_grad():
@@ -85,6 +94,11 @@ class T5(HFTransformersInterface):
             A batch of encoder hidden states to attend to in the decoder's cross-attention.
         output_hidden_states : bool
             Whether to output all hidden states or just the last ones.
+
+        Returns
+        -------
+        torch.Tensor
+            Output logits from the decoder.
         """
         # decoder outputs consists of (dec_features, past_key_value, dec_hidden, dec_attn)
         decoder_outputs = self.model.decoder(
@@ -100,7 +114,9 @@ class T5(HFTransformersInterface):
         return lm_logits
 
     @torch.no_grad()
-    def decode(self, memory, enc_states, enc_lens=None):
+    def decode(
+        self, memory, enc_states, enc_lens=None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Performs a single decoding step for this transformer model.
         It is required for the searchers to decode step by step.
@@ -111,8 +127,15 @@ class T5(HFTransformersInterface):
             The sequence of already decoded.
         enc_states : torch.LongTensor
             Hidden output of the encoder.
-        enc_len : torch.LongTensor
+        enc_lens : torch.LongTensor
             The actual length of encoder states.
+
+        Returns
+        -------
+        torch.Tensor
+            Output logits from the decoder.
+        torch.Tensor
+            Last layer's cross-attention weights.
         """
 
         output = self.model.decoder(
