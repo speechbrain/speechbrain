e0c14e9f624285a7b2d725d5e23e495fb7f9c384
Adel Moumen <adelmoumen.pro@gmail.com>
2024-11-12 16:35:21 -0500

    add uLM

---
+++ speechbrain/lobes/models/huggingface_transformers/gslm.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn as nn
+
+from speechbrain.lobes.models.huggingface_transformers.huggingface import (
+    HFTransformersInterface,
+)
+from speechbrain.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+from transformers import AutoModelForCausalLM
+
+class unitLM(nn.Module):
+    def __init__(
+        self,
+        source: str,
+        cache_dir: str,
+        block_size: int = 1024,
+        n_codebooks: int = 1,
+        vocabsize: int = 1024,
+        audio_pad_token: int = 0,
+        tie_embds: bool = False,
+    ) -> None:
+        super().__init__()
+
+        self.source = source
+        self.cache_dir = cache_dir
+
+        self.block_size = block_size
+        self.n_codebooks = n_codebooks
+        self.vocabsize = vocabsize
+
+        # TODO: modify this 
+        self.model = AutoModelForCausalLM.from_pretrained(
+            source,
+            torch_dtype=torch.bfloat16,
+            attn_implementation="flash_attention_2",
+            cache_dir=self.cache_dir,
+        )
+
+        config = self.model.config
+        self.dim = config.hidden_size
+        self.n_layers = config.num_hidden_layers
+        self.n_heads = config.num_attention_heads
+        self.audio_pad_token = audio_pad_token
+
+        # remove embeddings and output projections from the backbone
+        self.model.set_input_embeddings(None)
+        self.model.set_output_embeddings(None)
+
+        self.audio_in_embds = nn.ModuleList(
+            [
+                nn.Embedding(self.vocabsize, self.dim) 
+                for _ in range(self.n_codebooks)
+            ]
+        )
+        self.audio_out = nn.ModuleList(
+            [
+                nn.Linear(self.dim, self.vocabsize, bias=False) 
+                for _ in range(self.n_codebooks)
+            ]
+        )
+
+        for module in [self.audio_in_embds, self.audio_out]:
+            module.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        # regular inits    
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def apply_delay_pattern(self, tokens):
+        # todo: undertsansd this
+        B, K, T = tokens.shape
+        result = torch.full((B, K, T), self.audio_pad_token, dtype=tokens.dtype, device=tokens.device)
+        for i in range(K):
+            result[:, i, i: T] = tokens[:, i, :T - i]
+        return result
+
+    def undelay_logits(self, logits):
+        B, K, T, D = logits.shape
+        unpadded_length = T - K
+        # Create an empty tensor to store the reconstructed sequence
+        undelayed_logits = torch.full((B, K, T, D), float('nan'), dtype=logits.dtype, device=logits.device)
+        undelayed_logits_mask = torch.ones((B, K, T), dtype=bool, device=logits.device)
+        undelayed_logits_mask[..., -K:] = False
+        # Reconstruct the original sequence by removing the delays
+        for i in range(K):
+            undelayed_logits[:, i, :-K] = logits[:, i, i:i+unpadded_length, :]
+        return undelayed_logits, undelayed_logits_mask
+
+    def forward(
+        self, 
+        input_tokens: torch.Tensor, 
+    ):
+        batch_size, n_codebooks, seq_length = input_tokens.size()
+        assert seq_length <= self.block_size, f"Sequence beyond maximum length of {self.block_size}"
+        assert n_codebooks == self.n_codebooks, "Sequence shape must match the specified number of codebooks"
+
+        input_audio_tokens = self.apply_delay_pattern(input_tokens)
+        # compute the frame audio embeddings as the sum of codebook embeddings
+        h = sum([self.audio_in_embds[k](input_audio_tokens[:, k]) for k in range(n_codebooks)])
+
+        # obtain contextual embeddings
+        h = self.model.model(inputs_embeds=h, use_cache=False)['last_hidden_state']
+
+        logits_audio = torch.stack([self.audio_out[k](h) for k in range(self.n_codebooks)], dim=1)
+
+        logits_audio, logits_audio_mask = self.undelay_logits(logits_audio)
+
+        return logits_audio, logits_audio_mask
+
+if __name__ == "__main__":
+    import torchaudio
+    audio_file = "/scratch/adelmou/datasets/sLM21-dataset/semantic/dev/librispeech/vuNdkweosd.wav"
+
+    wav, sr = torchaudio.load(audio_file)
+
+    source = "/scratch/adelmou/pretrained_models/SmolLM2-135M"
+
+    model = unitLM(
+        source = source,
+        cache_dir = "/scratch/adelmou/pretrained_models/",
+        block_size = 1024,
+        n_codebooks  = 1,
+        audio_pad_token  = 0,
+        tie_embds = False,
+    ).to("cuda").to(torch.bfloat16)
+
+    batch_size, n_codebooks, seq_length = 1, 1, 10 
+    fake_inp = torch.randint(0, 1024, (batch_size, n_codebooks, seq_length)).to("cuda").to(torch.bfloat16).to(torch.long)
+    print(fake_inp)
+
+    logits_audio, logits_audio_mask = model(fake_inp)   
+
+    print(logits_audio_mask)
