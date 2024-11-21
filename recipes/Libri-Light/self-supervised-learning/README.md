0c21d74c930e7cbbd867b34c8b8b1fd98f1c3e9e
Shucong Zhang/Embedded AI /SRUK/Engineer/Samsung Electronics <s1.zhang@sruk-ccn4.eu.corp.samsungelectronics.net>
2024-11-21 15:58:29 +0000

    Libri-Light data prepare

--- recipes/Libri-Light/self-supervised-learning/README.md
+++ recipes/Libri-Light/self-supervised-learning/README.md
@@ -1,17 +1,23 @@
-## Data preparetion of Libri-Light for SpeechBrain self-supervised-learning
+# Data preparation of Libri-Light for SpeechBrain self-supervised-learning
 
-This folder contains the script for preparing Libri-Light for the SSL pre-training through SpeechBrain.
+This folder contains the script for preparing Libri-Light dataset for the SSL pre-training through SpeechBrain.
 
-# How to run
+## How to run
 
-1- Download the Libri-Light data (small, or medium, or  large) from 
+1- Download the Libri-Light data (small, or medium, or  large) from
+
     https://github.com/facebookresearch/libri-light/tree/main/data_preparation
 
-2- Use the cut_by_vad.py script to do the VAD of each downloaded split. For example, if you want to the use the split, 
-    ``` python cut_by_vad.py --input_dir path_to_Libri-Light_small --output_dir Libri-Light_small_vad --target_len_sec 20 ```
-    If you want your audio clips in the SSL pre-training to have a maxium length of 20 seconds, then it is recommended to use "--target_len_sec" equal or less than 20 seconds
 
-3- Use the vad output_dir in step 2 as the input_dir to call make_librilight_csv.py to generate the csv file. For example,
-    ``` python make_librilight_csv.py --input_dir Libri-Light_small_vad --output_dir results --max_length 20 --n_processes 128 ```
+2- Use the cut_by_vad.py script from the Libri-Light repo to do the VAD of each downloaded split. For example, if you want to the use the small split
+
+    python cut_by_vad.py --input_dir path_to_Libri-Light_small --output_dir Libri-Light_small_vad --target_len_sec 20
+
+   If you want your audio clips in the SSL pre-training to have a maximum length of 20 seconds, then it is recommended to use "--target_len_sec" equal or less than 20 seconds
+
+3- Use the vad output_dir in step 2 as the input_dir to call make_librilight_csv.py to generate the csv file. For example
+
+    python make_librilight_csv.py --input_dir Libri-Light_small_vad --output_dir results --max_length 20 --n_processes 128
+
 
 4- Now, you can use the generated train.csv in the output_dir in step 3 as the "train_csv" in any SpeechBrain SSL pre-training yaml file
--- recipes/Libri-Light/self-supervised-learning/make_librilight_csv.py
+++ recipes/Libri-Light/self-supervised-learning/make_librilight_csv.py
@@ -1,14 +1,14 @@
 """ Create speebrain csv files for the Libri-Light dataset for self-supervised-learning
 
-1. Download the Libri-Light dataset through the toolkit in the Libri-Light github repo 
+1. Download the Libri-Light dataset through the toolkit in the Libri-Light github repo
     "https://github.com/facebookresearch/libri-light/"
 
-2. Use the data_preparation/cut_by_vad.py script of the Libri-Light repo to do the vad. For example, 
+2. Use the data_preparation/cut_by_vad.py script of the Libri-Light repo to do the vad. For example,
     "python cut_by_vad.py --input_dir path_to_Libri-Light_small --output_dir Libri-Light_small_vad --target_len_sec 20"
-    
+
 3. Use the vad output_dir in step 2 as the input_dir in this step to generate the csv file. For example,
-    "python make_librilight_csv.py --input_dir Libri-Light_small_vad --output_dir results --max_length 20 --n_processes 128" to generate the train.csv for the SSL pretraining 
-    
+    "python make_librilight_csv.py --input_dir Libri-Light_small_vad --output_dir results --max_length 20 --n_processes 128" to generate the train.csv for the SSL pretraining
+
 4. Now, you can use the generated train.csv in the output_dir in step 3 as the "train_csv" in any SpeechBrain SSL pre-training yaml file
 
 Authors
@@ -16,29 +16,31 @@ Authors
  * Titouan Parcollet 2024
 """
 
-
+import argparse
+import csv
+import multiprocessing
+import os
 import pathlib
+from pathlib import Path
+
 import torchaudio
 import tqdm
-import multiprocessing
-import csv
-from pathlib import Path
-import sys
-import os
-import argparse
+
 
 def make_csv_for_each(
     subpath_1_csv_file_folder_max_length: tuple,
 ) -> None:
-    """Prepare the csv fils for each subfolder of the Libri-Light splits
+    """Prepare the csv files for each subfolder of the Libri-Light splits
 
     Arguments
     ---------
     subpath_1_csv_file_folder_max_length : tuple
         A (subfolder_path, csv_output_path, max_utt_length) tuple
     """
-    subpath_1, csv_file_folder, max_length = subpath_1_csv_file_folder_max_length
-    for i, flac_file in enumerate(subpath_1.glob('**/*.flac')):
+    subpath_1, csv_file_folder, max_length = (
+        subpath_1_csv_file_folder_max_length
+    )
+    for i, flac_file in enumerate(subpath_1.glob("**/*.flac")):
         flac_file_name = flac_file.stem
         waveform, sample_rate = torchaudio.load(str(flac_file))
         num_frames = waveform.size(1)
@@ -47,20 +49,22 @@ def make_csv_for_each(
             continue
         audio_length_seconds = waveform.shape[1] / sample_rate
         csv_file = f"{csv_file_folder}/{flac_file.parent.stem}.csv"
-        with open(csv_file, mode='a', newline='') as csvfile:
+        with open(csv_file, mode="a", newline="", encoding="utf-8") as csvfile:
             csv_writer = csv.writer(
                 csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
             )
-            csv_writer.writerow([flac_file_name,audio_length_seconds,str(flac_file)])
+            csv_writer.writerow(
+                [flac_file_name, audio_length_seconds, str(flac_file)]
+            )
 
 
 def processes_folder(
-    data_path: str, 
-    csv_file_folder: str, 
-    max_length: float, 
+    data_path: str,
+    csv_file_folder: str,
+    max_length: float,
     n_processes: int,
 ) -> None:
-    """Prepare the csv fils for one of the Libri-Light splits
+    """Prepare the csv files for one of the Libri-Light splits
 
     Arguments
     ---------
@@ -68,9 +72,9 @@ def processes_folder(
         Path to the Libri-Light split after vad
     csv_file_folder: str
         Path to the output folder of generated csv files
-    max_length:
-        The max length for each prepared audio clip
-    n_processes:
+    max_length: float
+        The max length (seconds) for each prepared audio clip
+    n_processes: int
         Number of parallel processes
     """
     print("Processing each subfolder of this split")
@@ -81,11 +85,14 @@ def processes_folder(
     for x in list_dir.iterdir():
         tasks.append((x, f"{csv_file_folder}/tmp", max_length))
     with multiprocessing.Pool(processes=n_processes) as pool:
-        for _ in tqdm.tqdm(pool.imap_unordered(make_csv_for_each, tasks), total=len(tasks)):
+        for _ in tqdm.tqdm(
+            pool.imap_unordered(make_csv_for_each, tasks), total=len(tasks)
+        ):
             pass
 
+
 def merge_csv_files(
-    csv_file_folder: str, 
+    csv_file_folder: str,
 ) -> None:
     """Merges the csv files for each subfolder into one single file
 
@@ -95,19 +102,19 @@ def merge_csv_files(
         Path to the output folder of generated csv files
     """
     print("Merging the csvs of each subfolder into one csv")
-    file_list = [str(x) for x in Path(f"{csv_file_folder}/tmp").glob('*.csv')]
+    file_list = [str(x) for x in Path(f"{csv_file_folder}/tmp").glob("*.csv")]
     output_file = f"{csv_file_folder}/train.csv"
-    fieldnames = ["ID", "duration", "wav"]  
+    fieldnames = ["ID", "duration", "wav"]
 
-    with open(output_file, mode='a', newline='', encoding='utf-8') as outfile:
+    with open(output_file, mode="a", newline="", encoding="utf-8") as outfile:
         csv_writer = csv.writer(
-                    outfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
-                )
+            outfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
         csv_writer.writerow(fieldnames)
         for file_path in tqdm.tqdm(file_list):
-            with open(file_path, mode='r', encoding='utf-8') as infile:
+            with open(file_path, mode="r", encoding="utf-8") as infile:
                 reader = csv.reader(infile)
-                
+
                 # filter out bad rows
                 for row in reader:
                     if len(row) == 3 and os.path.exists(row[-1]):
@@ -117,23 +124,45 @@ def merge_csv_files(
                         print(f"bad row {row}")
 
     import shutil
+
     shutil.rmtree(f"{csv_file_folder}/tmp")
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', type=str, default=None,
-                        help="Path to the Libri-Light split after vad", required=True)
-    parser.add_argument('--output_dir', type=str, default=None,
-                        help="Path to the output folder of generated csv files", required=True)
-    parser.add_argument('--max_length', type=float, default=20.2,
-                        help="The max length for each prepared audio clip"
-                             "(default is 20.2)")
-    parser.add_argument('--n_processes', type=int, default=32,
-                        help="Number of parallel processes")
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+        default=None,
+        help="Path to the Libri-Light split after vad",
+        required=True,
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default=None,
+        help="Path to the output folder of generated csv files",
+        required=True,
+    )
+    parser.add_argument(
+        "--max_length",
+        type=float,
+        default=20.2,
+        help="The max length for each prepared audio clip" "(default is 20.2)",
+    )
+    parser.add_argument(
+        "--n_processes",
+        type=int,
+        default=32,
+        help="Number of parallel processes",
+    )
     args = parser.parse_args()
     return args
 
+
 if __name__ == "__main__":
     args = parse_args()
-    processes_folder(args.input_dir, args.output_dir, args.max_length, args.n_processes)
+    processes_folder(
+        args.input_dir, args.output_dir, args.max_length, args.n_processes
+    )
     merge_csv_files(args.output_dir)
