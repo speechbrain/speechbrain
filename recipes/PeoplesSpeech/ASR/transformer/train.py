59efc683a0c864d1a36130e9b41ec4fbdf5a9457
Titouan Parcollet <t.parcollet@samsung.com>
2024-11-22 16:57:40 +0000

    cleaning

--- recipes/PeoplesSpeech/ASR/transformer/train.py
+++ recipes/PeoplesSpeech/ASR/transformer/train.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python3
-"""Recipe for training a Transformer ASR system with librispeech.
+"""Recipe for training a Conformer ASR system with People's Speech.
 The system employs an encoder, a decoder, and an attention mechanism
-between them. Decoding is performed with (CTC/Att joint) beamsearch coupled with a neural
-language model.
+between them. Decoding is performed with (CTC/Att joint) beamsearch.
 
 To run this recipe, do the following:
-> python train.py hparams/transformer.yaml
-> python train.py hparams/conformer.yaml
+> python train.py hparams/conformer_large.yaml
 
-With the default hyperparameters, the system employs a convolutional frontend and a transformer.
-The decoder is based on a Transformer decoder. Beamsearch coupled with a Transformer
-language model is used  on the top of decoder probabilities.
+With the default hyperparameters, the system employs a convolutional frontend and a conformer.
+The decoder is based on a Transformer decoder.
 
 The neural network is trained on both CTC and negative-log likelihood
 targets and sub-word units estimated with Byte Pairwise Encoding (BPE)
 are used as basic recognition tokens. Training is performed on the full
-LibriSpeech dataset (960 h).
-
-The best model is the average of the checkpoints from last 5 epochs.
+People's Speech dataset (28,000 hours).
 
 The experiment file is flexible enough to support a large variety of
 different systems. By properly changing the parameter files, you can try
 different encoders, decoders, tokens (e.g, characters instead of BPE),
-training split (e.g, train-clean 100 rather than the full one), and many
-other possible variations.
+training split, and many other possible variations.
 
 
 Authors
- * Jianyuan Zhong 2020
- * Mirco Ravanelli 2020
- * Peter Plantinga 2020
- * Samuele Cornell 2020, 2021, 2022
- * Titouan Parcollet 2021, 2022, 2024
+ * Titouan Parcollet 2024
 """
 
 import os
@@ -115,10 +105,7 @@ class ASR(sb.core.Brain):
 
         (p_ctc, p_seq, wav_lens, hyps) = predictions
 
-        if hasattr(batch, "audio_id"):
-            ids = batch.audio_id
-        else:
-            ids = batch.id
+        ids = batch.audio_id
         tokens_eos, tokens_eos_lens = batch.tokens_eos
         tokens, tokens_lens = batch.tokens
 
@@ -162,8 +149,6 @@ class ASR(sb.core.Brain):
                 target_words = self.tokenizer(
                     target_words, task="decode_from_list"
                 )
-                print(predicted_words)
-                print(target_words)
 
                 self.wer_metric.append(ids, predicted_words, target_words)
 
@@ -226,41 +211,10 @@ class ASR(sb.core.Brain):
                 ) as w:
                     self.wer_metric.write_stats(w)
 
-            # save the averaged checkpoint at the end of the evaluation stage
-            # delete the rest of the intermediate checkpoints
-            # ACC is set to 1.1 so checkpointer only keeps the averaged checkpoint
-            # self.checkpointer.save_and_keep_only(
-            #    meta={"ACC": 1.1, "epoch": epoch},
-            #    max_keys=["ACC"],
-            #    num_to_keep=1,
-            # )
-
     def on_fit_batch_end(self, batch, outputs, loss, should_step):
         """At the end of the optimizer step, apply noam annealing."""
         if should_step:
             self.hparams.noam_annealing(self.optimizer)
-
-    def on_evaluate_start(self, max_key=None, min_key=None):
-        """Gets called at the beginning of ``evaluate()``
-
-        Default implementation loads the best-performing checkpoint for
-        evaluation, based on stored metrics.
-
-        Arguments
-        ---------
-        max_key : str
-            Key to use for finding best checkpoint (higher is better).
-            By default, passed to ``self.checkpointer.recover_if_possible()``.
-        min_key : str
-            Key to use for finding best checkpoint (lower is better).
-            By default, passed to ``self.checkpointer.recover_if_possible()``.
-        """
-
-        # Recover best checkpoint for evaluation
-        if self.checkpointer is not None:
-            ckpt = self.checkpointer.find_checkpoint()
-            self.checkpointer.load_checkpoint(ckpt)
-
 
 def dataio_prepare(hparams):
     """This function prepares the datasets to be used in the brain class.
@@ -301,17 +255,6 @@ def dataio_prepare(hparams):
         test_data,
     )
 
-    test_data_2 = sb.dataio.dataset.DynamicItemDataset.from_csv(
-        csv_path=hparams["test_2"],
-        replacements={"data_root": hparams["data_folder"]},
-    )
-
-    # We also sort the validation data so it is faster to validate
-    test_data_2 = test_data_2.filtered_sorted(sort_key="duration")
-
-    # For the test, we may want to allow custom test.csv as these ones are not
-    # super reliable for now?
-
     datasets = [train_data, valid_data, test_data]
 
     # 2. Define audio pipeline:
@@ -328,20 +271,6 @@ def dataio_prepare(hparams):
         yield sig
 
     sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline)
-
-    # 2. Define audio pipeline:
-    @sb.utils.data_pipeline.takes("wav")
-    @sb.utils.data_pipeline.provides("sig")
-    def audio_pipeline_2(wav):
-        info = torchaudio.info(wav)
-        sig = sb.dataio.dataio.read_audio(wav)
-        sig = torchaudio.transforms.Resample(
-            info.sample_rate,
-            16000,
-        )(sig)
-        return sig
-
-    sb.dataio.dataset.add_dynamic_item([test_data_2], audio_pipeline_2)
 
     # 3. Define text pipeline:
     @sb.utils.data_pipeline.takes("text")
@@ -361,26 +290,7 @@ def dataio_prepare(hparams):
         tokens = torch.LongTensor(tokens_list)
         yield tokens
 
-    # 3. Define text pipeline:
-    @sb.utils.data_pipeline.takes("wrd")
-    @sb.utils.data_pipeline.provides(
-        "wrd", "tokens_list", "tokens_bos", "tokens_eos", "tokens"
-    )
-    def text_pipeline_2(wrd):
-
-        wrd = english_specific_preprocess(wrd)
-        yield wrd
-        tokens_list = tokenizer.sp.encode_as_ids(wrd)
-        yield tokens_list
-        tokens_bos = torch.LongTensor([hparams["bos_index"]] + (tokens_list))
-        yield tokens_bos
-        tokens_eos = torch.LongTensor(tokens_list + [hparams["eos_index"]])
-        yield tokens_eos
-        tokens = torch.LongTensor(tokens_list)
-        yield tokens
-
     sb.dataio.dataset.add_dynamic_item(datasets, text_pipeline)
-    sb.dataio.dataset.add_dynamic_item([test_data_2], text_pipeline_2)
 
     # 4. Set output:
     sb.dataio.dataset.set_output_keys(
@@ -388,16 +298,12 @@ def dataio_prepare(hparams):
         ["id", "audio_id", "sig", "wrd", "tokens_bos", "tokens_eos", "tokens"],
     )
 
-    sb.dataio.dataset.set_output_keys(
-        [test_data_2],
-        ["id", "sig", "wrd", "tokens_bos", "tokens_eos", "tokens"],
-    )
-
     # 5. If Dynamic Batching is used, we instantiate the needed samplers.
     train_batch_sampler = None
     valid_batch_sampler = None
 
     dynamic_hparams = hparams["dynamic_batch_sampler"]
+    dynamic_hparams_valid = hparams["dynamic_batch_sampler_valid"]
     num_buckets = dynamic_hparams["num_buckets"]
 
     train_batch_sampler = DynamicBatchSampler(
@@ -411,18 +317,17 @@ def dataio_prepare(hparams):
 
     valid_batch_sampler = DynamicBatchSampler(
         valid_data,
-        100 * 1000,
+        dynamic_hparams_valid["max_batch_length"] * 1000,
         num_buckets=num_buckets,
         lengths_list=val_len_list,
-        shuffle=dynamic_hparams["shuffle"],
-        batch_ordering=dynamic_hparams["batch_ordering"],
+        shuffle=dynamic_hparams_valid["shuffle"],
+        batch_ordering=dynamic_hparams_valid["batch_ordering"],
     )
 
     return (
         train_data,
         valid_data,
         test_data,
-        test_data_2,
         train_batch_sampler,
         valid_batch_sampler,
     )
@@ -487,7 +392,6 @@ if __name__ == "__main__":
         train_data,
         valid_data,
         test_data,
-        test_data_2,
         train_bsampler,
         valid_bsampler,
     ) = dataio_prepare(hparams)
@@ -503,31 +407,28 @@ if __name__ == "__main__":
 
     # adding objects to trainer:
     asr_brain.tokenizer = tokenizer
-    train_dataloader_opts = hparams["train_dataloader_opts"]
-    valid_dataloader_opts = hparams["valid_dataloader_opts"]
 
-    if train_bsampler is not None:
-        collate_fn = None
-        if "collate_fn" in train_dataloader_opts:
-            collate_fn = train_dataloader_opts["collate_fn"]
+    # Setup dynamic batching specifics
+    collate_fn = None
+    if "collate_fn" in train_dataloader_opts:
+        collate_fn = train_dataloader_opts["collate_fn"]
 
-        train_dataloader_opts = {
-            "batch_sampler": train_bsampler,
-            "num_workers": hparams["num_workers"],
-        }
+    train_dataloader_opts = {
+        "batch_sampler": train_bsampler,
+        "num_workers": hparams["num_workers"],
+    }
 
-        if collate_fn is not None:
-            train_dataloader_opts["collate_fn"] = collate_fn
+    if collate_fn is not None:
+        train_dataloader_opts["collate_fn"] = collate_fn
 
-    if valid_bsampler is not None:
-        collate_fn = None
-        if "collate_fn" in valid_dataloader_opts:
-            collate_fn = valid_dataloader_opts["collate_fn"]
+    collate_fn = None
+    if "collate_fn" in valid_dataloader_opts:
+        collate_fn = valid_dataloader_opts["collate_fn"]
 
-        valid_dataloader_opts = {"batch_sampler": valid_bsampler}
+    valid_dataloader_opts = {"batch_sampler": valid_bsampler}
 
-        if collate_fn is not None:
-            valid_dataloader_opts["collate_fn"] = collate_fn
+    if collate_fn is not None:
+        valid_dataloader_opts["collate_fn"] = collate_fn
 
     # Training
     asr_brain.fit(
@@ -540,16 +441,6 @@ if __name__ == "__main__":
 
     # Testing
     os.makedirs(hparams["output_wer_folder"], exist_ok=True)
-
-    # report WER on valid data
-    asr_brain.hparams.test_wer_file = os.path.join(
-        hparams["output_wer_folder"], "test2_wer.txt"
-    )
-    asr_brain.evaluate(
-        test_data_2,
-        min_key="WER",
-        test_loader_kwargs=hparams["test_dataloader_opts"],
-    )
 
     # report WER on valid data
     asr_brain.hparams.test_wer_file = os.path.join(
