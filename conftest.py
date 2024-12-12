35a07478505764f3a7c4467b9e1ccb2915d13a43
Peter Plantinga <plantinga.peter@proton.me>
2024-12-12 11:51:47 -0500

    Ignore entire integrations folder in conftest

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize("device", [option_value])
 
 
-collect_ignore = ["setup.py"]
+collect_ignore = ["setup.py", "speechbrain/integrations/"]
 try:
     import numba  # noqa: F401
 except ModuleNotFoundError:
@@ -20,18 +20,6 @@ try:
 except ModuleNotFoundError:
     collect_ignore.append("speechbrain/decoders/language_model.py")
 try:
-    import fairseq  # noqa: F401
-except ModuleNotFoundError:
-    collect_ignore.append("speechbrain/lobes/models/fairseq_wav2vec.py")
-try:
-    from transformers import Wav2Vec2Model  # noqa: F401
-except ModuleNotFoundError:
-    collect_ignore.append("speechbrain/integrations/huggingface/wav2vec2.py")
-try:
-    from transformers import WhisperModel  # noqa: F401
-except ModuleNotFoundError:
-    collect_ignore.append("speechbrain/integrations/huggingface/whisper.py")
-try:
     import sklearn  # noqa: F401
 except ModuleNotFoundError:
     collect_ignore.append("speechbrain/utils/kmeans.py")
@@ -39,20 +27,6 @@ except ModuleNotFoundError:
         "speechbrain/integrations/huggingface/discrete_ssl.py"
     )
 try:
-    import peft  # noqa: F401
-except ModuleNotFoundError:
-    collect_ignore.append("speechbrain/integrations/huggingface/llama2.py")
-try:
     import sacrebleu  # noqa: F401
 except ModuleNotFoundError:
     collect_ignore.append("speechbrain/utils/bleu.py")
-try:
-    import vocos  # noqa: F401
-except ModuleNotFoundError:
-    collect_ignore.append("speechbrain/integrations/huggingface/vocos.py")
-try:
-    from speechtokenizer import SpeechTokenizer  # noqa: F401
-except ModuleNotFoundError:
-    collect_ignore.append(
-        "speechbrain/lobes/models/discrete/speechtokenizer_interface.py"
-    )
