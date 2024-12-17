dfcb9074bdb3c8568f8365a3ad365479d2926492
Peter Plantinga <plantinga.peter@protonmail.com>
2024-12-17 16:06:02 -0500

    Finish fixing doctests

--- tests/.run-doctests.sh
+++ tests/.run-doctests.sh
@@ -5,5 +5,5 @@ set -e -u -o pipefail
 # > pytest --doctest-modules speechbrain/
 # However, we take this more complex approach to avoid testing files not
 # tracked by git. We filter out tests that require optional dependencies.
-avoid="transducer_loss.py\|bleu.py\|ctc_segmentation.py\|check_url.py\|integrations"
+avoid="transducer_loss.py\|bleu.py\|ctc_segmentation.py\|check_url.py\|integrations\|fairseq_wav2vec.py\|language_model.py\|speechtokenizer_interface.py\|__init__.py"
 git ls-files speechbrain | grep -e "\.py$" | grep -v $avoid | xargs pytest --doctest-modules
--- tests/.run-third-party.sh
+++ tests/.run-third-party.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-pytest --doctest-modules speechbrain/integrations/
+set -e -u -o pipefail
+
+# To run third party doctests locally, the easiest approach is to do:
+# > pytest --doctest-modules speechbrain/integration/
+# However, we take this more complex approach to avoid files not tracked
+git ls-files speechbrain/integration | grep -e "\.py$" | xargs pytest --doctest-modules
