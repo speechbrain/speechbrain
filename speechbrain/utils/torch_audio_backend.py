74edcfa88f641e7f9e45f5b1ffb00307e631c12c
Adel Moumen <88119391+Adel-Moumen@users.noreply.github.com>
2025-02-10 21:07:24 +0000

    typo

--- speechbrain/utils/torch_audio_backend.py
+++ speechbrain/utils/torch_audio_backend.py
@@ -26,7 +26,7 @@ def try_parse_torchaudio_major_version() -> Optional[int]:
 
     version_split = torchaudio.__version__.split(".")
 
-    # expect in format x.y.zwhatever; we care only about x # cspell:ignore zwhatever
+    # expect in format x.y.z whatever; we care only about x # cspell:ignore zwhatever
 
     if len(version_split) <= 2:
         # not sure how to parse this
