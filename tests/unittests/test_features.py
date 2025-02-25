Fix input normalization and global normalization variance calculation

tests/unittests/test_features.py
@@ -95,22 +95,19 @@ def test_dtc(device):
 def test_input_normalization(device):
     from speechbrain.processing.features import InputNormalization
 
-    norm = InputNormalization().to(device)
+    # Check this can be traced after training is complete
+    norm = InputNormalization().to(device).eval()
     inputs = torch.randn([10, 101, 20], device=device)
     inp_len = torch.ones([10], device=device)
     assert torch.jit.trace(norm, (inputs, inp_len))
 
+    # Test default setup
     norm = InputNormalization().to(device)
-    inputs = (
-        torch.FloatTensor([1, 2, 3, 0, 0, 0])
-        .to(device)
-        .unsqueeze(0)
-        .unsqueeze(2)
-    )
+    inputs = torch.FloatTensor([1, 2, 3, 0, 0, 0]).view(1, -1, 1).to(device)
     inp_len = torch.FloatTensor([0.5]).to(device)
     out_norm = norm(inputs, inp_len).squeeze()
-    target = torch.FloatTensor([-1, 0, 1, -2, -2, -2]).to(device)
-    assert torch.equal(out_norm, target)
+    expected = torch.FloatTensor([-1, 0, 1, 0, 0, 0]).to(device)
+    assert torch.equal(out_norm, expected)
 
 
 def test_features_multimic(device):