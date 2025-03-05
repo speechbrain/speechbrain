efd9c005173a6db981ff7865e66cbb928fcbeadd
Rogier van Dalen <r.vandalen@samsung.com>
2025-03-05 11:03:09 +0000

    Make ddp_all_reduce mock clearer

--- tests/utils/mock_ddp_all_reduce.py
+++ tests/utils/mock_ddp_all_reduce.py
@@ -6,6 +6,7 @@ Authors
  * Rogier van Dalen 2025
 """
 
+import itertools
 from typing import Callable, List
 from unittest.mock import patch
 
@@ -47,13 +48,18 @@ def run_with_mocked_ddp_all_reduce(functions: List[Callable]) -> list:
     return_values: list
         A list with the return values of each of the functions.
     """
-    # This runs all the functions as many times as they call ddp_all_reduce.
+    # This runs each function as many times as there are calls to
+    # ddp_all_reduce, plus one.
     # This is because this function cannot run all the functions literally at
     # the same time, so it cancels each function call (by raising an exception)
-    # for every call to ddp_all_reduce after noting down the function's
-    # contribution.
-    # So it first runs all the functions up to their first call to
-    # ddp_all_reduce, then up to the second, et cetera.
+    # The first time it records what is passed in to the first call to
+    # ddp_all_reduce, and raises an exception.
+    # It then goes to the next function, and the next, until it knows what each
+    # function contributes in its first call to ddp_all_reduce.
+    # With that knowledge, it can perform the first ddp_all_reduce, and keep
+    # running the functions up to the second call to ddp_all_reduce and note
+    # down the contributions.
+    # And so on.
 
     # The arguments that each function passes in to ddp_all_reduce.
     intermediate_arguments = [[] for _ in functions]
@@ -62,7 +68,16 @@ def run_with_mocked_ddp_all_reduce(functions: List[Callable]) -> list:
     # Return values for each function.
     return_values = [None for _ in functions]
 
-    def make_mock_all_reduce(function_index):
+    def make_mock_all_reduce(function_index, max_num_all_reduce_calls):
+        """
+        Make a mock for ddp_all_reduce, which can be called multiple times.
+        On the `max_num_all_reduce_calls`th call, the argument is noted down,
+        and a NotFinishedYet exception is raised to cancel the call.
+        """
+        assert (
+            len(intermediate_arguments[function_index])
+            == max_num_all_reduce_calls
+        )
         # Count the number of calls.
         num_calls = [0]
 
@@ -71,7 +86,7 @@ def run_with_mocked_ddp_all_reduce(functions: List[Callable]) -> list:
         ):
             call_index = num_calls[0]
 
-            if call_index < len(intermediate_arguments[function_index]):
+            if call_index < max_num_all_reduce_calls:
                 # Keep going.
                 previous_argument = intermediate_arguments[function_index][
                     call_index
@@ -103,30 +118,36 @@ def run_with_mocked_ddp_all_reduce(functions: List[Callable]) -> list:
                 num_calls[0] += 1
 
             else:
-                assert call_index == len(intermediate_arguments[function_index])
+                assert call_index == max_num_all_reduce_calls
                 # Save the argument passed in and cancel the function call.
                 intermediate_arguments[function_index].append(tensor)
                 raise NotFinishedYet
 
         return mock_all_reduce
 
-    def run_function(index: int, function: Callable):
+    def run_function(
+        function_index: int, function: Callable, max_num_all_reduce_calls: int
+    ):
         try:
-            mock = make_mock_all_reduce(index)
+            mock = make_mock_all_reduce(
+                function_index, max_num_all_reduce_calls
+            )
             with patch(
                 "speechbrain.utils.distributed.ddp_all_reduce", new=mock
             ):
                 result = function()
 
             # No exceptions raised: this function is done.
-            done[index] = True
-            return_values[index] = result
+            done[function_index] = True
+            return_values[function_index] = result
         except NotFinishedYet:
             pass
 
-    while True:
-        for index, function in enumerate(functions):
-            run_function(index, function)
+    # First cancel all functions at the first call to ddp_all_reduce, then at
+    # the second, until all functions run to completion.
+    for max_num_all_reduce_calls in itertools.count():
+        for function_index, function in enumerate(functions):
+            run_function(function_index, function, max_num_all_reduce_calls)
         if any(done):
             assert all(
                 done
