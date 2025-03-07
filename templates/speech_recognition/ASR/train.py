7521ead9a4d3cdc46a2832070dac1209e96ef37c
Peter Plantinga <plantinga.peter@proton.me>
2025-03-07 11:05:04 -0500

    Fix doctest for input norm and global norm

--- templates/speech_recognition/ASR/train.py
+++ templates/speech_recognition/ASR/train.py
@@ -501,6 +501,3 @@ if __name__ == "__main__":
         min_key="WER",
         test_loader_kwargs=hparams["test_dataloader_opts"],
     )
-
-    # Save final checkpoint (fixed name)
-    asr_brain.checkpointer.save_checkpoint(name="latest")
