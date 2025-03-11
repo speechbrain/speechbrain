1560478d34cf6f47c7c005ce57f9e1df4754045a
Adel Moumen <adelmoumen.pro@gmail.com>
2025-03-11 07:18:10 -0400

    simplifications

--- speechbrain/utils/checkpoints.py
+++ speechbrain/utils/checkpoints.py
@@ -259,13 +259,13 @@ DEFAULT_LOAD_HOOKS = {
     torch.nn.Module: torch_recovery,
     torch.optim.Optimizer: torch_recovery,
     torch.optim.lr_scheduler.ReduceLROnPlateau: torch_recovery,
-    torch.cuda.amp.grad_scaler.GradScaler: torch_recovery,
+    torch.amp.grad_scaler.GradScaler: torch_recovery,
 }
 DEFAULT_SAVE_HOOKS = {
     torch.nn.Module: torch_save,
     torch.optim.Optimizer: torch_save,
     torch.optim.lr_scheduler.ReduceLROnPlateau: torch_save,
-    torch.cuda.amp.grad_scaler.GradScaler: torch_save,
+    torch.amp.grad_scaler.GradScaler: torch_save,
 }
 if version.parse(torch.__version__) < version.parse("2.0.0"):
     DEFAULT_LOAD_HOOKS[torch.optim.lr_scheduler._LRScheduler] = torch_recovery
@@ -273,6 +273,7 @@ if version.parse(torch.__version__) < version.parse("2.0.0"):
 else:
     DEFAULT_LOAD_HOOKS[torch.optim.lr_scheduler.LRScheduler] = torch_recovery
     DEFAULT_SAVE_HOOKS[torch.optim.lr_scheduler.LRScheduler] = torch_save
+
 
 DEFAULT_TRANSFER_HOOKS = {
     torch.nn.Module: torch_parameter_transfer,
