2a93db070bfc9692dbc3d913c5606de9da00d9a9
flexthink <flexthink@users.noreply.github.com>
2025-04-04 16:00:36 -0400

    Extraction: Cosmetic changes

--- recipes/LibriTTS/extraction/sanity_check.py
+++ recipes/LibriTTS/extraction/sanity_check.py
@@ -8,13 +8,12 @@ import sys
 from pathlib import Path
 
 from hyperpyyaml import load_hyperpyyaml
-from torch import nn
 
 import speechbrain as sb
 from speechbrain.dataio.dataset import DynamicItemDataset
+from speechbrain.dataio.preparation import prepared_features
 from speechbrain.utils.distributed import run_on_main
 from speechbrain.utils.logger import get_logger
-from speechbrain.dataio.preparation import prepared_features
 
 logger = get_logger(__name__)
 
@@ -56,7 +55,7 @@ if __name__ == "__main__":
             replacements={"data_root": data_folder},
         )
         if hparams["data_count"]:
-            dataset.data_ids = dataset.data_ids[:hparams["data_count"]]
+            dataset.data_ids = dataset.data_ids[: hparams["data_count"]]
         datasets[split] = dataset
 
     features = hparams["extract_features"]
@@ -65,13 +64,15 @@ if __name__ == "__main__":
         datasets,
         keys=features,
         storage=hparams["storage"],
-        storage_opts=hparams["storage_opts"]
+        storage_opts=hparams["storage_opts"],
     ):
         for split in splits:
             print(f"Sanity checking {split}")
             dataset = datasets[split]
             dataset.set_output_keys([id_key] + features)
-            loader = sb.dataio.dataloader.make_dataloader(dataset, **hparams["dataloader_opts"])
+            loader = sb.dataio.dataloader.make_dataloader(
+                dataset, **hparams["dataloader_opts"]
+            )
             for batch in loader:
                 data_ids = getattr(batch, id_key)
                 print("ID", data_ids)
