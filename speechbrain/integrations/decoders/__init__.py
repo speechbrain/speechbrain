4e9dba7bcc3fdf800d694ce09754f4e9b746895c
Peter Plantinga <plantinga.peter@proton.me>
2025-12-02 12:52:40 -0500

    Add missing init for decoders

---
+++ speechbrain/integrations/decoders/__init__.py
@@ -0,0 +1,5 @@
+"""
+In ASR, decoding is often done with the help of an n-gram language model,
+and we provide integration with a fast implementation through
+[KenLM](https://github.com/kpu/kenlm).
+"""
