a5fc69225765534d49f33c1864794e37885d355f
Adel Moumen <adelmoumen.pro@gmail.com>
2026-02-07 12:49:08 +0100

    pre-commit fix

--- speechbrain/integrations/hdf5/cached_item.py
+++ speechbrain/integrations/hdf5/cached_item.py
@@ -64,7 +64,9 @@ class CachedHDF5DynamicItem(CachedDynamicItem):
 
     def _cache(self, result, uid):
         """Save the result to the cache"""
-        self.hdf5file.create_dataset(uid, data=result, compression=self.compression)
+        self.hdf5file.create_dataset(
+            uid, data=result, compression=self.compression
+        )
 
     @property
     def hdf5_path(self):
