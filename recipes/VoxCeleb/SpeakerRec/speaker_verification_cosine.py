2645d52fb488363c5de7d0a6eab4f73adf8064e8
Othman Istaiteh <othmanistaiteh@gmail.com>
2026-03-04 09:58:29 +0100

    Update VoxCeleb scripts to support ECAPA2 and maintain backward compatibility

--- recipes/VoxCeleb/SpeakerRec/speaker_verification_cosine.py
+++ recipes/VoxCeleb/SpeakerRec/speaker_verification_cosine.py
@@ -46,7 +46,15 @@ def compute_embedding(wavs, wav_lens):
     embeddings : torch.Tensor
     """
     with torch.no_grad():
-        feats = params["compute_features"](wavs)
+        if (
+            "use_tacotron2_mel_spec" in params
+            and params["use_tacotron2_mel_spec"]
+        ):
+            feats = params["compute_features"](audio=wavs)
+            feats = torch.transpose(feats, 1, 2)
+        else:
+            feats = params["compute_features"](wavs)
+
         feats = params["mean_var_norm"](feats, wav_lens)
         embeddings = params["embedding_model"](feats, wav_lens)
     return embeddings.squeeze(1)
@@ -223,7 +231,7 @@ if __name__ == "__main__":
     sys.path.append(os.path.dirname(current_dir))
 
     # Load hyperparameters file with command-line overrides
-    params_file, run_opts, overrides = sb.core.parse_arguments(sys.argv[1:])
+    params_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
     with open(params_file, encoding="utf-8") as fin:
         params = load_hyperpyyaml(fin, overrides)
 
